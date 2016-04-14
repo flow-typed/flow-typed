ab8b59678c4adda5b37e335cc0d620904af64272
Murphy Randle <murphyrandle@gmail.com>
2016-04-14 08:01:26 -0600

    Add a 'search' command to the CLI that takes a --flowVersion argument
    Note, still need to autodetect the current flow version from the
    package.json

--- npm/src/lib/__tests__/libDef-test.js
+++ npm/src/lib/__tests__/libDef-test.js
@@ -17,7 +17,8 @@ jest.mock('../github.js', () => {
   }
 })
 
-import {getGHLibsAndFlowVersions} from '../libDef.js';
+import {getGHLibsAndFlowVersions, filterDefs, formatDefTable}
+  from '../libDef.js';
 
 describe('libDef helpers', () => {
   describe('getGHLibsAndFlowVersions', () => {
@@ -47,6 +48,70 @@ describe('libDef helpers', () => {
           flowVersion: { range: '>=', major: 0, minor: 18, patch: 'x' }
         }
       ])
+    })
+  })
+
+  describe('filterDefs()', () => {
+    it('filters properly based on flow version', () => {
+      const fixture = [
+        { pkgName: 'mori',
+          pkgVersion: { major: 0, minor: 3, patch: 'x' },
+          pkgVersionStr: 'v0.3.x',
+          pkgNameVersionStr: 'mori-v0.3.x',
+          flowVersionStr: '>=v0.22.x',
+          flowVersion: { range: '>=', major: 0, minor: 22, patch: 'x' }
+        },
+        { pkgName: 'mori',
+          pkgVersion: { major: 0, minor: 3, patch: 'x' },
+          pkgVersionStr: 'v0.3.x',
+          pkgNameVersionStr: 'mori-v0.3.x',
+          flowVersionStr: '>=v0.18.x',
+          flowVersion: { range: '>=', major: 0, minor: 18, patch: 'x' }
+        }
+      ]
+
+      const filtered = filterDefs('mori', fixture, '0.19.0')
+      expect(filtered.length).toEqual(1)
+      expect(filtered[0].flowVersionStr).toEqual('>=v0.18.x')
+    })
+  })
+
+  describe('formatDefTable()', () => {
+    it('beautifully formats a table of libDefs', () => {
+      const fixture = [
+        { pkgName: 'mori',
+          pkgVersion: { major: 0, minor: 3, patch: 'x' },
+          pkgVersionStr: 'v0.3.x',
+          pkgNameVersionStr: 'mori-v0.3.x',
+          flowVersionStr: '>=v0.22.x',
+          flowVersion: { range: '>=', major: 0, minor: 22, patch: 'x' }
+        },
+        { pkgName: 'mori',
+          pkgVersion: { major: 0, minor: 3, patch: 'x' },
+          pkgVersionStr: 'v0.3.x',
+          pkgNameVersionStr: 'mori-v0.3.x',
+          flowVersionStr: '>=v0.18.x',
+          flowVersion: { range: '>=', major: 0, minor: 18, patch: 'x' }
+        }
+      ]
+
+      const formatted = formatDefTable(fixture)
+      expect(formatted).toEqual(
+        "\nFound definitions:" + "\n" +
+        "╔══════╤═════════════════╤══════════════╗" + "\n" +
+        "║ Name │ Package Version │ Flow Version ║" + "\n" +
+        "╟──────┼─────────────────┼──────────────╢" + "\n" +
+        "║ mori │ v0.3.x          │ >=v0.22.x    ║" + "\n" +
+        "╟──────┼─────────────────┼──────────────╢" + "\n" +
+        "║ mori │ v0.3.x          │ >=v0.18.x    ║" + "\n" +
+        "╚══════╧═════════════════╧══════════════╝" + "\n"
+      )
+    })
+
+    it('gracefully handles not finding any definitions', () => {
+      const fixture = []
+      const formatted = formatDefTable(fixture)
+      expect(formatted).toEqual('No definitions found, sorry!')
     })
   })
 })
