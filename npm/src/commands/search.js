0fe1310fbdad8aaf86b376f1298e969750c0dee4
Murphy Randle <murphyrandle@gmail.com>
2016-04-19 05:25:16 -0600

    Take out autodetection of flow version for now

--- npm/src/commands/search.js
+++ npm/src/commands/search.js
@@ -8,45 +8,16 @@ export const name = "search";
 export const description =
   "Performs a simple search (by name) of available libdefs";
 
-function findFlowVersion(): string {
-  const nodeModulesBin = path.resolve('node_modules', '.bin')
-  const _path = `${nodeModulesBin}:${process.env.PATH}`
-
-  function fail() {
-    console.error('Couldn\'t identify your flow version. Please make sure ' +
-    'flow is installed and in your PATH, or flow-bin is installed ' +
-    'in your project.')
-    process.exit(-1)
-  }
-
-  let output = ''
-
-  try {
-    output = child_process.execSync('flow version', {
-      env: {PATH: _path}
-    }).toString()
-  } catch(e) {
-    fail()
-  }
-
-  const matches = output.match(/.*version (.*)\n$/)
-  if (!matches || !matches[1]) {
-    fail()
-  } else {
-    return matches[1]
-  }
-}
-
 export async function run(args: {}): Promise<number> {
   if (!args._ || !args._.length > 1) {
     console.error('Please provide a term for which to search!')
     return 1;
   }
 
-  const flowVersion = args.flowVersion || findFlowVersion()
+  const flowVersion = args.flowVersion || undefined
   const term = args._[1];
   const defs = await getGHLibsAndFlowVersions();
-  const filtered = filterDefs(term, defs, args.flowVersion || undefined)
+  const filtered = filterDefs(term, defs, flowVersion)
   console.log(formatDefTable(filtered))
   return 0;
 };
