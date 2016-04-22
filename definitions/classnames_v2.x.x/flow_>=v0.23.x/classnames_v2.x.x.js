f927a8b50c12f5494154e9091d180ba9a08b6ef4
marudor <marudor@marudor.de>
2016-04-22 21:48:04 +0200

    better libdef

--- definitions/classnames_v2.x.x/flow_>=v0.23.x/classnames_v2.x.x.js
+++ definitions/classnames_v2.x.x/flow_>=v0.23.x/classnames_v2.x.x.js
@@ -1,6 +1,6 @@
 declare module 'classnames' {
   declare class Classnames {
-    (...classes: Array<string | Object | void>): string
+    (...classes: Array<string | { [class: string]: bool }>): string
   }
   declare var exports: Classnames;
 }
