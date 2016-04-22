fe399d28ea0a83fd521ea21138069fd32338fd5a
marudor <marudor@marudor.de>
2016-04-22 22:12:49 +0200

    react-modal

---
+++ definitions/react-modal_v1.x.x/flow_>=0.23.x/react-modal_v1.x.x.js
@@ -0,0 +1,27 @@
+import type React from 'react';
+
+type ReactModal$DefaultProps = {
+  isOpen: false,
+  ariaHideApp: true,
+  closeTimeoutMS: 0,
+  shouldCloseOnOverlayClick: true,
+}
+
+type ReactModal$Props = {
+  isOpen: bool,
+  style?: {
+    content?: Object,
+    overlay?: Object,
+  },
+  appElement?: HTMLElement,
+  onRequestClose?: Function,
+  closeTimeoutMS: number,
+  ariaHideApp: bool,
+  shouldCloseOnOverlayClick: bool,
+}
+
+type ReactModal$State = void
+
+declare module 'react-modal' {
+  declare var exports: React.Component<DReactModal$efaultProps, ReactModal$Props, ReactModal$State>;
+}
