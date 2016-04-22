e426c6f316eada7486c857e130c8d06eaa2d5da0
marudor <marudor@marudor.de>
2016-04-22 20:47:15 +0200

    core-decorators

---
+++ definitions/core_decorators_v0.12.x/flow_>=v0.23.x/core_decorators_v0.12.x.js
@@ -0,0 +1,15 @@
+declare module 'core-decorators' {
+  declare function autobind(...args: any): any;
+  declare function debounce(...args: any): any;
+  declare function decorate(...args: any): any;
+  declare function deprecate(...args: any): any;
+  declare function enumerable(...args: any): any;
+  declare function lazyInitialize(...args: any): any;
+  declare function mixin(...args: any): any;
+  declare function nonconfigurable(...args: any): any;
+  declare function override(...args: any): any;
+  declare function readonly(...args: any): any;
+  declare function suppressWarnings(...args: any): any;
+  declare function throttle(...args: any): any;
+  declare function time(...args: any): any;
+}
