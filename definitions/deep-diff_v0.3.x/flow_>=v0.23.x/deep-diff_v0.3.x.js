87eb18e3edaadbcd69364aebb656b116097566fd
marudor <marudor@marudor.de>
2016-04-23 09:51:21 +0200

    prefix, mixed instead of any

--- definitions/deep-diff_v0.3.x/flow_>=v0.23.x/deep-diff_v0.3.x.js
+++ definitions/deep-diff_v0.3.x/flow_>=v0.23.x/deep-diff_v0.3.x.js
@@ -1,18 +1,17 @@
-type Difference = {
-  kind: 'N'|'D'|'E'|'A',
-  path: string[],
-  lhs: ?any,
-  rhs: ?any,
-  index?: number,
-  item?: Difference,
-}
+type npm$deepDiff$Difference =
+| {kind: "N", path: Array<string>, rhs: mixed}
+| {kind: "D", path: Array<string>, lhs: mixed}
+| {kind: "E", path: Array<string>, lhs: mixed, rhs: mixed}
+| {kind: "A", path: Array<string>, index: number, item: Difference}
 
-type PrefilterFn = (path: Array<string>, key: string) => bool|void;
+type npm$deepDiff$PrefilterFn = (path: Array<string>, key: string) => bool|void;
 
 declare module 'deep-diff' {
-  declare function exports(lhs: any, rhs: any, prefilter?: PrefilterFn, acc?: Array<any>): ?Array<Difference>;
-  declare function diff(lhs: any, rhs: any, prefilter?: PrefilterFn, acc?: Array<any>): ?Array<Difference>;
-  declare function observableDiff(lhs: any, rhs: any, observerFn: Function): void;
-  declare function applyChange(lhs: any, rhs: any, difference: Difference): void;
-  declare function revertChange(lhs: any, rhs: any, difference: Difference): void;
+  declare var exports: {
+    (lhs: mixed, rhs: mixed, prefilter?: npm$deepDiff$PrefilterFn, acc?: Array<mixed>),
+    diff(lhs: mixed, rhs: mixed, prefilter?: npm$deepDiff$PrefilterFn, acc?: Array<mixed>): ?Array<npm$deepDiff$Difference>,
+    observableDiff(lhs: mixed, rhs: mixed, observerFn: Function): void,
+    applyChange(lhs: mixed, rhs: mixed, difference: npm$deepDiff$Difference): void,
+    revertChange(lhs: mixed, rhs: mixed, difference: npm$deepDiff$Difference): void,
+  }
 }
