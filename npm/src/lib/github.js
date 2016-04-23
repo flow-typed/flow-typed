3004f40b4840d1f82dfa34caba8e2b31bbec1d25
Patrick Stapfer <patrick.stapfer@runtastic.com>
2016-04-23 19:45:29 +0200

    Add env-variable options for GH-client & secret

--- npm/src/lib/github.js
+++ npm/src/lib/github.js
@@ -6,8 +6,24 @@ import {versionToString} from "./semver.js";
 import type {LibDef} from "./libDef.js";
 
 const CLIENT = new GitHub({version: "3.0.0"});
-if (process.env.GH_TOK) {
-  CLIENT.authenticate({type: "oauth", token: process.env.GH_TOK});
+
+const {
+  GH_CLIENT_SECRET,
+  GH_CLIENT_ID,
+  GH_TOK,
+} = process.env;
+
+if (GH_TOK) {
+  CLIENT.authenticate({
+    type: "oauth",
+    token: process.env.GH_TOK
+  });
+} else if (GH_CLIENT_ID && GH_CLIENT_SECRET) {
+  CLIENT.authenticate({
+    type: "oauth",
+    key: GH_CLIENT_ID,
+    secret: GH_CLIENT_SECRET,
+  });
 }
 
 export function gitHubClient(): Object {
