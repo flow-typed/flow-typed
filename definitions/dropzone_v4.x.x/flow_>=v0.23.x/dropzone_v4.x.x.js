718d720865d98e791dcf54cb304cae11963c8590
marudor <marudor@marudor.de>
2016-04-26 20:19:08 +0200

    dropzone tests

--- definitions/dropzone_v4.x.x/flow_>=v0.23.x/dropzone_v4.x.x.js
+++ definitions/dropzone_v4.x.x/flow_>=v0.23.x/dropzone_v4.x.x.js
@@ -1,50 +1,48 @@
-type dropzone$FilesToString = (files: File|Array<File>) => string;
-type dropzone$DrawImageOptions = {
-  srcX: number,
-  srcY: number,
-  srcWidth: number,
-  srcHeight: number,
-}
-
-type dropzone$DropzoneOptions = {
-  url: string|dropzone$FilesToString,
-  method?: 'post'|'put'|dropzone$FilesToString,
-  parallelUploads?: number,
-  maxFilesize?: number,
-  filesizeBase?: number,
-  paramName?: string,
-  uploadMultiple?: bool,
-  headers?: {[key: string]: string},
-  addRemoveLinks?: bool,
-  previewsContainer?: HTMLElement|string,
-  clickable?: bool,
-  createImageThumbnails?: bool,
-  maxThumbnailFilesize?: number,
-  thumbnailWidth?: number,
-  thumbnailHeight?: number,
-  maxFiles?: number,
-  resize?: (file: File) => dropzone$DrawImageOptions,
-  init?: () => mixed,
-  acceptedFiles?: string,
-  autoProcessQueue?: bool,
-  previewTemplate?: string,
-  forceFallback?: bool,
-  fallback?: () => mixed,
-  dictDefaultMessage?: string,
-  dictFallbackMessage?: string,
-  dictFallbackText?: string,
-  dictInvalidFileType?: string,
-  dictFileTooBig?: string,
-  dictResponseError?: string,
-  dictCancelUpload?: string,
-  dictCancelUploadConfirmation?: string,
-  dictRemoveFile?: string,
-  dictMaxFilesExceeded?: string,
-}
-
 declare module 'dropzone' {
+  declare type FilesToString = (files: File|Array<File>) => string;
+  declare type DrawImageOptions = {
+    srcX: number,
+    srcY: number,
+    srcWidth: number,
+    srcHeight: number,
+  }
+  declare type DropzoneOptions = {
+    acceptedFiles?: string,
+    addRemoveLinks?: bool,
+    autoProcessQueue?: bool,
+    clickable?: bool,
+    createImageThumbnails?: bool,
+    dictCancelUpload?: string,
+    dictCancelUploadConfirmation?: string,
+    dictDefaultMessage?: string,
+    dictFallbackMessage?: string,
+    dictFallbackText?: string,
+    dictFileTooBig?: string,
+    dictInvalidFileType?: string,
+    dictMaxFilesExceeded?: string,
+    dictRemoveFile?: string,
+    dictResponseError?: string,
+    fallback?: () => mixed,
+    filesizeBase?: number,
+    forceFallback?: bool,
+    headers?: {[key: string]: string},
+    init?: () => mixed,
+    maxFiles?: number,
+    maxFilesize?: number,
+    maxThumbnailFilesize?: number,
+    method?: 'post'|'put'|FilesToString,
+    parallelUploads?: number,
+    paramName?: string,
+    previewsContainer?: HTMLElement|string,
+    previewTemplate?: string,
+    resize?: (file: File) => DrawImageOptions,
+    thumbnailHeight?: number,
+    thumbnailWidth?: number,
+    uploadMultiple?: bool,
+    url: string|FilesToString,
+  }
   declare class Dropzone {
-    static constructor(selector: string, options: dropzone$DropzoneOptions): Dropzone;
+    constructor(selector: string, options: DropzoneOptions): Dropzone;
     static autoDiscover: bool;
     static confirm(question: string, accepted: () => mixed, rejected?: () => mixed): void;
     disable(): void;
@@ -61,5 +59,5 @@ declare module 'dropzone' {
     removeFile(file: File): void;
     destroy(): void;
   }
-  declare var exports: Class<Dropzone>;
+  declare var exports: typeof Dropzone;
 }
