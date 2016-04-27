27877a790054cb05fc2f42d3eb45868aa7a4c7e3
marudor <marudor@marudor.de>
2016-04-27 21:50:21 +0200

    uuid-js tests

--- definitions/uuid-js_v0.7.x/flow_>=v0.23.x/uuid-js_v0.7.x.js
+++ definitions/uuid-js_v0.7.x/flow_>=v0.23.x/uuid-js_v0.7.x.js
@@ -1,15 +1,16 @@
 declare module 'uuid-js' {
   declare class UUID<V: 1|4> {
-    fromParts(timeLow: any, timeMid: any, timeHiAndVersion: any, clockSeqHiAndReserved: any, clockSeqLow: any, node: any): any;
+    fromParts(timeLow: mixed, timeMid: mixed, timeHiAndVersion: mixed, clockSeqHiAndReserved: mixed, clockSeqLow: mixed, node: mixed): mixed;
     hex: string;
-    toBytes(): Array<any>;
+    toBytes(): Array<mixed>;
     toString(): string;
     toURN(): string;
     version: V;
   }
-  declare function create<V: 1|4>(version?: V): UUID<V>;
+  declare function create(): UUID<1>;
+  declare function create<V: 1|4>(version: V): UUID<V>;
   declare function firstFromTime(time: number): UUID<1>;
-  declare function fromBinary(binary: any): UUID;
+  declare function fromBinary(binary: mixed): UUID;
   declare function fromBytes(bytes: number[]): UUID;
   declare function fromURN(string: string): UUID;
   declare function lastFromTime(time: number): UUID<1>;
