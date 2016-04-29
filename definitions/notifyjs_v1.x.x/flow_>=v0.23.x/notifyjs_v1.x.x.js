bb954a59a14d31a9c63dc320533c74f47c7d3cfc
marudor <marudor@marudor.de>
2016-04-29 20:10:03 +0200

    tests

--- definitions/notifyjs_v1.x.x/flow_>=v0.23.x/notifyjs_v1.x.x.js
+++ definitions/notifyjs_v1.x.x/flow_>=v0.23.x/notifyjs_v1.x.x.js
@@ -1,18 +1,18 @@
-type NotifyOptions = {
-  body?: string,
-  closeOnClick?: bool,
-  icon?: string,
-  lang?: string,
-  notifyClick?: Function,
-  notifyClose?: Function,
-  notifyError?: Function,
-  notifyShow?: Function,
-  requireInteraction?: bool,
-  tag?: string,
-  timeout?: number,
-};
-
 declare module 'notifyjs' {
+  declare type NotifyOptions = {
+    body?: string,
+    closeOnClick?: bool,
+    icon?: string,
+    lang?: string,
+    notifyClick?: Function,
+    notifyClose?: Function,
+    notifyError?: Function,
+    notifyShow?: Function,
+    requireInteraction?: bool,
+    tag?: string,
+    timeout?: number,
+  };
+
   declare class NotifyJS {
     static (title: string, options: NotifyOptions): NotifyJS;
     static requestPermission(onPermissionGrantedCallback?: Function, onPermissionDeniedCallback?: Function): void;
