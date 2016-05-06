95c2354c6944e0a4394a9e0fc2188362cedfe4aa
Jeff Morrison <jeffmo@fb.com>
2016-05-06 09:50:27 -0400

    mv npm cli

--- runtests.sh
+++ runtests.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 set -o errexit
 
-cd npm && npm install && npm run flow && npm run test-quick && npm run build && node dist/cli.js validate-defs && node dist/cli.js run-tests
+cd cli && npm install && npm run flow && npm run test-quick && npm run build && node dist/cli.js validate-defs && node dist/cli.js run-tests
