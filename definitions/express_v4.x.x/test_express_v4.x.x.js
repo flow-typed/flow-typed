f698e7a16f4fc511fc9aa8602b4cfbbbdcdd1f34
Steven Bassett <steven.j.bassett@gmail.com>
2016-05-11 10:53:42 -0700

    Update: first attempt at writting an express definition.

---
+++ definitions/express_v4.x.x/test_express_v4.x.x.js
@@ -0,0 +1,12 @@
+/* @flow */
+import express from 'express';
+
+const app = express();
+
+// $ExpectError property `foo` Property not found in Application
+app.foo();
+
+app.use((req, res, next) => {
+  // $ExpectError 
+  res.shouldNotExist();
+});
