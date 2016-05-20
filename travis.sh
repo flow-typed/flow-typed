36632f9295a8790a0bcb92d2032ad9c34ccacaa1
Jeff Morrison <jeffmo@fb.com>
2016-05-20 19:36:33 -0400

    Move webfontloader and jest definitions into `definitions/npm/`

--- travis.sh
+++ travis.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
-set -o errexit
+#set -o errexit
 
-cd cli
-npm install 
-npm run flow
-npm run test-quick 
-node dist/cli.js validate-defs 
+cd cli && \
+npm install && \
+./node_modules/.bin/flow && \
+npm run test-quick && \
+node dist/cli.js validate-defs && \
 node dist/cli.js run-tests
