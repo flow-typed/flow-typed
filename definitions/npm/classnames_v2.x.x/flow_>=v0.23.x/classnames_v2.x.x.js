f4c8529b8e16eac98c1f056a8a21871c2364d99f
Nat Mote <nmote@fb.com>
2016-05-20 10:45:26 -0700

    Make classnames more permissive (#110)

--- definitions/npm/classnames_v2.x.x/flow_>=v0.23.x/classnames_v2.x.x.js
+++ definitions/npm/classnames_v2.x.x/flow_>=v0.23.x/classnames_v2.x.x.js
@@ -1,5 +1,12 @@
-
-type $npm$classnames$Classes = string | {[className: string]: bool } | Array<string>
+type $npm$classnames$Classes =
+  string |
+  // We need both of these because if we just have the latter it won't accept objects typed
+  // explicitly as the former, due to mutation concerns.
+  {[className: string]: boolean } |
+  {[className: string]: ?boolean } |
+  Array<string> |
+  void |
+  null
 
 declare module 'classnames' {
   declare function exports(
