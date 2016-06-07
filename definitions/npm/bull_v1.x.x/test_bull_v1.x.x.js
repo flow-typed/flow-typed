f5518b7a53c2e672818b5dec5acd0bbce7ce78fe
Christian Legnitto <LegNeato@users.noreply.github.com>
2016-06-07 06:17:12 -0700

    Add types for 'bull' (#124)

---
+++ definitions/npm/bull_v1.x.x/test_bull_v1.x.x.js
@@ -0,0 +1,36 @@
+import Queue from 'bull';
+
+new Queue(
+  'my_queue',
+  9999,
+  'localhost',
+);
+Queue(
+  'my_queue',
+  9999,
+  'localhost',
+);
+new Queue(
+  'my_queue',
+  4444,
+  'localhost',
+  { redisOption: true },
+).process((job) => {
+  Promise.resolve();
+});
+Queue(
+  'my_queue',
+  4444,
+  'localhost',
+  { redisOption: true },
+).process((job, done) => {
+  done();
+});
+
+// $ExpectError
+Queue();
+Queue(1);
+Queue('my_queue', 9999, 'localhost')
+  .process((job, done) => {
+    Promise.resolve();
+  });
