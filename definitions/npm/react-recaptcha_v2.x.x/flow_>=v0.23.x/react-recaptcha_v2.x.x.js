e91ce67d38fa617cd35f1e01eb358311f27889fa
Steven Bassett <steven.j.bassett@gmail.com>
2016-06-13 16:28:16 -0700

    Update: add react-recaptcha

---
+++ definitions/npm/react-recaptcha_v2.x.x/flow_>=v0.23.x/react-recaptcha_v2.x.x.js
@@ -0,0 +1,40 @@
+declare module 'react-recaptcha' {
+  declare type DefaultProps = {
+    elementID: 'g-recaptcha',
+    onloadCallback: void,
+    onloadCallbackName: 'onloadCallback',
+    verifyCallback: void,
+    verifyCallbackName: 'verifyCallback',
+    expiredCallback: void,
+    expiredCallbackName: 'expiredCallback',
+    render: 'onload',
+    theme: 'light',
+    type: 'image',
+    size: 'normal',
+    tabindex: '0',
+  };
+  declare type Props = {
+    className?: string,
+    onloadCallbackName?: string,
+    elementID?: string,
+    onloadCallback?: () => void,
+    verifyCallback?: (value: string) => void,
+    expiredCallback?: () => void,
+    render?: string,
+    sitekey: string,
+    theme?: 'dark' | 'light',
+    type?: string,
+    verifyCallbackName?: string,
+    expiredCallbackName?: string,
+    size?: string,
+    tabindex?: string,
+  };
+
+  declare class Recaptcha extends React$Component {
+    defaultProps: DefaultProps;
+    props: Props;
+    componentDidMount(): void;
+    render(): React$Element;
+  }
+  declare var exports: Class<Recaptcha>;
+}
