66e7bbd37736dc68a87fead2ffe13af27d447af4
Steven Bassett <bassettsj@users.noreply.github.com>
2016-06-17 13:45:24 -0700

    Add libdef for react-recaptcha library (#141)
    Really simple library definition for [react-recaptcha](https://github.com/appleboy/react-recaptcha)

---
+++ definitions/npm/react-recaptcha_v2.x.x/flow_>=v0.26.x/react-recaptcha_v2.x.x.js
@@ -0,0 +1,40 @@
+// @flow
+declare module 'react-recaptcha' {
+  declare type DefaultProps = {
+    elementID: string,
+    onloadCallbackName: string,
+    verifyCallbackName: string,
+    expiredCallbackName: string,
+    render: string,
+    theme: 'dark' | 'light',
+    type: string,
+    size: string,
+    tabindex: string,
+  };
+
+  declare type Props = {
+    className?: string,
+    onloadCallbackName?: string,
+    elementID?: string,
+    onloadCallback?: () => void,
+    verifyCallback?: (value: string) => void,
+    expiredCallback?: () => void,
+    render?: string,
+    sitekey: string,
+    theme?: 'dark' | 'light',
+    type?: string,
+    verifyCallbackName?: string,
+    expiredCallbackName?: string,
+    size?: string,
+    tabindex?: string,
+  };
+
+  declare class Recaptcha extends React$Component {
+    static defaultProps: DefaultProps;
+    props: Props;
+    componentDidMount(): void;
+    render(): React$Element;
+  }
+
+  declare module.exports: Class<Recaptcha>;
+}
