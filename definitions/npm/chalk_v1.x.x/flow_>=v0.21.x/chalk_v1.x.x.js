a6e41447ac16c5a2f7d87434efd68e552e47d9aa
James Kyle <me@thejameskyle.com>
2016-07-05 11:26:19 -0700

    Remove duplicate chalk prefixing

--- definitions/npm/chalk_v1.x.x/flow_>=v0.21.x/chalk_v1.x.x.js
+++ definitions/npm/chalk_v1.x.x/flow_>=v0.21.x/chalk_v1.x.x.js
@@ -1,108 +1,108 @@
-type $npm$chalk$ChalkStyleElement = {
+type $npm$chalk$StyleElement = {
   open: string;
   close: string;
 };
 
-type $npm$chalk$ChalkChain = $npm$chalk$ChalkStyle & (...text: string[]) => string;
+type $npm$chalk$Chain = $npm$chalk$Style & (...text: string[]) => string;
 
-type $npm$chalk$ChalkStyle = {
+type $npm$chalk$Style = {
   // General
-  reset: $npm$chalk$ChalkChain;
-  bold: $npm$chalk$ChalkChain;
-  italic: $npm$chalk$ChalkChain;
-  underline: $npm$chalk$ChalkChain;
-  inverse: $npm$chalk$ChalkChain;
-  strikethrough: $npm$chalk$ChalkChain;
+  reset: $npm$chalk$Chain;
+  bold: $npm$chalk$Chain;
+  italic: $npm$chalk$Chain;
+  underline: $npm$chalk$Chain;
+  inverse: $npm$chalk$Chain;
+  strikethrough: $npm$chalk$Chain;
 
   // Text colors
-  black: $npm$chalk$ChalkChain;
-  red: $npm$chalk$ChalkChain;
-  green: $npm$chalk$ChalkChain;
-  yellow: $npm$chalk$ChalkChain;
-  blue: $npm$chalk$ChalkChain;
-  magenta: $npm$chalk$ChalkChain;
-  cyan: $npm$chalk$ChalkChain;
-  white: $npm$chalk$ChalkChain;
-  gray: $npm$chalk$ChalkChain;
-  grey: $npm$chalk$ChalkChain;
+  black: $npm$chalk$Chain;
+  red: $npm$chalk$Chain;
+  green: $npm$chalk$Chain;
+  yellow: $npm$chalk$Chain;
+  blue: $npm$chalk$Chain;
+  magenta: $npm$chalk$Chain;
+  cyan: $npm$chalk$Chain;
+  white: $npm$chalk$Chain;
+  gray: $npm$chalk$Chain;
+  grey: $npm$chalk$Chain;
 
   // Background colors
-  bgBlack: $npm$chalk$ChalkChain;
-  bgRed: $npm$chalk$ChalkChain;
-  bgGreen: $npm$chalk$ChalkChain;
-  bgYellow: $npm$chalk$ChalkChain;
-  bgBlue: $npm$chalk$ChalkChain;
-  bgMagenta: $npm$chalk$ChalkChain;
-  bgCyan: $npm$chalk$ChalkChain;
-  bgWhite: $npm$chalk$ChalkChain;
+  bgBlack: $npm$chalk$Chain;
+  bgRed: $npm$chalk$Chain;
+  bgGreen: $npm$chalk$Chain;
+  bgYellow: $npm$chalk$Chain;
+  bgBlue: $npm$chalk$Chain;
+  bgMagenta: $npm$chalk$Chain;
+  bgCyan: $npm$chalk$Chain;
+  bgWhite: $npm$chalk$Chain;
 };
 
-type $npm$chalk$ChalkStyleMap = {
+type $npm$chalk$StyleMap = {
   // General
-  reset: $npm$chalk$ChalkStyleElement;
-  bold: $npm$chalk$ChalkStyleElement;
-  italic: $npm$chalk$ChalkStyleElement;
-  underline: $npm$chalk$ChalkStyleElement;
-  inverse: $npm$chalk$ChalkStyleElement;
-  strikethrough: $npm$chalk$ChalkStyleElement;
+  reset: $npm$chalk$StyleElement;
+  bold: $npm$chalk$StyleElement;
+  italic: $npm$chalk$StyleElement;
+  underline: $npm$chalk$StyleElement;
+  inverse: $npm$chalk$StyleElement;
+  strikethrough: $npm$chalk$StyleElement;
 
   // Text colors
-  black: $npm$chalk$ChalkStyleElement;
-  red: $npm$chalk$ChalkStyleElement;
-  green: $npm$chalk$ChalkStyleElement;
-  yellow: $npm$chalk$ChalkStyleElement;
-  blue: $npm$chalk$ChalkStyleElement;
-  magenta: $npm$chalk$ChalkStyleElement;
-  cyan: $npm$chalk$ChalkStyleElement;
-  white: $npm$chalk$ChalkStyleElement;
-  gray: $npm$chalk$ChalkStyleElement;
+  black: $npm$chalk$StyleElement;
+  red: $npm$chalk$StyleElement;
+  green: $npm$chalk$StyleElement;
+  yellow: $npm$chalk$StyleElement;
+  blue: $npm$chalk$StyleElement;
+  magenta: $npm$chalk$StyleElement;
+  cyan: $npm$chalk$StyleElement;
+  white: $npm$chalk$StyleElement;
+  gray: $npm$chalk$StyleElement;
 
   // Background colors
-  bgBlack: $npm$chalk$ChalkStyleElement;
-  bgRed: $npm$chalk$ChalkStyleElement;
-  bgGreen: $npm$chalk$ChalkStyleElement;
-  bgYellow: $npm$chalk$ChalkStyleElement;
-  bgBlue: $npm$chalk$ChalkStyleElement;
-  bgMagenta: $npm$chalk$ChalkStyleElement;
-  bgCyan: $npm$chalk$ChalkStyleElement;
-  bgWhite: $npm$chalk$ChalkStyleElement;
+  bgBlack: $npm$chalk$StyleElement;
+  bgRed: $npm$chalk$StyleElement;
+  bgGreen: $npm$chalk$StyleElement;
+  bgYellow: $npm$chalk$StyleElement;
+  bgBlue: $npm$chalk$StyleElement;
+  bgMagenta: $npm$chalk$StyleElement;
+  bgCyan: $npm$chalk$StyleElement;
+  bgWhite: $npm$chalk$StyleElement;
 };
 
 declare module "chalk" {
   declare var enabled: boolean;
   declare var supportsColor: boolean;
-  declare var styles: $npm$chalk$ChalkStyleMap;
+  declare var styles: $npm$chalk$StyleMap;
 
   declare function stripColor(value: string): any;
   declare function hasColor(str: string): boolean;
 
   // General
-  declare var reset: $npm$chalk$ChalkChain;
-  declare var bold: $npm$chalk$ChalkChain;
-  declare var italic: $npm$chalk$ChalkChain;
-  declare var underline: $npm$chalk$ChalkChain;
-  declare var inverse: $npm$chalk$ChalkChain;
-  declare var strikethrough: $npm$chalk$ChalkChain;
+  declare var reset: $npm$chalk$Chain;
+  declare var bold: $npm$chalk$Chain;
+  declare var italic: $npm$chalk$Chain;
+  declare var underline: $npm$chalk$Chain;
+  declare var inverse: $npm$chalk$Chain;
+  declare var strikethrough: $npm$chalk$Chain;
 
   // Text colors
-  declare var black: $npm$chalk$ChalkChain;
-  declare var red: $npm$chalk$ChalkChain;
-  declare var green: $npm$chalk$ChalkChain;
-  declare var yellow: $npm$chalk$ChalkChain;
-  declare var blue: $npm$chalk$ChalkChain;
-  declare var magenta: $npm$chalk$ChalkChain;
-  declare var cyan: $npm$chalk$ChalkChain;
-  declare var white: $npm$chalk$ChalkChain;
-  declare var gray: $npm$chalk$ChalkChain;
-  declare var grey: $npm$chalk$ChalkChain;
+  declare var black: $npm$chalk$Chain;
+  declare var red: $npm$chalk$Chain;
+  declare var green: $npm$chalk$Chain;
+  declare var yellow: $npm$chalk$Chain;
+  declare var blue: $npm$chalk$Chain;
+  declare var magenta: $npm$chalk$Chain;
+  declare var cyan: $npm$chalk$Chain;
+  declare var white: $npm$chalk$Chain;
+  declare var gray: $npm$chalk$Chain;
+  declare var grey: $npm$chalk$Chain;
 
   // Background colors
-  declare var bgBlack: $npm$chalk$ChalkChain;
-  declare var bgRed: $npm$chalk$ChalkChain;
-  declare var bgGreen: $npm$chalk$ChalkChain;
-  declare var bgYellow: $npm$chalk$ChalkChain;
-  declare var bgBlue: $npm$chalk$ChalkChain;
-  declare var bgMagenta: $npm$chalk$ChalkChain;
-  declare var bgCyan: $npm$chalk$ChalkChain;
-  declare var bgWhite: $npm$chalk$ChalkChain;
+  declare var bgBlack: $npm$chalk$Chain;
+  declare var bgRed: $npm$chalk$Chain;
+  declare var bgGreen: $npm$chalk$Chain;
+  declare var bgYellow: $npm$chalk$Chain;
+  declare var bgBlue: $npm$chalk$Chain;
+  declare var bgMagenta: $npm$chalk$Chain;
+  declare var bgCyan: $npm$chalk$Chain;
+  declare var bgWhite: $npm$chalk$Chain;
 }
