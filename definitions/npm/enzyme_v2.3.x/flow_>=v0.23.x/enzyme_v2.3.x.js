85a0620cffea0c83b3eca1545519c2299971f760
Sami Kukkonen <sami@samikukkonen.fi>
2016-07-05 19:51:36 -0700

    Fix enzyme wrapper chaining

--- definitions/npm/enzyme_v2.3.x/flow_>=v0.23.x/enzyme_v2.3.x.js
+++ definitions/npm/enzyme_v2.3.x/flow_>=v0.23.x/enzyme_v2.3.x.js
@@ -1,19 +1,19 @@
 declare module 'enzyme' {
-  declare type PredicateFunction = (wrapper: Wrapper<any>) => boolean;
+  declare type PredicateFunction<T: Wrapper> = (wrapper: T) => boolean;
   declare type NodeOrNodes = React$Element<any> | Array<React$Element<any>>;
-  declare class Wrapper<ReturnClass: Wrapper<any>> {
-    find(selector: string): ReturnClass;
-    findWhere(predicate: PredicateFunction): ReturnClass;
-    filter(selector: string): ReturnClass;
-    filterWhere(predicate: PredicateFunction): ReturnClass;
+  declare class Wrapper {
+    find(selector: string): this;
+    findWhere(predicate: PredicateFunction<this>): this;
+    filter(selector: string): this;
+    filterWhere(predicate: PredicateFunction<this>): this;
     contains(nodeOrNodes: NodeOrNodes): boolean;
     equals(node: React$Element<any>): boolean;
     hasClass(className: string): boolean;
     instance(): React$Component<any, any, any>;
     is(selector: string): boolean;
     not(selector: string): boolean;
-    children(): ReturnClass;
-    childAt(index: number): ReturnClass;
+    children(): this;
+    childAt(index: number): this;
     props(): Object;
     name(): string;
     prop(key: string): any;
@@ -22,12 +22,12 @@ declare module 'enzyme' {
     html(): string;
     update(): this;
   }
-  declare class ReactWrapper<ReactWrapper> extends Wrapper<any> {}
-  declare class ShallowWrapper<ShallowWrapper> extends Wrapper<any> {
+  declare class ReactWrapper extends Wrapper {}
+  declare class ShallowWrapper extends Wrapper {
     shallow(options?: { context?: Object }): ShallowWrapper;
   }
-  declare class CheerioWrapper<CheerioWrapper> extends Wrapper<any> {}
-  declare function shallow(node: React$Element<any>, options?: { context?: Object }): ShallowWrapper<any>;
-  declare function mount(node: React$Element<any>, options?: { context?: Object, attachTo?: HTMLElement, childContextTypes?: Object }): ReactWrapper<any>;
-  declare function render(node: React$Element<any>, options?: { context?: Object }): CheerioWrapper<any>;
+  declare class CheerioWrapper extends Wrapper {}
+  declare function shallow(node: React$Element<any>, options?: { context?: Object }): ShallowWrapper;
+  declare function mount(node: React$Element<any>, options?: { context?: Object, attachTo?: HTMLElement, childContextTypes?: Object }): ReactWrapper;
+  declare function render(node: React$Element<any>, options?: { context?: Object }): CheerioWrapper;
 }
--- definitions/npm/enzyme_v2.3.x/test_enzyme-v2.3.js
+++ definitions/npm/enzyme_v2.3.x/test_enzyme-v2.3.js
@@ -1,5 +1,6 @@
 // @flow
 import enzyme from 'enzyme';
+import {shallow, ShallowWrapper} from 'enzyme';
 
 // $ExpectError
 enzyme.render();
@@ -7,6 +8,13 @@ shallowWrapper = enzyme.shallow(<div/>);
 (enzyme.render(<div/>, { context: { foo: true } }).text(): string)
 const A: bool = enzyme.render(<div/>, { context: { foo: true } }).find('foo').is('bla');
 const B: bool = enzyme.render(<div/>, { context: { foo: true } }).update().filter('bla').equals(<div/>);
+
+// $ExpectError
+enzyme.shallow(<div />).children().nonExistentMethod();
+const C: ShallowWrapper = shallow(<div />).children();
+
+// Test PredicateFunction
+shallow(<div />).findWhere((x: ShallowWrapper) => true);
 
 shallowWrapper.instance();
 shallowWrapper.find('someSelector');
