06864bb73a63488089bbfa090f861d7a56506d79
Sami Kukkonen <sami@samikukkonen.fi>
2016-07-05 22:28:29 -0700

    Add more tests for enzyme

--- definitions/npm/enzyme_v2.3.x/test_enzyme-v2.3.js
+++ definitions/npm/enzyme_v2.3.x/test_enzyme-v2.3.js
@@ -1,17 +1,19 @@
 // @flow
 import {shallow, mount, render, ShallowWrapper, ReactWrapper} from 'enzyme';
 
-// $ExpectError
-enzyme.render();
-shallowWrapper = shallow(<div/>);
+// $ExpectError too few arguments
+render();
+
+const shallowWrapper = shallow(<div/>);
 (render(<div/>, { context: { foo: true } }).text(): string)
 const A: bool = render(<div/>, { context: { foo: true } }).find('foo').is('bla');
 const B: bool = render(<div/>, { context: { foo: true } }).update().filter('bla').equals(<div/>);
 
 // Test against chaining returning `any`
 // $ExpectError
-shallow(<div />).children().nonExistentMethod();
-const C: ShallowWrapper = shallow(<div />).children();
+(shallow(<div />).children(): boolean);
+
+(shallow(<div />).children(): ShallowWrapper);
 
 // Test PredicateFunction
 shallow(<div />).findWhere((x: ShallowWrapper) => true);
@@ -33,3 +35,15 @@ shallowWrapper.instance();
 shallowWrapper.find('someSelector');
 shallowWrapper.prop('foo');
 shallowWrapper.props().foo;
+
+(mount(<div />).map((node) => true): Array<boolean>);
+
+(mount(<div />).reduce((acc: number, node, i) => i + 1): Array<number>);
+// $ExpectError
+(mount(<div />).reduce((acc: number, node, i) => i + 1): Array<boolean>);
+(mount(<div />).reduce((acc, node, i) => i + 1, 0): Array<number>);
+// $ExpectError
+(mount(<div />).reduce((acc, node, i) => i + 1, 0): Array<boolean>);
+
+// Cheerio
+render(<div />).contents();
