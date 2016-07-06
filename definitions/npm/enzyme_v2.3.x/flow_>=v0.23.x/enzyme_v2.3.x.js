18d1ba6e426384009b01800fc35074d9939cb761
Sami Kukkonen <sami@samikukkonen.fi>
2016-07-05 21:57:50 -0700

    Add missing enzyme methods

--- definitions/npm/enzyme_v2.3.x/flow_>=v0.23.x/enzyme_v2.3.x.js
+++ definitions/npm/enzyme_v2.3.x/flow_>=v0.23.x/enzyme_v2.3.x.js
@@ -8,23 +8,56 @@ declare module 'enzyme' {
     filter(selector: EnzymeSelector): this;
     filterWhere(predicate: PredicateFunction<this>): this;
     contains(nodeOrNodes: NodeOrNodes): boolean;
-    equals(node: React$Element<any>): boolean;
+    containsMatchingElement(node: React$Element<any>): boolean;
+    containsAllMatchingElements(nodes: NodeOrNodes): boolean;
+    containsAnyMatchingElements(nodes: NodeOrNodes): boolean;
+    matchesElement(node: React$Element<any>): boolean;
     hasClass(className: string): boolean;
-    instance(): React$Component<any, any, any>;
     is(selector: EnzymeSelector): boolean;
     not(selector: EnzymeSelector): boolean;
-    children(): this;
+    children(selector?: EnzymeSelector): this;
     childAt(index: number): this;
-    props(): Object;
-    name(): string;
-    prop(key: string): any;
-    type(): string | Function;
+    parents(selector?: EnzymeSelector): this;
+    parent(): this;
+    closest(selector: EnzymeSelector): this;
+    render(): CheerioWrapper;
+    unmount(): this;
     text(): string;
     html(): string;
+    get(index: number): React$Element<any>;
+    at(index: number): this;
+    first(): this;
+    last(): this;
+    state(key?: string): any;
+    context(key?: string): any;
+    props(): Object;
+    prop(key: string): any;
+    key(): string;
+    simulate(event: string, ...args: Array<any>): this;
+    setState(state: Object): this;
+    setProps(props: Object): this;
+    setContext(context: Object): this;
+    instance(): React$Component<any, any, any>;
     update(): this;
+    debug(): string;
+    type(): string | Function | null;
+    name(): string;
+    forEach(fn: (node: this) => any): this;
+    map<T>(fn: (node: this) => T): Array<T>;
+    reduce<T>(fn: (value: T, node: this, index: number) => T, initialValue?: T): Array<T>;
+    reduceRight<T>(fn: (value: T, node: this, index: number) => T, initialValue?: T): Array<T>;
+    some(selector: EnzymeSelector): boolean;
+    someWhere(predicate: PredicateFunction<this>): boolean;
+    every(selector: EnzymeSelector): boolean;
+    everyWhere(predicate: PredicateFunction<this>): boolean;
   }
-  declare class ReactWrapper extends Wrapper {}
+  declare class ReactWrapper extends Wrapper {
+    mount(): this;
+    ref(refName: string): this;
+    detach(): void;
+  }
   declare class ShallowWrapper extends Wrapper {
+    equals(node: React$Element<any>): boolean;
     shallow(options?: { context?: Object }): ShallowWrapper;
   }
   declare class CheerioWrapper extends Wrapper {}
