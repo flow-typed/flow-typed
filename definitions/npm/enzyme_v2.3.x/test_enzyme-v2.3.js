548c8e27cdd7c77fd93bdd085c64498168acbdbc
Sami Kukkonen <sami@samikukkonen.fi>
2016-07-05 20:06:36 -0700

    Use named imports from enzyme instead of default import

--- definitions/npm/enzyme_v2.3.x/test_enzyme-v2.3.js
+++ definitions/npm/enzyme_v2.3.x/test_enzyme-v2.3.js
@@ -1,16 +1,18 @@
 // @flow
 import enzyme from 'enzyme';
-import {shallow, ShallowWrapper} from 'enzyme';
+
+import {shallow, mount, render, ShallowWrapper, ReactWrapper} from 'enzyme';
 
 // $ExpectError
 enzyme.render();
-shallowWrapper = enzyme.shallow(<div/>);
-(enzyme.render(<div/>, { context: { foo: true } }).text(): string)
-const A: bool = enzyme.render(<div/>, { context: { foo: true } }).find('foo').is('bla');
-const B: bool = enzyme.render(<div/>, { context: { foo: true } }).update().filter('bla').equals(<div/>);
+shallowWrapper = shallow(<div/>);
+(render(<div/>, { context: { foo: true } }).text(): string)
+const A: bool = render(<div/>, { context: { foo: true } }).find('foo').is('bla');
+const B: bool = render(<div/>, { context: { foo: true } }).update().filter('bla').equals(<div/>);
 
+// Test against chaining returning `any`
 // $ExpectError
-enzyme.shallow(<div />).children().nonExistentMethod();
+shallow(<div />).children().nonExistentMethod();
 const C: ShallowWrapper = shallow(<div />).children();
 
 // Test PredicateFunction
