c70c87b6007d93feeda5f905858813b4e841191d
James Kyle <me@thejameskyle.com>
2016-07-05 17:14:18 -0700

    Support Inquirer 1.x & Through 2.x

---
+++ definitions/npm/through_2.x.x/test_through_v2.x.x.js
@@ -0,0 +1,12 @@
+import through from 'through';
+
+var i = 0;
+through(
+  function() {
+    this.queue((i++).toString());
+  },
+  function () {
+    this.queue(null);
+  },
+  { autoDestroy: true }
+).pipe(process.stdout);
