bed47b7857e03a3670ae4f871845ece25c4c4878
James Kyle <me@thejameskyle.com>
2016-07-07 13:23:29 -0700

    Add supports-color@3.x.x

---
+++ definitions/npm/supports-color_v3.x.x/flow_>=v0.28.x/supports-color_v3.x.x.js
@@ -0,0 +1,6 @@
+declare module 'supports-color' {
+  declare var exports: false |
+    { level: 1, hasBasic: true, has256: false, has16m: false } |
+    { level: 2, hasBasic: true, has256: true, has16m: false } |
+    { level: 3, hasBasic: true, has256: true, has16m: true };
+}
