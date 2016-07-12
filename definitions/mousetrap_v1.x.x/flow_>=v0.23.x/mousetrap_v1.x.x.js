acb3c99dc0ceaf50a11cf0a428ac6e56595c4109
marudor <marudor@marudor.de>
2016-07-12 19:00:02 +0200

    add tests

--- definitions/mousetrap_v1.x.x/flow_>=v0.23.x/mousetrap_v1.x.x.js
+++
@@ -1,7 +0,0 @@
-declare module 'mousetrap' {
-  declare function bind(key: string|Array<string>, fn: (e: Event, combo?: string) => void, eventType?: string): void;
-  declare function unbind(key: string): void;
-  declare function trigger(key: string): void;
-  declare var stopCallback: Function;
-  declare function reset(): void;
-}
