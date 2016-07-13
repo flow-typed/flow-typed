ad2217fa40936c6c7927787b9f1da7d4ef646626
Steven Bassett <steven.j.bassett@gmail.com>
2016-07-13 11:45:14 -0700

    Update: fix spaces in libdef platform

--- definitions/npm/platform_v1.x.x/flow_>=v0.21.x/platform_v1.x.x.js
+++ definitions/npm/platform_v1.x.x/flow_>=v0.21.x/platform_v1.x.x.js
@@ -1,22 +1,22 @@
 declare module 'platform' {
-    declare class Platform extends Object {
-        description: ?string;
-        layoyt: ?string;
-        manufacturer: ?string;
-        name: ?string;
-        parse(userAgent: string): Platform;
-        prerelease: ?string;
-        product: ?string;
-        toString(): string;
-        ua: ?string;
-        version: ?string;
-        os: {
-            toString(): string,
-            architecture: ?number,
-            version: ?string,
-            family: ?string
-        }
+  declare class Platform extends Object {
+    toString(): string;
+    parse(userAgent: string): Platform;
+    description: ?string;
+    layoyt: ?string;
+    manufacturer: ?string;
+    name: ?string;
+    prerelease: ?string;
+    product: ?string;
+    ua: ?string;
+    version: ?string;
+    os: {
+      toString(): string,
+      architecture: ?number,
+      version: ?string,
+      family: ?string
     }
-    declare type $Platform = Platform;
-    declare var exports: Platform;
+  }
+  declare type $Platform = Platform;
+  declare var exports: Platform;
 }
