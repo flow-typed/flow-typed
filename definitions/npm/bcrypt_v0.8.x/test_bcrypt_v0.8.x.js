e73a8739a6431ec4594ec5cc84a1cb2a27161dbb
Yuki Yamada <yamadayuuki0818@gmail.com>
2016-07-16 06:48:47 +0900

    Tweak

--- definitions/npm/bcrypt_v0.8.x/test_bcrypt_v0.8.x.js
+++ definitions/npm/bcrypt_v0.8.x/test_bcrypt_v0.8.x.js
@@ -2,36 +2,36 @@
 
 import bcrypt from 'bcrypt';
 
-let num: number = 10;
-let str: string = 'password';
-let bool: boolean = false;
+let num = 10;
+let str = 'password';
+let bool = false;
 
 str = bcrypt.genSaltSync();
 str = bcrypt.genSaltSync(num);
 
-bcrypt.genSalt((err: Error, salt: string): void => {
+bcrypt.genSalt((err, salt) => {
   str = salt;
 });
-bcrypt.genSalt(num, (err: Error, salt: string): void => {
+bcrypt.genSalt(num, (err, salt) => {
   str = salt;
 });
 // $ExpectError
-bcrypt.genSalt(str, (err: Error, salt: string): void => {
+bcrypt.genSalt(str, (err, salt) => {
   str = salt;
 });
 
 str = bcrypt.hashSync(str, str);
 str = bcrypt.hashSync(str, num);
 
-bcrypt.hash(str, str, (err: Error, encrypted: string): void => {
+bcrypt.hash(str, str, (err, encrypted) => {
   str = encrypted;
 });
-bcrypt.hash(str, num, (err: Error, encrypted: string): void => {
+bcrypt.hash(str, num, (err, encrypted) => {
   str = encrypted;
 });
 
 bool = bcrypt.compareSync(str, str);
-bcrypt.compare(str, str, (err: Error, same: boolean): void => {
+bcrypt.compare(str, str, (err, same) => {
   bool = same;
 });
 // $ExpectError
