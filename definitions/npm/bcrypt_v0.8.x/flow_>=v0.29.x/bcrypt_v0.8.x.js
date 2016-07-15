e390d97696d2d9895fc1252ed8ba2293c4b36f3e
Yuki Yamada <yamadayuuki0818@gmail.com>
2016-07-16 07:51:35 +0900

    Change data type

--- definitions/npm/bcrypt_v0.8.x/flow_>=v0.29.x/bcrypt_v0.8.x.js
+++ definitions/npm/bcrypt_v0.8.x/flow_>=v0.29.x/bcrypt_v0.8.x.js
@@ -4,11 +4,11 @@ declare module bcrypt {
   declare function genSaltSync(rounds?: number): string;
   declare function genSalt(rounds: number, callback: (err: Error, salt: string) => void): void;
   declare function genSalt(callback: (err: Error, salt:string) => void): void;
-  declare function hashSync(data: any, salt: string): string;
-  declare function hashSync(data: any, rounds: number): string;
-  declare function hash(data: any, salt: string, callback: (err: Error, encrypted: string) => void): void;
-  declare function hash(data: any, rounds: number, callback: (err: Error, encrypted: string) => void): void;
-  declare function compareSync(data: any, encrypted: string): boolean;
-  declare function compare(data: any, encrypted: string, callback: (err: Error, same: boolean) => void): void;
+  declare function hashSync(data: string, salt: string): string;
+  declare function hashSync(data: string, rounds: number): string;
+  declare function hash(data: string, salt: string, callback: (err: Error, encrypted: string) => void): void;
+  declare function hash(data: string, rounds: number, callback: (err: Error, encrypted: string) => void): void;
+  declare function compareSync(data: string, encrypted: string): boolean;
+  declare function compare(data: string, encrypted: string, callback: (err: Error, same: boolean) => void): void;
   declare function getRounds(encrypted: string): number;
 }
