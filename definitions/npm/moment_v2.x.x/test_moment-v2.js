09f1f22d6a1825aacb87b681e107f9025e75ad25
marudor <marudor@marudor.de>
2016-07-15 10:49:13 +0200

    unbreak moment

--- definitions/npm/moment_v2.x.x/test_moment-v2.js
+++ definitions/npm/moment_v2.x.x/test_moment-v2.js
@@ -1,10 +1,16 @@
 // @flow
 import moment from 'moment';
 
-const m: moment = moment().utc();
-// $ExpectError
-const invalid: moment = moment([123, 123]);
-const m1: momemnt = m.add(2, 'day');
+const m: moment = moment.utc();
+const m1: moment = m.add(2, 'day');
 const m2: moment = m.subtract(1, 's');
+const m3: moment = moment([123, 123]);
 const A: Date = m.toDate();
 const x: string = m.toISOString();
+
+//$ExpectError
+moment.unix('1234');
+// $ExpectError
+moment().millisecond().seconds();
+
+moment().milliseconds(100).seconds();
