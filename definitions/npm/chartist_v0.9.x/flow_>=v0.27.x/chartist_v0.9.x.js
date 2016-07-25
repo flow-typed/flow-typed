9cced7c1ebd161265404502c576a6111377a93b5
marudor <marudor@marudor.de>
2016-07-26 00:34:16 +0200

    update

---
+++ definitions/npm/chartist_v0.9.x/flow_>=v0.27.x/chartist_v0.9.x.js
@@ -0,0 +1,86 @@
+declare module 'chartist' {
+  declare type Axis = {
+    offset?: number,
+    position?: string,
+    labelOffset: ?{
+      x: number,
+      y: number,
+    },
+    showLabel?: bool,
+    showGrid?: bool,
+    labelInterpolationFnc?: Function,
+    scaleMinSpace?: number,
+    onlyInteger?: bool,
+  }
+  declare type BarOptions = {
+    axisX?: Axis,
+    axiyY?: Axis,
+    width?: string,
+    height?: string,
+    high?: any,
+    low?: any,
+    onlyInteger?: bool,
+    chartPadding?: {
+      top?: number,
+      right?: number,
+      bottom?: number,
+      left?: number,
+    },
+    seriesBarDistance?: number,
+    stackBars?: bool,
+    stackMode?: string,
+    horizontalBars?: bool,
+    distributeSeries?: bool,
+    reverseData?: bool,
+    classNames?: {
+      chart?: string,
+      horizontalBars?: string,
+      label?: string,
+      labelGroup?: string,
+      series?: string,
+      bar?: string,
+      grid?: string,
+      gridGroup?: string,
+      vertical?: string,
+      horizontal?: string,
+      start?: string,
+      end?: string,
+    },
+  }
+  declare type PieOptions = {
+    width?: string,
+    height?: string,
+    chartPadding?: number|{
+      top?: number,
+      left?: number,
+      right?: number,
+      bottom?: number,
+    },
+    classNames?: {
+      chartPie?: string,
+      chartDonus?: string,
+      series?: string,
+      slicePie?: string,
+      sliceDonue?: string,
+      label?: string,
+    },
+    startAnge?: number,
+    total?: number,
+    donut?: bool,
+    donutWidth?: number,
+    showLabel?: bool,
+    labelOffset?: number,
+    labelPosition?: 'inside'|'outside'|'center',
+    labelInterpolationFnc?: Function,
+    labelDirection?: 'neutral'|'explode'|'implose',
+    reverseData?: bool,
+  }
+  declare class Bar {
+    static defaultOptions: BarOptions;
+    static (query: string|Node, data: Object, options?: BarOptions, responsiveOptions?: Array<any>): Bar;
+  }
+  declare class Pie {
+    static defaultOptions: PieOptions;
+    static (query: string|Node, data: Object, options?: PieOptions, responsiveOptions?: Array<any>): Pie;
+  }
+}
