b6eee46f11a1e2650cce508838e72c4a37af81cb
Steven Bassett <steven.j.bassett@gmail.com>
2016-08-02 16:28:01 -0700

    Update: initial work for backbone lib def

--- definitions/npm/backbone_v1.3.x/test_backbone-v1.3.x.js
+++ definitions/npm/backbone_v1.3.x/test_backbone-v1.3.x.js
@@ -1,6 +1,30 @@
 // @flow
 import Backbone from 'backbone';
+import _ from 'underscore';
+(Backbone.version: string);
 
-Backbone.Events.on('change', function<Event>(e: Event) {
-    console.log(e);
+(Backbone.$: typeof $);
+
+(Backbone._: typeof _);
+
+(Backbone.Events.on: Function);
+interface Fooable extends Backbone.Model {
+    foo(): string;
+    view: Backbone.View;
+};
+const FooModel: Class<Fooable> = Backbone.Model.extend({
+    foo(): string {
+        return '';
+    }
 });
+
+
+
+
+const instance = new FooModel();
+
+// $ExpectError
+instance.set(10);
+
+// $ExpectError
+(instance.foo(): number);
