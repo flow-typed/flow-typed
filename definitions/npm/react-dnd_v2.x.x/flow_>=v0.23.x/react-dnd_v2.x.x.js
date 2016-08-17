d88cacb00ab83593007c6075181ad2304253750e
Christine Chambers <christine.d.hang@gmail.com>
2016-08-16 22:34:09 -0700

    Improve type information for react-dnd
    Updated the react-dnd definitions based on existing TypeScript
    definitions from DefinitelyTyped. Type definitions are now complete
    based on react-dnd documents.
    
    Added unit tests exercising this functionality.
    
    Also fixed incorrect signatures for DragLayer.

--- definitions/npm/react-dnd_v2.x.x/flow_>=v0.23.x/react-dnd_v2.x.x.js
+++ definitions/npm/react-dnd_v2.x.x/flow_>=v0.23.x/react-dnd_v2.x.x.js
@@ -1,6 +1,202 @@
+// Shared
+// ----------------------------------------------------------------------
+// todo: add symbol once flow supports it
+type Identifier = string;
+
+type ClientOffset = {
+  x: number,
+  y: number
+}
+
+type DndOptions<P> = {
+  arePropsEqual?: (props: P, otherProps: P) => boolean
+};
+
+// Decorated Components
+// ----------------------------------------------------------------------
+declare class DndComponent<P> extends React$Component<any, P, any> {
+  static DecoratedComponent: Class<this>;
+  getDecoratedComponentInstance(): this;
+  getHandlerId(): Identifier;
+}
+
+declare class ContextComponent<P> extends React$Component<any, P, any> {
+  static DecoratedComponent: Class<this>;
+  getDecoratedComponentInstance(): this;
+  // getManager is not yet documented in ReactDnd Docs
+  getManager(): any;
+}
+
+// Drag Source
+// ----------------------------------------------------------------------
+type DragSourceType<P> =
+  Identifier |
+  (props: P) => Identifier;
+
+type DragSourceSpec<P> = {
+  beginDrag: (
+    props: P,
+    monitor?: DragSourceMonitor,
+    component?: React$Component<any, P, any>
+  ) => Object,
+
+  endDrag?: (
+    props: P,
+    monitor?: DragSourceMonitor,
+    component?: ?React$Component<any, P, any>
+  ) => void,
+
+  canDrag?: (
+    props: P,
+    monitor?: DragSourceMonitor
+  ) => boolean,
+
+  isDragging?: (
+    props: P,
+    monitor?: DragSourceMonitor
+  ) => boolean
+};
+
+type DragSourceMonitor = {
+  canDrag: () => boolean,
+  isDragging: () => boolean,
+  getItemType: () => Identifier,
+  getItem: () => Object,
+  getDropResult: () => Object,
+  didDrop: () => boolean,
+  getInitialClientOffset: () => ClientOffset,
+  getInitialSourceClientOffset: () => ClientOffset,
+  getClientOffset: () => ClientOffset,
+  getDifferenceFromInitialOffset: () => ClientOffset,
+  getSourceClientOffset: () => ClientOffset
+}
+
+type DragSourceConnector = {
+  dragSource: () => ConnectDragSource,
+  dragPreview: () => ConnectDragPreview
+}
+
+type DragSourceOptions = {
+  dropEffect?: string
+}
+
+type DragPreviewOptions = {
+  captureDraggingState?: boolean,
+  anchorX?: number,
+  anchorY?: number
+}
+
+type ConnectDragSource = (
+  elementOrNode: React$Element<any>
+  options?: DragSourceOptions
+) => React$Element<any>;
+
+type ConnectDragPreview = (
+  elementOrNode: React$Element<any>
+  options?: DragPreviewOptions
+) => React$Element<any>;
+
+type DragSourceCollector = (
+  connect: DragSourceConnector,
+  monitor: DragSourceMonitor
+) => Object;
+
+type DragSource = <P, C: React$Component<any, P, any>>(
+  type: DragSourceType<P>,
+  spec: DragSourceSpec<P>,
+  collect: DragSourceCollector,
+  options?: DndOptions<P>
+) => (component: Class<C>) => Class<DndComponent<P>>;
+
+// Drop Target
+// ----------------------------------------------------------------------
+type DropTargetTypes<P> =
+  Identifier |
+  Array<Identifier> |
+  (props: P) => Identifier | Array<Identifier>;
+
+type DropTargetSpec<P> = {
+  drop?: (
+    props: P,
+    monitor?: DropTargetMonitor,
+    component?: React$Component<any, P, any>
+  ) => ?Object,
+
+  hover?: (
+    props: P,
+    monitor?: DropTargetMonitor,
+    component?: React$Component<any, P, any>
+  ) => void,
+
+  canDrop?: (
+    props: P,
+    monitor?: DropTargetMonitor
+  ) => boolean
+};
+
+type DropTargetMonitor = {
+  canDrop: () => boolean,
+  isOver: (options?: { shallow: boolean }) => boolean,
+  getItemType: () => Identifier,
+  getItem: () => Object,
+  getDropResult: () => Object,
+  didDrop: () => boolean,
+  getInitialClientOffset: () => ClientOffset,
+  getInitialSourceClientOffset: () => ClientOffset,
+  getClientOffset: () => ClientOffset,
+  getDifferenceFromInitialOffset: () => ClientOffset,
+  getSourceClientOffset: () => ClientOffset
+}
+
+type DropTargetConnector = {
+  dropTarget: () => ConnectDropTarget
+}
+
+type ConnectDropTarget = (
+  elementOrNode: React$Element<any>
+) => React$Element<any>;
+
+type DropTarget = <P, C: React$Component<any, P, any>>(
+  types: DropTargetTypes<P>,
+  spec: DropTargetSpec<P>,
+  collect: (
+    connect: DropTargetConnector,
+    monitor: DropTargetMonitor
+  ) => Object,
+  options?: DndOptions<P>
+) => (component: Class<C>) => Class<DndComponent<P>>;
+
+// Drag Layer
+// ----------------------------------------------------------------------
+type DragLayerMonitor = {
+  isDragging: () => boolean;
+  getItemType: () => Identifier;
+  getItem: () => Object;
+  getInitialClientOffset: () => ClientOffset;
+  getInitialSourceClientOffset: () => ClientOffset;
+  getClientOffset: () => ClientOffset;
+  getDifferenceFromInitialOffset: () => ClientOffset;
+  getSourceClientOffset: () => ClientOffset;
+}
+
+type DragLayer = <P, C: React$Component<any, P, any>>(
+  collect: (monitor: DragLayerMonitor) => Object,
+  options?: DndOptions<P>
+) => (component: Class<C>) => Class<DndComponent<P>>;
+
+// Drag Drop Context
+// ----------------------------------------------------------------------
+type DragDropContext = <P, C: React$Component<any, P, any>>(
+  backend: mixed
+) => (component: Class<C>) => Class<ContextComponent<P>>;
+
+// Top-level API
+// ----------------------------------------------------------------------
 declare module 'react-dnd' {
-  declare function DropTarget<T: ReactClass<*>>(type: string|string[], spec?: Object, collect?: Object, options?: mixed): (component: T) => T;
-  declare function DragSource<T: ReactClass<*>>(type: string|string[], spec?: Object, collect?: Object, options?: mixed): (component: T) => T;
-  declare function DragLayer<T: ReactClass<*>>(type: string|string[], spec?: Object, collect?: Object, options?: mixed): (component: T) => T;
-  declare function DragDropContext(backend: mixed): ReactClass<*>;
+  declare var exports : {
+    DragSource: DragSource,
+    DropTarget: DropTarget,
+    DragLayer: DragLayer,
+    DragDropContext: DragDropContext
+  }
 }
--- definitions/npm/react-dnd_v2.x.x/test_react-dnd-v2.js
+++ definitions/npm/react-dnd_v2.x.x/test_react-dnd-v2.js
@@ -1,21 +1,195 @@
-// @flow
+/* @flow */
 
-import ReactDND from 'react-dnd';
+import React from 'react';
+import { DragSource, DropTarget, DragLayer, DragDropContext } from 'react-dnd';
 
-const A = ReactDND.DropTarget('test');
-class AComponent extends React$Component<*, *, *> {};
-(A(AComponent): Class<AComponent>);
+// Test Drag Source
+// ----------------------------------------------------------------------
+type KnightProps = {
+  connectDragSource: (e: React$Element<*>) => React$Element<*>,
+  connectDragPreview: (e: React$Element<*>) => React$Element<*>,
+  isDragging: boolean
+}
+
+const knightSource = {
+  beginDrag() {
+    return {};
+  }
+};
+
+function knightCollect(connect, monitor) {
+  return {
+    connectDragSource: connect.dragSource(),
+    connectDragPreview: connect.dragPreview(),
+    isDragging: monitor.isDragging()
+  };
+}
+
+class Knight extends React.Component<void, KnightProps, void> {
+  componentDidMount() {
+    const img = React.DOM.img();
+    img.onload = () => this.props.connectDragPreview(img);
+  }
+
+  render() {
+    const { connectDragSource, isDragging } = this.props;
+    return connectDragSource(
+      <div style={{
+        fontSize: 40,
+        fontWeight: 'bold',
+        cursor: 'move',
+        opacity: isDragging ? 0.5 : 1
+      }}>
+        ♘
+      </div>
+    );
+  }
+}
+
+const DndKnight = DragSource('knight', knightSource, knightCollect)(Knight);
+(DndKnight: Class<DndComponent<KnightProps>>);
 // $ExpectError
-(A(AComponent): number);
+(DndKnight: number);
 
-const B = ReactDND.DragSource('test');
-class BComponent extends React$Component<*, *, *> {};
-(B(BComponent): Class<BComponent>);
-// $ExpectError
-(B(BComponent): number);
+// Test Drop Target
+// ----------------------------------------------------------------------
+function moveKnight(toX: number, toY: number) {
+}
 
-const C = ReactDND.DragLayer('test');
-class CComponent extends React$Component<*, *, *> {};
-(C(CComponent): Class<CComponent>);
+function canMoveKnight(toX: number, toY: number) {
+  return true;
+}
+
+type BoardSquareProps = {
+  x: number,
+  y: number,
+  isOver: boolean,
+  canDrop: boolean,
+  connectDropTarget: (e: React$Element<*>) => React$Element<*>
+};
+
+const boardSquareTarget = {
+  canDrop(props) {
+    return canMoveKnight(props.x, props.y);
+  },
+
+  drop(props) {
+    moveKnight(props.x, props.y);
+  }
+};
+
+function boardSquareCollect(connect, monitor) {
+  return {
+    connectDropTarget: connect.dropTarget(),
+    isOver: monitor.isOver(),
+    canDrop: monitor.canDrop()
+  };
+}
+
+class Square extends React.Component<void, { black: boolean }, void> {
+  render() {
+    const { black } = this.props;
+    const fill = black ? 'black' : 'white';
+
+    return <div style={{ backgroundColor: fill }} />;
+  }
+}
+
+class BoardSquare extends React.Component<void, BoardSquareProps, void> {
+  renderOverlay(color: string) {
+    return (
+      <div style={{
+        position: 'absolute',
+        top: 0,
+        left: 0,
+        height: '100%',
+        width: '100%',
+        zIndex: 1,
+        opacity: 0.5,
+        backgroundColor: color,
+      }} />
+    );
+  }
+
+  render() {
+    const { x, y, connectDropTarget, isOver, canDrop } = this.props;
+    const black = (x + y) % 2 === 1;
+
+    return connectDropTarget(
+      <div style={{
+        position: 'relative',
+        width: '100%',
+        height: '100%'
+      }}>
+        <Square black={black} />
+        {isOver && !canDrop && this.renderOverlay('red')}
+        {!isOver && canDrop && this.renderOverlay('yellow')}
+        {isOver && canDrop && this.renderOverlay('green')}
+      </div>
+    );
+  }
+}
+
+const DndBoardSquare = DropTarget('boardsquare', boardSquareTarget, boardSquareCollect)(BoardSquare);
+(DndBoardSquare: Class<DndComponent<BoardSquareProps>>);
 // $ExpectError
-(C(CComponent): number);
+(DndBoardSquare: string);
+
+// Test Custom Drag Layer
+// ----------------------------------------------------------------------
+type CustomDragLayerProps = {
+  isDragging: boolean;
+  title: string;
+}
+
+function dragLayerCollect(monitor) {
+  return {
+    isDragging: monitor.isDragging(),
+    item: monitor.getItem(),
+  };
+}
+
+function CustomDragLayer(props: CustomDragLayerProps) {
+  const { title, isDragging } = props;
+  if (!isDragging) {
+    return null;
+  }
+
+  return (
+    <div>this.props.title</div>
+  );
+}
+
+const DndCustomDragLayer = DragLayer(dragLayerCollect)(CustomDragLayer);
+(DndCustomDragLayer: Class<DndComponent<CustomDragLayer>>);
+// $ExpectError
+(DndCustomDragLayer: number);
+
+// Test Drag Drop Context
+// ----------------------------------------------------------------------
+type BoardProps = {
+  width: number,
+  height: number
+};
+
+function Board(props: BoardProps) {
+  const styles = {
+    width: props.width,
+    height: props.height
+  }
+  return <div style={ styles } />;
+}
+
+const DndBoard = DragDropContext({})(Board);
+(DndBoard: Class<ContextComponent<Board>>);
+// $ExpectError
+(DndBoard: string);
+
+// Test Decorated Components
+// ----------------------------------------------------------------------
+(DndKnight.DecoratedComponent: Class<DndKnight>);
+const dk = <DndKnight />;
+
+// TODO: is it possible to type check the following? type of dk is React$Element<{}>
+// (dk.getDecoratedComponentInstance(): DndKnight);
+// (dk.getHandlerId(): string);
