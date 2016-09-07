5c55d0a368cc842ecfa51a002bd5f7667d23b49b
Steven Bassett <steven.j.bassett@gmail.com>
2016-09-07 14:59:16 -0700

    Update: add improved defitions for redux-form@v5

--- definitions/npm/redux-form_v5.x.x/test_redux-form_v5.js
+++ definitions/npm/redux-form_v5.x.x/test_redux-form_v5.js
@@ -1,7 +1,21 @@
 /** @flow */
 import { reduxForm, reducer } from 'redux-form';
+import type { InputProps } from 'redux-form';
 import React from 'react';
+const Input = (props: InputProps) => {
+    (props.onChange: (v: string) => mixed);
+    (props.onChange: (v: SyntheticEvent) => mixed);
+    (props.onUpdate: (v: string) => mixed);
+    (props.onUpdate: (v: SyntheticEvent) => mixed);
 
+    // $ExpectError
+    (props.onChange: (v: number) => mixed);
+    // $ExpectError
+    (props.onUpdate: (v: number) => void);
+    return (
+        <input {...props}/>
+    )
+}
 
 const form = (): React$Element<any> => (
   <form name="name">
