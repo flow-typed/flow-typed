6d456d6695ef4df4651f4b7b78356bc4cce09bdd
Steven Bassett <steven.j.bassett@gmail.com>
2016-09-27 15:21:48 -0700

    Add react-addons-css-transition-group

---
+++ definitions/npm/react-addons-css-transition-group_v15.x.x/test_react-addons-css-transition-group.js
@@ -0,0 +1,80 @@
+// @flow
+import React from 'react';
+import ReactCSSTransitionGroup from 'react-addons-css-transition-group';
+
+
+const shouldWork = () => (
+  <div>
+    <ReactCSSTransitionGroup
+      transitionName="div"
+      transitionEnterTimeout={500}
+      transitionLeaveTimeout={300}>
+      <div />
+    </ReactCSSTransitionGroup>
+  </div>
+)
+
+const fail = () => (
+  // $ExpectError should require a name
+  <ReactCSSTransitionGroup transitionName={null} />
+)
+
+
+const customClasses = () => (
+  <ReactCSSTransitionGroup
+    transitionName={{
+      enter: 'enter',
+      leave: 'leave',
+      appear: 'appear',
+    }}
+    transitionEnterTimeout={500}
+    transitionLeaveTimeout={300}>
+    <div />
+  </ReactCSSTransitionGroup>
+);
+
+
+const customClassesFail = () => (
+
+  <ReactCSSTransitionGroup
+    // $ExpectError
+    transitionName={{}}
+    transitionEnterTimeout={500}
+    transitionLeaveTimeout={300}>
+    <div />
+  </ReactCSSTransitionGroup>
+);
+
+
+
+const customClassesActive = () => (
+  <ReactCSSTransitionGroup
+    transitionName={{
+      enter: 'enter',
+      enterActive: 'enter-active',
+      leave: 'leave',
+      leaveActive: 'leave-active',
+      appear: 'appear',
+      appearActive: 'appearActive',
+    }}
+    transitionEnterTimeout={500}
+    transitionLeaveTimeout={300}>
+    <div />
+  </ReactCSSTransitionGroup>
+);
+
+
+const customClassesActiveFail = () => (
+
+  <ReactCSSTransitionGroup
+    // $ExpectError
+    transitionName={{
+      enterActive: null,
+      leaveActive: null,
+      appearActive: null,
+    }}
+    transitionEnterTimeout={500}
+    transitionLeaveTimeout={300}>
+    <div />
+  </ReactCSSTransitionGroup>
+);
