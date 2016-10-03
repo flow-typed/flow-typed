e4f22f979bd64d3f011dd81a9b5774ba48a598bb
Matt Revell <mattrevell82@me.com>
2016-10-03 23:18:03 +0100

    Fix tests on local repo

--- definitions/npm/react-highlight-words_v0.4.x/test_react-highlight-words_v0.4.x.js
+++ definitions/npm/react-highlight-words_v0.4.x/test_react-highlight-words_v0.4.x.js
@@ -1,10 +1,11 @@
 // @flow
 import ReactHighlightWords from 'react-highlight-words';
 
-//
+// $ExpectError
 const A = <ReactHighlightWords />;
-// $ExpectError missing props searchWords and textToHighlight
+
+// $ExpectError
 const B = <ReactHighlightWords searchWords={[1, 2, 3]} textToHighlight="The dog jumped over the fox" />
-// $ExpectError incorrect prop type of number[] given
+
+// $ExpectError
 const C = <ReactHighlightWords searchWords={'dog', 'cat', 'worm'} textToHighlight={1} />
-// $ExpectError incorrect prop type of number given
