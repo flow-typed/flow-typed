015cdf0fc8a5a2eb6a2c199cf0ac0a61b24b0e91
marudor <marudor@marudor.de>
2016-10-12 11:23:15 +0200

    uuid

---
+++ definitions/npm/uuid_v2.x.x/flow_v0.33.x-/uuid_v2.x.x.js
@@ -0,0 +1,14 @@
+declare module 'uuid' {
+  declare function v1(options?: {|
+    node?: number[],
+    clockseq?: number,
+    msecs?: number | Date,
+    nsecs?: number,
+  |}, buffer?: number[] | Buffer, offset?: number): string;
+  declare function v4(options?: {|
+    random?: number[],
+    rng?: Function,
+  |}, buffer?: number[] | Buffer, offset?: number): string;
+  declare function parse(id: string, buffer?: number[] | Buffer, offset?: number): Buffer;
+  declare function unparse(buffer?: number[] | Buffer, offset?: number): string;
+}
