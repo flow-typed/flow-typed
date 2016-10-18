7154c86e070bc773fb3efa94028580e4b19ad80f
grumpyOldRussian <alexey.golev@wearegoat.com>
2016-10-18 18:26:25 +0100

    [libdef] slug v 0.9.x (#388)

---
+++ definitions/npm/slug_v0.9.x/test_slug_v0.9.x.js
@@ -0,0 +1,32 @@
+import slug from 'slug'
+
+const result1: string = slug('Hello world')
+
+// $ExpectError
+slug(2)
+
+const result2: string = slug('i ♥ unicode', '_')
+
+// $ExpectError
+slug('i ♥ unicode', 2)
+
+// $ExpectError
+slug('Hello world', { mode: 'invalid_mode' })
+
+// $ExpectError
+slug('Hello world', { remove: 'hello' })
+
+// $ExpectError
+slug('Hello world', { symbols: 'hello' })
+
+// $ExpectError
+slug('Hello world', { charmap: 'hello' })
+
+// $ExpectError
+slug('Hello world', { multicharmap: 'hello' })
+
+// $ExpectError
+slug('Hello world', { lower: 'hello' })
+
+// $ExpectError
+slug.defaults.modes['hello']
