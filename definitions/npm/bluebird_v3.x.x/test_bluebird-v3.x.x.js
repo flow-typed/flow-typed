4830a4d0b63789268319730c958d37e40d962d00
Jonathan del Strother <jdelStrother@gmail.com>
2016-10-29 17:58:58 +0100

    Improve Bluebird's interoperability with native promises
    eg:
    
    Bluebird.resolve(Promise.resolve('hello')).then(function(a: string) { })
    
    should typecheck, rather than think that 'a' is a native promise.

--- definitions/npm/bluebird_v3.x.x/test_bluebird-v3.x.x.js
+++ definitions/npm/bluebird_v3.x.x/test_bluebird-v3.x.x.js
@@ -20,3 +20,23 @@ Bluebird.reject('test');
 Bluebird.all([
   1
 ]);
+
+
+Bluebird.resolve([1,2,3]).then(function(arr) {
+  let l = arr.length;
+  // $ExpectError Property not found in Array
+  arr.then(r => r);
+})
+
+let response = fetch('/').then(r => r.text())
+Bluebird.resolve(response).then(function(responseBody) {
+  let length: number = responseBody.length;
+  // $ExpectError Property not found in string
+  responseBody.then(r => r);
+})
+
+Bluebird.all([1, Bluebird.resolve(1), Promise.resolve(1)]).then(function(r: Array<number>) { })
+Bluebird.all(['hello', Bluebird.resolve('world'), Promise.resolve('!')]).then(function(r: Array<string>) { })
+
+// $ExpectError
+Bluebird.all([1, Bluebird.resolve(1), Promise.resolve(1)]).then(function(r: Array<string>) { })
