e7dde4ef06d46be9b33e3e5aa744f0f627e78866
Andrew Hyndman <ajhyndman@hotmail.com>
2016-11-18 02:30:14 +1100

    Use yarn to manage dependencies
    Fixes #415

--- travis.sh
+++ travis.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 #set -o errexit
 
+npm install --global yarn
 cd definitions && \
-npm install && \
-npm test && \
+yarn install && \
+yarn test && \
 cd ../cli && \
-npm install && \
+yarn install && \
 ./node_modules/.bin/flow && \
 node dist/cli.js validate-defs && \
 node dist/cli.js run-tests
