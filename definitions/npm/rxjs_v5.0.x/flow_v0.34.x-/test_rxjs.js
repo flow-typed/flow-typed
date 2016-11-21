cb053e487642bcfe26a04a18bf93dc3c1f9c256f
Max Sherman <john@maxsherman.com>
2016-11-21 14:55:05 -0800

    Add distinct type and tests (#472)

--- definitions/npm/rxjs_v5.0.x/flow_v0.34.x-/test_rxjs.js
+++ definitions/npm/rxjs_v5.0.x/flow_v0.34.x-/test_rxjs.js
@@ -6,3 +6,12 @@ const numbers = Observable.of(1);
 Observable.create(observer => {
   numbers.subscribe(observer);
 });
+
+// $ExpectError
+const distinct1: Observable<number> = Observable.of(1).distinct(1);
+const distinct2: Observable<number> = Observable.of(1).distinct();
+const distinct3: Observable<{a: string}> = Observable.of({a: 'a'}).distinct(field => field.a);
+const distinct4: Observable<{a: string}> = Observable.of({a: 'a'}).distinct(
+  field => field.a,
+  Observable.never(),
+);
