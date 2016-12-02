3f2e7dd9ecab030be478db070d794a08278777e2
evan.henley <evan.henley@help.com>
2016-12-01 18:13:17 -0600

    fix default props and state for class components

--- definitions/npm/react-i18next_v1.10.x/flow_v0.36.x-/react-i18next_v1.10.x.js
+++ definitions/npm/react-i18next_v1.10.x/flow_v0.36.x-/react-i18next_v1.10.x.js
@@ -6,9 +6,12 @@ declare module 'react-i18next' {
 
   declare type StatelessComponent<P> = (props: P) => ?React$Element<any>;
 
-  declare type Comp<P> = StatelessComponent<P> | Class<React$Component<void, P, void>>;
-  declare type TranslatedComponent<OP> = Class<React$Component<void, $Diff<OP, { t: TFunction }>, void>>;
-  declare type Translator<OP, P> = (component: Comp<P>) => TranslatedComponent<OP>;
+  declare type Comp<P> = StatelessComponent<P> | Class<React$Component<*, P, *>>;
+
+  declare type Translator<OP, P> = {
+    (component: StatelessComponent<P>): Class<React$Component<void, OP, void>>;
+    <Def, St>(component: Class<React$Component<Def, P, St>>): Class<React$Component<Def, OP, St>>;
+  }
 
   declare function translate<OP, P>(locales: Locales): Translator<OP, P>;
 
