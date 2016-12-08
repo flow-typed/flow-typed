7334eae62109a5124ada6a18f8f877a677f3a19c
M. Walker Wells <git@mwalkerwells.com>
2016-12-08 06:13:39 -0800

    Express: Make use method covariant to allow class extensions (#508)
    * Use method needs to be covariant
    
    * 1) Using global types instead of imported types 2) Added class extension test case
    
    * Adding one more middleware test case

---
+++ definitions/npm/express_v4.x.x/flow_v0.28.x-/test_overrideUseMethodClassExtension.js
@@ -0,0 +1,59 @@
+// @flow
+import express from 'express'
+
+// Class Extensions: Global Class/Type Declarations (prefixed to prevent name clashes)
+// Use method needs to be covarient: https://github.com/facebook/flow/issues/2770#issuecomment-258955097
+declare class test_express$CustomRequest extends express$Request {
+  foo: string;
+}
+declare class test_express$CustomResponse extends express$Response {
+  bar: string;
+}
+declare type test_express$CustomNextFunction = express$NextFunction;
+
+declare type test_express$CustomMiddleware =
+  ((req: test_express$CustomRequest, res: test_express$CustomResponse, next: test_express$CustomNextFunction) => mixed) |
+  ((error: ?Error, req: test_express$CustomRequest, res: test_express$CustomResponse, next: test_express$CustomNextFunction) => mixed);
+
+declare class test_express$CustomApplication extends express$Application {
+  use(middleware: test_express$CustomMiddleware): this;
+  use(...middleware: Array<test_express$CustomMiddleware>): this;
+  use(path: string|RegExp|string[], ...middleware: Array<test_express$CustomMiddleware>): this;
+  use(path: string, router: express$Router): this;
+}
+
+// Class Extensions: Test Functions
+function test_express$CustomApplication(
+  expressConstructor: () => express$Application
+) {
+  const express = expressConstructor();
+  express.use((req: any, res: any, next: express$NextFunction) => {
+    // Private Constructor Mutation: Add new properties
+    req.foo = 'hello';
+    res.bar = 'goodbye';
+    next();
+  });
+  return express;
+}
+
+// Class Extensions: Test
+const customApp = new test_express$CustomApplication(express);
+
+customApp.use('/something', (req: express$Request, res: express$Response, next: express$NextFunction) => {
+  // $ExpectError
+  req.foo;
+  // $ExpectError
+  res.bar;
+  next();
+});
+customApp.use('/something', (req: test_express$CustomRequest, res: test_express$CustomResponse, next: test_express$CustomNextFunction) => {
+  req.foo;
+  res.bar;
+  // $ExpectError
+  req.notHere;
+  // $ExpectError
+  res.notHere;
+});
+
+// $ExpectError
+customApp.use('/something', (req: string, res: string, next: Function) => { next() });
