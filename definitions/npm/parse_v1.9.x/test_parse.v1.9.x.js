a04b999e6c21bd04b8261af48817e7d614a0dbe7
robarnold <robarnold@cs.cmu.edu>
2017-01-02 12:42:57 -0800

    Update parse_v1.9.x.js (#588)
    * Update parse_v1.9.x.js
    
    Updates the parse definitions to match the documentation at https://parseplatform.github.io/Parse-SDK-JS/api/index.html along with my reading of the code where it is ambiguous or unclear. This is all according to my current use, so I've probably missed some things.
    
    * Update test_parse.v1.9.x.js

--- definitions/npm/parse_v1.9.x/test_parse.v1.9.x.js
+++ definitions/npm/parse_v1.9.x/test_parse.v1.9.x.js
@@ -1,8 +1,31 @@
 // @flow
 import Parse from 'parse/node'
 
+Parse.initialize('appid');
+Parse.initialize('appid', 'jskey');
+
+Parse.masterKey = 'masterkey';
+Parse.masterKey = null;
+Parse.serverURL = 'http://localhost:8080';
+
 const parseObject = new Parse.Object('Test')
-parseObject.get('username')
+parseObject.get('username');
+parseObject.destroy();
+
+class ES6TestObject extends Parse.Object {
+  constructor() {
+    super("ES6TestObject");
+  }
+}
+
+Parse.Object.registerSubclass("ES6TestObject", ES6TestObject);
+
+const usernameFor = async (sessionToken: string): Promise<?string> => {
+  const user = await Parse.User.become(sessionToken);
+  const username = user.getUsername();
+  await Parse.User.logOut();
+  return username;
+};
 
 // $ExpectError
 parseObject.get(false)
