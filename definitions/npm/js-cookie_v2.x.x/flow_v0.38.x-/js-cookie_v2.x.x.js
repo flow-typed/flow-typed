cd78efc61a748df728c0330573533cfa76091b5d
marudor <marudor@marudor.de>
2017-01-18 21:20:22 +0100

    js-cookie v0.38 fix

---
+++ definitions/npm/js-cookie_v2.x.x/flow_v0.38.x-/js-cookie_v2.x.x.js
@@ -0,0 +1,25 @@
+declare module 'js-cookie' {
+    declare type CookieOptions = {
+        expires?: number | Date,
+        path?: string,
+        domain?: string,
+        secure?: boolean
+    }
+    declare type ConverterFunc = (value: string, name: string) => string;
+    declare type ConverterObj = {
+        read: ConverterFunc,
+        write: ConverterFunc
+    };
+    declare class Cookie {
+        defaults: CookieOptions;
+        set(name: string, value: mixed, options?: CookieOptions): void;
+        get(...args: Array<void>): { [key: string]: string };
+        get(name: string, ...args: Array<void>): string | void;
+        remove(name: string, options?: CookieOptions): void;
+        getJSON(name: string): mixed;
+        withConverter(converter: ConverterFunc | ConverterObj): this;
+        noConflict(): this;
+    }
+
+    declare module.exports: Cookie;
+}
