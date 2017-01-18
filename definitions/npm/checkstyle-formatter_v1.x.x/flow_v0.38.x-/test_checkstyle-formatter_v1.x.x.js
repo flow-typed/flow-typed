169e4ee1cecd165fa9280b8169d96e3c6f836232
marudor <marudor@marudor.de>
2017-01-18 15:40:34 +0100

    checksyle-formatter for 0.38

---
+++ definitions/npm/checkstyle-formatter_v1.x.x/flow_v0.38.x-/test_checkstyle-formatter_v1.x.x.js
@@ -0,0 +1,45 @@
+// @flow
+import checkstyleFormatter from 'checkstyle-formatter'
+
+const results = [
+  {
+    filename: 'foo.js',
+    messages: [
+      { line: 1, column: 2, severity: 'warning', message: 'the quick' },
+      { line: 3, column: 4, severity: 'error', message: 'brown fox' }
+    ]
+  },
+  {
+    filename: 'bar.js',
+    messages: [
+      { line: 5, column: 6, severity: 'warning', message: 'jumped over' },
+      { line: 7, column: 8, severity: 'error', message: 'the lazy dog' }
+    ]
+  }
+];
+
+const output: string = checkstyleFormatter(results);
+
+// $ExpectError
+const n: number = checkstyleFormatter(results);
+
+const wrongResultFormat1 = [
+  // $ExpectError
+  {
+    filename: 'foo.js',
+    messages: ''
+  },
+];
+
+checkstyleFormatter(wrongResultFormat1);
+
+const wrongResultFormat2 = [
+  {
+    filename: 'foo.js',
+    // $ExpectError
+    messages: [ { line: 1, column: 2, severity: 'unknown'} ]
+  },
+];
+
+// $ExpectError
+checkstyleFormatter(wrongResultFormat2);
