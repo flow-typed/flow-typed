cb8cc4ee061917d4b6cf70a6afb2d3f66b93553c
Shuhei Kagawa <shuhei.kagawa@zalando.de>
2017-02-02 18:42:58 +0100

    express: pass an array value to Response#set()
    https://github.com/expressjs/express/blob/4.14.1/lib/response.js#L701

--- definitions/npm/express_v4.x.x/flow_v0.32.x-/express_v4.x.x.js
+++ definitions/npm/express_v4.x.x/flow_v0.32.x-/express_v4.x.x.js
@@ -82,7 +82,7 @@ declare class express$Response extends http$ServerResponse mixins express$Reques
   sendStatus(statusCode: number): this;
   header(field: string, value?: string): this;
   header(headers: {[name: string]: string}): this;
-  set(field: string, value?: string): this;
+  set(field: string, value?: string|string[]): this;
   set(headers: {[name: string]: string}): this;
   status(statusCode: number): this;
   type(type: string): this;
