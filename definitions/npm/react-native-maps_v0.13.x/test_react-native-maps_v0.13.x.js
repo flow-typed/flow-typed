ec98c4adf2066ccc8b9838ee447dfc256670e957
Cristian Cedeño <cristiancedegallego@gmail.com>
2017-02-12 12:41:23 -0500

    test react-native-maps

---
+++ definitions/npm/react-native-maps_v0.13.x/test_react-native-maps_v0.13.x.js
@@ -0,0 +1,14 @@
+import MapView, { Polyline, Polygon, Circle } from 'react-native-maps';
+
+// $ExpectError only null undefined of "google allowed"
+const A = <MapView provider="invalid" />
+// $ExpectError coordinates required
+const B = <Polyline />
+// $ExpectError coordinates required
+const C = <Polygon />
+
+// $ExpectError radius required
+const D = <Circle center={{ latitude: 10000, longitude: 1000 }} />
+
+// $ExpectError center required
+const E = <Circle radius={100} />
