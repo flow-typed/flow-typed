8b6b4d99bfe46ff12ab37e2f109c589ec6e8aefc
Jonathan Kim <jon.kim@tegocover.com>
2017-03-13 14:38:17 +0000

    Remove trailing spaces and new lines from git commit

--- cli/src/lib/git.js
+++ cli/src/lib/git.js
@@ -28,10 +28,13 @@ async function getGitPath() {
 export async function getDiff() {
   const gitPath = await getGitPath();
   try {
-    const {stdout:commit} = await child_process.spawnP(
+    let {stdout:commit} = await child_process.spawnP(
       gitPath,
       ['merge-base', 'origin/master', 'HEAD'],
     );
+    // Remove trailing spaces and new lines
+    commit = commit.replace(/^\s+|\s+$/g, '');
+
     const {stdout} = await child_process.spawnP(
       gitPath,
       ['diff', '--name-only', commit],
