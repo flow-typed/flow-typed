3bccc9db5a72a15d257f68ca8e3d7a5ea4b12c27
Kim Joar Bekkelund <kjbekkelund@gmail.com>
2017-03-27 16:20:13 -0400

    libdef for yargs 7

---
+++ definitions/npm/yargs_v7.x.x/test_yargs.js
@@ -0,0 +1,71 @@
+// @flow
+
+import yargs from "yargs";
+
+const argv = yargs
+  .usage('Usage: $0 <cmd> [options]')
+  .command('install', 'install a package (name@version)')
+  .command('publish', 'publish the package inside the current working directory')
+  .option('f', {
+    array: true,
+    description: 'an array of files',
+    default: 'test.js',
+    alias: 'file'
+  })
+  .alias('f', 'fil')
+  .option('h', {
+    alias: 'help',
+    description: 'display help message'
+  })
+  .string(['user', 'pass'])
+  .implies('user', 'pass')
+  .help('help')
+  .demand('q')
+  .version('1.0.1', 'version', 'display version information')
+  .alias('version', 'v')
+  .example('npm install npm@latest -g', 'install the latest version of npm')
+  .epilog('for more information visit https://github.com/chevex/yargs')
+  .showHelpOnFail(false, 'whoops, something went wrong! run with --help')
+  .argv;
+
+const argv2 = yargs(['-x'])
+  .usage('Usage: $0 <cmd> [options]')
+  .command('install', 'install a package (name@version)')
+  .command('publish', 'publish the package inside the current working directory')
+  .option('f', {
+    array: true,
+    description: 'an array of files',
+    default: 'test.js',
+    alias: 'file'
+  })
+  .alias('f', 'fil')
+  .option('h', {
+    alias: 'help',
+    description: 'display help message'
+  })
+  .string(['user', 'pass'])
+  .implies('user', 'pass')
+  .help('help')
+  .demand('q')
+  .version('1.0.1', 'version', 'display version information')
+  .alias('version', 'v')
+  .example('npm install npm@latest -g', 'install the latest version of npm')
+  .epilog('for more information visit https://github.com/chevex/yargs')
+  .showHelpOnFail(false, 'whoops, something went wrong! run with --help')
+  .argv;
+
+yargs(['-x'])
+  // $ExpectError
+  .alias(true, [])
+
+yargs(['-x'])
+  // $ExpectError
+  .help(() => {})
+
+// $ExpectError
+yargs.nope;
+
+// $ExpectError
+yargs.coerce({
+  date: 'foo'
+})
