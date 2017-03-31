edbc5401201542f74873ec570091b7085720fc90
thomas.monte <thomas.monte@insidesales.com>
2017-03-31 12:44:59 -0600

    bringing defs in

--- definitions/npm/react-jss_v5.3.x/flow_v0.39.x-/react-jss_v5.3.x.js
+++ definitions/npm/react-jss_v5.3.x/flow_v0.39.x-/react-jss_v5.3.x.js
@@ -1,13 +1,11 @@
-declare type Klasses<CSS> = {
-	[classname: $Keys<CSS>]: string,
-};
-
-declare type Injector = {
-	<Props, State, DefaultProps, CSS>(component: ClassComponent<DefaultProps, Props, State>): ClassComponent<DefaultProps, $Diff<Props, JSSProps<CSS>>, void>;
-	<Props, CSS>(component: FunctionComponent<Props>): FunctionComponent<$Diff<Props, JSSProps<CSS>>>;
-};
-
 declare module 'react-jss' {
+	declare type Klasses<CSS> = {
+		[classname: $Keys<CSS>]: string,
+	};
+	declare type Injector = {
+		<Props, State, DefaultProps, CSS>(component: ClassComponent<DefaultProps, Props, State>): ClassComponent<DefaultProps, $Diff<Props, JSSProps<CSS>>, void>;
+		<Props, CSS>(component: FunctionComponent<Props>): FunctionComponent<$Diff<Props, JSSProps<CSS>>>;
+	};
 	declare type FunctionComponent<P> = (props: P) => ?React$Element<any>;
 	declare type ClassComponent<D, P, S> = Class<React$Component<D, P, S>>;
 	declare type JSSProps<CSS> = {
