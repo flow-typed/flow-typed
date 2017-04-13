769d99466c32b490d1eecbd8e1b750a8da846e35
Julien Polo <julien.polo@gmail.com>
2017-04-13 12:02:59 +0200

    Add mobile-detect definition

---
+++ definitions/npm/mobile-detect_v1.x.x/test_mobile-detect_v1.x.x.js
@@ -0,0 +1,22 @@
+// @flow
+import MobileDetect from 'mobile-detect';
+
+/* Trivial case */
+
+// $ExpectError
+MobileDetect.foo();
+
+const detect = new MobileDetect('');
+
+detect.mobile();
+detect.mobileGrade();
+detect.os();
+detect.phone();
+detect.tablet();
+detect.userAgent();
+detect.userAgents();
+detect.version('Mobile');
+detect.versionStr('Mobile');
+
+// $ExpectError
+detect.foo();
