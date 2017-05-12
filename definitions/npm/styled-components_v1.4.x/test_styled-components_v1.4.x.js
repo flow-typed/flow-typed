0ca6f777604a447c66940cea5df556925719c537
Joey Baker <joey@scriptdash.com>
2017-05-12 11:09:08 -0700

    Fix(styled-components) add react-native types
    fixes https://github.com/styled-components/styled-components/issues/708

--- definitions/npm/styled-components_v1.4.x/test_styled-components_v1.4.x.js
+++ definitions/npm/styled-components_v1.4.x/test_styled-components_v1.4.x.js
@@ -1,5 +1,11 @@
 import styled, {ThemeProvider, withTheme, keyframes} from 'styled-components'
 import type {Theme} from 'styled-components'
+import nativeStyled, {
+  ThemeProvider as NativeThemeProvider,
+  withTheme as nativeWithTheme,
+  keyframes as nativeKeyframe
+} from 'styled-components/native'
+import type {Theme as NativeTheme} from 'styled-components/native'
 
 const Title = styled.h1`
   font-size: 1.5em;
@@ -51,6 +57,64 @@ const NoExistingComponentWrapper = styled()`
 
 // $ExpectError
 const NumberWrapper = styled(num)`
+  padding: 4em;
+  background: papayawhip;
+`;
+
+// native
+
+const NativeTitle = nativeStyled.Text`
+  font-size: 1.5em;
+  text-align: center;
+  color: palevioletred;
+`;
+
+const ExtendedNativeTitle = nativeStyled(NativeTitle)`
+  font-size: 2em;
+`
+
+const NativeWrapper = nativeStyled.View`
+  padding: 4em;
+  background: ${({theme}) => theme.background};
+`;
+
+const nativeTheme: NativeTheme = {
+  background: "papayawhip"
+}
+
+const NativeComponent = () => (
+  <NativeThemeProvider theme={nativeTheme}>
+    <NativeWrapper>
+      <NativeTitle>
+        Hello World, this is my first native styled component!
+      </NativeTitle>
+    </NativeWrapper>
+  </NativeThemeProvider>
+)
+
+const NativeComponentWithTheme = nativeWithTheme(NativeComponent)
+
+const NativeOpacityKeyFrame = nativeKeyframes`
+  0%   { opacity: 0; }
+  100% { opacity: 1; }
+`;
+
+// $ExpectError
+const NativeNoExistingElementWrapper = nativeStyled.nonexisting`
+  padding: 4em;
+  background: papayawhip;
+`;
+
+const nativeNum = 9
+
+// $ExpectError
+const NativeNoExistingComponentWrapper = nativeStyled()`
+  padding: 4em;
+  background: papayawhip;
+`;
+
+// $ExpectError
+const NativeNumberWrapper = nativeStyled(num)`
   padding: 4em;
   background: papayawhip;
 `;
