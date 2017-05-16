08ca61021e8f1bfb4b598ba71df71df3d22dc090
Sebastian Volland <seb-github@baunz.net>
2017-05-16 14:57:41 +0200

    Add def for 'react-dnd-html5-backend'.NativeTypes (#843)

--- definitions/npm/react-dnd-html5-backend_v2.1.x/test_react-dnd-html5-backend.js
+++ definitions/npm/react-dnd-html5-backend_v2.1.x/test_react-dnd-html5-backend.js
@@ -1,7 +1,15 @@
 // @flow
 
-import {getEmptyImage} from 'react-dnd-html5-backend';
+import {getEmptyImage, NativeTypes} from 'react-dnd-html5-backend';
 
 (getEmptyImage(): Image);
 // $ExpectError
 (getEmptyImage(): number);
+
+var a: $npm$reactDnd$NativeTypes = NativeTypes.FILE
+var a: $npm$reactDnd$NativeTypes = NativeTypes.URL
+var a: $npm$reactDnd$NativeTypes = NativeTypes.TEXT
+// $ExpectError
+var a: $npm$reactDnd$NativeTypes = NativeTypes.NOPE
+// $ExpectError
+var a: $npm$reactDnd$NativeTypes = 'FILE'
