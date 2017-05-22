09dfb3c80e0a3290c3e9063d394362f266834f8a
marudor <marudor@marudor.de>
2017-05-22 15:24:00 +0200

    get diffs in travis

--- cli/src/lib/git.js
+++ cli/src/lib/git.js
@@ -1,7 +1,7 @@
 // @flow
 
 import whichCb from 'which';
-import {child_process} from "./node";
+import { child_process } from './node';
 
 async function getGitPath() {
   try {
@@ -11,7 +11,7 @@ async function getGitPath() {
       `Unable to find ${'`'}git${'`'} installed on this system: ${e.message}`
     );
   }
-};
+}
 
 function which(executable): Promise<string> {
   return new Promise((res, rej) => {
@@ -28,33 +28,24 @@ function which(executable): Promise<string> {
 export async function add(repoPath: string, pathToAdd: string) {
   const gitPath = await getGitPath();
   try {
-    await child_process.spawnP(
-      gitPath,
-      ["add", pathToAdd],
-      {cwd: repoPath},
-    );
+    await child_process.spawnP(gitPath, ['add', pathToAdd], { cwd: repoPath });
   } catch (e) {
     throw new Error(`Error adding staged file(s) to git repo: ${e.message}`);
   }
-};
+}
 
-export async function commit(
-  dirPath: string,
-  message: string,
-) {
+export async function commit(dirPath: string, message: string) {
   const gitPath = await getGitPath();
 
   try {
-    await child_process.spawnP(
-      gitPath,
-      ["commit", "-a", "-m", message],
-      {cwd: dirPath},
-    );
+    await child_process.spawnP(gitPath, ['commit', '-a', '-m', message], {
+      cwd: dirPath
+    });
   } catch (e) {
     console.error(e);
     //throw new Error(`Error creating a commit in git repo: ${e.message}`);
   }
-};
+}
 
 export async function setLocalConfig(
   dirPath: string,
@@ -66,26 +57,32 @@ export async function setLocalConfig(
   try {
     await child_process.spawnP(
       gitPath,
-      ["config", name, JSON.stringify(value)],
-      {cwd: dirPath},
+      ['config', name, JSON.stringify(value)],
+      { cwd: dirPath }
     );
   } catch (e) {
     console.error(e);
     //throw new Error(`Error creating a commit in git repo: ${e.message}`);
   }
-};
+}
 
 export async function getDiff() {
   const gitPath = await getGitPath();
   try {
-    const {stdout:commit} = await child_process.spawnP(
-      gitPath,
-      ['merge-base', 'origin/master', 'HEAD'],
-    );
-    const {stdout} = await child_process.spawnP(
-      gitPath,
-      ['diff', '--name-only', commit.replace('\n','')],
-    );
+    let { stdout } = await child_process.spawnP(gitPath, [
+      'diff',
+      '--name-only'
+    ]);
+
+    if (stdout === '') {
+      // We are probably already on master, so compare to the last commit.
+      const { stdout: headDiff } = await child_process.spawnP(gitPath, [
+        'diff',
+        'HEAD~1',
+        '--name-only'
+      ]);
+      stdout = headDiff;
+    }
     return stdout.split('\n');
   } catch (e) {
     return [];
@@ -95,27 +92,20 @@ export async function getDiff() {
 export async function cloneInto(gitURL: string, destDirPath: string) {
   const gitPath = await getGitPath();
   try {
-    await child_process.spawnP(
-      gitPath,
-      ['clone', gitURL, destDirPath],
-    );
+    await child_process.spawnP(gitPath, ['clone', gitURL, destDirPath]);
   } catch (e) {
     throw new Error(`Error cloning repo: ${e.message}`);
   }
-};
+}
 
 export async function init(dirPath: string) {
   const gitPath = await getGitPath();
   try {
-    await child_process.spawnP(
-      gitPath,
-      ['init'],
-      {cwd: dirPath}
-    );
+    await child_process.spawnP(gitPath, ['init'], { cwd: dirPath });
   } catch (e) {
     throw new Error(`Error init-ing git repo: ${e.message}`);
   }
-};
+}
 
 export async function findLatestFileCommitHash(
   repoPath: string,
@@ -123,10 +113,10 @@ export async function findLatestFileCommitHash(
 ): Promise<string> {
   const gitPath = await getGitPath();
   try {
-    const {stdout} = await child_process.spawnP(
+    const { stdout } = await child_process.spawnP(
       gitPath,
-      ['log', "--pretty=%H", filePath],
-      {cwd: repoPath}
+      ['log', '--pretty=%H', filePath],
+      { cwd: repoPath }
     );
     return stdout.trim();
   } catch (e) {
@@ -134,32 +124,28 @@ export async function findLatestFileCommitHash(
       `Error finding latest commit hash for ${filePath}: ${e.message}`
     );
   }
-};
+}
 
 export async function rebaseRepoMaster(repoDirPath: string) {
   const gitPath = await getGitPath();
-  await child_process.spawnP(
-    gitPath,
-    ['checkout', 'master'],
-    {cwd: repoDirPath}
-  ).catch(({stderr}) => {
-    throw new Error(
-      'Error checking out the `master` branch of the following repo:\n' +
-      `${repoDirPath}\n\n${stderr}`
-    );
-  });
+  await child_process
+    .spawnP(gitPath, ['checkout', 'master'], { cwd: repoDirPath })
+    .catch(({ stderr }) => {
+      throw new Error(
+        'Error checking out the `master` branch of the following repo:\n' +
+          `${repoDirPath}\n\n${stderr}`
+      );
+    });
 
   try {
-    await child_process.execFileP(
-      gitPath,
-      ['pull', '--rebase'],
-      {cwd: repoDirPath}
-    );
+    await child_process.execFileP(gitPath, ['pull', '--rebase'], {
+      cwd: repoDirPath
+    });
   } catch (e) {
-    const {stderr} = e;
+    const { stderr } = e;
     throw new Error(
       'Error rebasing the `master` branch of the following repo:\n' +
-      `${repoDirPath}\n\n${stderr}`
+        `${repoDirPath}\n\n${stderr}`
     );
   }
-};
+}
