5edc16d26c51e621073cfc9f67115c28ef34f6df
Turadg Aleahmad <turadg@aleahmad.net>
2017-05-25 17:54:32 -0700

    fixed some defs by battle testing, and ran prettier

--- definitions/npm/backbone-model_v1.x.x/flow_v0.25.x-/backbone-model_v1.x.x.js
+++ definitions/npm/backbone-model_v1.x.x/flow_v0.25.x-/backbone-model_v1.x.x.js
@@ -1,92 +1,194 @@
 // subset of the `backbone` module
-declare module 'backbone-model' {
+
+// repeated in backbone, backbone-model, and backbone-collection
+declare type Backbone$Attrs = { [name: string]: any };
+declare type Backbone$Id = number | string;
+
+declare module "backbone-model" {
   declare type eventCallback = (event: Event) => void | mixed;
-  declare type Attrs = {[name: string]: mixed};
-  declare type CRUDMethod = 'create' | 'read' | 'update' | 'delete';
 
   /**
    * Events Module - http://backbonejs.org/#Events
    */
   declare class Events {
     // Not sure the best way of adding these to the declaration files
-    on(event: string, callback: eventCallback, context?: Object): void;
-    once(event: string, callback: eventCallback, context?: Object): void;
-    bind(event: string, callback: eventCallback, context?: Object): void;
-    off(event: ?string, callback?: ?eventCallback, context?: Object): void;
-    unbind(event: ?string, callback?: ?eventCallback, context?: Object): void;
-    trigger(event: string, ...args?: Array<mixed>): void;
-    listenTo(other: Events, event: string, callback: eventCallback): void;
-    listenToOnce(other: Events, event: string, callback: eventCallback): void;
-    stopListening(other: Events, callback?: ?eventCallback, context?: Object): void;
-    static on(event: string, callback: eventCallback, context?: Object): void;
-    static bind(event: string, callback: eventCallback, context?: Object): void;
-    static off(event: ?string, callback?: ?eventCallback, context?: Object): void;
-    static unbind(event: ?string, callback?: ?eventCallback, context?: Object): void;
-    static trigger(event: string, ...args?: Array<mixed>): void;
-    static listenTo(other: Events, event: string, callback: eventCallback): void;
-    static stopListening(other: Events, callback?: ?eventCallback, context?: Object): void;
+    on(event: string, callback: eventCallback, context?: Object): void,
+    once(event: string, callback: eventCallback, context?: Object): void,
+    bind(event: string, callback: eventCallback, context?: Object): void,
+    off(event: ?string, callback?: ?eventCallback, context?: Object): void,
+    unbind(event: ?string, callback?: ?eventCallback, context?: Object): void,
+    trigger(event: string, ...args?: Array<mixed>): void,
+    listenTo(other: Events, event: string, callback: eventCallback): void,
+    listenToOnce(other: Events, event: string, callback: eventCallback): void,
+    stopListening(
+      other: Events,
+      callback?: ?eventCallback,
+      context?: Object
+    ): void,
+    static on(event: string, callback: eventCallback, context?: Object): void,
+    static bind(event: string, callback: eventCallback, context?: Object): void,
+    static off(
+      event: ?string,
+      callback?: ?eventCallback,
+      context?: Object
+    ): void,
+    static unbind(
+      event: ?string,
+      callback?: ?eventCallback,
+      context?: Object
+    ): void,
+    static trigger(event: string, ...args?: Array<mixed>): void,
+    static listenTo(
+      other: Events,
+      event: string,
+      callback: eventCallback
+    ): void,
+    static stopListening(
+      other: Events,
+      callback?: ?eventCallback,
+      context?: Object
+    ): void
   }
 
   /**
    * Model Class - http://backbonejs.org/#Model
    */
   declare type ModelOpts = {
+    [optionName: string]: mixed,
     collection?: Collection<*>,
-    parse?: Function,
-    [optionName: string]: mixed
+    parse?: Function
   };
 
-  declare class Model mixins Events {
-    static extend<P, CP>(instanceProperies: P, classProperties?: CP): Class<Model & P> & CP;
-    constructor(attributes?: Attrs, options?: ModelOpts): void;
-    static initialize(attributes?: Attrs, options?: ModelOpts): void;
-    initialize(): void;
+  declare class Model {
+    static extend<P, CP>(
+      instanceProperies: P,
+      classProperties?: CP
+    ): Class<Model & P> & CP,
+    constructor(attributes?: Backbone$Attrs, options?: ModelOpts): void,
+    static initialize(attributes?: Backbone$Attrs, options?: ModelOpts): void,
+    initialize(): void,
     get(attr: ?string): any,
-    set(attrs: Attrs, options?: Object): this;
-    set(attr: string, value: mixed, options?: Object): this;
-    escape(attr: string): mixed;
-    has(attr: string): boolean;
-    unset(attr: string, options?: { unset?: boolean }): this;
-    clear(options?: Object): this;
-    id: string | number;
-    idAttribute: string;
-    cid: string;
-    cidPrefix: string;
-    attributes: Attrs;
-    changed: ?Object;
-    defaults(attr: Object): void;
-    defaults(attr: () => void): void;
-    toJSON(): Attrs;
+    set(attrs: Backbone$Attrs, options?: Object): this,
+    set(attr: string, value: mixed, options?: Object): this,
+    escape(attr: string): mixed,
+    has(attr: string): boolean,
+    unset(attr: string, options?: { unset?: boolean }): this,
+    clear(options?: Object): this,
+    id: Backbone$Id,
+    idAttribute: string,
+    cid: string,
+    cidPrefix: string,
+    attributes: Backbone$Attrs,
+    changed: Object,
+    defaults(): Backbone$Attrs,
+    toJSON(): Backbone$Attrs,
     //Start jQuery XHR
     // @TODO should return a jQuery XHR, but I cannot define this without the dependency on jquery lib def
-    fetch(options?: Object): any;
-    save(attrs: Attrs, options?: Object): any;
-    save(attr: string, value: mixed, options?: Object): any;
-    destroy(options?: Object): any;
+    fetch(options?: Object): any,
+    save(attrs: Backbone$Attrs, options?: Object): any,
+    save(attr: string, value: mixed, options?: Object): any,
+    destroy(options?: Object): any,
     // End jQuery XHR
-    validate(attrs: Attrs, options?: Object): boolean;
-    validationError: ?Object;
-    isValid(): boolean;
-    url(): string;
-    urlRoot: string | () => string;
-    parse(response: Object, options?: Object): any;
-    clone(): this;
-    isNew(): boolean;
-    hasChanged(attribute?: string): boolean;
-    chagnedAttributes(attributes?: {[attr: string]: mixed}): boolean;
-    previous(attribute: string): mixed;
-    previousAttributes(): Attrs;
+    validate(attrs: Backbone$Attrs, options?: Object): boolean,
+    validationError: ?Object,
+    isValid(): boolean,
+    url(): string,
+    urlRoot: string | (() => string),
+    parse(response: Object, options?: Object): any,
+    clone(): this,
+    isNew(): boolean,
+    hasChanged(attribute?: string): boolean,
+    changedAttributes(attributes?: { [attr: string]: mixed }): boolean,
+    previous(attribute: string): mixed,
+    previousAttributes(): Backbone$Attrs,
     // Start Underscore methods
     // @TODO Underscore Methods should be defined by the library definition
-    keys(): string[];
-    values(): mixed[];
-    pairs: Function;
-    invert: Function;
-    pick: Function;
-    omit: Function;
-    chain(): Function;
-    isEmpty(): boolean;
+    keys(): string[],
+    values(): mixed[],
+    pairs: Function,
+    invert: Function,
+    pick: Function,
+    omit: Function,
+    chain(): Function,
+    isEmpty(): boolean
     // End underscore methods
+  }
+
+  /**
+   * Collection Class - http://backbonejs.org/#Collection
+   */
+  declare class Collection<TModel> {
+    static extend<P, CP>(
+      instanceProperies: P,
+      classProperties?: CP
+    ): Class<Collection<*> & P> & CP,
+    constructor(models?: TModel, options?: Object): void,
+    initialize(models?: TModel, options?: Object): void,
+    model: TModel,
+    modelId(attributes: TModel): string,
+    models: TModel[],
+    toJSON(options?: Object): TModel[],
+    // Underscore Methods
+    // @TODO should be defined by the underscore library defintion and not as generic functions.
+    forEach: Function, //(each)
+    map: Function, //(collect)
+    reduce: Function, // (foldl, inject)
+    reduceRight: Function, //(foldr)
+    find: Function, // (detect)
+    findIndex: Function,
+    findLastIndex: Function,
+    filter: Function, //(select)
+    reject: Function,
+    every: Function, //(all)
+    some: Function, //(any)
+    contains: Function, //(includes)
+    invoke: Function,
+    max: Function,
+    min: Function,
+    sortBy: Function,
+    groupBy: Function,
+    shuffle: Function,
+    toArray: Function,
+    size: Function,
+    first: Function, //(head, take)
+    initial: Function,
+    rest: Function, //(tail, drop)
+    last: Function,
+    without: Function,
+    indexOf: Function,
+    lastIndexOf: Function,
+    isEmpty: Function,
+    chain: Function,
+    difference: Function,
+    sample: Function,
+    partition: Function,
+    countBy: Function,
+    indexBy: Function,
+    // end underscore methods
+    add(models: Array<TModel>, options?: Object): void,
+    remove(models: Array<TModel | Backbone$Id>, options?: Object): void,
+    reset(models?: Array<TModel>, options?: Object): void,
+    set(models: Array<TModel>, options?: Object): void,
+    get(id: ?string): ?TModel,
+    at(index: number): ?TModel,
+    push(model: TModel, options?: Object): void,
+    pop(otions?: Object): void,
+    unshift(model: TModel, options?: Object): void,
+    shift(options?: Object): TModel,
+    slice(begin: number, end: number): Array<TModel>,
+    length: number,
+    comparator:
+      | string
+      | ((attr: string) => any | ((attrA: TModel, attrB: TModel) => number)),
+    sort(options?: Object): Array<TModel>,
+    pluck(attribute: string): Array<TModel>,
+    where(attributes: { [attributeName: string]: mixed }): Array<TModel>,
+    findWhere(attributes: { [attributeName: string]: mixed }): TModel,
+    url: () => string | string,
+    parse(response: Object, options: Object): Object,
+    clone(): this,
+    fetch(options?: Object): void,
+    create(attributes: Object, options?: Object): void
   }
 
   /**
@@ -94,7 +196,7 @@ declare module 'backbone-model' {
    */
   declare class Backbone {
     // subset of the `backbone` module
-    Model: typeof Model;
+    Model: typeof Model
   }
 
   declare var exports: Backbone;
