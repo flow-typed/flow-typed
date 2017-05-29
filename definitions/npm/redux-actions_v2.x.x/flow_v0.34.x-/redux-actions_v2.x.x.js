a1baf03f9663e1346563750aca06d616ab83c3a8
Tomáš Ehrlich <tomas.ehrlich@gmail.com>
2017-05-29 10:26:54 +0200

    Handle Function in redux-actions/handleActions

--- definitions/npm/redux-actions_v2.x.x/flow_v0.34.x-/redux-actions_v2.x.x.js
+++ definitions/npm/redux-actions_v2.x.x/flow_v0.34.x-/redux-actions_v2.x.x.js
@@ -81,7 +81,7 @@ declare module 'redux-actions' {
   ): Reducer<State, Action>;
 
   declare function handleActions<State, Action>(
-    reducers: { [key: string]: Reducer<State, Action> | ReducerMap<State, Action> },
+    reducers: { [key: string | Function]: Reducer<State, Action> | ReducerMap<State, Action> },
     defaultState?: State
   ): Reducer<State, Action>;
 
