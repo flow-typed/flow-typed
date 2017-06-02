b2f2b5b2aee4db08a299dc0f219b2b8fcce24f11
Steven Yap <stevenyap@futureworkz.com>
2017-06-02 12:03:55 +0800

    Extend firebase v4 for other older flow versions
    This commit extends the typing for firebase v4 to flow version 0.34.
    The versioning of the lib type def is also renamed to v4.x.x instead of
    v4.0.0 for better compatibility to future minor versions of firebase v4.
    
    This commit also fixes two small bugs with Query.remove() and
    ServerValue.TIMESTAMP()

---
+++ definitions/npm/firebase_v4.x.x/flow_v0.34.x-v0.45.x/test_firebase.js
@@ -0,0 +1,121 @@
+// @flow
+
+import firebase from 'firebase';
+import app from 'firebase/app';
+import auth from 'firebase/auth';
+import database from 'firebase/database';
+
+// #1
+const a1: firebase.app.App = firebase.initializeApp({
+  apiKey: 'apiKey',
+  databaseURL: 'databaseURL',
+  projectId: '42',
+});
+
+// #2
+// $ExpectError
+const a2: firebase.app.App = firebase.initializeApp({
+  apiKey: 'apiKey',
+  storageBucker: 'storageBucket',
+  projectId: '42',
+});
+
+// #3
+const a3: app.App = app('DEFAULT');
+
+// #4
+firebase.auth()
+  .createUserWithEmailAndPassword('email', 'password')
+  .then()
+  .catch()
+
+// #5
+firebase.auth()
+  .onAuthStateChanged(user => {
+    if (user) {
+      (user.displayName);
+      (user.email);
+      (user.emailVerified);
+      (user.photoURL);
+      (user.isAnonymous);
+      (user.uid);
+      (user.providerData);
+      // $ExpectError
+      (user.foobar);
+    }
+  });
+
+// #6
+firebase.auth()
+  .currentUser
+  .linkAndRetrieveDataWithCredential({
+    providerId: 'providerId',
+  })
+  .then(userCredential => {
+    (userCredential.user);
+    // $ExpectError
+    (userCredential.foobar);
+  })
+
+// #7
+const provider = new firebase.auth.GithubAuthProvider();
+firebase.auth()
+  .currentUser
+  .linkWithPopup(provider)
+  .then(result => {
+    (result.credential);
+    (result.additionalUserInfo);
+    (result.operationType);
+    (result.user);
+    // $ExpectError
+    (result.foobar);
+  });
+
+// #8
+const provider2 = new firebase.auth.EmailAuthProvider();
+// $ExpectError
+firebase.auth()
+  .currentUser
+  .linkWithPopup(provider2)
+  .then(result => {
+    (result.credential);
+    (result.additionalUserInfo);
+    (result.operationType);
+    (result.user);
+    // $ExpectError
+    (result.foobar);
+  });
+
+// #9
+const prevUser = firebase.auth().currentUser;
+const credential = { providerId: 'providerId' };
+firebase.auth()
+  .signInWithCredential(credential)
+  .then(user => user.delete().then(() => prevUser.linkWithCredential(credential)))
+  .then(() => firebase.auth().signInWithCredential(credential))
+
+// #10
+firebase.database().ref('users/42').set({username: 'foobar'});
+
+// #11
+firebase.database().ref('users/42').on('value', snp => {
+  (snp.val());
+});
+
+// #12
+// $ExpectError
+firebase.database().ref('users/42').on('foo', snp => {
+  (snp.val());
+});
+
+// #13
+firebase.database()
+  .ref('users/42')
+  .orderByKey()
+  .limitToLast(17)
+  .once('child_added')
+  .then(snp => snp.forEach(_ => true))
+
+// #14
+// $ExpectError
+firebase.database().ref('users/42').orderByKey().limitToLast('foo')
