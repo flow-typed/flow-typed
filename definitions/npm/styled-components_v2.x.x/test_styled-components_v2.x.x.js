628868042d204dd5cc74ff01e5a41c0089bd7b73
Rolf Erik Lekang <me@rolflekang.com>
2017-06-06 22:15:20 +0200

    Add libdef for styled-components v2

---
+++ definitions/npm/styled-components_v2.x.x/test_styled-components_v2.x.x.js
@@ -0,0 +1,72 @@
+// @flow
+import {renderToString} from 'react-dom/server'
+import styled, {ThemeProvider, withTheme, keyframes, ServerStyleSheet, StyleSheetManager} from 'styled-components'
+import type {Theme} from 'styled-components'
+
+const Title = styled.h1`
+  font-size: 1.5em;
+  text-align: center;
+  color: palevioletred;
+`;
+
+const ExtendedTitle = styled(Title)`
+  font-size: 2em;
+`
+
+const Wrapper = styled.section`
+  padding: 4em;
+  background: ${({theme}) => theme.background};
+`;
+
+const theme: Theme = {
+  background: "papayawhip"
+}
+
+const Component = () => (
+  <ThemeProvider theme={theme}>
+    <Wrapper>
+      <Title>Hello World, this is my first styled component!</Title>
+    </Wrapper>
+  </ThemeProvider>
+)
+
+const ComponentWithTheme = withTheme(Component)
+
+const OpacityKeyFrame = keyframes`
+  0%   { opacity: 0; }
+  100% { opacity: 1; }
+`;
+
+// $ExpectError
+const NoExistingElementWrapper = styled.nonexisting`
+  padding: 4em;
+  background: papayawhip;
+`;
+
+const num = 9
+
+// $ExpectError
+const NoExistingComponentWrapper = styled()`
+  padding: 4em;
+  background: papayawhip;
+`;
+
+// $ExpectError
+const NumberWrapper = styled(num)`
+  padding: 4em;
+  background: papayawhip;
+`;
+
+const sheet = new ServerStyleSheet()
+const html = renderToString(sheet.collectStyles(<Component />))
+const css = sheet.getStyleTags()
+
+const sheet2 = new ServerStyleSheet()
+const html2 = renderToString(
+  <StyleSheetManager sheet={sheet}>
+    <Component />
+  </StyleSheetManager>
+)
+
+const css2 = sheet.getStyleTags()
+const css3 = sheet.getStyleElement()
