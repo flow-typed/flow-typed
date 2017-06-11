c8a2911a732c8117f052ab2060785bcc0d97f769
Fredrik Fossum <fredrik.fossum@gmail.com>
2017-06-11 15:15:04 +0200

    Add rxjs Observable buffer methods

--- definitions/npm/rxjs_v5.0.x/flow_v0.34.x-/test_rxjs.js
+++ definitions/npm/rxjs_v5.0.x/flow_v0.34.x-/test_rxjs.js
@@ -20,3 +20,46 @@ const distinct4: Observable<{ a: string }> = Observable.of({ a: 'a' }).distinct(
 
 const timer$: Observable<number> = Observable.timer(1000).startWith(10);
 const numbers2: Observable<number> = Observable.of(1, 2, 3).repeat();
+
+// http://reactivex.io/rxjs/class/es6/Observable.js~Observable.html#instance-method-buffer
+var clicks = Observable.fromEvent(document, 'click');
+var interval = Observable.interval(1000);
+var buffered = interval.buffer(clicks);
+buffered.subscribe(x => console.log(x));
+
+// http://reactivex.io/rxjs/class/es6/Observable.js~Observable.html#instance-method-bufferCount
+// $ExpectError
+clicks.bufferCount();
+var buffered = clicks.bufferCount(2);
+buffered.subscribe(x => console.log(x));
+
+var buffered = clicks.bufferCount(2, 1);
+buffered.subscribe(x => console.log(x));
+
+// http://reactivex.io/rxjs/class/es6/Observable.js~Observable.html#instance-method-bufferTime
+// $ExpectError
+clicks.bufferTime();
+var buffered = clicks.bufferTime(1000);
+buffered.subscribe(x => console.log(x));
+
+var buffered = clicks.bufferTime(2000, 5000);
+buffered.subscribe(x => console.log(x));
+
+// http://reactivex.io/rxjs/class/es6/Observable.js~Observable.html#instance-method-bufferToggle
+var openings = Observable.interval(1000);
+var buffered = clicks.bufferToggle(openings, i =>
+  i % 2 ? Observable.interval(500) : Observable.empty()
+);
+// $ExpectError
+clicks.bufferToggle(openings, i =>
+  i.length % 2 ? Observable.interval(500) : Observable.empty()
+);
+buffered.subscribe(x => console.log(x));
+
+// http://reactivex.io/rxjs/class/es6/Observable.js~Observable.html#instance-method-bufferWhen
+var buffered = clicks.bufferWhen(() =>
+  Observable.interval(1000 + Math.random() * 4000)
+);
+// $ExpectError
+click.bufferWhen(() => true);
+buffered.subscribe(x => console.log(x));
