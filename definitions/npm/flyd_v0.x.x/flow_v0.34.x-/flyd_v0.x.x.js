d6bbe1f55a1f626aaa59e0ac941b791615c1751b
Douglas Brown <doug@github.douggo.com>
2017-06-14 15:50:59 -0400

    flyd - replace `any` with `*`

--- definitions/npm/flyd_v0.x.x/flow_v0.34.x-/flyd_v0.x.x.js
+++ definitions/npm/flyd_v0.x.x/flow_v0.34.x-/flyd_v0.x.x.js
@@ -6,32 +6,32 @@ type Stream = {
   end(E: boolean): void,
   map(F: (*) => *): Stream,
   ap(Stream): Stream,
-  of(any): Stream,
-  val: any,
+  of(*): Stream,
+  val: *,
 }
-type CombineFn = (...args: Array<Stream>) => any;
-type MapFn = (any) => any;
-type ScanFn = (any, any) => any;
-type LiftFn = (...args: Array<any>) => any;
+type CombineFn = (...args: Array<Stream>) => *;
+type MapFn = (*) => *;
+type ScanFn = (*, *) => *;
+type LiftFn = (...args: Array<*>) => *;
 
-type RegularObj = { [key: string]: any | RegularObj };
+type RegularObj = { [key: string]: * | RegularObj };
 type StreamObj = { [key: string]: Stream | StreamObj };
 
 declare module 'flyd' {
   declare type Stream = Stream;
 
   declare module.exports: {
-    stream(v: any): Stream,
+    stream(v: *): Stream,
     immediate(s: Stream): Stream,
-    isStream(s: any): boolean,
+    isStream(s: *): boolean,
     combine: CurriedFunction2<CombineFn, Array<Stream>, Stream>,
     endsOn: CurriedFunction2<Stream, Stream, Stream>,
     map: CurriedFunction2<MapFn, Stream, Stream>,
     on: CurriedFunction2<MapFn, Stream, Stream>,
-    scan: CurriedFunction3<ScanFn, any, Stream, Stream>,
+    scan: CurriedFunction3<ScanFn, *, Stream, Stream>,
     merge: CurriedFunction2<Stream, Stream, Stream>,
     transduce: CurriedFunction2<MapFn | Function, Stream, Stream>,
-    curryN(length: number, fn: (...args: Array<any>) => any): Function,
+    curryN(length: number, fn: (...args: Array<*>) => *): Function,
   };
 }
 
@@ -52,7 +52,7 @@ declare module 'flyd/module/every' {
 }
 
 declare module 'flyd/module/filter' {
-  declare module.exports: CurriedFunction2<(any) => boolean, Stream, Stream>;
+  declare module.exports: CurriedFunction2<(*) => boolean, Stream, Stream>;
 }
 
 declare module 'flyd/module/flatmap' {
@@ -60,7 +60,7 @@ declare module 'flyd/module/flatmap' {
 }
 
 declare module 'flyd/module/forwardto' {
-  declare module.exports: CurriedFunction2<Stream, (any) => any, Stream>;
+  declare module.exports: CurriedFunction2<Stream, (*) => *, Stream>;
 }
 
 declare module 'flyd/module/inlast' {
@@ -96,7 +96,7 @@ declare module 'flyd/module/sampleon' {
 }
 
 declare module 'flyd/module/scanmerge' {
-  declare module.exports: CurriedFunction2<[Stream, ScanFn], any, Stream>;
+  declare module.exports: CurriedFunction2<[Stream, ScanFn], *, Stream>;
 }
 
 declare module 'flyd/module/switchlatest' {
