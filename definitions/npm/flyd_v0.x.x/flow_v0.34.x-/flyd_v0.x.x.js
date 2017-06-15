bab266e11015ac94f2420bd11c4fbbb5e236d53e
Doug Brown <doug@github.douggo.com>
2017-06-15 18:09:08 -0400

    Adding initial types for flyd (#951)
    * Add Initial Typing for flyd v0.x.x
    
    I'm using these already by manually including them in my project, I'm
    just looking to pay it forward.  :)
    
    I'm not entirely certain why the tests are failing... I swear I had
    these definitions working moments ago.  Wouldn't mind another pair of
    eyes.
    
    * flyd - replace `any` with `*`
    
    * Fix Flyd Tests - Impliment Generics
    
    The tests were failing because flow just straight-up didn't understand
    how the Stream type worked.  Implimenting as a class allows us to use
    generics correctly and fixes flow's understanding of flyd.
    
    * Add stream w/ generic for more strict typing
    
    Late in the game, it occurs to me that some may want to specify more
    strict stream types.  It'd be helpful to expose the stream class from
    the module...
    
    * flyd stream type export - capitalize to fix shadow
    
    Okay apparantly flow doesn't like shadow vars... oops.

---
+++ definitions/npm/flyd_v0.x.x/flow_v0.34.x-/flyd_v0.x.x.js
@@ -0,0 +1,137 @@
+declare class flyd$Stream<+T> {
+  static val: T;
+
+  end(boolean): flyd$Stream<boolean>;
+  map<T, V>((T) => V): flyd$Stream<V>;
+  ap<V>(flyd$Stream<V>): flyd$Stream<V>;
+  of<V>(...values: V[]): flyd$Stream<V>;
+
+  (...rest: Array<void>): T;
+  (*): flyd$Stream<*>;
+}
+type Stream = flyd$Stream<*>;
+type CombineFn = (...args: Array<Stream>) => *;
+type MapFn = (*) => *;
+type ScanFn = (*, *) => *;
+type LiftFn = (...args: Array<*>) => *;
+
+type RegularObj = { [key: string]: * | RegularObj };
+type StreamObj = { [key: string]: Stream | StreamObj };
+
+declare module 'flyd' {
+  declare type Stream = Stream;
+  declare type Flyd$Stream<T> = flyd$Stream<T>;
+
+  declare module.exports: {
+    stream<T>(v: T): flyd$Stream<T>,
+    immediate<T>(s: flyd$Stream<?T>): flyd$Stream<?T>,
+    isStream(s: *): boolean,
+    combine: CurriedFunction2<CombineFn, Array<flyd$Stream<*>>, Stream>,
+    endsOn: CurriedFunction2<Stream, Stream, Stream>,
+    map: CurriedFunction2<MapFn, Stream, Stream>,
+    on: CurriedFunction2<MapFn, Stream, Stream>,
+    scan: CurriedFunction3<ScanFn, *, Stream, Stream>,
+    merge: CurriedFunction2<Stream, Stream, Stream>,
+    transduce: CurriedFunction2<MapFn | Function, Stream, Stream>,
+    curryN(length: number, fn: (...args: Array<*>) => *): Function,
+  };
+}
+
+declare module 'flyd/module/aftersilence' {
+  declare module.exports: CurriedFunction2<number, Stream, Stream>;
+}
+
+declare module 'flyd/module/diff' {
+  declare module.exports: CurriedFunction2<ScanFn, Stream, Stream>;
+}
+
+declare module 'flyd/module/droprepeats' {
+  declare module.exports: (s: Stream) => Stream;
+}
+
+declare module 'flyd/module/every' {
+  declare module.exports: (number) => Stream;
+}
+
+declare module 'flyd/module/filter' {
+  declare module.exports: CurriedFunction2<(*) => boolean, Stream, Stream>;
+}
+
+declare module 'flyd/module/flatmap' {
+  declare module.exports: CurriedFunction2<MapFn, Stream, Stream>;
+}
+
+declare module 'flyd/module/forwardto' {
+  declare module.exports: CurriedFunction2<Stream, (*) => *, Stream>;
+}
+
+declare module 'flyd/module/inlast' {
+  declare module.exports: CurriedFunction2<number, Stream, Stream>;
+}
+
+declare module 'flyd/module/keepwhen' {
+  declare module.exports: CurriedFunction2<Stream, Stream, Stream>;
+}
+
+declare module 'flyd/module/lift' {
+  declare module.exports: (LiftFn, ...args: Array<Stream>) => Stream;
+}
+
+declare module 'flyd/module/mergeall' {
+  declare module.exports: (Array<Stream>) => Stream;
+}
+
+declare module 'flyd/module/obj' {
+  declare module.exports: {
+    streamProps(obj: RegularObj): StreamObj,
+    extractProps(obj: StreamObj): RegularObj,
+    stream(obj: StreamObj): Stream,
+  };
+}
+
+declare module 'flyd/module/previous' {
+  declare module.exports: (Stream) => Stream;
+}
+
+declare module 'flyd/module/sampleon' {
+  declare module.exports: CurriedFunction2<Stream, Stream, Stream>;
+}
+
+declare module 'flyd/module/scanmerge' {
+  declare module.exports: CurriedFunction2<[Stream, ScanFn], *, Stream>;
+}
+
+declare module 'flyd/module/switchlatest' {
+  declare module.exports: (Stream) => Stream;
+}
+
+declare module 'flyd/module/takeuntil' {
+  declare module.exports: CurriedFunction2<Stream, Stream, Stream>;
+}
+
+// Filename aliases
+declare module 'flyd/flyd' {
+  declare module.exports: $Exports<'flyd'>;
+}
+declare module 'flyd/flyd.js' {
+  declare module.exports: $Exports<'flyd'>;
+}
+declare module 'flyd/lib' {
+  declare module.exports: $Exports<'flyd'>;
+}
+declare module 'flyd/lib/index' {
+  declare module.exports: $Exports<'flyd/lib'>;
+}
+declare module 'flyd/lib/index.js' {
+  declare module.exports: $Exports<'flyd/lib'>;
+}
+
+// copy from ramda because importing won't work...
+type CurriedFunction2<T1, T2, R> =
+  & ((t1: T1, t2: T2) => R)
+  & ((t1: T1, ...rest: Array<void>) => (t2: T2) => R);
+
+declare type CurriedFunction3<T1, T2, T3, R> =
+  & ((t1: T1, t2: T2, t3: T3) => R)
+  & ((t1: T1, t2: T2, ...rest: Array<void>) => (t3: T3) => R)
+  & ((t1: T1, ...rest: Array<void>) => CurriedFunction2<T2, T3, R>)
