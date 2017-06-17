2606066f48a08a544880aa6029d41d86e3cf0b4b
Oleg <oleglustenko@gmail.com>
2017-06-17 22:12:11 +0300

    add koa-route v3.2.x

---
+++ definitions/npm/koa-route_v3.2.x/flow_all/koa-route_v3.2.x.js
@@ -0,0 +1,54 @@
+
+type RoutePath = string | RegExp | Array<string | RegExp>;
+
+type RouteHandler = (this: Koa.Context, ctx: Koa.Context, ...params: any[]) => any;
+
+type CreateRoute = (routeFunc: RouteHandler) => Koa.Middleware;
+
+interface RouteMethod {
+    (path: RoutePath): CreateRoute;
+    (path: RoutePath, fn: RouteHandler, opts?: pathToRegexp.ParseOptions & pathToRegexp.RegExpOptions): Koa.Middleware;
+}
+
+type CreateRouteMethod = (method: string) => RouteMethod;
+
+interface KoaRoutes {
+    all: CreateRouteMethod;
+    acl: RouteMethod;
+    bind: RouteMethod;
+    checkout: RouteMethod;
+    connect: RouteMethod;
+    copy: RouteMethod;
+    delete: RouteMethod;
+    del: RouteMethod;
+    get: RouteMethod;
+    head: RouteMethod;
+    link: RouteMethod;
+    lock: RouteMethod;
+    msearch: RouteMethod;
+    merge: RouteMethod;
+    mkactivity: RouteMethod;
+    mkcalendar: RouteMethod;
+    mkcol: RouteMethod;
+    move: RouteMethod;
+    notify: RouteMethod;
+    options: RouteMethod;
+    patch: RouteMethod;
+    post: RouteMethod;
+    propfind: RouteMethod;
+    proppatch: RouteMethod;
+    purge: RouteMethod;
+    put: RouteMethod;
+    rebind: RouteMethod;
+    report: RouteMethod;
+    search: RouteMethod;
+    subscribe: RouteMethod;
+    trace: RouteMethod;
+    unbind: RouteMethod;
+    unlink: RouteMethod;
+    unlock: RouteMethod;
+    unsubscribe: RouteMethod;
+}
+
+declare module.exports: KoaRoutes
+
