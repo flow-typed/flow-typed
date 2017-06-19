7fd0a6404e718c1b6dec9360017c53c7d3fbeeda
Aaron Reisman <aaron@lifeiscontent.net>
2017-06-19 20:20:46 +0700

    Fix classnames to support classnames/bind (#931)

--- definitions/npm/classnames_v2.x.x/test_classnames_v2.x.x.js
+++ definitions/npm/classnames_v2.x.x/test_classnames_v2.x.x.js
@@ -1,17 +1,17 @@
 // @flow
 
-import classnames from "classnames";
+import classnames from 'classnames';
 
 classnames();
 classnames('a');
 classnames('a', 'b');
-classnames({a: true});
-classnames({a: true}, {b: true});
-classnames('a', {b: true});
-classnames('a', {b: 'truthy'});
-classnames('a', {b: null});
-classnames({a: true}, 'b');
-classnames({a: null, b: undefined});
+classnames({ a: true });
+classnames({ a: true }, { b: true });
+classnames('a', { b: true });
+classnames('a', { b: 'truthy' });
+classnames('a', { b: null });
+classnames({ a: true }, 'b');
+classnames({ a: null, b: undefined });
 classnames(undefined);
 classnames(null);
 classnames('a', false);
