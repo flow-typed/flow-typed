47190617d390ed7b8ef6ade889858f418799a5cd
Aaron Reisman <aaron@lifeiscontent.net>
2017-06-20 16:43:21 +0700

    Update test_tinycolor2_v1.4.x.js

--- definitions/npm/tinycolor2_v1.4.x/test_tinycolor2_v1.4.x.js
+++ definitions/npm/tinycolor2_v1.4.x/test_tinycolor2_v1.4.x.js
@@ -1,4 +1,10 @@
 import tinycolor from 'tinycolor2';
 
+const color = tinycolor('#F00');
+
+color.toHSL();
+color.toHSV();
+color.toHex();
+
 // $ExpectError
-tinycolor('#foo').toThing();
+color.toThing();
