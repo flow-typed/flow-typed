ef40eafff2d112bdde1e220953efd8692b5cc565
Aaron Reisman <aaron@lifeiscontent.net>
2017-06-21 22:41:16 +0700

    Update test_tinycolor2_v1.4.x.js

--- definitions/npm/tinycolor2_v1.4.x/test_tinycolor2_v1.4.x.js
+++ definitions/npm/tinycolor2_v1.4.x/test_tinycolor2_v1.4.x.js
@@ -5,7 +5,7 @@ import type { TinyColorInstance, RgbaColor } from 'tinycolor2';
 const color = tinycolor('rgb(0,0,0)');
 
 (color: TinyColorInstance<'rgb(0,0,0)'>);
-tinycolor2.fromRatio({ r: 0, g: 0, b: 0 });
+tinycolor.fromRatio({ r: 0, g: 0, b: 0 });
 color.toRgb().r;
 (color.toRgb(): RgbaColor);
 // $ExpectError
