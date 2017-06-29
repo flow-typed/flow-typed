5bd730d6053b0144f0b3893480139b3f60af7645
Facundo Viale <facundo.m.viale@gmail.com>
2017-06-29 06:55:11 -0300

    Add export of types (#984)

--- definitions/npm/next_v2.x.x/flow_v0.37.x-/next_v2.x.x.js
+++ definitions/npm/next_v2.x.x/flow_v0.37.x-/next_v2.x.x.js
@@ -1,6 +1,6 @@
 declare module "next" {
-  declare type RequestHandler = (req: http$IncomingMessage, res: http$ServerResponse, parsedUrl: any) => Promise<void>;
-  declare type NextApp = {
+  declare export type RequestHandler = (req: http$IncomingMessage, res: http$ServerResponse, parsedUrl: any) => Promise<void>;
+  declare export type NextApp = {
     prepare(): Promise<void>,
     getRequestHandler(): RequestHandler,
     render(req: http$IncomingMessage, res: http$ServerResponse, pathname: string, query: any): any,
@@ -8,7 +8,7 @@ declare module "next" {
     renderError(err: Error, req: http$IncomingMessage, res: http$ServerResponse, pathname: any, query: any): any,
     renderErrorToHTML(err: Error, req: http$IncomingMessage, res: http$ServerResponse, pathname: string, query: any): string,
   };
-  declare type Options = {
+  declare export type Options = {
     dev?: boolean,
     dir?: string,
     quiet?: boolean,
@@ -26,14 +26,14 @@ declare module "next/head" {
 declare module "next/link" {
   import type {Component} from 'react';
 
-  declare type State = { href: string };
+  declare export type State = { href: string };
   declare export default Class<Component<void, State, *>>;
 }
 
 declare module "next/prefetch" {
   import type {Component} from 'react';
 
-  declare type State = {
+  declare export type State = {
     href: string,
     prefetch?: boolean,
   };
@@ -62,7 +62,7 @@ declare module "next/router" {
 declare module "next/document" {
   import type {Component} from 'react';
 
-  declare type Context = {
+  declare export type Context = {
     pathname: string,
     query: any,
     req?: any,
