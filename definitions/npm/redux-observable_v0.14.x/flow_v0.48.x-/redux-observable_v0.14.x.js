a2902081e618ecf64474af117de50e919f522e20
ailrun <jjc9310@gmail.com>
2017-07-03 09:08:52 -0400

    Add redux-observable type definition

---
+++ definitions/npm/redux-observable_v0.14.x/flow_v0.48.x-/redux-observable_v0.14.x.js
@@ -0,0 +1,34 @@
+import { Observable } from 'rxjs';
+
+declare module 'redux-observable' {
+  import type { MiddlewareAPI, Dispatch } from 'redux';
+
+  declare export type Epic<S, A, D> = (action$: ActionsObservable<A>, store: MiddlewareAPI<S, A>, dependencies: D) => Observable<A>;
+
+  declare export type EpicMiddlewareAdapter<A> = {
+    input: Observable<A> => Observable<A>,
+    output: Observable<A> => Observable<A>
+  };
+  declare export type EpicMiddlewareOptions<A, D> = {
+    adapter?: EpicMiddlewareAdapter<A>,
+    dependencies?: D
+  };
+  declare export type EpicMiddleware<S, A> = {
+    (api: MiddlewareAPI<S, A>): (next: Dispatch<A>) => Dispatch<A>;
+    replaceEpic<D>(epic: Epic<S, A, D>): void;
+  }
+
+  declare export class ActionsObservable<A: { +type: $Subtype<string> }> extends Observable<A> {
+    static of(...actions: Array<A>): ActionsObservable<A>;
+    static from(actions: Iterable<A>, scheduler: rxjs$SchedulerClass): ActionsObservable<A>;
+    constructor(actionsSubject: Observable<A>): void;
+    lift(operator: Function): ActionsObservable<A>;
+    ofType(...keys: Array<$PropertyType<A, 'type'>>): ActionsObservable<A>;
+  }
+
+  declare export function combineEpics<S, A, D>(...epics: Array<Epic<S, A, D>>): Epic<S, A, D>;
+
+  declare export function createEpicMiddleware<S, A, D>(epic: Epic<S, A, D>, options?: EpicMiddlewareOptions<A, D>): EpicMiddleware<S, A>;
+
+  declare export var EPIC_END: '@@redux-observable/EPIC_END';
+}
