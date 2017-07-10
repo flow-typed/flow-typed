538804b7bcf98cec3044d68ffd0142008e29c1f3
Tristan Amini <tamini@uber.com>
2017-07-10 16:07:24 -0700

    Added T16 option to reselect/SelectorCreator definitions.

--- definitions/npm/reselect_v3.x.x/flow_v0.37.x-/reselect_v3.x.x.js
+++ definitions/npm/reselect_v3.x.x/flow_v0.37.x-/reselect_v3.x.x.js
@@ -4,6 +4,81 @@ declare module 'reselect' {
   };
 
   declare type SelectorCreator = {
+  <TState, TProps, TResult, T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12, T13, T14, T15, T16>(
+    selector1: Selector<TState, TProps, T1>,
+    selector2: Selector<TState, TProps, T2>,
+    selector3: Selector<TState, TProps, T3>,
+    selector4: Selector<TState, TProps, T4>,
+    selector5: Selector<TState, TProps, T5>,
+    selector6: Selector<TState, TProps, T6>,
+    selector7: Selector<TState, TProps, T7>,
+    selector8: Selector<TState, TProps, T8>,
+    selector9: Selector<TState, TProps, T9>,
+    selector10: Selector<TState, TProps, T10>,
+    selector11: Selector<TState, TProps, T11>,
+    selector12: Selector<TState, TProps, T12>,
+    selector13: Selector<TState, TProps, T13>,
+    selector14: Selector<TState, TProps, T14>,
+    selector15: Selector<TState, TProps, T15>,
+    selector16: Selector<TState, TProps, T16>,
+    resultFunc: (
+      arg1: T1,
+      arg2: T2,
+      arg3: T3,
+      arg4: T4,
+      arg5: T5,
+      arg6: T6,
+      arg7: T7,
+      arg8: T8,
+      arg9: T9,
+      arg10: T10,
+      arg11: T11,
+      arg12: T12,
+      arg13: T13,
+      arg14: T14,
+      arg15: T15,
+      arg16: T16
+    ) => TResult
+  ): Selector<TState, TProps, TResult>,
+  <TState, TProps, TResult, T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12, T13, T14, T15, T16>(
+    selectors: [
+      Selector<TState, TProps, T1>,
+      Selector<TState, TProps, T2>,
+      Selector<TState, TProps, T3>,
+      Selector<TState, TProps, T4>,
+      Selector<TState, TProps, T5>,
+      Selector<TState, TProps, T6>,
+      Selector<TState, TProps, T7>,
+      Selector<TState, TProps, T8>,
+      Selector<TState, TProps, T9>,
+      Selector<TState, TProps, T10>,
+      Selector<TState, TProps, T11>,
+      Selector<TState, TProps, T12>,
+      Selector<TState, TProps, T13>,
+      Selector<TState, TProps, T14>,
+      Selector<TState, TProps, T15>,
+      Selector<TState, TProps, T16>
+    ],
+    resultFunc: (
+      arg1: T1,
+      arg2: T2,
+      arg3: T3,
+      arg4: T4,
+      arg5: T5,
+      arg6: T6,
+      arg7: T7,
+      arg8: T8,
+      arg9: T9,
+      arg10: T10,
+      arg11: T11,
+      arg12: T12,
+      arg13: T13,
+      arg14: T14,
+      arg15: T15,
+      arg16: T16
+    ) => TResult
+  ): Selector<TState, TProps, TResult>,
+
     <TState, TProps, TResult, T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12, T13, T14, T15>(
       selector1: Selector<TState, TProps, T1>,
       selector2: Selector<TState, TProps, T2>,
