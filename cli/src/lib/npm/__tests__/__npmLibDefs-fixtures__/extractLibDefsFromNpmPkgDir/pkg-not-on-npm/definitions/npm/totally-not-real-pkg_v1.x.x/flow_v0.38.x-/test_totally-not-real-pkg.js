0e09181f4cc87f42122cba108c3886b3cec8e65f
Georges-Antoine Assi <georges.antoine.assi@gmail.com>
2017-07-14 14:44:56 -0400

    Remove lines from totally-not-real-pgk

--- cli/src/lib/npm/__tests__/__npmLibDefs-fixtures__/extractLibDefsFromNpmPkgDir/pkg-not-on-npm/definitions/npm/totally-not-real-pkg_v1.x.x/flow_v0.38.x-/test_totally-not-real-pkg.js
+++ cli/src/lib/npm/__tests__/__npmLibDefs-fixtures__/extractLibDefsFromNpmPkgDir/pkg-not-on-npm/definitions/npm/totally-not-real-pkg_v1.x.x/flow_v0.38.x-/test_totally-not-real-pkg.js
@@ -1,5 +1,5 @@
 /** @flow */
-import _ from 'totally-not-real-pkg';
+import tnrp from 'totally-not-real-pkg';
 
 // $ExpectError
-_.zip([{x:1}], [{x:2,y:1}])[0][2]
+tnrp.zip([{x:1}], [{x:2,y:1}])[0][2];
