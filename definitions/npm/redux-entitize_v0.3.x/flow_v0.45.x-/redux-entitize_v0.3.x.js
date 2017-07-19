9604c1dead31df2f98f7fd7c9fbda9e9573ffbd6
Leo Selig <dev.selig@gmail.com>
2017-07-19 11:58:06 +0200

    Add lib defs for redux-entitize@0.3.x

---
+++ definitions/npm/redux-entitize_v0.3.x/flow_v0.45.x-/redux-entitize_v0.3.x.js
@@ -0,0 +1,51 @@
+import { type Schema } from "normalizr";
+
+declare module "redux-entitize" {
+  declare type SchemaMapType = {
+    [schemaName: string]: Schema
+  };
+
+  //eslint-disable-next-line flowtype/no-weak-types
+  declare type EntityType = any;
+
+  declare type SchemaEntitiesMapType = {
+    [id: string]: EntityType
+  };
+
+  declare type StateType = {
+    [schema: string]: SchemaEntitiesMapType
+  };
+
+  declare type UpdateEntityActionType = {
+    type: "redux-entitize/UPDATE_ENTITY",
+    payload: {
+      data: EntityType,
+      schema: string
+    }
+  };
+
+  declare type UpdateEntitiesActionType = {
+    type: "redux-entitize/UPDATE_ENTITIES",
+    payload: {
+      data: EntityType[],
+      schema: string
+    }
+  };
+
+  // eslint-disable-next-line flowtype/no-weak-types
+  declare type ReducerType = (state: StateType, action: Object) => StateType;
+
+  // Reducer factory
+  declare function createEntitiesReducer(schemas: SchemaMapType): ReducerType;
+
+  // Action creators
+  declare function updateEntityAction(schema: string, data: EntityType): UpdateEntityActionType;
+  declare function updateEntitiesAction(schema: string, data: EntityType[]): UpdateEntitiesActionType;
+
+  // Selectors
+  declare type SelectorsType = {
+    selectEntity: (state: { entities: StateType }, schema: string, id: string) => ?EntityType;
+    selectEntities: (state: { entities: StateType }, schema: string, ids?: string[]) => EntityType[];
+  };
+  declare function createSelectors(schemaMap: SchemaMapType): SelectorsType;
+}
