89767fb1f1c5367618f1ad8d0202b348168ab5db
Max Edmands <git@maxedmands.com>
2017-07-21 10:02:47 -0700

    Add promise-retry

---
+++ definitions/npm/promise-retry_v1.1.x/flow_v0.45.x-/test_promise-retry.js
@@ -0,0 +1,28 @@
+// @flow
+import promiseRetry from 'promise-retry';
+
+function promiseFn (retry: (err: Error) => void, attemptNumber: Number): Promise<string> {
+   return Promise.resolve('foo');
+}
+
+promiseRetry(promiseFn).then(function (str: string) { console.log(str); });
+
+promiseRetry(promiseFn, {
+  retries: 1,
+  factor: 1,
+  minTimeout: 1000,
+  maxTimeout: 5000,
+  randomize: true,
+})
+.then(function (str: string) {
+  console.log(str);
+});
+
+// $ExpectError
+promiseRetry(2);
+
+// $ExpectError
+promiseRetry(function () { /* noop */ }, {retries: 'banana'});
+
+// $ExpectError
+promiseRetry(promiseFn).then(function (num: number) { console.log(num); });
