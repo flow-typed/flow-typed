c09faaef56779cdd0266b553c5692b878629d8b5
cybice <istarkov@gmail.com>
2017-07-24 21:36:03 +0300

    Fix recompose libdef

--- definitions/npm/recompose_v0.24.x/flow_v0.47.x-/test_withProps.js
+++ definitions/npm/recompose_v0.24.x/flow_v0.47.x-/test_withProps.js
@@ -21,7 +21,7 @@ const Comp = ({ hello, b }) =>
     }
   </div>
 
-const enhacer: HOC<*, EnhancedCompProps> = compose(
+const enhancer: HOC<*, EnhancedCompProps> = compose(
   withProps(({ a, b }) => ({
     hello: a,
     b: `${b}`,
@@ -31,16 +31,23 @@ const enhacer: HOC<*, EnhancedCompProps> = compose(
     // $ExpectError (This type is incompatible with number)
     c: (b: number),
   })),
+  // check non functional form of with props
+  withProps({
+    d: 'hi',
+  }),
   withProps(props => ({
     a: (props.a: string),
+    d: (props.d: string),
     // $ExpectError property not found
     err: props.iMNotExists,
     // $ExpectError a not a number and not any
     aErr: (props.a: number),
+    // $ExpectError d not a number and not any
+    dErr: (props.d: number),
   }))
 )
 
-const EnhancedComponent = enhacer(Comp)
+const EnhancedComponent = enhancer(Comp)
 
 ;<EnhancedComponent a={'1'} b={1} />
 
