50966dd86f3f51ffe293b4b60096a38f2b694120
Stéphan DONIN <stephan.donin@gmail.com>
2017-08-02 22:09:32 +0900

    [knex_v0.12.x] Add a new definition for knex.where() (#1094)

--- definitions/npm/knex_v0.12.x/flow_v0.38.x-/knex_v0.12.x.js
+++ definitions/npm/knex_v0.12.x/flow_v0.38.x-/knex_v0.12.x.js
@@ -24,6 +24,7 @@ declare class Knex$QueryBuilder mixins Promise {
   where(builder: Knex$QueryBuilderFn): this,
   where(column: string, value: any): this,
   where(column: string, operator: string, value: any): this,
+  where(mixed: { [string]: any }): this,
   whereNot(builder: Knex$QueryBuilderFn): this,
   whereNot(column: string, value: any): this,
   whereNot(column: string, operator: string, value: any): this,
