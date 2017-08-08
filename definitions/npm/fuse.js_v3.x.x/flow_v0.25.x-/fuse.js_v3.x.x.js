0e32f509dfeaa1f3ec8ee3dd2b8bc3e22e6e7285
Roman Kurbatov <rkurbatov@gmail.com>
2017-08-08 14:31:43 +0300

    Fuse v3.x defintions (#1106)
    * adds fuse.js v3 definitions
    
    * Update fuse.js_v3.x.x.js
    
    Some changes after usage

---
+++ definitions/npm/fuse.js_v3.x.x/flow_v0.25.x-/fuse.js_v3.x.x.js
@@ -0,0 +1,28 @@
+declare module 'fuse.js' {
+  declare type FuseOptions = {
+    caseSensitive?: boolean,
+    includeScore?: boolean,
+    includeMatches?: boolean,
+    minMatchCharLength?: number,
+    shouldSort?: boolean,
+    tokenize?: boolean,
+    matchAllTokens?: boolean,
+    findAllMatches?: boolean,
+    id?: string,
+    keys?: Array<any>,
+    location?: number,
+    threshold?: number,
+    distance?: number,
+    maxPatternLength?: number,
+    getFn?: (obj: any, path: string) => any,
+    sortFn?: (a: any, b: any) => boolean,
+    verbose?: boolean,
+    tokenSeparator?: RegExp,
+  }
+  declare class Fuse<T> {
+    constructor(items: Array<T>, options?: FuseOptions): Fuse<T>,
+    search(pattern: string): Array<T>,
+    setCollection<U: Array<T>>(list: U): U,
+  }
+  declare module.exports: typeof Fuse
+}
