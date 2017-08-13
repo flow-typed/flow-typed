1ffc40b09e53d06ea0d1842679a6ae8dd764acfa
Vladimir Guguiev <wizardzloy@gmail.com>
2017-08-13 12:33:54 +0200

    Add typings for react-jss@7.x.x

--- definitions/npm/react-jss_v7.x.x/flow_v0.30.x-v0.52.x/react-jss_v7.x.x.js
+++ definitions/npm/react-jss_v7.x.x/flow_v0.30.x-v0.52.x/react-jss_v7.x.x.js
@@ -12,6 +12,8 @@ declare module 'react-jss' {
 
   declare type StatelessComponent<P> = (props: P) => ?React$Element<any>;
 
+  declare type ClassComponent<Def, P, St> = Class<React$Component<Def, P, St>>;
+
   declare class ConnectedComponent<OP, P, Def, St> extends React$Component<void, OP, void> {
     static InnerComponent: Class<React$Component<Def, P, St>>;
     static defaultProps: void;
@@ -23,7 +25,7 @@ declare module 'react-jss' {
 
   declare type Connector<OP, P> = {
     (component: StatelessComponent<P>): ConnectedComponentClass<OP, P, void, void>;
-    <Def, St>(component: Class<React$Component<Def, P, St>>): ConnectedComponentClass<OP, P, Def, St>;
+    <Def, St>(component: ClassComponent<Def, P, St>): ConnectedComponentClass<OP, P, Def, St>;
     (component: Null): ConnectedComponentClass<OP, P, void, void>;
   };
 
@@ -42,11 +44,35 @@ declare module 'react-jss' {
   declare type InjectedProps<Styles> = {
     classes: Classes<Styles>,
     sheet: Sheet<Styles>,
+    theme?: Dictionary
   };
 
+  declare type Theme = Dictionary | (parentTheme: Theme) => Dictionary
+
+  declare type StylesCreatorFn<S> = (theme: Theme) => S
+
   declare export default function injectSheet<OP, Styles: Dictionary>(
-    styles: Styles
+    stylesOrCreator: StylesCreatorFn<Styles> | Styles
   ): Connector<OP, $Supertype<InjectedProps<Styles> & OP>>;
 
-  declare export function create(): typeof injectSheet
+  // TODO: improve `JssProvider` type
+  declare export class JssProvider extends React$Component<any, any, any> {}
+
+  // TODO: improve `ThemeProvider` type
+  declare export class ThemeProvider extends React$Component<any, any, any> {}
+
+  declare export function withTheme<ThemeType, OP>(
+    component: StatelessComponent<$Supertype<{ theme: ThemeType } & OP>>
+  ): ConnectedComponentClass<OP, $Supertype<{ theme: ThemeType } & OP>, void, void>;
+
+  declare export function withTheme<ThemeType, OP, Def, St>(
+    component: ClassComponent<Def, $Supertype<{ theme: ThemeType } & OP>, St>
+  ): ConnectedComponentClass<OP, $Supertype<{ theme: ThemeType } & OP>, Def, St>;
+
+  declare export function createTheming(customChannel: string): {
+    channel: string,
+    withTheme: Function,
+    ThemeProvider: ThemeProvider,
+    themeListener: {},
+  };
 };
