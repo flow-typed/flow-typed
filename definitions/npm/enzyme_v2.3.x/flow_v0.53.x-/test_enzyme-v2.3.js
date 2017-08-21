673870dddd9a8c6fa9f50ba7dc8027d886ec7e06
Will Mruzek <wmruzek@Will-Mruzek.local>
2017-08-21 13:12:25 -0500

    Isolate enzyme_v2.x.x flow defs, test flow versions individually

--- definitions/npm/enzyme_v2.3.x/flow_v0.53.x-/test_enzyme-v2.3.js
+++ definitions/npm/enzyme_v2.3.x/flow_v0.53.x-/test_enzyme-v2.3.js
@@ -1,13 +1,16 @@
 // @flow
-import {shallow, mount, render, ShallowWrapper, ReactWrapper} from 'enzyme';
+import * as React from "react";
+import { shallow, mount, render, ShallowWrapper, ReactWrapper } from "enzyme";
 
-// $ExpectError too few arguments
-render();
-
-const shallowWrapper = shallow(<div/>);
-(render(<div/>, { context: { foo: true } }).text(): string)
-const A: bool = render(<div/>, { context: { foo: true } }).find('foo').is('bla');
-const B: bool = render(<div/>, { context: { foo: true } }).update().filter('bla').equals(<div/>);
+const shallowWrapper = shallow(<div />);
+(render(<div />, { context: { foo: true } }).text(): string);
+const A: boolean = render(<div />, { context: { foo: true } })
+  .find("foo")
+  .is("bla");
+const B: boolean = render(<div />, { context: { foo: true } })
+  .update()
+  .filter("bla")
+  .equals(<div />);
 
 // Test against chaining returning `any`
 // $ExpectError
@@ -20,23 +23,22 @@ shallow(<div />).findWhere((x: ShallowWrapper) => true);
 
 // Test selector functionality
 
-declare class ClassComponent extends React$Component<*, *, *> {
-}
+declare class ClassComponent extends React.Component<*, *> {}
 
 const StatelessComponent = () => <div />;
 
 shallow(<div />).find(ClassComponent);
 shallow(<div />).find(StatelessComponent);
-shallow(<div />).find({a: 1});
+shallow(<div />).find({ a: 1 });
 // $ExpectError
 shallow(<div />).find(true);
 
 shallowWrapper.instance();
-shallowWrapper.find('someSelector');
-shallowWrapper.prop('foo');
+shallowWrapper.find("someSelector");
+shallowWrapper.prop("foo");
 shallowWrapper.props().foo;
 
-(mount(<div />).map((node) => true): Array<boolean>);
+(mount(<div />).map(node => true): Array<boolean>);
 
 (mount(<div />).reduce((acc: number, node, i) => i + 1): Array<number>);
 // $ExpectError
