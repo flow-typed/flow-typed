038f8a4fbec6adcec29e03943984b1a5fc84c8ed
Will Mruzek <wmruzek@Will-Mruzek.local>
2017-08-21 13:32:58 -0500

    Update EnzymeSelector type

--- definitions/npm/enzyme_v2.3.x/flow_v0.53.x-/enzyme_v2.3.x.js
+++ definitions/npm/enzyme_v2.3.x/flow_v0.53.x-/enzyme_v2.3.x.js
@@ -6,7 +6,7 @@ declare module "enzyme" {
     index: number
   ) => boolean;
   declare type NodeOrNodes = React.Node | Array<React.Node>;
-  declare type EnzymeSelector = string | React.ComponentType<any> | Object;
+  declare type EnzymeSelector = string | Class<React.Component<*, *>> | Object;
 
   // CheerioWrapper is a type alias for an actual cheerio instance
   // TODO: Reference correct type from cheerio's type declarations
