1a0762c13e94fb41b465860bb8ed83878a91844b
David Buchan-Swanson <david.buchanswanson@gmail.com>
2017-09-04 10:45:38 +1000

    alias v4 to default export for uuid

--- definitions/npm/uuid_v3.x.x/flow_v0.32.x-/uuid_v3.x.x.js
+++ definitions/npm/uuid_v3.x.x/flow_v0.32.x-/uuid_v3.x.x.js
@@ -1,12 +1,21 @@
-declare module 'uuid' {
-  declare function v1(options?: {|
-    node?: number[],
-    clockseq?: number,
-    msecs?: number | Date,
-    nsecs?: number,
-  |}, buffer?: number[] | Buffer, offset?: number): string;
-  declare function v4(options?: {|
-    random?: number[],
-    rng?: () => number[] | Buffer,
-  |}, buffer?: number[] | Buffer, offset?: number): string;
+declare module "uuid" {
+  declare function v1(
+    options?: {|
+      node?: number[],
+      clockseq?: number,
+      msecs?: number | Date,
+      nsecs?: number
+    |},
+    buffer?: number[] | Buffer,
+    offset?: number
+  ): string;
+  declare function v4(
+    options?: {|
+      random?: number[],
+      rng?: () => number[] | Buffer
+    |},
+    buffer?: number[] | Buffer,
+    offset?: number
+  ): string;
+  declare module.exports: v4;
 }
