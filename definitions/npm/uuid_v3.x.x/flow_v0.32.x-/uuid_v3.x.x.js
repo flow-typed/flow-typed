5d7ad3d19b7fe4f9eb60769801af2bc366c6778d
Joey Baker <joey@scriptdash.com>
2017-09-05 15:09:05 -0700

    Fix(uuid) add default export
    While it's a deprecated API, it does exist: https://github.com/kelektiv/node-uuid#deprecated-api
    
    This is currently used in react-native, so using this definition breaks
    flow types in RN >= 0.48.

--- definitions/npm/uuid_v3.x.x/flow_v0.32.x-/uuid_v3.x.x.js
+++ definitions/npm/uuid_v3.x.x/flow_v0.32.x-/uuid_v3.x.x.js
@@ -1,12 +1,21 @@
 declare module 'uuid' {
-  declare function v1(options?: {|
-    node?: number[],
-    clockseq?: number,
-    msecs?: number | Date,
-    nsecs?: number,
-  |}, buffer?: number[] | Buffer, offset?: number): string;
-  declare function v4(options?: {|
-    random?: number[],
-    rng?: () => number[] | Buffer,
-  |}, buffer?: number[] | Buffer, offset?: number): string;
+  declare class uuid {
+    static (options?: {|
+      random?: number[],
+      rng?: () => number[] | Buffer,
+    |}, buffer?: number[] | Buffer, offset?: number): string;
+
+    static v1(options?: {|
+      node?: number[],
+      clockseq?: number,
+      msecs?: number | Date,
+      nsecs?: number,
+    |}, buffer?: number[] | Buffer, offset?: number): string;
+
+    static v4(options?: {|
+      random?: number[],
+      rng?: () => number[] | Buffer,
+    |}, buffer?: number[] | Buffer, offset?: number): string;
+  }
+  declare module.exports: Class<uuid>
 }
