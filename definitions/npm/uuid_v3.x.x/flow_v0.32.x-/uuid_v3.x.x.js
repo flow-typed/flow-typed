25c1e28c9c38fc9a6e0aaafa680f92e27fcfdf70
Frank Schmid <fschmid@stashimi.com>
2017-09-08 16:30:43 +0200

    Added full support for the uuid module including the proposed usage

--- definitions/npm/uuid_v3.x.x/flow_v0.32.x-/uuid_v3.x.x.js
+++ definitions/npm/uuid_v3.x.x/flow_v0.32.x-/uuid_v3.x.x.js
@@ -1,21 +1,78 @@
-declare module 'uuid' {
+declare module "uuid" {
   declare class uuid {
-    static (options?: {|
-      random?: number[],
-      rng?: () => number[] | Buffer,
-    |}, buffer?: number[] | Buffer, offset?: number): string;
+    static (
+      options?: {|
+        random?: number[],
+        rng?: () => number[] | Buffer
+      |},
+      buffer?: number[] | Buffer,
+      offset?: number
+    ): string,
 
-    static v1(options?: {|
-      node?: number[],
-      clockseq?: number,
-      msecs?: number | Date,
-      nsecs?: number,
-    |}, buffer?: number[] | Buffer, offset?: number): string;
+    static v1(
+      options?: {|
+        node?: number[],
+        clockseq?: number,
+        msecs?: number | Date,
+        nsecs?: number
+      |},
+      buffer?: number[] | Buffer,
+      offset?: number
+    ): string,
 
-    static v4(options?: {|
-      random?: number[],
-      rng?: () => number[] | Buffer,
-    |}, buffer?: number[] | Buffer, offset?: number): string;
+    static v4(
+      options?: {|
+        random?: number[],
+        rng?: () => number[] | Buffer
+      |},
+      buffer?: number[] | Buffer,
+      offset?: number
+    ): string
   }
-  declare module.exports: Class<uuid>
+  declare module.exports: Class<uuid>;
+}
+
+declare module "uuid/v1" {
+  declare class v1 {
+    static (
+      options?: {|
+        node?: number[],
+        clockseq?: number,
+        msecs?: number | Date,
+        nsecs?: number
+      |},
+      buffer?: number[] | Buffer,
+      offset?: number
+    ): string
+  }
+
+  declare module.exports: Class<v1>;
+}
+
+declare module "uuid/v4" {
+  declare class v4 {
+    static (
+      options?: {|
+        random?: number[],
+        rng?: () => number[] | Buffer
+      |},
+      buffer?: number[] | Buffer,
+      offset?: number
+    ): string
+  }
+
+  declare module.exports: Class<v4>;
+}
+
+declare module "uuid/v5" {
+  declare class v5 {
+    static (
+      name?: string | number[],
+      namespace?: string | number[],
+      buffer?: number[] | Buffer,
+      offset?: number
+    ): string
+  }
+
+  declare module.exports: Class<v5>;
 }
