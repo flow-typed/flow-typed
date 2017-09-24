419b88b3a3a8ae09630246c0ddae1438b0b1e7ba
Richard Samuelson <noobtoothfairy@gmail.com>
2017-09-24 16:39:44 +0200

    all jsx sibbling errors should be gone

--- definitions/npm/react-hot-loader_v3.x.x/flow_v0.55.x-/test_AppContainer.js
+++ definitions/npm/react-hot-loader_v3.x.x/flow_v0.55.x-/test_AppContainer.js
@@ -1,28 +1,42 @@
 // @flow
 import React from "react";
-import { AppContainer } from 'react-hot-loader';
+import { AppContainer } from "react-hot-loader";
 
 errorReporter = () => <div>error message</div>;
 
-// passing
-<AppContainer><div>some child</div></AppContainer>
-
 // $ExpectError - missing children prop
-<AppContainer />
+(() => {
+  <div>
+    <AppContainer />
+  </div>;
+})();
 
-// $ExpectError - not one child
-<AppContainerr><div>some child</div><div>some child</div></AppContainer>
+// passing
+<AppContainer>
+  <div>some child</div>
+</AppContainer>(() => {
+  // $ExpectError - not one child
+  <AppContainer>{[1, 2].map(() => <div>some child</div>)}</AppContainer>;
+})();
 
 // passing - errorReporter as NodeElement or function returning NodeElement
 <div>
-<AppContainer errorReporter={errorReporter}><div>some child</div></AppContainer>
-<AppContainer errorReporter={<i>Err</i>}><div>some child</div></AppContainer>
-</div>
+  <AppContainer errorReporter={errorReporter}>
+    <div>some child</div>
+  </AppContainer>
+  <AppContainer errorReporter={<i>Err</i>}>
+    <div>some child</div>
+  </AppContainer>
+</div>;
 
-errorReporter = () => [1,2,3].map(i => <i>{i}</i>);
+errorReporter = () => [1, 2, 3].map(i => <i>{i}</i>);
 // $ExpectError - errorReporter cannot be array of children
-<AppContainer errorReporter={errorReporter}><div>some child</div></AppContainer>
+<AppContainer errorReporter={errorReporter}>
+  <div>some child</div>
+</AppContainer>;
 
-errorReporter = [1,2,3].map(i => <i>{i}</i>);
+errorReporter = [1, 2, 3].map(i => <i>{i}</i>);
 // $ExpectError - errorReporter cannot be array of children
-<AppContainer errorReporter={errorReporter}><div>some child</div></AppContainer>
+<AppContainer errorReporter={errorReporter}>
+  <div>some child</div>
+</AppContainer>;
