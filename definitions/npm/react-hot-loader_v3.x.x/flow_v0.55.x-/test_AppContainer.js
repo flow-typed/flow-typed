f2ca6bef6aed34d9464a931331036aefaf896c44
Richard Samuelson <noobtoothfairy@gmail.com>
2017-09-24 16:14:29 +0200

    refactored code to avoid jsx errors about not wrapping sibbling elements in a parent element

--- definitions/npm/react-hot-loader_v3.x.x/flow_v0.55.x-/test_AppContainer.js
+++ definitions/npm/react-hot-loader_v3.x.x/flow_v0.55.x-/test_AppContainer.js
@@ -2,21 +2,22 @@
 import React from "react";
 import { AppContainer } from 'react-hot-loader';
 
-let errorReporter = () => <div>error message</div>;
+errorReporter = () => <div>error message</div>;
 
 // passing
 <AppContainer><div>some child</div></AppContainer>
 
-// passing
-<AppContainer errorReporter={errorReporter}><div>some child</div></AppContainer>
-
-// passing
-<AppContainer errorReporter={<i>Err</i>}><div>some child</div></AppContainer>
-
 // $ExpectError - missing children prop
 <AppContainer />
+
 // $ExpectError - not one child
 <AppContainerr><div>some child</div><div>some child</div></AppContainer>
+
+// passing - errorReporter as NodeElement or function returning NodeElement
+<div>
+<AppContainer errorReporter={errorReporter}><div>some child</div></AppContainer>
+<AppContainer errorReporter={<i>Err</i>}><div>some child</div></AppContainer>
+</div>
 
 errorReporter = () => [1,2,3].map(i => <i>{i}</i>);
 // $ExpectError - errorReporter cannot be array of children
