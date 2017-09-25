0bdbd3d3e8537541baf762d93e2b7c25fc38a024
Matthew Stevens <mattgstevens@gmail.com>
2017-09-25 11:27:08 +0200

    feat/ramda latest assoc and assoc path mixed source object (#1264)
    * chore: run prettier over latest ramda definitions
    
    * feat: update ramda to allow for mixed source into assoc and assocPath

--- definitions/npm/ramda_v0.x.x/flow_v0.39.x-/test_ramda_v0.x.x_es_modules.js
+++ definitions/npm/ramda_v0.x.x/flow_v0.39.x-/test_ramda_v0.x.x_es_modules.js
@@ -1,10 +1,14 @@
 /* @flow */
 /*eslint-disable no-undef, no-unused-vars, no-console*/
-import _, { compose, match, toLower, trim } from 'ramda'
+import _, { compose, match, toLower, trim } from "ramda";
 
 //$ExpectError
-inc()
+inc();
 
-const str: Array<string|void> = compose(match(/2/), toLower, trim)(' 1,2,3 ')
-const str1: Array<string> = _.compose(_.split(','), _.toLower, _.trim)(' 1,2,3 ')
-const str2: string = _.compose(_.replace('3', '4'), _.toLower, _.trim)(' 1,2,3 ')
+const str: Array<string | void> = compose(match(/2/), toLower, trim)(" 1,2,3 ");
+const str1: Array<string> = _.compose(_.split(","), _.toLower, _.trim)(
+  " 1,2,3 "
+);
+const str2: string = _.compose(_.replace("3", "4"), _.toLower, _.trim)(
+  " 1,2,3 "
+);
