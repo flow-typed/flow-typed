6b33696b7d0a57cbbc42f0f8b8516e570ed7f0db
davidohayon <noyaho@gmail.com>
2017-09-28 18:30:26 +0300

    Fixed parameter type of repeat operator in rxjs v5.0.x (flow >= v0.34.x)

--- definitions/npm/rxjs_v5.0.x/flow_v0.34.x-/test_rxjs.js
+++ definitions/npm/rxjs_v5.0.x/flow_v0.34.x-/test_rxjs.js
@@ -5,8 +5,8 @@ import {
   Observable,
   Scheduler,
   Subject,
-  Subscriber,
-} from 'rxjs';
+  Subscriber
+} from "rxjs";
 
 const numbers = Observable.of(1);
 Observable.create(observer => {
@@ -16,25 +16,29 @@ Observable.create(observer => {
 // $ExpectError
 const distinct1: Observable<number> = Observable.of(1).distinct(1);
 const distinct2: Observable<number> = Observable.of(1).distinct();
-const distinct3: Observable<{ a: string }> = Observable.of({ a: 'a' }).distinct(
+const distinct3: Observable<{ a: string }> = Observable.of({ a: "a" }).distinct(
   field => field.a
 );
-const distinct4: Observable<{ a: string }> = Observable.of({ a: 'a' }).distinct(
+const distinct4: Observable<{ a: string }> = Observable.of({ a: "a" }).distinct(
   field => field.a,
   Observable.never()
 );
 
 const timer$: Observable<number> = Observable.timer(1000).startWith(10);
-const numbers2: Observable<number> = Observable.of(1, 2, 3).repeat();
+const numbers2: Observable<number> = Observable.of(1, 2, 3).repeat(2);
 
-const subscriber: Subscriber<number> = Subscriber.create(console.log, console.error, console.log);
+const subscriber: Subscriber<number> = Subscriber.create(
+  console.log,
+  console.error,
+  console.log
+);
 const subscriber2: Subscriber<number> = new Subscriber(x => console.log(x));
 
 const numberObserver = {
   next: (x: number) => null,
   error: (e: Error) => null,
-  complete: () => null,
-}
+  complete: () => null
+};
 
 const anonymousSubject: AnonymousSubject<number> = new AnonymousSubject(
   numberObserver,
