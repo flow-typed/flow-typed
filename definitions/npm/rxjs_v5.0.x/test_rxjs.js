f19caeb75fde96982a3489926894b58d0f00e62b
Alexey Shpakov <li0liq@gmail.com>
2017-10-05 21:35:20 +1100

    [rxjs] Add support for combineLatest with a single argument (#1293)
    * Add support for combineLatest with a single argument
    
    * Fixed variable names
    
    * Trying to make it work using _: void

--- definitions/npm/rxjs_v5.0.x/test_rxjs.js
+++ definitions/npm/rxjs_v5.0.x/test_rxjs.js
@@ -54,6 +54,10 @@ const combined: Observable<{n: number, s: string}> = Observable.combineLatest(
 
 const combined2: Observable<[number, string]> = Observable.combineLatest(numbers, strings);
 
+const combined3: Observable<[number]> = Observable.combineLatest(
+  numbers
+);
+
 // $ExpectError
 const combinedBad: Observable<{n: number, s: string}> = Observable.combineLatest(
   numbers,
@@ -61,16 +65,16 @@ const combinedBad: Observable<{n: number, s: string}> = Observable.combineLatest
   (n, s) => ({n, s})
 );
 
-const combined3: Observable<{n: number, s: string}> = Observable.forkJoin(
+const forked: Observable<{n: number, s: string}> = Observable.forkJoin(
   numbers,
   strings,
   (n, s) => ({n, s})
 );
 
-const combined4: Observable<[number, string]> = Observable.forkJoin(numbers, strings);
+const forked2: Observable<[number, string]> = Observable.forkJoin(numbers, strings);
 
 // $ExpectError
-const combinedBad2: Observable<{n: number, s: string}> = Observable.forkJoin(
+const forkedBad: Observable<{n: number, s: string}> = Observable.forkJoin(
   numbers,
   numbers,
   (n, s) => ({n, s})
