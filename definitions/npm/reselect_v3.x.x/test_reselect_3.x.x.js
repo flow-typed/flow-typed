b87ec8a3d5e5bb1f0b52dd23c83cfcd1a27cde41
Julien Wajsberg <felash@gmail.com>
2017-10-09 20:36:47 +0200

    reselect for flow v0.54

--- definitions/npm/reselect_v3.x.x/test_reselect_3.x.x.js
+++ definitions/npm/reselect_v3.x.x/test_reselect_3.x.x.js
@@ -4,7 +4,7 @@ import {
   defaultMemoize,
   createSelectorCreator,
   createStructuredSelector
-} from 'reselect';
+} from "reselect";
 
 // TEST: Should pass for 2 selectors given as arguments
 type State = {
@@ -16,21 +16,18 @@ const test1Selector = createSelector(
   (state: State) => state.x,
   (state: State) => state.y,
   (x, y) => {
-    return x + y
+    return x + y;
   }
-)({x: 100, y: 200})
+)({ x: 100, y: 200 });
 // END TEST
 
 // TEST: Should pass for 2 selectors given as array
 createSelector(
-  [
-    (state: State) => state.x,
-    (state: State) => state.y
-  ] ,
+  [(state: State) => state.x, (state: State) => state.y],
   (x, y) => {
-    return x + y
+    return x + y;
   }
-)({x: 100, y: 200})
+)({ x: 100, y: 200 });
 // END TEST
 
 // TEST: Should pass when selectors have additional Props argument
@@ -42,11 +39,14 @@ createSelector(
   (state: State, props: TestProps) => state.x + props.x,
   (state: State, props: TestProps) => state.y + props.x,
   (x, y) => {
-    return x + y
+    return x + y;
   }
-)({x: 100, y: 200}, {
-  x: 10
-});
+)(
+  { x: 100, y: 200 },
+  {
+    x: 10
+  }
+);
 // END TEST
 
 // TEST: Should pass for additional arguments
@@ -54,11 +54,27 @@ createSelector(
   (state: State, props: TestProps, test) => state.x + props.x + test,
   (state: State, props: TestProps, test) => state.y + props.x + test,
   (x, y) => {
-    return x + y
+    return x + y;
   }
-)({x: 100, y: 200}, {
-  x: 10
-}, 10);
+)(
+  { x: 100, y: 200 },
+  {
+    x: 10
+  },
+  10
+);
+// END TEST
+
+// TEST: Should work when using another selector as functions
+const simpleSelector = (state: State): string => "foo";
+const resultSelector = (arg: string): string => "foo";
+
+const combinedSelector1 = createSelector(simpleSelector, resultSelector);
+
+const combinedSelector2: (state: State) => string = createSelector(
+  combinedSelector1,
+  resultSelector
+);
 // END TEST
 
 defaultMemoize((a: number) => a + 1)(2);
@@ -66,26 +82,26 @@ defaultMemoize((a: number) => a + 1, (a, b) => a + b)(2);
 
 // TEST 8: Should threat newly created selector as normal one
 let x = createSelectorCreator(defaultMemoize)(
-  (state) => state.x,
-  (state) => state.y,
+  state => state.x,
+  state => state.y,
   (x, y) => {
     return x + y;
   }
-)
+);
 
 x({
   x: 10,
   y: 20
-})
+});
 // END TEST
 
 createStructuredSelector({
-  first: (state) => state.x,
-  second: (state) => state.y
+  first: state => state.x,
+  second: state => state.y
 })({
   x: 10,
   y: 20
-})
+});
 
 type TestState1 = {
   x: number,
@@ -101,53 +117,53 @@ createSelector(
   // $ExpectError: Should not pass when selectors handle different states
   (state: TestState2) => state.y,
   (x, y) => {
-    return x + y
+    return x + y;
   }
-)({x: 100, y: 200});
+)({ x: 100, y: 200 });
 
 createSelector(
   // $ExpectError: Should not pass when selectors handle different states
   (state, props) => state.x + props.d,
-  (state) => state.y,
+  state => state.y,
   (x, y) => {
-    return x + y
+    return x + y;
   }
-)({x: 100, y: 200}, { x: 20 });
+)({ x: 100, y: 200 }, { x: 20 });
 
 // $ExpectError: Should not result do not include property
 createSelector(
-  (state) => state.x,
-  (state) => state.y,
+  state => state.x,
+  state => state.y,
   (x, y) => {
     return {
       x,
       y
-    }
+    };
   }
-)({x: 100, y: 200}, { x: 20 }).d;
+)({ x: 100, y: 200 }, { x: 20 }).d;
 
 // $ExpectError
-defaultMemoize((a: number) => a + 1)('');
+defaultMemoize((a: number) => a + 1)("");
 // $ExpectError
-defaultMemoize((a: number) => a + 1, (a, b) => '')(2);
+defaultMemoize((a: number) => a + 1, (a, b) => "")(2);
 
 createSelectorCreator(defaultMemoize)(
-  (state) => state.x,
+  state => state.x,
   // $ExpectError: Should fail when state don't have good properties
-  (state) => state.y,
+  state => state.y,
   (x, y) => {
     return x + y;
   }
 )({
   x: 10,
   d: 20
-})
+});
 
 createStructuredSelector({
   // $ExpectError: Should fail when state don't have good properties
-  first: (state) => state.d,
-  second: (state) => state.y
+  first: state => state.d,
+  second: state => state.y
 })({
   x: 10,
   y: 20
-})
+});
