2b8923a76cf29b5b19eace1f27f5415dde036651
Matija Marohnić <matija.marohnic@gmail.com>
2017-10-12 15:53:19 +0200

    Fully support array arguments in classnames (#1334)
    * classnames changes by Prettier
    
    * Fully support array arguments in classnames
    
    Other than strings, array arguments can contain objects and falsy
    values.

--- definitions/npm/classnames_v2.x.x/flow_v0.25.x-/classnames_v2.x.x.js
+++ definitions/npm/classnames_v2.x.x/flow_v0.25.x-/classnames_v2.x.x.js
@@ -1,19 +1,20 @@
 type $npm$classnames$Classes =
   | string
   | { [className: string]: * }
-  | Array<string>
   | false
   | void
   | null;
 
-declare module 'classnames' {
-  declare function exports(...classes: Array<$npm$classnames$Classes>): string;
+declare module "classnames" {
+  declare function exports(
+    ...classes: Array<$npm$classnames$Classes | Array<$npm$classnames$Classes>>
+  ): string;
 }
 
-declare module 'classnames/bind' {
-  declare module.exports: $Exports<'classnames'>;
+declare module "classnames/bind" {
+  declare module.exports: $Exports<"classnames">;
 }
 
-declare module 'classnames/dedupe' {
-  declare module.exports: $Exports<'classnames'>;
+declare module "classnames/dedupe" {
+  declare module.exports: $Exports<"classnames">;
 }
--- definitions/npm/classnames_v2.x.x/test_classnames_v2.x.x.js
+++ definitions/npm/classnames_v2.x.x/test_classnames_v2.x.x.js
@@ -1,20 +1,23 @@
 // @flow
 
-import classnames from 'classnames';
+import classnames from "classnames";
 
 classnames();
-classnames('a');
-classnames('a', 'b');
+classnames("a");
+classnames("a", "b");
 classnames({ a: true });
 classnames({ a: true }, { b: true });
-classnames('a', { b: true });
-classnames('a', { b: 'truthy' });
-classnames('a', { b: null });
-classnames({ a: true }, 'b');
+classnames("a", { b: true });
+classnames("a", { b: "truthy" });
+classnames("a", { b: null });
+classnames({ a: true }, "b");
 classnames({ a: null, b: undefined });
 classnames(undefined);
 classnames(null);
-classnames('a', false);
+classnames("a", false);
+classnames("a", ["b", null, { c: "truthy", d: null }]);
 
 // $ExpectError
 classnames(42);
+// $ExpectError
+classnames("a", ["b", 42]);
