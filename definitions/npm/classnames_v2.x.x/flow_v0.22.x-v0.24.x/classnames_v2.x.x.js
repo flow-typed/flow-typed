3f5016e8b815af66c482c7eeb53ac78675a73f05
Matija Marohnić <matija.marohnic@gmail.com>
2017-10-12 15:53:19 +0200

    Fully support array arguments in classnames (#1334)
    * classnames changes by Prettier
    
    * Fully support array arguments in classnames
    
    Other than strings, array arguments can contain objects and falsy
    values.

--- definitions/npm/classnames_v2.x.x/flow_v0.22.x-v0.24.x/classnames_v2.x.x.js
+++ definitions/npm/classnames_v2.x.x/flow_v0.22.x-v0.24.x/classnames_v2.x.x.js
@@ -1,11 +1,12 @@
 type $npm$classnames$Classes =
   | string
   | { [className: string]: * }
-  | Array<string>
   | false
   | void
   | null;
 
-declare module 'classnames' {
-  declare function exports(...classes: Array<$npm$classnames$Classes>): string;
+declare module "classnames" {
+  declare function exports(
+    ...classes: Array<$npm$classnames$Classes | Array<$npm$classnames$Classes>>
+  ): string;
 }
