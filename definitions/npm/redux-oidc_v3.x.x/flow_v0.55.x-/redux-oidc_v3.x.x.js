84196047ba2000f06d6cdf2af5d1a721f67370a3
Logan Barnett <logustus@gmail.com>
2017-10-13 16:24:50 -0700

    add redux-oidc v3 types (#1347)
    * add redux-oidc types
    
    * add comment for workaround for pulling in other modules

---
+++ definitions/npm/redux-oidc_v3.x.x/flow_v0.55.x-/redux-oidc_v3.x.x.js
@@ -0,0 +1,47 @@
+import type { Reducer, Store } from "redux";
+
+declare module "redux-oidc" {
+  declare type UserManager = {
+    signinRedirect: () => Promise<*>,
+    signoutRedirect: () => Promise<*>
+  };
+  declare type User<P> = {
+    expired: ?boolean,
+    profile: P
+  };
+  declare type UserManagerSettings = {|
+    client_id: string,
+    authority: string,
+    redirect_uri: string,
+    response_type?: string,
+    scope?: string,
+    loadUserInfo?: boolean,
+    silent_redirect_uri?: string,
+    automaticSilentRenew?: boolean,
+    accessTokenExpiringNotificationTime?: number,
+    silentRequestTimeout?: number,
+    filterProtocolClaims?: boolean
+  |};
+  declare type OidcReducerState = {
+    user: ?User<*>
+  };
+  declare type OidcReducer = Reducer<OidcReducerState, *>;
+
+  declare function createUserManager(
+    settings: UserManagerSettings
+  ): UserManager;
+  declare var reducer: OidcReducer;
+
+  declare class CallbackComponent extends React$Component<{
+    userManager: UserManager,
+    successCallback: () => mixed,
+    errorCallback?: () => mixed
+  }> {}
+
+  declare class OidcProvider extends React$Component<{
+    store: Store<*, *>,
+    userManager: UserManager
+  }> {}
+
+  declare function processSilentRenew(): void;
+}
