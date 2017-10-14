bd04723fb94b0069f1704f919ec0f710f7242c1c
Luis Stanley Jovel <stanley@generalui.com>
2017-10-14 06:13:53 -0700

    Add flow definitions for react-collapsible package (#1349)
    * Add react-collapsible types
    
    * Improve react-collapsile component's types for props
    
    * improve test file coverage

---
+++ definitions/npm/react-collapsible_v2.0.x/flow_v0.54.x-/react-collapsible_v2.0.x.js
@@ -0,0 +1,38 @@
+import { Component } from "react";
+
+declare module "react-collapsible" {
+  declare type Props = {
+    trigger: string | React$Node,
+    triggerWhenOpen?: string | React$Node,
+    triggerDisabled?: false,
+    transitionTime?: number,
+    easing?: string,
+    open?: boolean,
+    accordionPosition?: string,
+    handleTriggerClick?: (accordionPosition?: string | number) => void,
+    onOpen?: () => void,
+    onClose?: () => void,
+    onOpening?: () => void,
+    onClosing?: () => void,
+    lazyRender?: boolean,
+    overflowWhenOpen?:
+      | "hidden"
+      | "visible"
+      | "auto"
+      | "scroll"
+      | "inherit"
+      | "initial"
+      | "unset",
+    triggerSibling?: React$Node,
+    classParentString?: string,
+    className?: string,
+    openedClassName?: string,
+    triggerClassName?: string,
+    triggerOpenedClassName?: string,
+    contentOuterClassName?: string,
+    contentInnerClassName?: string
+  };
+
+  declare class Collapsible extends Component<Props> {}
+  declare var exports: typeof Collapsible;
+}
