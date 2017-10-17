2b435a575dde09421e35cdd15093363bd421554d
Kenan Yildirim <kenan@kenany.me>
2017-10-17 08:24:57 -0400

    Add pino v3 typings

---
+++ definitions/npm/pino_v3.x.x/test_pino_v3.js
@@ -0,0 +1,31 @@
+// @flow
+
+import pino from "pino";
+
+(pino.LOG_VERSION: number);
+
+const p: pino.Logger = pino();
+p.info("hello world");
+p.error("this is at error level");
+p.info("the answer is %d", 42);
+p.info({ obj: 42 }, "hello world");
+p.info({ obj: 42, b: 2 }, "hello world");
+p.info({ obj: { aa: "bbb" } }, "another");
+setImmediate(p.info, "after setImmediate");
+p.error(new Error("an error"));
+
+// $ExpectError
+p("no log level");
+
+const pretty = pino.pretty();
+pretty.pipe(process.stdout);
+const log = pino(
+  {
+    name: "app",
+    safe: true
+  },
+  pretty
+);
+
+log.child({ widget: "foo" }).info("hello");
+log.child({ widget: "bar" }).warn("hello 2");
