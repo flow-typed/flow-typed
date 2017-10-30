4c8679a1c4c4d3d2e9595e270863b3f861d1c16a
Dieter Oberkofler <doberkofler@users.noreply.github.com>
2017-10-30 15:04:41 +0100

    Finalized the error and logging api (#1424)

--- definitions/npm/selenium-webdriver_v3.x.x/flow_v0.28.x-/test_selenium-webdriver_v3.x.x.js
+++ definitions/npm/selenium-webdriver_v3.x.x/flow_v0.28.x-/test_selenium-webdriver_v3.x.x.js
@@ -1,5 +1,5 @@
 // @flow
-import * as webdriver from 'selenium-webdriver';
+import * as webdriver from "selenium-webdriver";
 
 // webdriver.Capabilities
 const capabilities = webdriver.Capabilities.chrome();
@@ -7,7 +7,10 @@ capabilities.setScrollBehavior(1);
 
 // webdriver.logging.Preferences
 const logPreferences = new webdriver.logging.Preferences();
-logPreferences.setLevel(webdriver.logging.Type.BROWSER, webdriver.logging.Level.DEBUG);
+logPreferences.setLevel(
+  webdriver.logging.Type.BROWSER,
+  webdriver.logging.Level.DEBUG
+);
 capabilities.setLoggingPrefs(logPreferences);
 
 // webdriver.Builder
@@ -15,9 +18,15 @@ const builder = new webdriver.Builder().withCapabilities(capabilities);
 const driver = builder.build();
 
 // timeouts
-driver.manage().timeouts().implicitlyWait(0);
-driver.manage().timeouts().pageLoadTimeout(1);
-driver.manage().timeouts().setScriptTimeout(2);
-
-// $ExpectError
-const capabilities_: string = webdriver.Capabilities.chrome();
+driver
+  .manage()
+  .timeouts()
+  .implicitlyWait(0);
+driver
+  .manage()
+  .timeouts()
+  .pageLoadTimeout(1);
+driver
+  .manage()
+  .timeouts()
+  .setScriptTimeout(2);
