a4251b005581d9932efd25727439b6cafb1d9799
StefanoMagrassi <stefano.magrassi@gmail.com>
2017-11-06 09:25:57 +0100

    data.maybe improved tests
    tests for:
    
    - fromNullable (static constructor method)
    - ap (applicative functor)

--- definitions/npm/data.maybe_v1.x.x/test_data.maybe_v1.x.x.js
+++ definitions/npm/data.maybe_v1.x.x/test_data.maybe_v1.x.x.js
@@ -1,16 +1,25 @@
 // @flow
-import Maybe from 'data.maybe';
+import Maybe from "data.maybe";
 
+const concat = x => y => x + y;
 
-
-let a = Maybe.Nothing();
-let b = Maybe.Just(5);
-let c = Maybe.Just(10);
+const a = Maybe.Nothing();
+const b = Maybe.Just(5);
+const c = Maybe.Just(10);
+const d = Maybe.fromNullable(null);
+const e = Maybe.of(concat);
 
 b.map(x => x + 5);
-b.chain(x => x < 5 ? Maybe.Just(x + 5) : Maybe.Nothing());
+b.chain(x => (x < 5 ? Maybe.Just(x + 5) : Maybe.Nothing()));
+d.map(x => `is ${x}`);
 
-const x : number = b.getOrElse(10);
+const x: number = b.getOrElse(10);
+const y: string = d.getOrElse("boo");
+const z: Maybe<string> = e.ap(Maybe.of("a")).ap(Maybe.of("b"));
 
+// --- Errors
 // $ExpectError
 b.chain(x => x + x);
+
+// $ExpectError
+const _z = e.ap("a");
