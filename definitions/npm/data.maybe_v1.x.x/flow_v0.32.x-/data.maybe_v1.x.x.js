04802565c31f33a971970fe806a681f2f3fa2c0f
Stefano Magrassi <stefano.magrassi@gmail.com>
2017-11-06 17:49:29 +0100

    data.maybe - fromEither, fromValidation, ap (#1475)
    * Fix swagger-schema-official version
    
    Fix swagger-schema-official directory name in order to match prerelease version
    
    * Definitions for data.either 1.x.x
    
    * git ignore vscode workspace settings
    
    * data.either: better definition and tests
    
    * data.maybe: fromEither(), fromValidation(), ap() methods
    
    * data.maybe improved tests
    
    tests for:
    
    - fromNullable (static constructor method)
    - ap (applicative functor)

--- definitions/npm/data.maybe_v1.x.x/flow_v0.32.x-/data.maybe_v1.x.x.js
+++ definitions/npm/data.maybe_v1.x.x/flow_v0.32.x-/data.maybe_v1.x.x.js
@@ -1,7 +1,28 @@
-// flow-typed signature: 9ed0f65c3fef605d7b40569f85a56b00
-// flow-typed version: <<STUB>>/data.maybe_v1.2.2/flow_v0.35.0
+// @flow
 
-declare module 'data.maybe' {
+import type Either from "data.either";
+
+interface Functor<A> {
+  map<B>(f: (a: A) => B): Functor<B>;
+}
+
+interface Apply<A> extends Functor<A> {
+  ap<B>(fab: Apply<(a: A) => B>): Apply<B>;
+}
+
+interface Applicative<A> extends Apply<A> {
+  static of<B>(b: B): Applicative<B>;
+}
+
+interface Chain<A> extends Apply<A> {
+  chain<B>(f: (a: A) => Chain<B>): Chain<B>;
+}
+
+interface Monad<A> extends Applicative<A>, Chain<A> {}
+
+interface Validation<F, S> extends Applicative<S> {}
+
+declare module "data.maybe" {
   declare class IMaybe<T> {
     isNothing: boolean;
     isJust: boolean;
@@ -10,14 +31,17 @@ declare module 'data.maybe' {
     toJSON(): Object;
     get(): T;
     getOrElse(defaultValue: T): T;
-    map<B>(f: (v:T) => B): IMaybe<B>;
-    chain<B>(f: (v:T) => IMaybe<B>): IMaybe<B>;
+    map<B>(f: (v: T) => B): IMaybe<B>;
+    ap<B>(fb: IMaybe<B> | Applicative<B>): IMaybe<B>;
+    chain<B>(f: (v: T) => IMaybe<B>): IMaybe<B>;
     orElse<B>(f: () => IMaybe<B>): IMaybe<B>;
-    cata<B>(patterns: {| Nothing: () => B, Just: (v:T) => B |}): B;
+    cata<B>(patterns: {| Nothing: () => B, Just: (v: T) => B |}): B;
     static Nothing<T>(): IMaybe<T>;
     static Just<T>(value: T): IMaybe<T>;
     static of<T>(value: T): IMaybe<T>;
     static fromNullable(value: ?T): IMaybe<T>;
+    static fromEither<L, R>(em: Either<L, R>): IMaybe<R>;
+    static fromValidation<F, S>(vm: Validation<F, S>): IMaybe<S>;
   }
 
   declare var exports: typeof IMaybe;
