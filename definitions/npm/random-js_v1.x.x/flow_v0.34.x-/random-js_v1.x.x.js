71d40600327012bbd4f233c586390441ad21f444
marudor <marudor@marudor.de>
2017-11-10 23:39:22 +0100

    fix random-js engines

--- definitions/npm/random-js_v1.x.x/flow_v0.34.x-/random-js_v1.x.x.js
+++ definitions/npm/random-js_v1.x.x/flow_v0.34.x-/random-js_v1.x.x.js
@@ -1,6 +1,5 @@
-declare module 'random-js' {
-  declare class Engine {
-  }
+declare module "random-js" {
+  declare class Engine {}
   declare class MtEngine extends Engine {
     $call: () => number;
     seed(value?: mixed): this;
@@ -11,17 +10,17 @@ declare module 'random-js' {
   }
   declare class Random {
     static engines: {
-      nativeMath(): Engine,
-      browserCrypto(): Engine,
-      mt19937(): MtEngine,
+      +nativeMath: Engine,
+      +browserCrypto: Engine,
+      +mt19937: MtEngine
     };
     static (engine?: Engine): Random;
     constructor(engine?: Engine): Random;
-    integer(min: number, max: number, inclusive?: bool): number;
-    real(min: number, max: number, inclusive?: bool): number;
-    bool(): bool;
-    bool(percentage: number): bool;
-    bool(numerator: number, denominator: number): bool;
+    integer(min: number, max: number, inclusive?: boolean): number;
+    real(min: number, max: number, inclusive?: boolean): number;
+    bool(): boolean;
+    bool(percentage: number): boolean;
+    bool(numerator: number, denominator: number): boolean;
     pick<T>(array: T[], begin?: number, end?: number): T;
     shuffle<T>(array: T[]): T[];
     sample<T>(array: T[], sampleSize: number): T[];
@@ -30,24 +29,42 @@ declare module 'random-js' {
     uuid(): string;
     string(length: number): string;
     string(length: number, pool: string): string;
-    hex(length: number, uppercase?: bool): string;
+    hex(length: number, uppercase?: boolean): string;
     date(start: Date, end: Date): Date;
 
-    static integer(min: number, max: number, inclusive?: bool): (engine: Engine) => number;
-    static real(min: number, max: number, inclusive?: bool): (engine: Engine) => number;
-    static bool(): (engine: Engine) => bool;
-    static bool(percentage: number): (engine: Engine) => bool;
-    static bool(numerator: number, denominator: number): (engine: Engine) => bool;
+    static integer(
+      min: number,
+      max: number,
+      inclusive?: boolean
+    ): (engine: Engine) => number;
+    static real(
+      min: number,
+      max: number,
+      inclusive?: boolean
+    ): (engine: Engine) => number;
+    static bool(): (engine: Engine) => boolean;
+    static bool(percentage: number): (engine: Engine) => boolean;
+    static bool(
+      numerator: number,
+      denominator: number
+    ): (engine: Engine) => boolean;
     static pick<T>(engine: Engine, array: T[], begin?: number, end?: number): T;
-    static picker<T>(array: T[], begin?: number, end?: number): (engine: Engine) => T;
+    static picker<T>(
+      array: T[],
+      begin?: number,
+      end?: number
+    ): (engine: Engine) => T;
     static shuffle<T>(engine: Engine, array: T[]): T[];
     static sample<T>(engine: Engine, array: T[], sampleSize: number): T[];
     static die(sideCount: number): (engine: Engine) => number;
-    static dice(sideCount: number, dieCount: number): (engine: Engine) => number[];
+    static dice(
+      sideCount: number,
+      dieCount: number
+    ): (engine: Engine) => number[];
     static uuid(engine: Engine): string;
     static string(): (engine: Engine, length: number) => string;
     static string(pool: string): (length: number, engine: Engine) => string;
-    static hex(uppercase?: bool): (length: number, engine: Engine) => string;
+    static hex(uppercase?: boolean): (length: number, engine: Engine) => string;
     static date(start: Date, end: Date): (engine: Engine) => Date;
   }
   declare module.exports: typeof Random;
