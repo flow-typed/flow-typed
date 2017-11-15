5701a1190657461809bf70c93edb19e881d50a18
Logan Barnett <logustus@gmail.com>
2017-11-14 16:38:45 -0800

    stop declaring that ramda's partial returns curried functions, and support up to 10 argument functions (#1533)

--- definitions/npm/ramda_v0.x.x/flow_v0.49.x-/test_ramda_v0..x.x_partial.js
+++ definitions/npm/ramda_v0.x.x/flow_v0.49.x-/test_ramda_v0..x.x_partial.js
@@ -5,9 +5,13 @@ import { partial } from "ramda";
 // --- Partial ---
 const method1 = (foo: string, bah: number, baz: boolean) => baz;
 
-const partial1: Function = partial(method1, ["foo"]);
+const partial1: (number, boolean) => boolean = partial(method1, ["foo"]);
 const partial2: boolean = partial(method1, ["foo"])(4, true);
 // $ExpectError
-const partial3: Function = partial(method1, [4]);
+const partial3 = partial(method1, [4]);
 const partial4: boolean = partial(method1, ["foo", 4])(true);
 const partial5: boolean = partial(method1, ["foo", 4, true])();
+
+// Functions produced from partial are not curried.
+// $ExpectError
+const partial6 = partial(method1, ["foo"])(4);
