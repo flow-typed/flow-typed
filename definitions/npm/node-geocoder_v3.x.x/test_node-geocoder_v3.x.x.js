e34ae7822b13bfca9e48c5a6fa4db27fb8a44f22
Tokyo <ahmed.tokyo1@gmail.com>
2017-11-16 07:27:46 +0200

    :pencil2: libdef/node-geocoder_v3.x.x fix tests

--- definitions/npm/node-geocoder_v3.x.x/test_node-geocoder_v3.x.x.js
+++ definitions/npm/node-geocoder_v3.x.x/test_node-geocoder_v3.x.x.js
@@ -14,20 +14,20 @@ geocoder.geocode(address);
 const geocoderOptionsMinimal = {
   provider: "google"
 };
-const geocoder = NodeGeocoder(geocoderOptionsMinimal);
+const geocoderMinimal = NodeGeocoder(geocoderOptionsMinimal);
 
 const geocoderOptionsErr1 = {
   provider: 3
 };
 // $ExpectError
-const geocoderErr1 = NodeGeocoder(geocoderOptions);
+const geocoderErr1 = NodeGeocoder(geocoderOptionsErr1);
 const addressErr1 = 2;
 // $ExpectError
 geocoder.geocode(addressErr1);
 
 const geocoderOptionsErr2 = 2;
 // $ExpectError
-const geocoderErr2 = NodeGeocoder(geocoderOptions);
+const geocoderErr2 = NodeGeocoder(geocoderOptionsErr2);
 const addressErr2 = null;
 // $ExpectError
 geocoder.geocode(addressErr2);
