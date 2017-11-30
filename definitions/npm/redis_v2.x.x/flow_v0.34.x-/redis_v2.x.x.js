ea1a4757dc5317228ccb4c31662ac4e741c45b01
rimiti <dimitri.dobairro@dimsolution.com>
2017-11-30 16:13:17 +0100

    feat(npm/redis_v2.x.x/flow_v0.34.x-/redis_v2.x.x.js) new methods added

--- definitions/npm/redis_v2.x.x/flow_v0.34.x-/redis_v2.x.x.js
+++ definitions/npm/redis_v2.x.x/flow_v0.34.x-/redis_v2.x.x.js
@@ -4,6 +4,10 @@ declare module "redis" {
   declare class RedisClient extends events$EventEmitter {
     hmset: (key: string, map: any, callback: (?Error) => void) => void;
     rpush: (key: string, value: string, callback: (?Error) => void) => void;
+    lpush: (key: string, value: any) => void;
+    hset: (topic: string, key: string, value: string) => number;
+    hget: (topic: string, key: string, value: string) => string;
+    hgetall: (topic: string, key: string) => Array<string>;
     get: (key: string) => any;
     set: (key: string, value: any) => void;
     del: (...keys: Array<string>) => void;
