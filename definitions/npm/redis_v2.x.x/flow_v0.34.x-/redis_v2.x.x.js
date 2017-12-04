ecf8a05bb633d293477acf0e4b191127776c96b6
Dimitri DO BAIRRO <dimitri.dobairro@dimsolution.com>
2017-12-04 16:39:17 +0100

    feat(npm/redis_v2.x.x/flow_v0.34.x-/redis_v2.x.x.js) redis promisifie… (#1603)
    * feat(npm/redis_v2.x.x/flow_v0.34.x-/redis_v2.x.x.js) redis promisified definition created
    
    * feat(npm/redis_v2.x.x/flow_v0.34.x-/redis_v2.x.x.js) some changes
    
    * feat(npm/redis_v2.x.x/flow_v0.34.x-/redis_v2.x.x.js) mixins RedisClientPromisified
    
    * fix(npm/redis_v2.x.x/flow_v0.34.x-/redis_v2.x.x.js) hget method only accept two parameters

--- definitions/npm/redis_v2.x.x/flow_v0.34.x-/redis_v2.x.x.js
+++ definitions/npm/redis_v2.x.x/flow_v0.34.x-/redis_v2.x.x.js
@@ -1,12 +1,19 @@
-/* This module definition is by no means complete. A lot of methods of the
-RedisClient class are missing */
+/* This module definition is by no means complete. A lot of methods of the RedisClient class are missing */
 declare module "redis" {
-  declare class RedisClient extends events$EventEmitter {
+  declare class RedisClient extends events$EventEmitter mixins RedisClientPromisified {
     hmset: (key: string, map: any, callback: (?Error) => void) => void;
     rpush: (key: string, value: string, callback: (?Error) => void) => void;
     lpush: (key: string, value: any) => number;
-    lrem: (topic: string, cursor: number, value: string) => Array<string> | void;
-    lrange: (topic: string, cursor: number, cursor2: number) => Array<string> | void;
+    lrem: (
+      topic: string,
+      cursor: number,
+      value: string
+    ) => Array<string> | void;
+    lrange: (
+      topic: string,
+      cursor: number,
+      cursor2: number
+    ) => Array<string> | void;
     hset: (topic: string, key: string, value: string) => number;
     hget: (topic: string, key: string, value: string) => string | void;
     hgetall: (topic: string, key: string) => Array<string> | void;
@@ -23,9 +30,53 @@ declare module "redis" {
     quit: () => void;
   }
 
+  declare class RedisClientPromisified extends RedisClient {
+    hmsetAsync: (
+      key: string,
+      map: any,
+      callback: (?Error) => void
+    ) => Promise<void>;
+    rpushAsync: (
+      key: string,
+      value: string,
+      callback: (?Error) => void
+    ) => Promise<void>;
+    lpushAsync: (key: string, value: any) => Promise<void>;
+    lremAsync: (
+      topic: string,
+      cursor: number,
+      value: string
+    ) => Promise<Array<string>> | Promise<void>;
+    lrangeAsync: (
+      topic: string,
+      cursor: number,
+      cursor2: number
+    ) => Promise<Array<string>> | Promise<void>;
+    hsetAsync: (topic: string, key: string, value: string) => Promise<number>;
+    hgetAsync: (
+      topic: string,
+      key: string
+    ) => Promise<string> | Promise<void>;
+    hgetallAsync: (
+      topic: string,
+      key: string
+    ) => Promise<Array<string>> | Promise<void>;
+    hdelAsync: (topic: string, key: string) => Promise<number>;
+    getAsync: (key: string) => Promise<any>;
+    setAsync: (key: string, value: any) => Promise<void>;
+    delAsync: (...keys: Array<string>) => Promise<void>;
+    publishAsync: (topic: string, value: any) => Promise<void>;
+    subscribeAsync: (topic: string) => Promise<void>;
+    unsubscribeAsync: (topic: string) => Promise<void>;
+    psubscribeAsync: (pattern: string) => Promise<void>;
+    punsubscribeAsync: (pattern: string) => Promise<void>;
+    duplicateAsync: () => Promise<RedisClientPromisified>;
+    quitAsync: () => Promise<void>;
+  }
+
   declare module.exports: {
     RedisClient: typeof RedisClient,
-
-    createClient: (settings: any) => RedisClient
+    RedisClientPromisified: typeof RedisClientPromisified,
+    createClient: (settings: any) => RedisClient | RedisClientPromisified
   };
 }
