0b0ab59434e1e990e93db463e682c119ee22b14d
Andy Edwards <jedwards@fastmail.com>
2017-12-05 22:04:29 -0600

    fix(redis): fix createClient def and add end method (#1621)
    * fix(redis): fix createClient def and add end method
    
    * fix(redis): fix CreateOptions
    
    * fix(redis): fix db option type

--- definitions/npm/redis_v2.x.x/flow_v0.34.x-/test_redis_v2.x.x.js
+++ definitions/npm/redis_v2.x.x/flow_v0.34.x-/test_redis_v2.x.x.js
@@ -3,6 +3,21 @@
 const redis = require("redis");
 
 const client: redis.RedisClient = redis.createClient();
+const options = {
+  host: 'localhost',
+  port: 6379,
+  string_numbers: false,
+  detect_buffers: true,
+  socket_keepalive: true,
+  enable_offline_queue: true,
+}
+redis.createClient(6379)
+redis.createClient(6379, options)
+redis.createClient(6379, '127.0.0.1')
+redis.createClient(6379, '127.0.0.1', options)
+redis.createClient('redis://localhost:6739')
+redis.createClient('redis://localhost:6739', options)
+redis.createClient(options)
 
 client.hmset("some-key", { key1: "value1" }, err =>
   console.log("hmset error:", err)
