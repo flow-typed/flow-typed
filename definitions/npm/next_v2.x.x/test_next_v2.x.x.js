072e68a86adb51f147a2fd3886d8e9b16a5e4d51
Facundo Viale <facundo.m.viale@gmail.com>
2018-01-02 11:11:36 -0300

    Minor changes

--- definitions/npm/next_v2.x.x/test_next_v2.x.x.js
+++ definitions/npm/next_v2.x.x/test_next_v2.x.x.js
@@ -1,11 +1,17 @@
+import React from "react";
 import next from "next";
-import Link from 'next/link'
-import Prefetch from 'next/prefetch'
-import Head from 'next/head'
-import Router from 'next/router'
-import Document, {Main, Head as DocumentHead, NextScript} from "next/document"
-const { createServer } = require('http')
-const { parse } = require('url')
+import Link from "next/link";
+import Prefetch from "next/prefetch";
+import Head from "next/head";
+import Router from "next/router";
+import Document, {
+  type Context,
+  Main,
+  Head as DocumentHead,
+  NextScript
+} from "next/document";
+const { createServer } = require("http");
+const { parse } = require("url");
 
 // server
 // $ExpectError
@@ -13,46 +19,50 @@ next({ dev: 1 });
 // $ExpectError
 next({ dir: false });
 // $ExpectError
-next({ quiet: 'derp' });
+next({ quiet: "derp" });
 // $ExpectError
 next({ staticMarkup: 42 });
 
-const app = next({ dev: true, dir: ".", quiet: false});
-const handle = app.getRequestHandler()
-app.prepare()
-.then(() => {
+const app = next({ dev: true, dir: ".", quiet: false });
+const handle = app.getRequestHandler();
+app.prepare().then(() => {
   createServer((req, res) => {
-    const parsedUrl = parse(req.url, true)
-    const { pathname, query } = parsedUrl
+    const parsedUrl = parse(req.url, true);
+    const { pathname, query } = parsedUrl;
 
-    if (pathname === '/') {
-      app.render(req, res, '/index', query)
-    } else if (pathname === '/foo') {
-      app.render(req, res, '/index', query)
-    } else if (pathname === '/about') {
-      app.render(req, res, '/about', query)
+    if (pathname === "/") {
+      app.render(req, res, "/index", query);
+    } else if (pathname === "/foo") {
+      app.render(req, res, "/index", query);
+    } else if (pathname === "/about") {
+      app.render(req, res, "/about", query);
     } else {
-      handle(req, res, parsedUrl)
+      handle(req, res, parsedUrl);
     }
-  })
-  .listen("3500", (err) => {
-    if (err) throw err
-    console.log('> Ready on http://localhost:3500')
-  })
+  }).listen("3500", err => {
+    if (err) throw err;
+    console.log("> Ready on http://localhost:3500");
+  });
 });
 
 <Head>
-  <meta charSet='utf-8' />
+  <meta charSet="utf-8" />
 </Head>;
 
-<Link href='/'>Index</Link>;
-<Prefetch href='/'>Prefetch</Prefetch>;
-<Prefetch href='/' prefetch={false}>Prefetch</Prefetch>;
+<Link href="/">Index</Link>;
+<Prefetch href="/">Prefetch</Prefetch>;
+<Prefetch href="/" prefetch={false}>
+  Prefetch
+</Prefetch>;
 
 // $ExpectError
-<Link href={1}>InvalidNumLink</Link>|
-// $ExpectError
-<Prefetch href='/' prefetch={() => {}}>Prefetch</Prefetch>;
+<Link href={1}>InvalidNumLink</Link> |
+  (
+    // $ExpectError
+    <Prefetch href="/" prefetch={() => {}}>
+      Prefetch
+    </Prefetch>
+  );
 
 Router.onRouteChangeStart = (url: string) => {};
 Router.onRouteChangeStart = null;
@@ -62,35 +72,34 @@ Router.onRouteChangeComplete = null;
 
 Router.onRouteChangeError = (err, url) => {
   if (err.cancelled) {
-    console.log(`Route to ${url} was cancelled!`)
+    console.log(`Route to ${url} was cancelled!`);
   }
-}
+};
 
-Router.push('/about');
-Router.push('/about', '/');
-Router.replace('/about');
-Router.replace('/about', '/');
+Router.push("/about");
+Router.push("/about", "/");
+Router.replace("/about");
+Router.replace("/about", "/");
 
 const r: string = Router.route;
 const p: string = Router.pathname;
 const q: {} = Router.query;
 
 export default class TestDocument extends Document {
-  static async getInitialProps (ctx) {
-    const props = await Document.getInitialProps(ctx)
-    return { ...props, customValue: 'hi there!' }
+  static async getInitialProps(ctx: Context) {
+    const props = await Document.getInitialProps(ctx);
+    return { ...props, customValue: "hi there!" };
   }
 
-  render () {
+  render() {
     return (
-     <html>
-       <DocumentHead>
-       </DocumentHead>
-       <body>
-         <Main />
-         <NextScript />
-       </body>
-     </html>
-    )
+      <html>
+        <DocumentHead />
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </html>
+    );
   }
 }
