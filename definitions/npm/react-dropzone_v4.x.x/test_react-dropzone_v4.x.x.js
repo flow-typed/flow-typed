9e8f6b2f55e3858358df6b3d4f840bef6b3dd792
Thomas Ladd <thomas.rye.ladd@gmail.com>
2018-02-12 09:01:22 -0600

    libdefs: Add react-dropzone type definition (#1833)

---
+++ definitions/npm/react-dropzone_v4.x.x/test_react-dropzone_v4.x.x.js
@@ -0,0 +1,94 @@
+import React from "react";
+import Dropzone, { type ChildrenProps } from "react-dropzone";
+
+<Dropzone />;
+
+<Dropzone>
+  <div />
+</Dropzone>;
+
+<Dropzone
+  accept="image/jpeg, image/png, application/pdf"
+  disableClick={true}
+  disabled={true}
+  disablePreview={true}
+  preventDropOnDocument={true}
+  inputProps={{ className: "input-class" }}
+  multiple={true}
+  name="name"
+  maxSize={1000}
+  minSize={100}
+  className="className"
+  activeClassName="active"
+  acceptClassName="accept"
+  rejectClassName="reject"
+  disabledClassName="disabled"
+  style={{ height: '50px' }}
+  activeStyle={{ height: '60px' }}
+  acceptStyle={{ height: '70px' }}
+  rejectStyle={{ height: '80px' }}
+  disabledStyle={{ height: '90px' }}
+  onClick={(event: SyntheticMouseEvent<>) => {}}
+  onDrop={(acceptedFiles: Array<File>, rejectedFiles: Array<File>, event: SyntheticDragEvent<>) => {}}
+  onDropAccepted={(acceptedFiles: Array<File>, event: SyntheticDragEvent<>) => {}}
+  onDropRejected={(rejectedFiles: Array<File>, event: SyntheticDragEvent<>) => {}}
+  onDragStart={(event: SyntheticDragEvent<>) => {}}
+  onDragEnter={(event: SyntheticDragEvent<>) => {}}
+  onDragOver={(event: SyntheticDragEvent<>) => {}}
+  onDragLeave={(event: SyntheticDragEvent<>) => {}}
+  onFileDialogCancel={() => {}}
+>
+  {
+    ({
+      draggedFiles,
+      acceptedFile,
+      rejectedFiles,
+      isDragActive,
+      isDragAccept,
+      isDragReject,
+    }: ChildrenProps) => (
+      <div />
+    )
+  }
+</Dropzone>;
+
+<Dropzone
+  accept="image/jpeg, image/png, application/pdf"
+  onClick={() => {}}
+  onDrop={(acceptedFiles: Array<File>) => {}}
+  onDropAccepted={(acceptedFiles: Array<File>) => {}}
+  onDropRejected={(rejectedFiles: Array<File>) => {}}
+  onDragStart={() => {}}
+  onDragEnter={() => {}}
+  onDragOver={() => {}}
+  onDragLeave={() => {}}
+>
+  {
+    ({
+      isDragActive,
+    }: { isDragActive: boolean }) => (
+      <div />
+    )
+  }
+</Dropzone>;
+
+// $ExpectError - Wrong type for function handler
+<Dropzone onDrop={(event: SyntheticDragEvent<>) => {}} />;
+
+<Dropzone>
+  {
+    ({
+      acceptedFiles,
+    // $ExpectError - Wrong type for children function
+    }: { acceptedFiles: Array<string> }) => (
+      <div />
+    )
+  }
+</Dropzone>;
+
+<Dropzone>
+  {
+    // $ExpectError - Wrong return type for children function
+    () => ({})
+  }
+</Dropzone>;
