7efcc94edf131071b144ea965de16f400b9f45a4
Edward Drapkin <edward.drapkin@flexshopper.com>
2018-02-20 11:01:08 -0500

    Params were actually optional

--- definitions/npm/sha3_v1.2.x/flow_v0.56.x-/sha3_v1.2.x.js
+++ definitions/npm/sha3_v1.2.x/flow_v0.56.x-/sha3_v1.2.x.js
@@ -1,8 +1,8 @@
 declare module "sha3" {
   declare class SHA3Hash {
-    constructor(hashLength: 224 | 256 | 384 | 512): SHA3Hash;
+    constructor(hashLength?: 224 | 256 | 384 | 512): SHA3Hash;
     update(data: any, inputEncoding?: 'binary' | 'ascii' | 'utf8'): void;
-    digest(type: 'hex' | 'binary'): string;
+    digest(type?: 'hex' | 'binary'): string;
   }
 
   declare module .exports: {
