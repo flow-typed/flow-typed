f12aa11d8c15c02458a8eabfb5f8e409e3f18b3e
Edward Drapkin <edward.drapkin@flexshopper.com>
2018-02-21 14:32:14 -0500

    Add Header.HEIGHT property

--- definitions/npm/react-navigation_v1.x.x/flow_v0.60.x-/react-navigation_v1.x.x.js
+++ definitions/npm/react-navigation_v1.x.x/flow_v0.60.x-/react-navigation_v1.x.x.js
@@ -949,7 +949,9 @@ declare module 'react-navigation' {
   };
   declare export var SafeAreaView: React$ComponentType<_SafeAreaViewProps>;
 
-  declare export var Header: React$ComponentType<HeaderProps>;
+  declare export var Header: React$ComponentType<HeaderProps> & {
+    HEIGHT: number
+  };
 
   declare type _HeaderTitleProps = {
     children: React$Node,
