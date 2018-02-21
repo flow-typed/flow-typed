83b18274764163d9b3e00c9f828a2c07d616732a
saadq <saad@saadq.com>
2018-02-20 20:07:42 -0500

    add react-color

---
+++ definitions/npm/react-color_v2.x.x/flow_v0.56.x-/react-color_v2.x.x.js
@@ -0,0 +1,220 @@
+declare module "react-color" {
+  declare export type HexColor = string;
+
+  declare export type HSLColor = {|
+    h: number,
+    s: number,
+    l: number,
+    a?: number
+  |};
+
+  declare export type RGBColor = {|
+    r: number,
+    g: number,
+    b: number,
+    a?: number
+  |};
+
+  declare export type Color = HexColor | HSLColor | RGBColor;
+
+  declare export type ColorResult = {|
+    hex: HexColor,
+    hsl: HSLColor,
+    rgb: RGBColor
+  |};
+
+  declare export type ColorChangeHandler = (color: ColorResult) => void;
+
+  declare export type ColorPickerProps = {|
+    color?: Color,
+    onChange?: ColorChangeHandler,
+    onChangeComplete?: ColorChangeHandler
+  |};
+
+  declare export type AlphaPickerProps = {|
+    ...ColorPickerProps,
+    width?: string,
+    height?: string,
+    direction?: "horizontal" | "vertical",
+    renderers?: Object,
+    pointer?: React$ComponentType<any>
+  |};
+
+  declare export type BlockPickerProps = {|
+    ...ColorPickerProps,
+    width?: string,
+    colors?: Array<string>,
+    triangle?: "hide" | "top",
+    onSwatchHover?: (color: Color, event: SyntheticMouseEvent<*>) => void
+  |};
+
+  declare export type ChromePickerProps = {|
+    ...ColorPickerProps,
+    disableAlpha?: boolean,
+    renderers?: Object
+  |};
+
+  declare export type CirclePickerProps = {|
+    ...ColorPickerProps,
+    width?: string,
+    colors?: Array<string>,
+    circleSize?: number,
+    circleSpacing?: number,
+    onSwatchHover?: (color: Color, event: SyntheticMouseEvent<*>) => void
+  |};
+
+  declare export type CompactPickerProps = {|
+    ...ColorPickerProps,
+    colors?: Array<string>,
+    onSwatchHover?: (color: Color, event: SyntheticMouseEvent<*>) => void
+  |};
+
+  declare export type GithubPickerProps = {|
+    ...ColorPickerProps,
+    width?: string,
+    colors?: Array<string>,
+    triangle?: "hide" | "top-left" | "top-right",
+    onSwatchHover?: (color: Color, event: SyntheticMouseEvent<*>) => void
+  |};
+
+  declare export type HuePickerProps = {|
+    ...ColorPickerProps,
+    width?: string,
+    height?: string,
+    direction?: "horizontal" | "vertical",
+    pointer?: React$ComponentType<any>
+  |};
+
+  declare export type MaterialPickerProps = {|
+    ...ColorPickerProps
+  |};
+
+  declare export type PhotoshopPickerProps = {|
+    ...ColorPickerProps,
+    header?: string,
+    onAccept?: () => void,
+    onCancel?: () => void
+  |};
+
+  declare export type SketchPickerProps = {|
+    ...ColorPickerProps,
+    disableAlpha?: boolean,
+    presetColors?: Array<string | {| color: string, title: string |}>,
+    width?: number,
+    renderers?: Object,
+    onSwatchHover?: (color: Color, event: SyntheticMouseEvent<*>) => void
+  |};
+
+  declare export type SliderPickerProps = {|
+    ...ColorPickerProps,
+    pointer?: React$ComponentType<any>
+  |};
+
+  declare export type SwatchesPickerProps = {|
+    ...ColorPickerProps,
+    width?: number,
+    height?: number,
+    colors?: Array<Array<string>>,
+    onSwatchHover?: (color: Color, event: SyntheticMouseEvent<*>) => void
+  |};
+
+  declare export type TwitterPickerProps = {|
+    ...ColorPickerProps,
+    width?: string,
+    colors?: Array<string>,
+    triangle?: "hide" | "top-left" | "top-right",
+    onSwatchHover?: (color: Color, event: SyntheticMouseEvent<*>) => void
+  |};
+
+  declare export type ColorWrapChangeHandler = (
+    color: Color | ColorResult
+  ) => void;
+
+  declare export type InjectedColorProps = {
+    hex?: string,
+    hsl?: HSLColor,
+    rgb?: RGBColor,
+    onChange?: ColorWrapChangeHandler
+  };
+
+  declare export type ExportedColorProps = {
+    color?: Color,
+    onChange?: ColorChangeHandler,
+    onChangeComplete?: ColorChangeHandler
+  };
+
+  declare export class AlphaPicker extends React$Component<AlphaPickerProps> {}
+  declare export class BlockPicker extends React$Component<BlockPickerProps> {}
+  declare export class ChromePicker extends React$Component<ChromePickerProps> {}
+  declare export class CirclePicker extends React$Component<CirclePickerProps> {}
+  declare export class CompactPicker extends React$Component<CompactPickerProps> {}
+  declare export class GithubPicker extends React$Component<GithubPickerProps> {}
+  declare export class HuePicker extends React$Component<HuePickerProps> {}
+  declare export class MaterialPicker extends React$Component<MaterialPickerProps> {}
+  declare export class PhotoshopPicker extends React$Component<PhotoshopPickerProps> {}
+  declare export class SketchPicker extends React$Component<SketchPickerProps> {}
+  declare export class SliderPicker extends React$Component<SliderPickerProps> {}
+  declare export class SwatchesPicker extends React$Component<SwatchesPickerProps> {}
+  declare export class TwitterPicker extends React$Component<TwitterPickerProps> {}
+
+  declare export function CustomPicker<A>(
+    component: React$ComponentType<A & InjectedColorProps>
+  ): Class<A & ExportedColorProps>;
+}
+
+declare module "react-color/lib/components/common" {
+  import type {
+    HexColor,
+    RGBColor,
+    HSLColor,
+    ColorChangeHandler
+  } from "react-color";
+
+  declare type PartialColorResult = {|
+    hex?: HexColor,
+    hsl?: HSLColor,
+    rgb?: RGBColor
+  |};
+
+  declare export type AlphaProps = {|
+    ...PartialColorResult,
+    pointer?: React$ComponentType<any>,
+    onChange?: ColorChangeHandler
+  |};
+
+  declare export type EditableInputProps = {|
+    label?: string,
+    value?: any,
+    onChange?: ColorChangeHandler,
+    style?: {|
+      input?: Object,
+      label?: Object,
+      wrap?: Object
+    |}
+  |};
+
+  declare export type HueProps = {|
+    ...PartialColorResult,
+    pointer?: React$ComponentType<any>,
+    onChange?: ColorChangeHandler,
+    direction?: "horizontal" | "vertical"
+  |};
+
+  declare export type SaturationProps = {|
+    ...PartialColorResult,
+    pointer?: React$ComponentType<any>,
+    onChange?: ColorChangeHandler
+  |};
+
+  declare export type CheckboardProps = {|
+    size?: number,
+    white?: string,
+    grey?: string
+  |};
+
+  declare export class Alpha extends React$Component<AlphaProps> {}
+  declare export class EditableInput extends React$Component<EditableInputProps> {}
+  declare export class Hue extends React$Component<HueProps> {}
+  declare export class Saturation extends React$Component<SaturationProps> {}
+  declare export class Checkboard extends React$Component<CheckboardProps> {}
+}
