be6ced0af20b9e406beb6bfee8e2d102143e39be
omeid matten <public@omeid.me>
2018-03-02 10:29:58 +1100

    libdef: react-router: fix Match.params
    React-Router params are never null, they either exists or not.

--- definitions/npm/react-router-native_v4.x.x/flow_v0.53.x-/react-router-native_v4.x.x.js
+++ definitions/npm/react-router-native_v4.x.x/flow_v0.53.x-/react-router-native_v4.x.x.js
@@ -60,7 +60,7 @@ declare module "react-router-native" {
   };
 
   declare export type Match = {
-    params: { [key: string]: ?string },
+    params: { [key: string]: string },
     isExact: boolean,
     path: string,
     url: string
