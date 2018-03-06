1ff28f08ba6efa8e21390e34e182bef3a974c0a8
Ville Saukkonen <ville@smartly.io>
2018-03-06 22:10:00 +0200

    fix inputRef type annotations

--- definitions/npm/react-bootstrap_v0.32.x/flow_v0.53.x-/test_react-bootstrap_v0.32.x.js
+++ definitions/npm/react-bootstrap_v0.32.x/flow_v0.53.x-/test_react-bootstrap_v0.32.x.js
@@ -1,5 +1,5 @@
 import * as React from 'react';
-import { Dropdown, Overlay } from 'react-bootstrap';
+import { Dropdown, Overlay, FormControl, Checkbox, Radio } from 'react-bootstrap';
 
 
 // normal components works
@@ -8,3 +8,8 @@ import { Dropdown, Overlay } from 'react-bootstrap';
 
 // nested components works too
 (<Dropdown.Toggle></Dropdown.Toggle>: React.Node);
+
+// inputRefs gives HTMLInputElements
+(<FormControl inputRef={(ref) => {(ref: ?HTMLInputElement)}}/>: React.Node);
+(<Checkbox inputRef={(ref) => {(ref: ?HTMLInputElement)}} />: React.Node);
+(<Radio inputRef={(ref) => {(ref: ?HTMLInputElement)}} />: React.Node);
