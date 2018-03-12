ec6eac95892ffa7525dff1cc34781cab3e699718
Dimitri DO BAIRRO <dimitri.dobairro@dimsolution.com>
2018-03-12 17:30:19 +0100

    feat(npm/@sendgrid/mail_v6.x.x/flow_v0.30.x-/test_mail_v6.x.x.js) send function uncommented

--- definitions/npm/@sendgrid/mail_v6.x.x/flow_v0.30.x-/test_mail_v6.x.x.js
+++ definitions/npm/@sendgrid/mail_v6.x.x/flow_v0.30.x-/test_mail_v6.x.x.js
@@ -1,7 +1,6 @@
 import mail from '@sendgrid/mail';
 
 mail.setApiKey('myApiKey');
-// Waiting for issue: https://github.com/facebook/flow/issues/5943
-// mail.send({
-//   from: {email: "test@example.com"},
-// });
+mail.send({
+  from: {email: "test@example.com"},
+});
