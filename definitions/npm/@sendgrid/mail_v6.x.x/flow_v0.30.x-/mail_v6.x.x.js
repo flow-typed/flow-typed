302430654ca3b33ed4897eb90dab8da8b7b93c83
Dimitri DO BAIRRO <dimitri.dobairro@dimsolution.com>
2018-03-13 15:30:58 +0100

    Adding @sengrid/mail definition (#1949)
    * feat(npm/@sendgrid/mail_v6.x.x/flow_v0.30.x-/mail_v6.x.x.js) definition created
    
    * feat(npm/@sendgrid/mail_v6.x.x/flow_v0.30.x-/test_mail_v6.x.x.js) test created
    
    * feat(npm/@sendgrid/mail_v6.x.x/flow_v0.30.x-/test_mail_v6.x.x.js) send function uncommented
    
    * feat(npm/@sendgrid/mail_v6.x.x/flow_v0.30.x-/mail_v6.x.x.js) export type changed
    
    * chore(npm/@sendgrid/mail_v6.x.x/flow_v0.30.x-/test_mail_v6.x.x.js) many tests added
    
    * Added statements expected to fail

---
+++ definitions/npm/@sendgrid/mail_v6.x.x/flow_v0.30.x-/mail_v6.x.x.js
@@ -0,0 +1,138 @@
+declare module '@sendgrid/mail' {
+
+  declare class ResponseError extends Error {
+    code: number;
+    message: string;
+    response: {
+      headers: { [key: string]: string; };
+      body: string;
+    };
+  }
+
+  declare interface PersonalizationData {
+    to: EmailData | EmailData[],
+    cc?: EmailData | EmailData[],
+    bcc?: EmailData | EmailData[],
+    subject?: string;
+    headers?: { [key: string]: string };
+    substitutions?: { [key: string]: string };
+    customArgs?: { [key: string]: string };
+    sendAt?: number;
+  }
+
+  declare interface AttachmentData {
+    content: string;
+    filename: string;
+    type?: string;
+    disposition?: string;
+    contentId?: string;
+  }
+
+  declare interface ASMOptions {
+    groupId: number;
+    groupsToDisplay?: number[];
+  }
+
+  declare interface MailSettings {
+    bcc?: {
+      enable?: boolean;
+      email?: string;
+    };
+    bypassListManagement?: {
+      enable?: boolean;
+    };
+    footer?: {
+      enable?: boolean;
+      text?: string;
+      html?: string;
+    };
+    sandboxMode?: {
+      enable?: boolean;
+    };
+    spamCheck?: {
+      enable?: boolean;
+      threshold?: number;
+      postToUrl?: string;
+    };
+  }
+
+  declare interface TrackingSettings {
+    clickTracking?: {
+      enable?: boolean;
+      enableText?: boolean;
+    };
+    openTracking?: {
+      enable?: boolean;
+      substitutionTag?: string;
+    };
+    subscriptionTracking?: {
+      enable?: boolean;
+      text?: string;
+      html?: string;
+      substitutionTag?: string;
+    };
+    ganalytics?: {
+      enable?: boolean;
+      utmSource?: string;
+      utmMedium?: string;
+      utmTerm?: string;
+      utmContent?: string;
+      utmCampaign?: string;
+    };
+  }
+
+  declare type EmailData = string | { name?: string; email: string; };
+
+  declare interface MailContent {
+    type: string;
+    value: string;
+  }
+
+  declare export type MailData = {
+    to?: EmailData | EmailData[],
+    cc?: EmailData | EmailData[],
+    bcc?: EmailData | EmailData[],
+
+    from: EmailData,
+    replyTo?: EmailData,
+
+    sendAt?: number,
+
+    subject?: string,
+    text?: string,
+    html?: string,
+    content?: MailContent[],
+    templateId?: string,
+
+    personalizations?: PersonalizationData[],
+    attachments?: AttachmentData[],
+
+    ipPoolName?: string,
+    batchId?: string,
+
+    sections?: { [key: string]: string },
+    headers?: { [key: string]: string },
+
+    categories?: string[],
+    category?: string,
+
+    customArgs?: { [key: string]: any },
+    asm?: ASMOptions,
+
+    mailSettings?: MailSettings,
+    trackingSettings?: TrackingSettings,
+
+    substitutions?: { [key: string]: string },
+    substitutionWrappers?: string[],
+
+    isMultiple?: boolean,
+  }
+
+  declare class MailService {
+    setApiKey(apiKey: string): void;
+    setSubstitutionWrappers(left: string, right: string): void;
+    send(data: MailData | MailData[], isMultiple?: boolean, cb?: (err: Error | ResponseError, result: [http$ClientRequest, {}]) => void): Promise<[http$ClientRequest, {}]>;
+  }
+
+  declare export default MailService & { MailService: MailService };
+}
