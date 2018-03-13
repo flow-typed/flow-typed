34fcf12a993921667b4f4cb1c6fe0ceb7b2eae72
Maxim Malov <maxmalov@users.noreply.github.com>
2018-03-13 23:52:50 +0600

    Rework react-loadable types to fulfill the current API (#1955)
    * Rework react-loadable types to fulfill the current API
    
    * Rewrite tests using `describe` and `it` blocks
    
    * Remove unused type
    
    * Provide additional annotations in tests

--- definitions/npm/react-loadable_v5.x.x/flow_v0.56.0-/react-loadable_v5.x.x.js
+++ definitions/npm/react-loadable_v5.x.x/flow_v0.56.0-/react-loadable_v5.x.x.js
@@ -1,13 +1,53 @@
 declare module 'react-loadable' {
-  declare type LoaderResult<T> = T | { 'default': T };
+  declare type LoadingProps = {
+    isLoading: boolean,
+    pastDelay: boolean,
+    timedOut: boolean,
+    error: boolean
+  };
 
-  declare module.exports: <P>(opts: {
-    loader(): Promise<LoaderResult<React$ComponentType<P>>>,
-    loading: React$ComponentType<*>,
+  declare type CommonOptions = {
+    loading: React$ComponentType<LoadingProps>,
     delay?: number,
     timeout?: number,
-    render?: (r: LoaderResult<React$ComponentType<P>>, p: P) => React$Node,
-    webpack?: () => Array<string>,
-    modules?: Array<string>
-  }) => React$ComponentType<P>;
+    modules?: Array<string>,
+    webpack?: () => Array<number>
+  };
+
+  declare type OptionsWithoutRender<TProps> = {
+    ...CommonOptions,
+    loader(): Promise<React$ComponentType<TProps> | { default: React$ComponentType<TProps> }>
+  };
+
+  declare type OptionsWithRender<TProps, TModule> = {
+    ...CommonOptions,
+    loader(): Promise<TModule>,
+    render(loaded: TModule, props: TProps): React$Node
+  };
+
+  declare type Options<TProps, TModule> = OptionsWithoutRender<TProps> | OptionsWithRender<TProps, TModule>;
+
+  declare type MapOptions<TProps, TModules: { [key: string]: * }> = {
+    ...CommonOptions,
+    loader: {
+      [key: $Keys<TModules>]: () => Promise<*>
+    },
+    render(loaded: TModules, props: TProps): React$Node
+  };
+
+  declare class LoadableComponent<TProps> extends React$Component<TProps> {
+    static preload(): Promise<void>
+  }
+
+  declare type CaptureProps = {
+    report(moduleName: string): void
+  };
+
+  declare module.exports: {
+    <TProps, TModule>(opts: Options<TProps, TModule>): Class<LoadableComponent<TProps>>,
+    Map<TProps, TModules>(opts: MapOptions<TProps, TModules>): Class<LoadableComponent<TProps>>,
+    Capture: React$ComponentType<CaptureProps>,
+    preloadAll(): Promise<void>,
+    preloadReady(): Promise<void>,
+  };
 }
--- definitions/npm/react-loadable_v5.x.x/flow_v0.56.0-/test_react-loadable_v5.x.x.js
+++ definitions/npm/react-loadable_v5.x.x/flow_v0.56.0-/test_react-loadable_v5.x.x.js
@@ -2,86 +2,176 @@
 
 import React from 'react';
 import Loadable from 'react-loadable';
-
-// $ExpectError options object is required
-Loadable();
-
-// $ExpectError loader option is required
-Loadable({
-  loading: () => null
-});
-
-// $ExpectError loader option should return a promise that resolves to a react component or a module object with default export which is a react component
-Loadable({
-  loader: () => Promise.resolve(1),
-  loading: () => null
-});
+import { describe, it } from 'flow-typed-test';
 
 type Props = { a: string };
-
 class Component extends React.Component<Props> {}
 
-// $ExpectError delay option should be a number
-Loadable({
-  loader: () => Promise.resolve(Component),
-  loading: () => null,
-  delay: ''
-});
-
-// $ExpectError timeout option should be a number
-Loadable({
-  loader: () => Promise.resolve(Component),
-  loading: () => null,
-  timeout: ''
-});
-
-// $ExpectError webpack option should be a function
-Loadable({
-  loader: () => Promise.resolve(Component),
-  loading: () => null,
-  webpack: []
-});
-
-// $ExpectError modules option should be an array of module names
-Loadable({
-  loader: () => Promise.resolve(Component),
-  loading: () => null,
-  modules: [1]
-});
-
-// $ExpectError render option should take a resolved module and related component props
-Loadable({
-  loader: () => Promise.resolve(Component),
-  loading: () => null,
-  render: (a: string, b: string) => null
-});
-
-Loadable({
-  loader: () => Promise.resolve(Component),
-  loading: () => null,
-  render: (C: React$ComponentType<Props> | { default: React$ComponentType<Props> }, props: Props) => null
-});
-
-{
-  const Loaded = Loadable({
-    loader: () => Promise.resolve(Component),
-    loading: () => null
+describe('options', () => {
+  it('should fail when props is not provided', () => {
+    // $ExpectError
+    Loadable();
   });
 
-  // $ExpectError "a" property should be a string (inherited from Component)
-  const noWay = <Loaded a={1} />;
-
-  <Loaded a="foo"/>
-}
-
-{
-  const Loaded = Loadable({
-    loader: () => Promise.resolve({ default: Component }),
-    loading: () => null
+  it('should fail when loader is missing', () => {
+    // $ExpectError
+    Loadable({
+      loading: () => null
+    });
   });
 
-  // $ExpectError "a" property should be a string (inherited from Component)
-  const noWay = <Loaded a={1} />;
+  it('should fail when loader option does not return a promise that resolves to a react component or a module object with default export which is a react component', () => {
+    // $ExpectError
+    Loadable({
+      loader: () => Promise.resolve(1),
+      loading: () => null
+    });
+  });
 
-  <Loaded a="foo"/>
-}
+  it('delay property should be a number', () => {
+    // $ExpectError
+    Loadable({
+      loader: () => Promise.resolve(Component),
+      loading: () => null,
+      delay: ''
+    });
+
+    Loadable({
+      loader: () => Promise.resolve(Component),
+      loading: () => null,
+      delay: 1
+    });
+  });
+
+  it('timeout property should be a number', () => {
+    // $ExpectError
+    Loadable({
+      loader: () => Promise.resolve(Component),
+      loading: () => null,
+      timeout: ''
+    });
+
+    Loadable({
+      loader: () => Promise.resolve(Component),
+      loading: () => null,
+      timeout: 1
+    });
+  });
+
+  it('webpack option should be a function', () => {
+    // $ExpectError
+    Loadable({
+      loader: () => Promise.resolve(Component),
+      loading: () => null,
+      webpack: []
+    });
+
+    Loadable({
+      loader: () => Promise.resolve(Component),
+      loading: () => null,
+      webpack: () => [1]
+    });
+  });
+
+  it('modules options should be an array of strings', () => {
+    // $ExpectError modules option should be an array of module names
+    Loadable({
+      loader: () => Promise.resolve(Component),
+      loading: () => null,
+      modules: [1]
+    });
+
+    Loadable({
+      loader: () => Promise.resolve(Component),
+      loading: () => null,
+      modules: ['f', 'o', 'o']
+    });
+  });
+
+  it('render option should take a resolved module and related component props', () => {
+    // $ExpectError render option should take a resolved module and related component props
+    Loadable({
+      loader: () => Promise.resolve(Component),
+      loading: () => null,
+      render: (a: string, b: string) => null
+    });
+  });
+});
+
+describe('props refinements', () => {
+  class SyncComponent extends React.Component<{ a: string, b: number }> {}
+
+  it('should refine props type from render function', () => {
+    const LoadableComponent = Loadable({
+      loader: (): Promise<number> => Promise.resolve(1),
+      loading: () => null,
+      render: (b: number, props: Props) => <SyncComponent a={props.a} b={b} />
+    });
+
+    const foo = <LoadableComponent a="foo"/>;
+    // $ExpectError
+    const bar = <LoadableComponent a={1} />;
+  });
+
+  it('should refine props type from loader option', () => {
+    const Loaded1 = Loadable({
+      loader: () => Promise.resolve(Component),
+      loading: () => null
+    });
+
+    // $ExpectError
+    const noWay = <Loaded1 a={1} />;
+    <Loaded1 a="foo"/>;
+
+    const Loaded2 = Loadable({
+      loader: () => Promise.resolve({ default: Component }),
+      loading: () => null
+    });
+
+    // $ExpectError
+    const noWay = <Loaded2 a={1} />;
+
+    <Loaded2 a="foo"/>
+  });
+});
+
+describe('result component', () => {
+  it('should be a react component', () => {
+    const LoadableComponent: React$ComponentType<Props> = Loadable({
+      loader: () => Promise.resolve({ default: Component }),
+      loading: () => null
+    });
+  });
+
+  it('should have preload() static method', () => {
+    const LoadableComponent = Loadable({
+      loader: () => Promise.resolve({ default: Component }),
+      loading: () => null
+    });
+
+    LoadableComponent.preload();
+  });
+});
+
+it('Loadable.Map() should work', () => {
+  class Component extends React.Component<{ a: string, b: number }> {}
+
+  const LoadableMap = Loadable.Map({
+    loading: () => null,
+    loader: {
+      Cmp: () => Promise.resolve({ foo: Component }),
+      b: () => Promise.resolve(1)
+    },
+    render: (loaded: { Cmp: { foo: React$ComponentType<{ a: string, b: number }> }, b: number }, props: { a: string }) => <loaded.Cmp.foo a={props.a} b={loaded.b} />
+  });
+
+  <LoadableMap a="foo" />
+});
+
+it('Loadable.preloadAll()', () => {
+  Loadable.preloadAll();
+});
+
+it('Loadable.preloadReady()', () => {
+  Loadable.preloadReady();
+});
