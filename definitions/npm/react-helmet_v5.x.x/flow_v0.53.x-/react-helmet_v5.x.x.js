5af8bb59019b99f215d4b3226c6062ed5537337b
mrm007 <mrm_dev@outlook.com>
2018-03-30 01:03:43 +1100

    Don't import React definitions globally in react-helmet (#2005)
    * Don't import React definitions globally in react-helmet
    
    * Use raw React types directly, without importing them

--- definitions/npm/react-helmet_v5.x.x/flow_v0.53.x-/react-helmet_v5.x.x.js
+++ definitions/npm/react-helmet_v5.x.x/flow_v0.53.x-/react-helmet_v5.x.x.js
@@ -1,53 +1,51 @@
-import type { Node, Element } from 'react';
-
 declare module 'react-helmet' {
-    declare type Props = {
-      base?: Object,
-      bodyAttributes?: Object,
-      children?: Node,
-      defaultTitle?: string,
-      defer?: boolean,
-      encodeSpecialCharacters?: boolean,
-      htmlAttributes?: Object,
-      link?: Array<Object>,
-      meta?: Array<Object>,
-      noscript?: Array<Object>,
-      onChangeClientState?: (
-        newState?: Object,
-        addedTags?: Object,
-        removeTags?: Object
-      ) => any,
-      script?: Array<Object>,
-      style?: Array<Object>,
-      title?: string,
-      titleAttributes?: Object,
-      titleTemplate?: string,
-    }
+  declare type Props = {
+    base?: Object,
+    bodyAttributes?: Object,
+    children?: React$Node,
+    defaultTitle?: string,
+    defer?: boolean,
+    encodeSpecialCharacters?: boolean,
+    htmlAttributes?: Object,
+    link?: Array<Object>,
+    meta?: Array<Object>,
+    noscript?: Array<Object>,
+    onChangeClientState?: (
+      newState?: Object,
+      addedTags?: Object,
+      removeTags?: Object
+    ) => any,
+    script?: Array<Object>,
+    style?: Array<Object>,
+    title?: string,
+    titleAttributes?: Object,
+    titleTemplate?: string,
+  }
 
-    declare interface TagMethods {
-      toString(): string;
-      toComponent(): [Element<*>] | Element<*> | Array<Object>;
-    }
+  declare interface TagMethods {
+    toString(): string;
+    toComponent(): [React$Element<*>] | React$Element<*> | Array<Object>;
+  }
 
-    declare interface StateOnServer {
-      base: TagMethods;
-      bodyAttributes: TagMethods,
-      htmlAttributes: TagMethods;
-      link: TagMethods;
-      meta: TagMethods;
-      noscript: TagMethods;
-      script: TagMethods;
-      style: TagMethods;
-      title: TagMethods;
-    }
+  declare interface StateOnServer {
+    base: TagMethods;
+    bodyAttributes: TagMethods,
+    htmlAttributes: TagMethods;
+    link: TagMethods;
+    meta: TagMethods;
+    noscript: TagMethods;
+    script: TagMethods;
+    style: TagMethods;
+    title: TagMethods;
+  }
 
-    declare class Helmet extends React$Component<Props> {
-      static rewind(): StateOnServer;
-      static renderStatic(): StateOnServer;
-      static canUseDom(canUseDOM: boolean): void;
-    }
+  declare class Helmet extends React$Component<Props> {
+    static rewind(): StateOnServer;
+    static renderStatic(): StateOnServer;
+    static canUseDom(canUseDOM: boolean): void;
+  }
 
-    declare export default typeof Helmet
-    declare export var Helmet: typeof Helmet
+  declare export default typeof Helmet
+  declare export var Helmet: typeof Helmet
 }
 
