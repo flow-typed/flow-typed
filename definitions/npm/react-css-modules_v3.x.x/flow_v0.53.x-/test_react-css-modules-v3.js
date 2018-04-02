1803cf4c4f2b5fe56b017e49084598f358ec3a63
devalon <shimokawa@devalon.biz>
2018-04-02 12:44:22 +0900

    Support stateless functional component style in `react-css-modules`

--- definitions/npm/react-css-modules_v3.x.x/flow_v0.53.x-/test_react-css-modules-v3.js
+++ definitions/npm/react-css-modules_v3.x.x/flow_v0.53.x-/test_react-css-modules-v3.js
@@ -14,12 +14,28 @@ class ExampleModule extends Component<{
   }
 }
 
+type Props = {};
+
+type State = {};
+
+const ExampleModule2 = (props: Props, state: State) => {
+  return (<div>hoge</div>)
+}
+
+const ExampleModule3 = (props: Props, state: State) => {
+  return (<div>hoge</div>)
+}
+
 const ExampleCSSModules = CSSModules(ExampleModule, styles);
 const ExampleCSSModules2 = CSSModules(ExampleModule, styles, { allowMultiple: true });
+const ExampleCSSModules3 = CSSModules(ExampleModule2, styles);
+const ExampleCSSModules4 = CSSModules(ExampleModule2, styles, { allowMultiple: true });
+const ExampleCSSModules5 = CSSModules(ExampleModule3, styles);
+const ExampleCSSModules6 = CSSModules(ExampleModule3, styles, { allowMultiple: true });
 
 // $ExpectError invalid module option.
 const BustedCSSModule = CSSModules(ExampleModule, styles, { wubbaLubba: 'dub-dub' });
-1
+
 class Failure1 extends Component<{}> {
   render() {
 
