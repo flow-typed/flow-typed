7f9c7ae9dd7553a0f6ed9479689ccae313f853ca
Aaron Black <aaron.black@1stdibs.com>
2018-04-06 14:26:03 -0400

    update puppeteer typedef and tests

--- definitions/npm/puppeteer_v1.2.x/flow_v0.58.x-/test_puppeteer_v1.2.x_connect.js
+++ definitions/npm/puppeteer_v1.2.x/flow_v0.58.x-/test_puppeteer_v1.2.x_connect.js
@@ -4,15 +4,50 @@ import { connect } from 'puppeteer';
 import { describe, it } from 'flow-typed-test';
 
 describe('connect', () => {
-  it('should return Promise<$npm$puppeteer$Browser>', () => {
-    (connect(): Promise<$npm$puppeteer$Browser>);
-  });
   it('should complain if you pass in a string arg', () => {
-    // $ExpectError
-    (connect('testing...'): Promise<$npm$puppeteer$Browser>);
+    // $ExpectError string is incompatible with `ConnectOptions`
+    connect('testing...');
   });
   it('should complain if an option arg key is of the wrong type', () => {
-    //$ExpectError
-    (connect({ ignoreHTTPSErrors: 'testing...' }): Promise<$npm$puppeteer$Browser>);
+    // $ExpectError string is incompatible with boolean in property `ignoreHTTPSErrors`
+    connect({ ignoreHTTPSErrors: 'testing...' });
+  });
+  it('should preserve Browser type', () => {
+    connect().then(browser => {
+      // $ExpectError string is incompatible with enum
+      browser.on('response');
+      browser.newPage().then(page => {
+        page.on('request', request => {
+          request.url();
+          // $ExpectError property `testing` is missing in `Request`
+          request.testing();
+        });
+      });
+      browser.on('targetcreated', target => {
+        target.createCDPSession().then(cdpSession => {
+          // $ExpectError property `connect` is missing in `CDPSession`
+          cdpSession.connect();
+          cdpSession.detach();
+        });
+      });
+    });
+  });
+  it('should preserve Page type', () => {
+    connect().then(browser =>
+      browser.newPage().then(page => {
+        // $ExpectError string is incompatible with enum in type argument `K`
+        page.on('testing...');
+        page.on('request', request => {
+          request.url();
+          // $ExpectError property `testing` is missing in `Request`
+          request.testing();
+        });
+        page.on('response', response => {
+          response.request.url();
+          // $ExpectError property `testing` is missing in `Response`
+          response.testing();
+        });
+      })
+    );
   });
 });
--- definitions/npm/puppeteer_v1.2.x/flow_v0.58.x-/test_puppeteer_v1.2.x_launch.js
+++ definitions/npm/puppeteer_v1.2.x/flow_v0.58.x-/test_puppeteer_v1.2.x_launch.js
@@ -4,15 +4,43 @@ import { launch } from 'puppeteer';
 import { describe, it } from 'flow-typed-test';
 
 describe('launch', () => {
-  it('should return Promise<$npm$puppeteer$Browser>', () => {
-    (launch(): Promise<$npm$puppeteer$Browser>);
-  });
   it('should complain if you pass in a string arg', () => {
-    // $ExpectError
-    (launch('testing...'): Promise<$npm$puppeteer$Browser>);
+    // $ExpectError string is incompatible with `LaunchOptions`
+    launch('testing...');
   });
   it('should complain if an option arg key is of the wrong type', () => {
-    // $ExpectError
-    (launch({ headless: 'testing...' }): Promise<$npm$puppeteer$Browser>);
+    // $ExpectError string is incompatible with boolean inproperty `headless`
+    launch({ headless: 'testing...' });
+  });
+  it('should preserve the Browser type', () => {
+    launch().then(browser => {
+      // $ExpectError string is incompatible with enum
+      browser.on('response');
+      browser.newPage().then(page => {
+        page.on('request', request => {
+          request.url();
+          // $ExpectError property `testing` is missing in `Request`
+          request.testing();
+        });
+      });
+    });
+  });
+  it('should preserver Page type through launch, etc.', () => {
+    launch().then(browser =>
+      browser.newPage().then(page => {
+        // $ExpectError string is incompatible with enum in type argument `K`
+        page.on('testing...');
+        page.on('request', request => {
+          request.url();
+          // $ExpectError property `testing` is missing in `Request`
+          request.testing();
+        });
+        page.on('response', response => {
+          response.request.url();
+          // $ExpectError property `testing` is missing in `Response`
+          response.testing();
+        });
+      })
+    );
   });
 });
