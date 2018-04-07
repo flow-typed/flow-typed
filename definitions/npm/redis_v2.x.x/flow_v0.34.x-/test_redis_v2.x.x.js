4be73663e5c8ed484c2f75c2fa051fb02f71d447
Christian Budde Christensen <budde377@gmail.com>
2018-04-07 17:37:14 +0200

    Update redis set and delete (#2052)
    * Add support for options on set
    * Add support for rest arguments on del

--- definitions/npm/redis_v2.x.x/flow_v0.34.x-/test_redis_v2.x.x.js
+++ definitions/npm/redis_v2.x.x/flow_v0.34.x-/test_redis_v2.x.x.js
@@ -1,8 +1,7 @@
 // @flow
-
+import { describe, it } from 'flow-typed-test';
 const redis = require("redis");
 
-const client: redis.RedisClient = redis.createClient();
 const options = {
   host: 'localhost',
   port: 6379,
@@ -11,127 +10,239 @@ const options = {
   socket_keepalive: true,
   enable_offline_queue: true,
 }
-redis.createClient(6379)
-redis.createClient(6379, options)
-redis.createClient(6379, '127.0.0.1')
-redis.createClient(6379, '127.0.0.1', options)
-redis.createClient('redis://localhost:6739')
-redis.createClient('redis://localhost:6739', options)
-redis.createClient(options)
 
-client.get('some-key', (error, value) => {
-  if (error !== null) {
-    console.error(error)
-    return;
-  }
-  console.log(value);
-});
+describe('client', () => {
+  const client: redis.RedisClient = redis.createClient();
+  describe('createClient', () => {
+    redis.createClient(6379)
+    redis.createClient(6379, options)
+    redis.createClient(6379, '127.0.0.1')
+    redis.createClient(6379, '127.0.0.1', options)
+    redis.createClient('redis://localhost:6739')
+    redis.createClient('redis://localhost:6739', options)
+    redis.createClient(options)
+  })
+  describe('get', () => {
+    client.get('some-key', (error, value) => {
+      if (error !== null) {
+        console.error(error)
+        return;
+      }
+      console.log(value);
+    });
+  })
+  describe('set', () => {
 
-client.set('some-key', 'Some value');
-client.set('some-key', 'Some value', (error: ?Error, res: 'OK') => {});
-// $ExpectError
-client.set('some-key');
-// $ExpectError
-client.set('some-key', { foo: 'bar' });
+    client.set('some-key', 'Some value');
+    client.set('some-key', 'Some value', (error: ?Error, res: 'OK') => {});
+    // $ExpectError
+    client.set('some-key');
+    // $ExpectError
+    client.set('some-key', { foo: 'bar' });
 
-client.setex('some-key', 12345, 'value');
-client.setex('some-key', 12345, 'value', (error: ?Error, res: 'OK') => {});
-// $ExpectError
-client.setx('somekey', 'value');
+    // With condition
 
-client.ttl('key', (error: ?Error, ttl: number) => {});
-// $ExpectError
-client.ttl('key', (error: ?Error, ttl: string) => {});
+    client.set('some-key', 'some value', 'NX')
 
-client.del(['key1', 'key2', 'key3']);
-client.del(['key1', 'key2', 'key3'], (error: ?Error, numRemoved: number) => {});
-// $ExpectError
-client.del(['key1', 'key2', 'key3'], (error: ?Error, numRemoved: string) => {});
+    client.set('some-key', 'some value', 'XX')
 
-client.hmset("some-key", { key1: "value1" }, err =>
-  console.log("hmset error:", err)
-);
-client.rpush("some-list", "some-value", err =>
-  console.log("rpush error:", err)
-);
+    // With expire
 
-client.lpush("key", "value", (err, newLength: number) => {
-  if (err) {
-    console.log(`lpush error: ${err.message}`);
-  }
-  if (newLength) console.log(`New length: ${newLength}`);
-});
-client.lpush("key", "value");
-// $ExpectError
-client.lpush("key");
-// $ExpectError
-client.lpush("key", { foo: 'bar' });
+    client.set('some-key', 'some value', 'EX', 1)
 
-client.lrange("key", 0, 5, (error, entries: Array<string>) => {
-  if (error !== null) {
-    console.error(error);
-    return;
-  }
-  if (entries) console.log(entries.join(','));
-});
+    client.set('some-key', 'some value', 'PX', 1000)
 
-client.llen('key', (error, length: number) => {
-  if (error !== null) {
-    console.error(error);
-    return;
-  }
-  if (length) console.log(length);
-});
+    // With condition before expire
 
-// $ExpectError
-client.mget(["key1", "key2"], (error, entries: ?Array<string>) => {})
-client.mget(["key1", "key2"], (error, entries: Array<?string>) => {
-  if (error === null) {
-    console.log('Error!');
-    return;
-  }
-  if (entries) console.log(entries.join(','));
-});
+    client.set('some-key', 'some value', 'NX', 'EX', 1)
 
-client.mset(["key1", "value1", "key2", "value"], (error: ?Error, res: 'OK') => {});
-client.mset(["key1", "value1", "key2", "value"]);
-// $ExpectError
-client.mset([
-  {
-    key: 'key1',
-    value: 'value1'
-  }
-], (error) => {
-  if (error !== null) console.error(error);
-});
+    client.set('some-key', 'some value', 'NX', 'PX', 1000)
 
-client.flushall();
-client.flushall((error: ?Error, res: 'OK') => {});
+    client.set('some-key', 'some value', 'XX', 'EX', 1)
 
-client.hgetall("key", (error: ?Error, result: {[key: string]: string}) => {})
-// $ExpectError
-client.hgetall("key", "bad extra argument in past type defs", (error: ?Error, result: {[key: string]: string}) => {})
+    client.set('some-key', 'some value', 'XX', 'PX', 1000)
 
-client.hdel('topic', 'key', (error: ?Error, numRemoved: number) => {});
-// $ExpectError
-client.hdel('topic', 'key', (error: ?Error, numRemoved: string) => {});
-// $ExpectError
-client.hdel('topic', 'key', (error: ?Error, res: string) => {});
+    // With condition after expire
 
-client.rpoplpush('source-key', 'destination-key', (error: ?Error, result: string) => {});
-// $ExpectError
-client.rpoplpush('source-key', 'destination-key', (error: ?Error, result: number) => {});
+    client.set('some-key', 'some value', 'EX', 1, 'NX')
 
-// $ExpectError
-client.hgetall("key", "bad extra argument in past type defs", (error: ?Error, result: ?{[key: string]: string}) => {})
+    client.set('some-key', 'some value', 'PX', 1000, 'NX')
+
+    client.set('some-key', 'some value', 'EX', 1, 'XX')
+
+    client.set('some-key', 'some value', 'PX', 1000, 'XX')
 
 
-client.lrem('my-key', 5, 'value', (error: ?Error, count: number) => {});
-// $ExpectError
-client.lrem('my-key', 5, (error: ?Error, count: number) => {});
-// $ExpectError
-client.lrem('my-key', 5, 'value', (error: ?Error, result: string) => {});
+    // With condition and callback
 
-client.publish('topic', 'value', (error: ?Error, numReceivers: number) => {});
-// $ExpectError
-client.publish('topic', 'value', (error: ?Error, numReceivers: string) => {});
+    client.set('some-key', 'some value', 'NX', (err: ?Error, res: ?'OK') => {})
+
+    client.set('some-key', 'some value', 'XX', (err: ?Error, res: ?'OK') => {})
+
+    // With expire and callback
+
+    client.set('some-key', 'some value', 'EX', 1, (err: ?Error, res: 'OK') => {})
+
+    client.set('some-key', 'some value', 'PX', 1000, (err: ?Error, res: 'OK') => {})
+
+    // With condition before expire and callback
+
+    client.set('some-key', 'some value', 'NX', 'EX', 1, (err: ?Error, res: ?'OK') => {})
+
+    client.set('some-key', 'some value', 'NX', 'PX', 1000, (err: ?Error, res: ?'OK') => {})
+
+    client.set('some-key', 'some value', 'XX', 'EX', 1, (err: ?Error, res: ?'OK') => {})
+
+    client.set('some-key', 'some value', 'XX', 'PX', 1000, (err: ?Error, res: ?'OK') => {})
+
+    // With condition after expire and callback
+
+    client.set('some-key', 'some value', 'EX', 1, 'NX', (err: ?Error, res: ?'OK') => {})
+
+    client.set('some-key', 'some value', 'PX', 1000, 'NX', (err: ?Error, res: ?'OK') => {})
+
+    client.set('some-key', 'some value', 'EX', 1, 'XX', (err: ?Error, res: ?'OK') => {})
+
+    client.set('some-key', 'some value', 'PX', 1000, 'XX', (err: ?Error, res: ?'OK') => {})
+
+
+
+    // $ExpectError
+    client.set('some-key', 'some value', 'NX', 'EX', 1, 'NX')
+
+    // $ExpectError
+    client.set('some-key', 'some value', 'NX', 'PX', 1000, 'NX')
+
+    // $ExpectError
+    client.set('some-key', 'some value', 'EX')
+
+    // $ExpectError
+    client.set('some-key', 'some value', 'PX')
+
+  })
+  describe('setex', () => {
+    client.setex('some-key', 12345, 'value');
+    client.setex('some-key', 12345, 'value', (error: ?Error, res: 'OK') => {});
+    // $ExpectError
+    client.setx('somekey', 'value');
+  })
+  describe('ttl', () => {
+    client.ttl('key', (error: ?Error, ttl: number) => {});
+    // $ExpectError
+    client.ttl('key', (error: ?Error, ttl: string) => {});
+  })
+  describe('del', () => {
+    client.del(['key1', 'key2', 'key3']);
+    client.del(['key1', 'key2', 'key3'], (error: ?Error, numRemoved: number) => {});
+    // $ExpectError
+    client.del(['key1', 'key2', 'key3'], (error: ?Error, numRemoved: string) => {});
+
+    client.del('key1')
+    client.del('key1', 'key2')
+    client.del('key1', 'key2', 'key3')
+
+    client.del('key1', (error: ?Error, numRemoved: number) => {})
+    client.del('key1', 'key2', (error: ?Error, numRemoved: number) => {})
+    client.del('key1', 'key2', 'key3', (error: ?Error, numRemoved: number) => {})
+  })
+  describe('hmset', () => {
+    client.hmset("some-key", { key1: "value1" }, err =>
+      console.log("hmset error:", err)
+    );
+  })
+  describe('rpush', () => {
+    client.rpush("some-list", "some-value", err =>
+      console.log("rpush error:", err)
+    );
+  })
+  describe('lpush', () => {
+    client.lpush("key", "value", (err, newLength: number) => {
+      if (err) {
+        console.log(`lpush error: ${err.message}`);
+      }
+      if (newLength) console.log(`New length: ${newLength}`);
+    });
+    client.lpush("key", "value");
+    // $ExpectError
+    client.lpush("key");
+    // $ExpectError
+    client.lpush("key", { foo: 'bar' });
+  })
+  describe('lrange', () => {
+    client.lrange("key", 0, 5, (error, entries: Array<string>) => {
+      if (error !== null) {
+        console.error(error);
+        return;
+      }
+      if (entries) console.log(entries.join(','));
+    });
+  })
+  describe('llen', () => {
+    client.llen('key', (error, length: number) => {
+      if (error !== null) {
+        console.error(error);
+        return;
+      }
+      if (length) console.log(length);
+    });
+  })
+  describe('mget', () => {
+    // $ExpectError
+    client.mget(["key1", "key2"], (error, entries: ?Array<string>) => {})
+    client.mget(["key1", "key2"], (error, entries: Array<?string>) => {
+      if (error === null) {
+        console.log('Error!');
+        return;
+      }
+      if (entries) console.log(entries.join(','));
+    });
+  })
+  describe('mset', () => {
+    client.mset(["key1", "value1", "key2", "value"], (error: ?Error, res: 'OK') => {});
+    client.mset(["key1", "value1", "key2", "value"]);
+    // $ExpectError
+    client.mset([
+      {
+        key: 'key1',
+        value: 'value1'
+      }
+    ], (error) => {
+      if (error !== null) console.error(error);
+    });
+  })
+  describe('flushall', () => {
+    client.flushall();
+    client.flushall((error: ?Error, res: 'OK') => {});
+  })
+  describe('hgetall', () => {
+    client.hgetall("key", (error: ?Error, result: {[key: string]: string}) => {})
+    // $ExpectError
+    client.hgetall("key", "bad extra argument in past type defs", (error: ?Error, result: {[key: string]: string}) => {})
+    // $ExpectError
+    client.hgetall("key", "bad extra argument in past type defs", (error: ?Error, result: ?{[key: string]: string}) => {})
+  })
+  describe('hdel', () => {
+    client.hdel('topic', 'key', (error: ?Error, numRemoved: number) => {});
+    // $ExpectError
+    client.hdel('topic', 'key', (error: ?Error, numRemoved: string) => {});
+    // $ExpectError
+    client.hdel('topic', 'key', (error: ?Error, res: string) => {});
+  })
+  describe('rpoplpush', () => {
+    client.rpoplpush('source-key', 'destination-key', (error: ?Error, result: string) => {});
+    // $ExpectError
+    client.rpoplpush('source-key', 'destination-key', (error: ?Error, result: number) => {});
+  })
+  describe('lrem', () => {
+    client.lrem('my-key', 5, 'value', (error: ?Error, count: number) => {});
+    // $ExpectError
+    client.lrem('my-key', 5, (error: ?Error, count: number) => {});
+    // $ExpectError
+    client.lrem('my-key', 5, 'value', (error: ?Error, result: string) => {});
+  })
+  describe('publish', () => {
+    client.publish('topic', 'value', (error: ?Error, numReceivers: number) => {});
+    // $ExpectError
+    client.publish('topic', 'value', (error: ?Error, numReceivers: string) => {});
+  })
+})
