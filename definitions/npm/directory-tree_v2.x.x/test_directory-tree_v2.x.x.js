967b0511fb590da144a1d045f1c63661f5c27cf4
HsuTing <hsuting0106@gmail.com>
2018-05-04 09:26:05 +0800

    test(definitions/npm/directory-tree_v2.x.x): use describe, it

--- definitions/npm/directory-tree_v2.x.x/test_directory-tree_v2.x.x.js
+++ definitions/npm/directory-tree_v2.x.x/test_directory-tree_v2.x.x.js
@@ -1,14 +1,20 @@
-// @flow
+import { describe, it } from 'flow-typed-test';
 
 import dirTree from 'directory-tree';
 
-const tree: {
-  path: string,
-  name: string,
-  size: number,
-  type: 'directory' | 'file',
-  extension?: string,
-} = dirTree(__dirname);
+describe('# directory-tree', () => {
+  it('work', () => {
+    const tree: {
+      path: string,
+      name: string,
+      size: number,
+      type: 'directory' | 'file',
+      extension?: string,
+    } = dirTree(__dirname);
+  });
 
-// $ExpectError
-const expectArray: Array<{}> = dirTree(__dirname);
+  it('type error', () => {
+    // $ExpectError
+    const expectNumber: number = dirTree(__dirname);
+  });
+});
