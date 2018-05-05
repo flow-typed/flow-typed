5eb2187a56a4fe1a27d0ff47f294d14ae7a20932
Thomas Ladd <thomas.ladd@stackpath.com>
2018-05-05 11:37:49 -0500

    Fix problems with Formik types
    * Correct FormikHandlers to match v0.11.x, rather than the 1.0.0 alpha
    * Add missing validateForm to FormikActions
    * Add some tests and use describe/it

--- definitions/npm/formik_v0.11.x/test_formik.js
+++ definitions/npm/formik_v0.11.x/test_formik.js
@@ -1,38 +1,120 @@
 // @flow
+
 import * as React from "react";
-import {
-  Formik,
-  Form,
-  Field,
-  type FieldProps,
-  type FormikActions,
-  type FormikConfig
+import { Formik, Form, Field } from "formik";
+import type {
+  FieldProps,
+  FormikErrors,
+  FormikTouched,
+  FormikActions,
+  FormikProps
 } from "formik";
+import { describe, it } from "flow-typed-test";
 
-<Formik
-  initialValues={{ foo: "hi" }}
-  onSubmit={(values: *, formikActions: FormikActions<*>) => {
-    values.foo;
-    values.bar;
-  }}
-/>;
+const i18n = { loadNamespaces: () => {} };
 
-const config1: FormikConfig = {
-  onSubmit: (values: *, formikActions: FormikActions<*>) => {}
-};
-// $ExpectError
-const configRequiresOnSubmit: FormikConfig = {
-  onSubmit: null
+type Values = {
+  text: string
 };
 
-const CustomInputComponent = (props: FieldProps) => <input />;
+describe("formik", () => {
+  describe("<Formik />", () => {
+    it("passes when used properly", () => {
+      <Formik
+        initialValues={{ text: "text" }}
+        validate={(values: Values) => {
+          const errors = {};
+          if (!values.text) {
+            errors.text = "Text Required";
+          }
+          return errors;
+        }}
+        onSubmit={(values: Values) => {}}
+        render={({
+          values,
+          errors,
+          touched,
+          handleChange,
+          handleBlur,
+          handleSubmit,
+          isSubmitting
+        }: FormikProps<Values>) => {
+          return (
+            <form onSubmit={handleSubmit}>
+              <input
+                type="text"
+                name="text"
+                onChange={(e: SyntheticInputEvent<>) => {
+                  handleChange(e);
+                }}
+                onBlur={handleBlur}
+                value={values.text}
+              />
+              {touched.text && errors.text && <div>{errors.text}</div>}
+              <button type="submit" disabled={isSubmitting}>
+                Submit
+              </button>
+            </form>
+          );
+        }}
+      />;
+    });
 
-<Form>
-  <Field type="email" name="email" placeholder="Email" />
-  <Field component="select" name="color">
-    <option value="red">Red</option>
-    <option value="green">Green</option>
-    <option value="blue">Blue</option>
-  </Field>
-  <Field component={CustomInputComponent} name="firstName" />
-</Form>;
+    it("raises error when trying to access values not in Values", () => {
+      <Formik
+        initialValues={{ text: "text" }}
+        validate={(values: Values) => {
+          const errors = {};
+          if (!values.text) {
+            errors.text = "Text Required";
+          }
+          return errors;
+        }}
+        onSubmit={(values: Values) => {}}
+        render={({
+          values,
+          errors,
+          touched,
+          handleChange,
+          handleBlur,
+          handleSubmit,
+          isSubmitting
+        }: FormikProps<Values>) => {
+          return (
+            <form onSubmit={handleSubmit}>
+              <input
+                type="text"
+                name="text"
+                onChange={handleChange}
+                onBlur={handleBlur}
+                // $ExpectError - email missing in values
+                value={values.email}
+              />
+              {/* $ExpectError - touched missing in values */}
+              {touched.email && errors.email && <div>{errors.email}</div>}
+              <button type="submit" disabled={isSubmitting}>
+                Submit
+              </button>
+            </form>
+          );
+        }}
+      />;
+    });
+  });
+
+  describe("<Form /> and <Field />", () => {
+    it("passes when used properly", () => {
+      const CustomInputComponent = (props: FieldProps) => <input />;
+
+      <Form>
+        <Field type="email" name="email" placeholder="Email" />
+        <Field component="select" name="color">
+          <option value="red">Red</option>
+          <option value="green">Green</option>
+          <option value="blue">Blue</option>
+        </Field>
+        <Field component={CustomInputComponent} name="firstName" />
+      </Form>;
+    });
+  });
+});
