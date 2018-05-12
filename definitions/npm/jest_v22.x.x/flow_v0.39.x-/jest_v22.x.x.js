5830f277001c2b2e49948758a86d4b3d3ada1b5d
Alphonse Bendt <alphonse.bendt@gmail.com>
2018-05-12 22:36:31 +0200

    support Jest Custom jQuery Matchers (#2205)
    * add support for Jest Custom jQuery Matchers
    
    * change return type to void

--- definitions/npm/jest_v22.x.x/flow_v0.39.x-/jest_v22.x.x.js
+++ definitions/npm/jest_v22.x.x/flow_v0.39.x-/jest_v22.x.x.js
@@ -153,8 +153,34 @@ type DomTestingLibraryType = {
   toHaveAttribute(name: string, expectedValue?: string): void
 };
 
+// Jest JQuery Matchers: https://github.com/unindented/custom-jquery-matchers
+type JestJQueryMatchersType = {
+  toExist(): void,
+  toHaveLength(len: number): void,
+  toHaveId(id: string): void,
+  toHaveClass(className: string): void,
+  toHaveTag(tag: string): void,
+  toHaveAttr(key: string, val?: any): void,
+  toHaveProp(key: string, val?: any): void,
+  toHaveText(text: string | RegExp): void,
+  toHaveData(key: string, val?: any): void,
+  toHaveValue(val: any): void,
+  toHaveCss(css: {[key: string]: any}): void,
+  toBeChecked(): void,
+  toBeDisabled(): void,
+  toBeEmpty(): void,
+  toBeHidden(): void,
+  toBeSelected(): void,
+  toBeVisible(): void,
+  toBeFocused(): void,
+  toBeInDom(): void,
+  toBeMatchedBy(sel: string): void,
+  toHaveDescendant(sel: string): void,
+  toHaveDescendantWithText(sel: string, text: string | RegExp): void
+};
+
 type JestExpectType = {
-  not: JestExpectType & EnzymeMatchersType & DomTestingLibraryType,
+  not: JestExpectType & EnzymeMatchersType & DomTestingLibraryType & JestJQueryMatchersType,
   /**
    * If you have a mock function, you can use .lastCalledWith to test what
    * arguments it was last called with.
@@ -615,7 +641,7 @@ type JestPrettyFormatPlugins = Array<JestPrettyFormatPlugin>;
 /** The expect function is used every time you want to test a value */
 declare var expect: {
   /** The object that you want to make assertions against */
-  (value: any): JestExpectType & JestPromiseType & EnzymeMatchersType & DomTestingLibraryType,
+  (value: any): JestExpectType & JestPromiseType & EnzymeMatchersType & DomTestingLibraryType & JestJQueryMatchersType,
   /** Add additional Jasmine matchers to Jest's roster */
   extend(matchers: { [name: string]: JestMatcher }): void,
   /** Add a module that formats application-specific data structures. */
--- definitions/npm/jest_v22.x.x/flow_v0.39.x-/test_jest-v22.x.x.js
+++ definitions/npm/jest_v22.x.x/flow_v0.39.x-/test_jest-v22.x.x.js
@@ -350,3 +350,38 @@ expect(wrapper).toMatchSelector(true);
   // $ExpectError: expected attribute value should be a string
   expect(element).toHaveAttribute('foo', 1);
 }
+
+{
+  // in reality this would be a jquery object
+  const jquery = "$(someSelector)";
+
+ {
+  expect(jquery).toExist();
+  expect(jquery).not.toExist();
+
+  expect(jquery).toHaveLength(1);
+  // $ExpectError: parameter required
+  expect(jquery).toHaveLength();
+
+  expect(jquery).toHaveId('username');
+
+  expect(jquery).toHaveClass('myclass');
+  expect(jquery).toHaveTag('div');
+  expect(jquery).toHaveAttr("attr1");
+  expect(jquery).toHaveAttr("attr1", "value1");
+
+  expect(jquery).toHaveProp("attr1");
+  expect(jquery).toHaveProp("attr1", "value1");
+
+  expect(jquery).toHaveData("attr1");
+  expect(jquery).toHaveData("attr1", "value1");
+
+  expect(jquery).toHaveText("MyText");
+  expect(jquery).toHaveText(/MyTe.t/);
+
+  expect(jquery).toHaveDescendantWithText("selector", "text");
+  expect(jquery).toHaveDescendantWithText("selector", /text/);
+
+  expect(jquery).toHaveCss({key: 'value'});
+  }
+}
