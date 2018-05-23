c599bc2100d30688d7db7346549a96941f24ecf5
Kevin Grandon <kevingrandon@yahoo.com>
2018-05-23 00:35:27 -0700

    Update material-ui import paths
    Fixes #2240

--- definitions/npm/@material-ui/core_v1.x.x/test_module_imports_core_v1.x.x.js
+++ definitions/npm/@material-ui/core_v1.x.x/test_module_imports_core_v1.x.x.js
@@ -2,51 +2,51 @@
 
 import React from "react";
 
-import AppBar from "material-ui/AppBar";
-import Avatar from "material-ui/Avatar";
-import Badge from "material-ui/Badge";
+import AppBar from "@material-ui/core/AppBar";
+import Avatar from "@material-ui/core/Avatar";
+import Badge from "@material-ui/core/Badge";
 import BottomNavigation, {
   BottomNavigationAction
-} from "material-ui/BottomNavigation";
-import Button from "material-ui/Button";
-import ButtonBase from "material-ui/ButtonBase";
+} from "@material-ui/core/BottomNavigation";
+import Button from "@material-ui/core/Button";
+import ButtonBase from "@material-ui/core/ButtonBase";
 import Card, {
   CardActions,
   CardContent,
   CardHeader,
   CardMedia
-} from "material-ui/Card";
-import Checkbox from "material-ui/Checkbox";
-import Chip from "material-ui/Chip";
-import ClickAwayListener from "material-ui/utils/ClickAwayListener";
-import CssBaseline from "material-ui/CssBaseline";
+} from "@material-ui/core/Card";
+import Checkbox from "@material-ui/core/Checkbox";
+import Chip from "@material-ui/core/Chip";
+import ClickAwayListener from "@material-ui/core/utils/ClickAwayListener";
+import CssBaseline from "@material-ui/core/CssBaseline";
 import Dialog, {
   DialogActions,
   DialogContent,
   DialogContentText,
   DialogTitle,
   withMobileDialog
-} from "material-ui/Dialog";
-import Divider from "material-ui/Divider";
-import Drawer from "material-ui/Drawer";
+} from "@material-ui/core/Dialog";
+import Divider from "@material-ui/core/Divider";
+import Drawer from "@material-ui/core/Drawer";
 import ExpansionPanel, {
   ExpansionPanelActions,
   ExpansionPanelDetails,
   ExpansionPanelSummary
-} from "material-ui/ExpansionPanel";
+} from "@material-ui/core/ExpansionPanel";
 import {
   FormControl,
   FormGroup,
   FormLabel,
   FormHelperText,
   FormControlLabel
-} from "material-ui/Form";
-import Hidden from "material-ui/Hidden";
-import Icon from "material-ui/Icon";
-import IconButton from "material-ui/IconButton";
-import Input, { InputLabel, InputAdornment } from "material-ui/Input";
-import Grid from "material-ui/Grid";
-import GridList, { GridListTile, GridListTileBar } from "material-ui/GridList";
+} from "@material-ui/core/Form";
+import Hidden from "@material-ui/core/Hidden";
+import Icon from "@material-ui/core/Icon";
+import IconButton from "@material-ui/core/IconButton";
+import Input, { InputLabel, InputAdornment } from "@material-ui/core/Input";
+import Grid from "@material-ui/core/Grid";
+import GridList, { GridListTile, GridListTileBar } from "@material-ui/core/GridList";
 
 import List, {
   ListItem,
@@ -55,35 +55,35 @@ import List, {
   ListItemSecondaryAction,
   ListItemText,
   ListSubheader
-} from "material-ui/List";
-import Menu, { MenuItem, MenuList } from "material-ui/Menu";
-import MobileStepper from "material-ui/MobileStepper";
-import Modal, { Backdrop, ModalManager } from "material-ui/Modal";
-import Paper from "material-ui/Paper";
-import Popover from "material-ui/Popover";
-import Portal from "material-ui/Portal";
-import { CircularProgress, LinearProgress } from "material-ui/Progress";
-import Radio, { RadioGroup } from "material-ui/Radio";
-import Select from "material-ui/Select";
-import Snackbar, { SnackbarContent } from "material-ui/Snackbar";
+} from "@material-ui/core/List";
+import Menu, { MenuItem, MenuList } from "@material-ui/core/Menu";
+import MobileStepper from "@material-ui/core/MobileStepper";
+import Modal, { Backdrop, ModalManager } from "@material-ui/core/Modal";
+import Paper from "@material-ui/core/Paper";
+import Popover from "@material-ui/core/Popover";
+import Portal from "@material-ui/core/Portal";
+import { CircularProgress, LinearProgress } from "@material-ui/core/Progress";
+import Radio, { RadioGroup } from "@material-ui/core/Radio";
+import Select from "@material-ui/core/Select";
+import Snackbar, { SnackbarContent } from "@material-ui/core/Snackbar";
 import Stepper, {
   Step,
   StepButton,
   StepIcon,
   StepContent,
   StepLabel
-} from "material-ui/Stepper";
+} from "@material-ui/core/Stepper";
 import {
   MuiThemeProvider,
   withStyles,
   withTheme,
   createMuiTheme,
   jssPreset
-} from "material-ui/styles";
+} from "@material-ui/core/styles";
 
-import SvgIcon from "material-ui/SvgIcon";
-import SwipeableDrawer from "material-ui/SwipeableDrawer";
-import Switch from "material-ui/Switch";
+import SvgIcon from "@material-ui/core/SvgIcon";
+import SwipeableDrawer from "@material-ui/core/SwipeableDrawer";
+import Switch from "@material-ui/core/Switch";
 import Table, {
   TableBody,
   TableCell,
@@ -92,12 +92,12 @@ import Table, {
   TablePagination,
   TableRow,
   TableSortLabel
-} from "material-ui/Table";
-import Tabs, { Tab } from "material-ui/Tabs";
-import Typography from "material-ui/Typography";
-import TextField from "material-ui/TextField";
-import Toolbar from "material-ui/Toolbar";
-import Tooltip from "material-ui/Tooltip";
-import { Slide, Grow, Fade, Collapse, Zoom } from "material-ui/transitions";
+} from "@material-ui/core/Table";
+import Tabs, { Tab } from "@material-ui/core/Tabs";
+import Typography from "@material-ui/core/Typography";
+import TextField from "@material-ui/core/TextField";
+import Toolbar from "@material-ui/core/Toolbar";
+import Tooltip from "@material-ui/core/Tooltip";
+import { Slide, Grow, Fade, Collapse, Zoom } from "@material-ui/core/transitions";
 
-import withWidth from "material-ui/utils/withWidth";
+import withWidth from "@material-ui/core/utils/withWidth";
