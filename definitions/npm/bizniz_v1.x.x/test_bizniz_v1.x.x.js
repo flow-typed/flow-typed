1f8456f19e1fc5d99f5d0b8cbd2c772e0a828206
Hendrik Ras <hendrik@Hendriks-MacBook-Pro.local>
2018-05-25 15:47:30 +0200

    fixed tests

--- definitions/npm/bizniz_v1.x.x/test_bizniz_v1.x.x.js
+++ definitions/npm/bizniz_v1.x.x/test_bizniz_v1.x.x.js
@@ -2,10 +2,17 @@
 
 import bizniz from "bizniz";
 
-(bizniz.addWeekDays(): Date);
-(bizniz.weekDaysBetween(): number);
+(bizniz.addWeekDays(new Date(0), 1): Date);
+(bizniz.subtractWeekDays(new Date(0), 1): Date);
+(bizniz.isWeekDay(new Date(0)): boolean);
+(bizniz.isWeekendDay(new Date(0)): boolean);
+(bizniz.weekDaysBetween(new Date(0), new Date(0)): number);
+(bizniz.weekendDaysBetween(new Date(0), new Date(0)): number);
+(bizniz.dateIsBefore(new Date(0), new Date(0)): boolean);
+(bizniz.daysBetween(new Date(0), new Date(0)): number);
 
 // $ExpectError
-(bizniz.weekDaysBetween(): string);
-(bizniz.addWeekDays(): number);
+(bizniz.weekDaysBetween('', 1): string);
+(bizniz.addWeekDays(new Date(0), 1): number);
+
 
