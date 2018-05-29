6259d78c6894d94362cb8c5ff1dfdf06d5ab281b
Andrej Mihajlov <and.mikhaylov@gmail.com>
2018-05-29 21:23:40 +0200

    Add opentok 2.6.2 defs

---
+++ definitions/npm/opentok_v2.6.x/flow_v0.61.x-/opentok_v2.6.x.js
@@ -0,0 +1,88 @@
+// @flow
+
+declare module 'opentok' {
+
+  // https://tokbox.com/developer/sdks/node/reference/OpenTok.html#createSession
+  declare type SessionOptions = {|
+    location?: string,
+    mediaMode?: 'routed' | 'relayed',
+    archiveMode?: 'always' | 'manual'
+  |};
+
+  // https://tokbox.com/developer/sdks/node/reference/Session.html
+  declare type Session = {
+    sessionId: string
+  };
+
+  // https://tokbox.com/developer/sdks/node/reference/OpenTok.html#generateToken
+  declare type GenerateTokenOptions = {|
+    role?: 'subscriber' | 'publisher' | 'moderator',
+    expireTime?: number,
+    data?: string,
+    initialLayoutClassList?: Array<string>
+  |};
+
+  // https://tokbox.com/developer/sdks/node/reference/OpenTok.html#getArchive
+  declare type ListArchivesOptions = {|
+    count?: number,
+    offset?: number,
+    sessionId?: string
+  |};
+
+  // https://tokbox.com/developer/sdks/node/reference/OpenTok.html#startArchive
+  declare type ArchiveOutputMode = 'composed' | 'individual';
+  declare type StartArchiveOptions = {|
+    name?: string,
+    hasAudio?: boolean,
+    hasVideo?: boolean,
+    outputMode?: ArchiveOutputMode
+  |};
+
+  // https://tokbox.com/developer/sdks/node/reference/OpenTok.html#dial
+  declare type DialOptions = {|
+    headers?: {[string]: any},
+    auth?: {
+      username: string,
+      password: string
+    },
+    secure?: boolean
+  |};
+  declare type SipInterconnect = {
+    id: string,
+    connectionId: string,
+    streamId: string
+  };
+
+  // https://tokbox.com/developer/sdks/node/reference/Archive.html
+  declare class Archive {
+    createdAt: number;
+    duration: string;
+    id: string;
+    name: string;
+    partnerId: string;
+    reason: string;
+    sessionId: string;
+    size: number;
+    status: 'available' | 'expired' | 'failed' | 'paused' | 'started' | 'stopped' | 'uploaded';
+    hasAudio: boolean;
+    hasVideo: boolean;
+    outputMode: ArchiveOutputMode;
+    url: string;
+    delete(callback: (?Error) => any): void;
+    stop(callback: (?Error, ?Archive) => any): void;
+  }
+
+  // https://tokbox.com/developer/sdks/node/reference/OpenTok.html
+  declare export default class OpenTok {
+    constructor(apiKey: string, apiSecret: string): void;
+    createSession(options: SessionOptions, callback: (error: ?Error, session: ?Session) => any): void;
+    deleteArchive(archiveId: string, callback: (error: ?Error) => any): void;
+    dial(sessionId: string, token: string, sipUri: string, options?: DialOptions): SipInterconnect;
+    forceDisconnect(sessionId: string, connectionId: string, callback: (error: ?Error) => any): void;
+    generateToken(sessionId: string, options?: GenerateTokenOptions): string;
+    getArchive(archiveId: string, callback: (?Error, ?Archive) => any): void;
+    listArchives(options: ListArchivesOptions, callback: (?Error, ?Array<Archive>) => any): void;
+    startArchive(sessionId: string, options: StartArchiveOptions, callback: (?Error, ?Archive) => any): void;
+    stopArchive(archiveId: string, callback: (?Error, ?Archive) => any): void;
+  }
+}
