e9b41928778be4654da4f47293308ab62a4bd3ad
Georges-Antoine Assi <GAntoine@users.noreply.github.com>
2018-06-03 17:02:32 -0400

    Fix cors folder name (#2319)
    * Fix cors folder name
    
    * Fixed defs and specs

--- definitions/npm/cors_v2.x.x/flow_v0.53.x/test_cors.js
+++
@@ -1,8 +0,0 @@
-// @flow
-import Cors from "cors";
-import express from "express";
-
-const api = express().use(Cors({ origin: true }));
-
-// $ExpectError
-const api2 = express().use(Cors({ origin: 1 }));
