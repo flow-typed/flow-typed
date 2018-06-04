cee07f9126ac730b87f0bf59ac9fb89f10b527e9
William Candillon <wcandillon@gmail.com>
2018-06-04 17:14:20 +0200

    💚

--- definitions/npm/@material-ui/icons_v1.x.x/test_icons.js
+++ definitions/npm/@material-ui/icons_v1.x.x/test_icons.js
@@ -1,11 +1,10 @@
 // @flow
 import {Settings} from "@material-ui/icons";
 import SettingsIcon from "@material-ui/icons/Settings";
+// $ExpectError
 import Foo from "@material-ui/icons/Foo";
 
 (
     <Settings />,
     <SettingsIcons />
 );
-// $ExpectError
-<Foo />
