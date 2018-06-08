26ccc7bc2a61da7fadd4a757fbe684ef5bc78b71
Caoilte O'Connor <caoilte@gmail.com>
2018-06-08 15:43:08 +0100

    add tests

--- definitions/npm/react-router-dom_v4.x.x/flow_v0.63.x-/test_react-router-dom.js
+++ definitions/npm/react-router-dom_v4.x.x/flow_v0.63.x-/test_react-router-dom.js
@@ -80,6 +80,19 @@ describe("react-router-dom", () => {
       </Link>;
     });
 
+    it("allows attributes of <a> element", () => {
+      <Link
+        to="/about"
+        download
+        hreflang="de"
+        ping="https://www.example.com"
+        referrerpolicy="no-referrer"
+        target="_self"
+        type="foo"
+        onClick={() => {}}
+      >About</Link>;
+    });
+
     it("raises error if passed incorrect props", () => {
       // $ExpectError - to prop is required
       <Link />;
@@ -116,6 +129,19 @@ describe("react-router-dom", () => {
       >
         About
       </NavLink>;
+    });
+
+    it("allows attributes of <a> element", () => {
+      <NavLink
+        to="/about"
+        download
+        hreflang="de"
+        ping="https://www.example.com"
+        referrerpolicy="no-referrer"
+        target="_self"
+        type="foo"
+        onClick={() => {}}
+      >About</NavLink>;
     });
 
     it("raises error if passed incorrect props", () => {
