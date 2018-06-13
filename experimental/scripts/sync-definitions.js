6213ac5a7fbac89bf76365e54311be1cec77e044
Aditya Vohra <avohra@yelp.com>
2018-06-13 14:09:13 -0700

    Copy libdefs into experiments/definitions/ with the new libdef structure (#2358)

---
+++ experimental/scripts/sync-definitions.js
@@ -0,0 +1,71 @@
+#!/usr/bin/env node
+
+const fs = require('fs');
+const glob = require('glob');
+const mkdirp = require('mkdirp');
+const path = require('path');
+const rimraf = require('rimraf');
+
+const projectRoot = path.dirname(require.resolve('../../package.json'));
+// TODO: Figure out what to do with libdefs in <ROOT>/definitions/browser.
+const srcDefinitionsRoot = path.resolve(projectRoot, './definitions/npm');
+const destDefinitionsRoot = path.resolve(projectRoot, './experimental/definitions')
+
+/**
+ * Create a structure like the following in `experimental/definitions`. This is
+ * a step towards a Yarn workspaces / Lerna setup for the flow-typed libdefs.
+ *
+ * ➜  ~/libraries/flow-typed $ tree experimental/definitions
+ * experimental/definitions
+ * ├── alasql
+ * │   └── alasql_v0.3.x
+ * │       └── flow_v0.25.x-
+ * │           └── index.js
+ * ...
+ * └── yargs
+ *    ├── yargs_v10.x.x
+ *    │   └── flow_v0.54.x-
+ *    │       └── index.js
+ * ...
+ *    └── yargs_v9.x.x
+ *        └── flow_v0.42.x-
+ *            └── index.js
+ */
+function main() {
+  // Blow away the `experiments/definitions` dir. This is mainly to account for
+  // libdefs being deleted or moved in <ROOT>/definitions/npm. TODO: Figure out
+  // a better solution for these cases for when we have monorepo things set up.
+  rimraf.sync(destDefinitionsRoot);
+
+  // Copy libdefs from <ROOT>/definitions/npm to <ROOT>/experimental/definitions/npm.
+  glob.sync('**/!(test_)*.js', { cwd: srcDefinitionsRoot }).forEach(libdef => {
+    const parts = libdef.split('/');
+
+    // TODO: Figure out how to deal with scoped libdefs!
+    if (parts[0].startsWith('@')) {
+      return;
+    }
+
+    const [libraryNameAndVersionRange, flowVersionRange] = parts;
+    const libraryName = libraryNameAndVersionRange.split('_v')[0];
+
+    // Create a dir like `experimental/definitions/yargs/yargs_v10.x.x/flow_v0.54.x-`.
+    const libDefDir = path.join(
+      destDefinitionsRoot,
+      libraryName,
+      libraryNameAndVersionRange,
+      flowVersionRange,
+    );
+
+    mkdirp.sync(libDefDir);
+
+    // Create a libdef like `yelp-flow-typed/definitions/yargs/yargs_v10.x.x/flow_v0.54.x-/index.js`.
+    fs.writeFileSync(path.join(libDefDir, 'index.js'),
+      fs.readFileSync(`${srcDefinitionsRoot}/${libdef}`).toString(),
+    );
+  });
+}
+
+if (require.main === module) {
+  main();
+}
