16a87f225a2712f8367a4cbeedb56c0a18aad379
Jakub Jirutka <jakub@jirutka.cz>
2018-06-30 23:47:23 +0200

    Add libdef for metalsmith

---
+++ definitions/npm/metalsmith_v2.x.x/flow_v0.75.x-/test_metalsmith.js
@@ -0,0 +1,58 @@
+// @flow
+import Metalsmith from 'metalsmith';
+import type { DoneCallback, File, FilesMap } from 'metalsmith';
+
+import * as fs from 'fs';
+
+const file: File = {
+  contents: new Buffer('Hello, world!'),
+  mode: '0644',
+  stats: fs.statSync('.dummy'),
+  foo: 42,
+  bar: 'allons-y!',
+};
+
+// $ExpectError
+const file: File = {
+  mode: '0644',
+};
+
+const dummyPlugin = (files: FilesMap, ms: Metalsmith, done: DoneCallback) => {
+  const dest: string = ms.destination();
+  const meta: {} = ms.metadata();
+  // $ExpectError
+  const src: number = ms.source();
+
+  const mode: ?string = files['index'].mode;
+  const extra = files['index'].extra;
+  // $ExpectError
+  const data: ?string = files['index'].contents;
+
+  done();
+};
+
+Metalsmith(__dirname)
+  .source('pages')
+  .destination('public')
+  .concurrency(3)
+  .clean(true)
+  .frontmatter(false)
+  .ignore('README.adoc')
+  .ignore(['.trash', '.tmp'])
+  // $ExpectError
+  .ignore([123])
+  .use(dummyPlugin)
+  // $ExpectError
+  .use(null)
+  .build(err => {
+    if (err) {
+      throw err;
+    } else {
+      console.log('Done!');
+    }
+  });
+
+Metalsmith(__dirname)
+  .source()
+  // $ExpectError
+  .destination('public');
