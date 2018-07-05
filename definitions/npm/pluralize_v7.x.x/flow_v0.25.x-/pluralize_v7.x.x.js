ccc52a41cccc5888bd58fe6a90a063c01ef523f3
Roman Gusev <rgusev@hwdtech.ru>
2018-07-05 15:26:51 +0600

    Update pluralize_v7.x.x.js

--- definitions/npm/pluralize_v7.x.x/flow_v0.25.x-/pluralize_v7.x.x.js
+++ definitions/npm/pluralize_v7.x.x/flow_v0.25.x-/pluralize_v7.x.x.js
@@ -3,8 +3,8 @@ declare module "pluralize" {
     (word: string, count?: number, inclusive?: boolean): string,
 
     addIrregularRule(single: string, plural: string): void,
-    addPluralRule(rule: string | RegExp, replacemant: string): void,
-    addSingularRule(rule: string | RegExp, replacemant: string): void,
+    addPluralRule(rule: string | RegExp, replacement: string): void,
+    addSingularRule(rule: string | RegExp, replacement: string): void,
     addUncountableRule(ord: string | RegExp): void,
     plural(word: string): string,
     singular(word: string): string,
