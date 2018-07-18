f11b45464183030298acab6dd56b107d3e26c54d
Jarlakxen <facundo.m.viale@gmail.com>
2018-07-18 14:43:56 -0300

    wip

---
+++ definitions/npm/newrelic_v4.x.x/flow_v0.53.x-/newrelic_v4.x.x.js
@@ -0,0 +1,71 @@
+// @flow strict
+/* eslint-disable flowtype/no-weak-types */
+
+// @see https://docs.newrelic.com/docs/agents/nodejs-agent/supported-features/nodejs-agent-api
+declare module 'newrelic' {
+
+  declare interface TransactionHandle {
+    end(callback?: () => any): void;
+    ignore(): void;
+  }
+
+  declare interface Instrument {
+    (opts: { moduleName: string, onRequire: () => void, onError?: (err: Error) => void }): void;
+    (moduleName: string, onRequire: () => void, onError?: (err: Error) => void): void;
+  }
+
+  declare type Metric = {
+    count: number;
+    total: number;
+    min: number;
+    max: number;
+    sumOfSquares: number;
+  }
+
+  declare interface NewRelicAPI {
+
+    //  Request API calls
+    setTransactionName(name: string): void;
+    setControllerName(name: string, action?: string): void;
+
+    //  Custom instrumentation API calls
+    instrument: Instrument;
+    instrumentDatastore: Instrument;
+    instrumentWebframework: Instrument;
+    instrumentMessages: Instrument;
+    startWebTransaction(url: string, handle: () => any): any;
+    startBackgroundTransaction(name: string, group?: string, handle: () => any): any;
+    startSegment<T>(name: string, record: boolean, handler: (cd: T => any) => T | Promise<T>): Promise<T>;
+    startSegment<T>(name: string, record: boolean, handler: (cd: T => any) => T | Promise<T>, callback: (error?: Error, T) => void): T;
+
+    getTransaction(): TransactionHandle;
+    endTransaction(): void;
+
+    // Custom metrics API calls
+    recordMetric(name: string, value: number | Metric): void;
+    incrementMetric(name: string, amount?: number): void;
+
+    // Custom events API calls
+    recordCustomEvent(eventType: string, attributes: { [key: string]: number | string | boolean }): void;
+
+    // Other API calls
+    addCustomAttribute(name: string, value: number | string | boolean): void;
+    addCustomAttributes(params: { [key: string]: number | string | boolean }): void;
+    getBrowserTimingHeader(): string;
+    setIgnoreTransaction(ignored: boolean): void;
+    noticeError(error: Error | string, customParameters?: { [key: string]: number | string | boolean }): void;
+    shutdown(
+      options?: {|
+        collectPendingData?: boolean,
+        timeout: number
+      |},
+      callback?: (error: ?Error, result: mixed) => void
+    ): void;
+
+    // API calls for rules
+    addNamingRule(pattern: string, name: string): void;
+    addIgnoringRule(pattern: string): void;
+  }
+
+  declare module.exports: NewRelicAPI;
+}
---
+++ definitions/npm/newrelic_v4.x.x/test_newrelic_v4.x.x.js
@@ -0,0 +1,44 @@
+// @flow
+
+import newrelic from 'newrelic';
+
+newrelic.recordMetric('test', 0)
+newrelic.recordMetric('test', {
+  count: 0,
+  total: 0,
+  min: 0,
+  max: 0,
+  sumOfSquares: 0
+})
+
+// $ExpectError
+newrelic.recordMetric('test', '')
+
+newrelic.incrementMetric('test', 1)
+newrelic.incrementMetric('test')
+
+// $ExpectError
+newrelic.incrementMetric('test', '')
+
+newrelic.addCustomAttribute('test', 10)
+newrelic.addCustomAttribute('test', true)
+newrelic.addCustomAttribute('test', 'value')
+// $ExpectError
+newrelic.addCustomAttribute('test', [])
+
+newrelic.addCustomAttributes('test', {
+  test1: 10,
+  test2: true,
+  test3: 'value'
+})
+
+// $ExpectError
+newrelic.addCustomAttributes('test', {
+  test1: 10,
+  test2: true,
+  test3: 'value',
+  test4: []
+})
+
+const seg1: Promise<string> = newrelic.startSegment('sqg', true, () => '')
+const seg1: Promise<string> = newrelic.startSegment('sqg', true, () => Promise.resolve(''))
