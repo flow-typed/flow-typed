c012918f9e4735f4badda2b5d08c845a6b8d67e5
Jarlakxen <facundo.m.viale@gmail.com>
2018-07-23 13:10:23 -0300

    initial def impl

---
+++ definitions/npm/fast-memoize_v2.x.x/flow_v0.53.x-/test_fast-memoize.js
@@ -0,0 +1,24 @@
+import { describe, it } from 'flow-typed-test';
+import memoize, { type Cache } from 'fast-memoize';
+
+describe('fast-memoize', () => {
+
+  it('should memoize', () => {
+    const fn: string => string = _ => _.trim();
+    const memoizedFn1: string => string = memoize(fn)
+    // $ExpectError
+    const memoizedFn2: number => number = memoize(fn)
+  })
+
+  it('should memoize with external cache', () => {
+    const cache: Cache<string, string> = {
+      get: (key: string) => key,
+      set: (key: string, value: V) => { },
+      has: (key: string) =>  true
+    }
+    const memoizedFn1: string => string = memoize(
+      fn,
+      { cache, serializer: () => '' }
+    )
+  })
+})
