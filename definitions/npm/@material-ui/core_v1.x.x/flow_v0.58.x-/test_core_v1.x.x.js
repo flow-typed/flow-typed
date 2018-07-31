a1c86238381e08744034d28205e03b08eb6e7bab
Eduardo Duran <eduardo.durandiaz@gmail.com>
2018-07-31 14:52:50 -0400

    @material-ui/core: update definitions of Type to Variant (#2541)

--- definitions/npm/@material-ui/core_v1.x.x/flow_v0.58.x-/test_core_v1.x.x.js
+++ definitions/npm/@material-ui/core_v1.x.x/flow_v0.58.x-/test_core_v1.x.x.js
@@ -7,6 +7,8 @@ import AppBar from "@material-ui/core/AppBar";
 // Import via index.js
 import Button from "@material-ui/core/Button";
 
+import Typography from "@material-ui/core/Typography";
+
 // $ExpectError invalid color value.
 let appBar = <AppBar color="black" />;
 
@@ -14,3 +16,7 @@ let button1 = <Button>Click me!</Button>;
 
 // $ExpectError invalid property value.
 let button2 = <Button disableRipple={3} />;
+
+// $ExpectError invalid variant
+let typography1 = <Typography variant="wrong" />
+let typography2 = <Typography variant="headline" />
