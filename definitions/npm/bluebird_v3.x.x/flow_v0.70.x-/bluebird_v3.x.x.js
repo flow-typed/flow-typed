aa6e56cc2752a4fa2c035cd6f7cc8e88b2672a33
Dustin Lam <mynameisdustinlam@gmail.com>
2018-08-09 16:16:25 -0700

    Fix Bluebird.all for flow 0.76+
    This solves an issue with stricter rules on generics introduced in flow 0.76

--- definitions/npm/bluebird_v3.x.x/flow_v0.70.x-/bluebird_v3.x.x.js
+++ definitions/npm/bluebird_v3.x.x/flow_v0.70.x-/bluebird_v3.x.x.js
@@ -54,8 +54,8 @@ declare class Bluebird$Promise<+R> extends Promise<R> {
   static Defer: Class<Bluebird$Defer>;
   static PromiseInspection: Class<Bluebird$PromiseInspection<*>>;
 
-  static all<T, Elem: $Promisable<T>>(
-    Promises: Iterable<Elem> | $Promisable<Iterable<Elem>>
+  static all<T>(
+    Promises: $Promisable<Iterable<$Promisable<T>>>
   ): Bluebird$Promise<Array<T>>;
   static props(
     input: Object | Map<*, *> | $Promisable<Object | Map<*, *>>
