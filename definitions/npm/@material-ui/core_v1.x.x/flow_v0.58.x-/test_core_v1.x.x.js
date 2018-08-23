c3391a2b78aee5dd806d9bfe9a2a665f65a5a96b
Benjamin Dobell <benjamin.dobell+github@glassechidna.com.au>
2018-08-24 04:47:48 +1000

    Added @material-ui/core Theme & refined withStyles, withTheme etc. (#2655)

--- definitions/npm/@material-ui/core_v1.x.x/flow_v0.58.x-/test_core_v1.x.x.js
+++ definitions/npm/@material-ui/core_v1.x.x/flow_v0.58.x-/test_core_v1.x.x.js
@@ -9,7 +9,12 @@ import Button from "@material-ui/core/Button";
 
 import Typography from "@material-ui/core/Typography";
 import withStyles from "@material-ui/core/styles/withStyles";
+import withTheme from "@material-ui/core/styles/withTheme";
 import CircularProgress from "@material-ui/core/CircularProgress";
+
+import { createMuiTheme } from "@material-ui/core";
+
+import type { Theme } from "@material-ui/core";
 
 // $ExpectError invalid color value.
 let appBar = <AppBar color="black" />;
@@ -23,41 +28,197 @@ let button2 = <Button disableRipple={3} />;
 let typography1 = <Typography variant="wrong" />;
 let typography2 = <Typography variant="headline" />;
 
-// withStyles
+// withStyles - required prop
 class TestComponent extends React.Component<{
   classes: {},
+  innerRef: string,
   requiredProp: string
+}> {}
+const StyledTestComponent = withStyles({})(TestComponent);
+
+function renderStyledTestComponentWithError () {
+  return (
+    // $ExpectError is missing required prop
+    <StyledTestComponent />
+  )
+}
+
+function renderStyledTestComponent () {
+  return (
+    // doesn't require the props "classes" or "innerRef", but does require "requiredProp"
+    <StyledTestComponent requiredProp='test' />
+  )
+}
+
+// withStyles x2 HOC
+const DoubleStyledComponent = withStyles({})(StyledTestComponent);
+function renderDoubleStyledComponent () {
+  return (
+    // doesn't require the props "classes" or "innerRef", but still requires "requiredProp"
+    <DoubleStyledComponent requiredProp='test' />
+  )
+}
+
+// withStyles + withTheme
+class TestComponentWithTheme extends React.Component<{
+  classes: {},
+  theme: {},
+  innerRef: string,
+}> {}
+
+const StyledWithThemeTestComponent = withTheme()(withStyles({})(TestComponent));
+
+function renderStyledWithThemeTestComponent () {
+  return (
+    // doesn't require "classes", "theme" or "innerRef"
+    <StyledWithThemeTestComponent />
+  )
+}
+
+// withStyles - default prop
+class TestComponentWithDefaultProps extends React.Component<{
+  classes: {},
+  innerRef: string,
+  value: string,
 }> {
   static defaultProps = {
     value: 'value'
   }
 }
-const CustomTestComponent = withStyles({})(TestComponent);
 
-function renderTestComponentWithError () {
+const DefaultPropsStyledComponent = withStyles({})(TestComponentWithDefaultProps);
+
+function renderTestComponentWithDefaultProps () {
   return (
-    // $ExpectError is missing 'requiredProp'
-    <CustomTestComponent />
+    // doesn't require the prop "value"
+    <DefaultPropsStyledComponent />
   )
 }
 
-function renderTestComponent () {
+function renderTestComponentWithSpecifiedDefaultProps () {
   return (
-    // doesn't require the prop 'classes' nor default prop 'value'
-    <CustomTestComponent requiredProp='test' />
+    // allows us to specify a different value for the (otherwise default) prop "value"
+    <DefaultPropsStyledComponent value='anotherValue'/>
   )
 }
 
-const DoubleStyledComponent = withStyles({})(CustomTestComponent);
-
-function renderDoubleStyledComponent () {
-  return (
-    // doesn't require the prop 'classes' nor default prop 'value'
-    <DoubleStyledComponent requiredProp='test' />
-  )
-}
-
+// test CircularProgress props "color" and "variant"
 let circularProgress1 = <CircularProgress color="secondary" variant="static" />;
 
-// $ExpectError invalid color value.
+// $ExpectError "black" is not a valid value for the prop "color"
 let circularProgress2 = <CircularProgress color="black" />;
+
+// test Theme, Breakpoints and Spacing definitions
+const useExistingThemeProps = (theme: Theme) => ({
+  root: {
+    margin: `${theme.spacing.unit}px auto`,
+
+    [theme.breakpoints.up('md')]: {
+      margin: `${2 * theme.spacing.unit}px auto`,
+    }
+  }
+});
+
+// test that our Theme definition forbids access to non-existent properties
+const useNonExistentThemeProps = (theme: Theme) => ({
+  root: {
+    // $ExpectError no property 'wigglywoo' in Theme
+    margin: `${theme.wigglywoo}px auto`,
+  }
+});
+
+// withStyles test valid options
+const withStylesValidOptions = () => (
+  withStyles({}, {
+    classNamePrefix: 'somePrefix', // Option inherited from the cssinjs/jss project
+    flip: true, // material-ui specific options
+  })(TestComponent)
+)
+
+// withStyles test invalid options
+const withStylesInvalidOptions = () => (
+  // $ExpectError no property 'wigglywoo' in WithStylesOptions
+  withStyles({}, {
+    wigglywoo: 42,
+  })(TestComponent)
+)
+
+// createMuiTheme test valid options - custom breakpoints and palette
+const customTheme = () => (
+  createMuiTheme({
+    breakpoints: {
+      keys: ['sm', 'md', 'lg'],
+      values: {
+        sm: 0,
+        md: 800,
+        lg: 1200
+      }
+    },
+    palette: {
+      type: 'dark',
+      primary: {main: '#0707f8'},
+      tonalOffset: 0.15,
+    }
+  })
+)
+
+// createMuiTheme test theme custom variables
+// https://material-ui.com/customization/themes/#custom-variables
+const customThemeVariables = () => (
+  createMuiTheme({
+    status: {
+      danger: 'someValue'
+    }
+  })
+)
+
+// createMuiTheme test adding custom palette colors and augmenting them
+const customPaletteColors = () => {
+  const theme = createMuiTheme({
+    palette: {
+      ourCustomColor: {
+        main: '#ce1100',
+      }
+    }
+  });
+
+  theme.palette.augmentColor(theme.palette.ourCustomColor);
+}
+
+// createMuiTheme test invalid palette color
+const invalidPaletteColors = () => (
+  createMuiTheme({
+    palette: {
+      // $ExpectError no property 'realdark' on SimplePaletteColorOptions
+      primary: {
+        main: '#cecece',
+        realDark: '#000000',
+      }
+    }
+  })
+)
+
+// createMuiTheme test invalid nested options
+const createMuiCustomThemeInvalidNestedOptions = () => (
+  createMuiTheme({
+    // $ExpectError no property 'wigglywoo' in BreakpointsOptions
+    breakpoints: {
+      wigglywoo: 42,
+    },
+    // $ExpectError no property 'wigglywoo' in MixinsOptions
+    mixins: {
+      wigglywoo: 42,
+    },
+    palette: {
+      // $ExpectError no property 'realdark' on SimplePaletteColorOptions
+      primary: {
+        main: '#cecece',
+        realDark: '#000000',
+      }
+    },
+    // $ExpectError no property 'wigglywoo' in SpacingOptions
+    spacing: {
+      wigglywoo: 42,
+    },
+  })
+)
