75e1f46da87fe037a0396dc60ad152d4f5e150ae
Callum Locke <callum.locke@thetrainline.com>
2018-08-23 14:19:45 +0100

    Make query-string definition more accurate

--- definitions/npm/query-string_v6.x.x/flow_v0.32.x-/query-string_v6.x.x.js
+++ definitions/npm/query-string_v6.x.x/flow_v0.32.x-/query-string_v6.x.x.js
@@ -11,10 +11,14 @@ declare module 'query-string' {
     sort?: false | <A, B>(A, B) => number,
   |}
 
+  declare type QueryParameters = {
+    [string]: string | Array<string> | null
+  }
+
   declare module.exports: {
     extract(str: string): string,
-    parse(str: string, opts?: ParseOptions): Object,
-    parseUrl(str: string, opts?: ParseOptions): { url: string, query: Object },
-    stringify(obj: Object, opts?: StringifyOptions): string,
+    parse(str: string, opts?: ParseOptions): QueryParameters,
+    parseUrl(str: string, opts?: ParseOptions): { url: string, query: QueryParameters },
+    stringify(obj: QueryParameters, opts?: StringifyOptions): string,
   }
 }
