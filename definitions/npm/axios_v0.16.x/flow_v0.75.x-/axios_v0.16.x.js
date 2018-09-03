dff05eeadfb9d3e8ab3b2a35af209ba299522c23
Dale Hui <dhui@users.noreply.github.com>
2018-09-02 20:47:13 -0700

    axios split progress config option into onUploadProgress and onDownloadProgress in v0.14 https://github.com/axios/axios/blob/master/UPGRADE_GUIDE.md#progress-config-option https://github.com/axios/axios/blob/master/CHANGELOG.md#0140-aug-27-2016

--- definitions/npm/axios_v0.16.x/flow_v0.75.x-/axios_v0.16.x.js
+++ definitions/npm/axios_v0.16.x/flow_v0.75.x-/axios_v0.16.x.js
@@ -36,7 +36,8 @@ declare module "axios" {
     maxRedirects?: number;
     params?: Object;
     paramsSerializer?: (params: Object) => string;
-    progress?: (progressEvent: Event) => void | mixed;
+    onUploadProgress?: (progressEvent: ProgressEvent) => void | mixed;
+    onDownloadProgress?: (progressEvent: ProgressEvent) => void | mixed;
     proxy?: ProxyConfig | false;
     responseType?:
       | "arraybuffer"
