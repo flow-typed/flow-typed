74183344bd8de93e967e4cf44e3b878c2cd441a4
Andrew Haines <andrew@haines.org.nz>
2018-09-04 10:15:17 +0100

    Add failing test case for overload of js-joda LocalDate.atTime

--- definitions/npm/js-joda_v1.8.x/flow_v0.25.x-/test_js-joda_v1.8.x.js
+++ definitions/npm/js-joda_v1.8.x/flow_v0.25.x-/test_js-joda_v1.8.x.js
@@ -7,6 +7,8 @@ const oneSecond: Duration = joda.Duration.ofSeconds(1);
 const timeNow: LocalTime = joda.LocalTime.now();
 const dateNow: LocalDate = joda.LocalDate.now();
 const dateTimeNow: LocalDateTime = joda.LocalDateTime.now();
+const endOfDay: LocalDateTime = joda.LocalDate.now().atTime(joda.LocalTime.MAX);
+const midday: LocalDateTime = joda.LocalDate.now().atTime(12, 0);
 
 // $ExpectError
 const notADate: Date = joda.LocalDateTime.now();
