b45227939d70c317237d9db91d10006734c7d7a2
Ramses <ramses@ramsesz.be>
2018-09-25 23:44:46 +0200

    reformat with prettier

--- definitions/npm/antd_v3.x.x/flow_v0.25.x-/antd_v3.x.x.js
+++ definitions/npm/antd_v3.x.x/flow_v0.25.x-/antd_v3.x.x.js
@@ -1,32 +1,44 @@
-import {Component, Node} from 'react';
+import { Component, Node } from "react";
 
-declare module 'antd' {
-
+declare module "antd" {
   declare type ModalReference = {
     destroy: () => void
-  }
+  };
 
   declare function messageFn<TReturn>(
     content: Node,
     duration?: number,
     onClose?: () => mixed
-  ): TReturn
+  ): TReturn;
 
-  declare type modalFnArguments = { title?: string, content?: Node, onOk?: () => mixed };
+  declare type modalFnArguments = {
+    title?: string,
+    content?: Node,
+    onOk?: () => mixed
+  };
 
   declare function modalFn(args: modalFnArguments): ModalReference;
 
   declare export class Alert extends Component<{}> {}
+
   declare export class Avatar extends Component<{}> {}
+
   declare export class AutoComplete extends Component<{}> {}
+
   declare export class Badge extends Component<{}> {}
+
   declare export class Button extends Component<{}> {}
+
   declare export class Card extends Component<{}> {}
+
   declare export class Checkbox extends Component<{}> {}
+
   declare export class Divider extends Component<{}> {}
+
   declare export class Col extends Component<{}> {}
 
   declare export type DatePickerProps = {};
+
   declare export class DatePicker extends Component<DatePickerProps> {
     static RangePicker: typeof DatePicker$RangePicker;
   }
@@ -35,7 +47,7 @@ declare module 'antd' {
 
   declare export class Grid extends Component<{
     className?: string,
-    style?: $Shape<CSSStyleDeclaration>,
+    style?: $Shape<CSSStyleDeclaration>
   }> {}
 
   declare export class Meta extends Component<{
@@ -43,7 +55,7 @@ declare module 'antd' {
     className?: string,
     description?: Node,
     style?: $Shape<CSSStyleDeclaration>,
-    title?: Node,
+    title?: Node
   }> {}
 
   declare export class Card extends Component<{
@@ -59,8 +71,8 @@ declare module 'antd' {
     loading?: boolean,
     tabList?: Array<{ key: string, tab: Node }>,
     title?: string | Node,
-    type?: 'inner',
-    onTabChange?: (key: string) => void,
+    type?: "inner",
+    onTabChange?: (key: string) => void
   }> {
     static Grid: typeof Grid;
     static Meta: typeof Meta;
@@ -70,7 +82,7 @@ declare module 'antd' {
     value: string,
     label: string,
     children?: CascaderOption[]
-  }
+  };
 
   declare export type CascaderProps = {
     allowClear?: boolean,
@@ -78,18 +90,18 @@ declare module 'antd' {
     changeOnSelect?: boolean,
     className?: string,
     disabled?: boolean,
-    expandTrigger?: 'click' | 'hover',
+    expandTrigger?: "click" | "hover",
     notFoundContent?: string,
     onChange?: (values: string[]) => mixed,
     options: CascaderOption[],
     placeholder?: string,
     popupClassName?: string,
-    popupPlacement?: 'bottomLeft' | 'bottomRight' | 'topLeft' | 'topRight',
+    popupPlacement?: "bottomLeft" | "bottomRight" | "topLeft" | "topRight",
     popupVisible?: boolean,
     showSearch?: boolean,
-    size?: 'large' | 'default' | 'small',
+    size?: "large" | "default" | "small",
     value?: string[]
-  }
+  };
 
   declare export class Cascader extends Component<CascaderProps> {}
 
@@ -97,14 +109,14 @@ declare module 'antd' {
 
   declare export class Col extends Component<{}> {}
 
-  declare export type CollapsePanelProps = {}
+  declare export type CollapsePanelProps = {};
 
   declare class CollapsePanel extends Component<CollapsePanelProps> {}
 
-  declare export type CollapseProps = {}
+  declare export type CollapseProps = {};
 
   declare export class Collapse extends Component<CollapseProps> {
-    static Panel: typeof CollapsePanel
+    static Panel: typeof CollapsePanel;
   }
 
   declare export class DatePicker extends Component<{}> {}
@@ -121,8 +133,14 @@ declare module 'antd' {
     required?: boolean,
     transform?: (value: mixed) => mixed,
     type?: string,
-    validator?: (rule: mixed, value: mixed, callback: mixed, source?: mixed, options?: mixed) => mixed,
-    whitespace?: boolean,
+    validator?: (
+      rule: mixed,
+      value: mixed,
+      callback: mixed,
+      source?: mixed,
+      options?: mixed
+    ) => mixed,
+    whitespace?: boolean
   };
   declare type ValidateCallback = (erros: mixed, values: mixed) => void;
   declare type GetFieldDecoratorOptions = {
@@ -134,13 +152,13 @@ declare module 'antd' {
     trigger?: string,
     validateFirst?: boolean,
     validateTrigger?: string | string[],
-    valuePropName?: string,
+    valuePropName?: string
   };
 
   declare type WrappedFormUtils = {
     getFieldDecorator(
       id: string,
-      options?: GetFieldDecoratorOptions,
+      options?: GetFieldDecoratorOptions
     ): (node: Node) => Node,
     getFieldError(name: string): mixed[],
     getFieldsError(names?: Array<string>): mixed,
@@ -153,38 +171,48 @@ declare module 'antd' {
     setFields(obj: Object): void,
     setFieldsValue(obj: Object): void,
     validateFields(callback: ValidateCallback): mixed,
-    validateFields(fieldNames: Array<string>, callback: ValidateCallback): mixed,
-    validateFields(fieldNames: Array<string>, options: Object, callback: ValidateCallback): mixed,
+    validateFields(
+      fieldNames: Array<string>,
+      callback: ValidateCallback
+    ): mixed,
+    validateFields(
+      fieldNames: Array<string>,
+      options: Object,
+      callback: ValidateCallback
+    ): mixed,
     validateFields(options: Object, callback: ValidateCallback): mixed,
     validateFieldsAndScroll(
       fieldNames?: Array<string>,
       options?: Object,
-      callback?: ValidateCallback,
+      callback?: ValidateCallback
     ): void,
     validateFieldsAndScroll(callback?: ValidateCallback): void,
-    validateFieldsAndScroll(fieldNames?: Array<string>, callback?: ValidateCallback): void,
-    validateFieldsAndScroll(options?: Object, callback?: ValidateCallback): void,
+    validateFieldsAndScroll(
+      fieldNames?: Array<string>,
+      callback?: ValidateCallback
+    ): void,
+    validateFieldsAndScroll(options?: Object, callback?: ValidateCallback): void
   };
 
   declare export type FormProps = {
-    className?: string;
-    form?: WrappedFormUtils;
-    hideRequiredMark?: boolean;
+    className?: string,
+    form?: WrappedFormUtils,
+    hideRequiredMark?: boolean,
     horizontal?: boolean,
-    inline?: boolean;
-    layout?: 'horizontal' | 'inline' | 'vertical';
-    onSubmit?: (e: SyntheticEvent<HTMLFormElement>) => void;
-    prefixCls?: string;
-    style?: Object;
-    vertical?: boolean;
-  }
+    inline?: boolean,
+    layout?: "horizontal" | "inline" | "vertical",
+    onSubmit?: (e: SyntheticEvent<HTMLFormElement>) => void,
+    prefixCls?: string,
+    style?: Object,
+    vertical?: boolean
+  };
 
   declare export type FormCreateOption<T> = {
-    onFieldsChange?: (props: T, fields: Array<mixed>) => void;
-    onValuesChange?: (props: T, values: mixed) => void;
-    mapPropsToFields?: (props: T) => void;
-    withRef?: boolean;
-  }
+    onFieldsChange?: (props: T, fields: Array<mixed>) => void,
+    onValuesChange?: (props: T, values: mixed) => void,
+    mapPropsToFields?: (props: T) => void,
+    withRef?: boolean
+  };
 
   declare export class Form extends Component<FormProps> {
     static Item: typeof FormItem;
@@ -194,27 +222,33 @@ declare module 'antd' {
   declare export type FormItemProps = {
     help?: Node,
     extra?: Node,
-    validateStatus?: 'success' | 'warning' | 'error' | 'validating' | ''
-  }
+    validateStatus?: "success" | "warning" | "error" | "validating" | ""
+  };
+
   declare class FormItem extends Component<FormItemProps> {}
 
   declare export type RangePickerProps = {};
-  declare export class DatePicker$RangePicker extends Component<RangePickerProps> {}
+
+  declare export class DatePicker$RangePicker extends Component<
+    RangePickerProps
+  > {}
 
   declare export class Icon extends Component<{}> {}
 
   declare export class InputNumber extends Component<{}> {}
+
   declare type InputProps = {
     onBlur?: (event: SyntheticFocusEvent<HTMLInputElement>) => mixed,
-    onChange?: (event: SyntheticKeyboardEvent<HTMLInputElement>) => mixed,
-  }
+    onChange?: (event: SyntheticKeyboardEvent<HTMLInputElement>) => mixed
+  };
+
   declare export class Input extends Component<InputProps> {
     static Search: typeof InputSearch;
     static TextArea: typeof InputTextArea;
   }
 
   declare class InputSearch extends Component<{}> {
-    input: { input: HTMLInputElement }
+    input: { input: HTMLInputElement };
   }
 
   declare class InputTextArea extends Component<{}> {}
@@ -232,23 +266,23 @@ declare module 'antd' {
   declare class LayoutSider extends Component<{}> {}
 
   declare export type ListItemProps = {};
+
   declare export class ListItem extends Component<ListItemProps> {}
+
   declare export class List extends Component<{}> {
     static Item: typeof ListItem;
   }
-
-
 
   declare export class LocaleProvider extends Component<{}> {}
 
   declare export type MenuProps = {
     onClick?: ({
-                 domEvent: SyntheticMouseEvent<HTMLElement>,
-                 item: Component<MenuItem>,
-                 key: string,
-                 keyPath: string[],
-               }) => mixed
-  }
+      domEvent: SyntheticMouseEvent<HTMLElement>,
+      item: Component<MenuItem>,
+      key: string,
+      keyPath: string[]
+    }) => mixed
+  };
 
   declare export class Menu extends Component<MenuProps> {
     static Item: typeof MenuItem;
@@ -261,22 +295,22 @@ declare module 'antd' {
 
   declare export class message {
     static config({
-                    duration?: number,
-                    getContainer?: () => HTMLElement,
-                    top?: number
-                  }): void;
+      duration?: number,
+      getContainer?: () => HTMLElement,
+      top?: number
+    }): void;
     static success: messageFn<mixed>;
-    static error:   messageFn<mixed>;
-    static info:    messageFn<mixed>;
+    static error: messageFn<mixed>;
+    static info: messageFn<mixed>;
     static warning: messageFn<mixed>;
-    static warn:    messageFn<mixed>;
+    static warn: messageFn<mixed>;
     static loading: messageFn<() => void>;
   }
 
   declare export class Modal extends Component<{}> {
-    static info:    typeof modalFn;
+    static info: typeof modalFn;
     static success: typeof modalFn;
-    static error:   typeof modalFn;
+    static error: typeof modalFn;
     static warning: typeof modalFn;
     static confirm: typeof modalFn;
   }
@@ -295,12 +329,12 @@ declare module 'antd' {
   declare export class Row extends Component<{}> {}
 
   declare export type SelectProps = {
-    expandTrigger?: 'click' | 'hover',
+    expandTrigger?: "click" | "hover",
     filterOption?: (input: string, option: React$Element<mixed>) => boolean,
     onFocus?: () => mixed,
     popupVisible?: boolean,
-    showSearch?: boolean,
-  }
+    showSearch?: boolean
+  };
 
   declare export class Select extends Component<SelectProps> {
     static Option: typeof SelectOption;
@@ -312,37 +346,37 @@ declare module 'antd' {
 
   declare export type SpinProps = {
     delay?: number,
-    size?: 'small' | 'default' | 'large',
+    size?: "small" | "default" | "large",
     spinning?: boolean,
-    tip?: string,
-  }
+    tip?: string
+  };
 
   declare export class Spin extends Component<SpinProps> {}
 
   declare export class Step extends Component<{
     description?: string | Node,
     icon?: string | Node,
-    status?: 'wait' | 'process' | 'finish' | 'error',
-    title?: string | Node,
+    status?: "wait" | "process" | "finish" | "error",
+    title?: string | Node
   }> {}
 
   declare export class Steps extends Component<{
     current?: number,
-    direction?: 'horizontal' | 'vertical',
-    labelPlacement?: 'horizontal' | 'vertical',
+    direction?: "horizontal" | "vertical",
+    labelPlacement?: "horizontal" | "vertical",
     progressDot?:
       | boolean
       | ((
-      iconDot: Node,
-      {
-        index: number,
-        status: 'wait' | 'process' | 'finish' | 'error',
-        title: string | Node,
-        description: string | Node,
-      }
-    ) => Node),
-    size?: 'default' | 'small',
-    status?: 'wait' | 'process' | 'finish' | 'error',
+          iconDot: Node,
+          {
+            index: number,
+            status: "wait" | "process" | "finish" | "error",
+            title: string | Node,
+            description: string | Node
+          }
+        ) => Node),
+    size?: "default" | "small",
+    status?: "wait" | "process" | "finish" | "error"
   }> {
     static Step: typeof Step;
   }
@@ -361,12 +395,12 @@ declare module 'antd' {
 
   declare export type TooltipProps = {
     title: string
-  }
+  };
 
   declare export class Tooltip extends Component<TooltipProps> {}
 
   declare export class TreeSelect extends Component<{}> {
-    static TreeNode: typeof TreeSelectTreeNode
+    static TreeNode: typeof TreeSelectTreeNode;
   }
 
   declare export class TreeSelectTreeNode extends Component<{}> {}
