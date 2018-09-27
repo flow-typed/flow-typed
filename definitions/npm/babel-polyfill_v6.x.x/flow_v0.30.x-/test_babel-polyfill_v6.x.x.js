65293a83531d6583f02528d09838071f83f37531
Ville Saukkonen <ville@smartly.io>
2018-09-27 18:07:49 +0300

    fail test

--- definitions/npm/babel-polyfill_v6.x.x/flow_v0.30.x-/test_babel-polyfill_v6.x.x.js
+++ definitions/npm/babel-polyfill_v6.x.x/flow_v0.30.x-/test_babel-polyfill_v6.x.x.js
@@ -4,5 +4,6 @@ import 'babel-polyfill';
 // This should fail but couldn't make it to do so
 import defaultValue from 'babel-polyfill';
 
-// $ExpectError does not export anything
 import { namedExport } from 'babel-polyfill';
+
+const test: number = '123'
