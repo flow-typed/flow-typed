a58555d8cf1b5403a0e5b04a413bf8320462c4ae
Roman Gusev <rgusev@hwdtech.ru>
2018-10-02 11:41:22 +0600

    Update test_smoothscroll_v0.4.x.js

--- definitions/npm/smoothscroll_v0.4.x/test_smoothscroll_v0.4.x.js
+++ definitions/npm/smoothscroll_v0.4.x/test_smoothscroll_v0.4.x.js
@@ -1,32 +1,25 @@
 // @flow
 
-import smoothScroll from 'smoothscroll';
+import smoothScroll from "smoothscroll";
 
-const target = document.createElement('div');
-const context = document.createElement('div');
+const target = document.createElement("div");
+const context = document.createElement("div");
 const callback = (x: number) => x;
-
-declare type smoothScroll = (
-    target: Target,
-    duration?: ?number,
-    callback?: ?(Target => any),
-    context?: ?Element
-) => void;
 
 smoothScroll(target);
 smoothScroll(0);
 smoothScroll(123);
 
 // $ExpectError
-smoothScroll('div');
+smoothScroll("div");
 // $ExpectError
-smoothScroll('.target');
+smoothScroll(".target");
 
 smoothScroll(target, 123);
 smoothScroll(123, 456);
 
 // $ExpectError
-smoothScroll(target, 'string');
+smoothScroll(target, "string");
 
 smoothScroll(target, 123, callback);
 smoothScroll(target, null, callback);
@@ -42,4 +35,4 @@ smoothScroll(target, 123, null, context);
 smoothScroll(target, 123, callback, context);
 
 // $ExpectError
-smoothScroll(target, null, null, '.context');
+smoothScroll(target, null, null, ".context");
