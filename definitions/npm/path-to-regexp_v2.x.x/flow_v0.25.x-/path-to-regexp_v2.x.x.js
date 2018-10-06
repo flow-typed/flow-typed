51d12ce2c514d11a3416ff1833bb1d39e16a4bc0
Dale Hui <dhui@users.noreply.github.com>
2018-10-06 03:16:41 -0700

    Add libdef for path-to-regexp (#2787)

---
+++ definitions/npm/path-to-regexp_v2.x.x/flow_v0.25.x-/path-to-regexp_v2.x.x.js
@@ -0,0 +1,48 @@
+declare module "path-to-regexp" {
+  declare export type Key = {
+    name: string | number;
+    prefix: string;
+    delimiter: string;
+    optional: boolean;
+    repeat: boolean;
+    pattern: string;
+    partial: boolean;
+  }
+
+  declare export type RegExpOptions = {
+    sensitive?: boolean;
+    strict?: boolean;
+    end?: boolean;
+    start?: boolean;
+    delimiter?: string;
+    endsWith?: string | string[];
+  }
+
+  declare export type ParseOptions = {
+    delimiter?: string;
+    delimiters?: string | string[];
+  }
+
+  declare type PathFunctionOptions = {
+    encode?: (value: string, token: Key) => string;
+  }
+
+  declare export type Token = string | Key;
+
+  declare export type PathFunction = (
+    data?: {},
+    options?: PathFunctionOptions
+  ) => string;
+
+  declare export var parse: (path: string, options?: ParseOptions) => Token[];
+
+  declare export var compile: (path: string, options?: {}) => PathFunction;
+
+  declare export var tokensToFunction: (tokens: Token[]) => PathFunction;
+
+  declare export var tokensToRegExp: (
+    tokens: Token[],
+    keys?: Key[],
+    options?: RegExpOptions
+  ) => RegExp;
+}
