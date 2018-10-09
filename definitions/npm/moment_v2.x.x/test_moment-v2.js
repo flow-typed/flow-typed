57ffe96a06b75928b6adf6d01c32ea314b7f33f2
Vesa Laakso <laakso.vesa@gmail.com>
2018-10-09 16:14:22 +0300

    Merge two competing moment type declarations (#2783)
    * Run moment type definitions through prettier
    
    This helps a _lot_ in figuring out the differences between these two
    type declarations.
    
    * Move moment types so they match each other a bit more
    
    This generates a more clear diff when comparing these two files, as the
    positions in the files is similar
    
    * Allow moment_v2.x.x to be ran with flow v0.25.x and above
    
    * Merge toISOString type and tests to moment_v2.x.x
    
    The boolean value had been added in version 2.20.0:
    https://momentjs.com/docs/#/displaying/as-iso-string/
    
    * Copy moment.duration().asWeeks() and weeks() types
    
    These were added only to the v2.3.x version a few months ago in this PR:
    https://github.com/flow-typed/flow-typed/pull/2476
    
    * Reconcile weekday(), isoWeekday() and dayOfYear() type differences
    
    weekday() does _not_ accept string arguments: https://momentjs.com/docs/#/get-set/weekday/
    This was incorrect in the original PR: https://github.com/flow-typed/flow-typed/pull/2427
    
    However the isoWeekday() does accept string arg: https://momentjs.com/docs/#/get-set/iso-weekday/
    and that was correct in the original PR: https://github.com/flow-typed/flow-typed/pull/2427
    
    The dayOfYear() does not accept string args, but was likely copy-pasted
    when the types were first introduced. https://momentjs.com/docs/#/get-set/day-of-year/
    
    * Add new tests to moment_v2.x.x from moment_v2.3.x
    
    These tests were added back here: https://github.com/flow-typed/flow-typed/pull/1257
    
    * Remove invalid moment().parseZone() type
    
    The parseZone method is a static method, not an instance method:
    https://momentjs.com/docs/#/parsing/parse-zone/
    
    * Merge moment.parseZone() types to v2.x.x
    
    This is the original reason of the type fork, and the cause of much
    confusion: https://github.com/flow-typed/flow-typed/pull/1242
    
    * Add various kinds of moment() constructor tests
    
    * Add more error case tests for moment constructor
    
    These tests should break as the current state of v2.x.x types are too
    lax. The types for v2.3.x are stricter
    
    * Merge more strict moment() constructor type handling to v2.x.x types
    
    * Remove obsolete moment_v2.3.x specific types

--- definitions/npm/moment_v2.x.x/test_moment-v2.js
+++ definitions/npm/moment_v2.x.x/test_moment-v2.js
@@ -1,20 +1,71 @@
 // @flow
 import moment from "moment";
+import { describe, it } from 'flow-typed-test';
 
 // Parse
 const m3: moment = moment([123, 123]);
+moment();
+moment('2015-01-01');
+moment("12-25-1995", "MM-DD-YYYY");
+moment('It is 2012-05-25', 'YYYY-MM-DD', true);
+// https://momentjs.com/docs/#/parsing/string-formats/
+moment("12-25-1995", ["MM-DD-YYYY", "YYYY-MM-DD"]);
+moment("29-06-1995", ["MM-DD-YYYY", "DD-MM-YYYY"], 'fr');
+moment("29-06-1995", ["MM-DD-YYYY", "DD-MM-YYYY"], true);
+moment("05-06-1995", ["MM-DD-YYYY", "DD-MM-YYYY"], 'fr', true);
+// $ExpectError
+moment("05-06-1995", ["MM-DD-YYYY", "DD-MM-YYYY"], 'fr', 1);
+// https://momentjs.com/docs/#/parsing/object/
+moment({ hour: 15, minute: 10 });
+moment([2015, 0]); // This would equal 2015-01-01
+// $ExpectError only string values can have formatting parameters
+moment([2015, 0], "MM-DD-YYYY");
+// $ExpectError
+moment([2015, 0], "MM-DD-YYYY", true);
+// $ExpectError
+moment([2015, 0], moment.ISO_8601);
+// $ExpectError
+moment({ hour: 15, minute: 10 }, "MM-DD-YYYY");
+// // https://momentjs.com/docs/#/parsing/special-formats/
+moment("2010-01-01T05:06:07", moment.ISO_8601);
+moment("2010-01-01T05:06:07", ["YYYY", moment.ISO_8601]);
+// https://momentjs.com/docs/#/parsing/unix-timestamp-milliseconds/
+moment(1318781876406);
 
 // $ExpectError
 moment.unix("1234");
 
 // Display
 const A: Date = moment().toDate();
-const x: string = moment().toISOString();
+describe('toISOString', () => {
+  it('should take no parameters', () => {
+    const x: string = moment().toISOString();
+  });
+
+  it('should take a boolean', () => {
+    const y: string = moment().toISOString(true);
+  });
+
+  // $ExpectError - should fail on non-boolean value
+  const y: string = moment().toISOString('foo');
+});
 
 // Get + Set
 // $ExpectError
 moment().millisecond().seconds();
 moment().milliseconds(100).seconds();
+(moment().weekday(): number);
+moment().weekday(1);
+// $ExpectError weekday does not take string arguments
+moment().weekday("monday");
+(moment().isoWeekday(): number);
+moment().isoWeekday(1);
+// ...but isoWeekday does accept string argument
+moment().isoWeekday("monday");
+moment().dayOfYear();
+moment().dayOfYear(1);
+// $ExpectError dayOfYear does not accept string arguments
+moment().dayOfYear("what");
 
 // Manipulate
 const m1: moment = moment().add(2, "day");
@@ -30,6 +81,8 @@ moment().isAfter();
 moment().isSameOrBefore();
 moment().isSameOrAfter();
 moment.isDate(new Date());
+moment().isBefore(new Date(), "day");
+moment().isSame(new Date(), "day");
 
 // CalendarTime
 moment().calendar(null, {
