abccd1d611e62c11ff80d6c372d380ec6f4da999
Gaëtan Renaudeau <renaudeau.gaetan@gmail.com>
2018-10-09 09:15:29 +0200

    Add missing 'delete' method in react-native-simple-store
    ctx https://github.com/flow-typed/flow-typed/pull/2786

--- definitions/npm/react-native-simple-store_v1.x.x/flow_v0.25.x-/react-native-simple-store_v1.x.x.js
+++ definitions/npm/react-native-simple-store_v1.x.x/flow_v0.25.x-/react-native-simple-store_v1.x.x.js
@@ -2,9 +2,14 @@ declare module 'react-native-simple-store' {
   declare type Key = string | Array<Array<any>>;
   declare type Value = string | boolean | Object | Array<any>;
 
-  declare function get(key: Key): Promise<?any>;
-  declare function save(key: Key, value: Value): Promise<?any>;
-  declare function update(key: string, value: Value): Promise<?any>;
-  declare function keys(): Promise<?any>;
-  declare function push(key: string, value: Value): Promise<?any>;
+  declare type SimpleStore = {
+    get: (key: Key)=> Promise<any>,
+    save: (key: Key, value: Value)=> Promise<any>,
+    update: (key: string, value: Value)=> Promise<any>,
+    keys: ()=> Promise<any>,
+    push: (key: string, value: Value)=> Promise<any>,
+    delete: (key: string | string[])=> Promise<any>,
+  }
+
+  declare module.exports: SimpleStore;
 }
