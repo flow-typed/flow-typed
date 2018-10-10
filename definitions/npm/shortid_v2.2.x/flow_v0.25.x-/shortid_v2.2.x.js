587cb4dca6e976b2b9bd058bcef2cd4bad85fc8d
Ville Saukkonen <villesau@users.noreply.github.com>
2018-10-10 04:17:04 +0300

    Make ShortIdModule private (#2799)

--- definitions/npm/shortid_v2.2.x/flow_v0.25.x-/shortid_v2.2.x.js
+++ definitions/npm/shortid_v2.2.x/flow_v0.25.x-/shortid_v2.2.x.js
@@ -1,13 +1,12 @@
-type ShortIdModule = {
-  (): string,
-  generate(): string,
-  seed(seed: number): ShortIdModule,
-  worker(workerId: number): ShortIdModule,
-  characters(characters: string): string,
-  decode(id: string): { version: number, worker: number },
-  isValid(id: mixed): boolean,
-};
-
 declare module 'shortid' {
+  declare type ShortIdModule = {|
+    (): string,
+    generate(): string,
+    seed(seed: number): ShortIdModule,
+    worker(workerId: number): ShortIdModule,
+    characters(characters: string): string,
+    decode(id: string): { version: number, worker: number },
+    isValid(id: mixed): boolean,
+  |};
   declare module.exports: ShortIdModule;
 };
