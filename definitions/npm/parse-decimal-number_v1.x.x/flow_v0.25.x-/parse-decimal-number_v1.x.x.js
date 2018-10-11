6ab874f350ea0d5ea96f4f86f3ccc259dc2d8bba
Bryan Manuele <bryan@flexport.com>
2018-10-10 18:41:51 -0700

    added flow types to .factorySettings, .setOptions, and .withOptions. Fixed flow types elsewhere

--- definitions/npm/parse-decimal-number_v1.x.x/flow_v0.25.x-/parse-decimal-number_v1.x.x.js
+++ definitions/npm/parse-decimal-number_v1.x.x/flow_v0.25.x-/parse-decimal-number_v1.x.x.js
@@ -1,3 +1,9 @@
 declare module 'parse-decimal-number' {
-  declare module.exports: (value: string, inOptions?: any, enforceGroupSize?: boolean) => number;
+  declare module.exports: {
+    setOptions(newOptions: {thousands: string, decimal: string}): void;
+    factoryReset(): void;
+    withOptions(options: string | [string, string] | {thousands: string, decimal: string}, enforceGroupSize?: boolean): ((value: string) => number | NaN);
+  }
+
+  declare module.exports (value: string, inOptions?: string | [string, string] | {thousands: string, decimal: string}, enforceGroupSize?: boolean) => number | NaN;
 }
