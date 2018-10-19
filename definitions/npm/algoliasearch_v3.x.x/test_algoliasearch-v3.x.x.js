d97e272a479ad47a91491fdb5b8c101c991ba225
marudor <marudor@marudor.de>
2018-10-19 14:20:44 +0200

    Fix algolia regression

--- definitions/npm/algoliasearch_v3.x.x/test_algoliasearch-v3.x.x.js
+++ definitions/npm/algoliasearch_v3.x.x/test_algoliasearch-v3.x.x.js
@@ -16,6 +16,18 @@ describe('algoliasearch', () => {
 
   it('works with `await import` syntax', async () => {
     const algoliasearch = await import('algoliasearch');
+    const searchIndex = algoliasearch.default('', '').initIndex('');
+    searchIndex.search('test').then(result => result.hits);
+  });
+
+  it('reactnative works', () => {
+    const algoliasearch = require('algoliasearch/reactnative');
+    const searchIndex = algoliasearch('', '').initIndex('');
+    searchIndex.search('test').then(result => result.hits);
+  });
+
+  it('lite works', () => {
+    const algoliasearch = require('algoliasearch/lite');
     const searchIndex = algoliasearch('', '').initIndex('');
     searchIndex.search('test').then(result => result.hits);
   });
