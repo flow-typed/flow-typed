a83a54e8b9fa3ba2c95cdc5925effb268d0dc6f3
Bukurov Alex <abukurov@hwdtech.ru>
2018-10-24 21:30:27 +0600

    [react-visibility-sensor] Add definitions

---
+++ definitions/npm/react-visibility-sensor_v5.x.x/test_react-visibility-sensor_v5.x.x.js
@@ -0,0 +1,38 @@
+// @flow
+
+import React from "react";
+import VisibilitySensor from "react-visibility-sensor";
+import type { Rect } from "react-visibility-sensor";
+import { it, describe } from "flow-typed-test";
+
+describe('react-visibility-sensor', () => {
+  const handleChange = (isVisible: boolean, visibilityRect?: Rect) => {}
+
+  it('should accept all parameters', () => {
+    <VisibilitySensor
+      active
+      offset={{}}
+      minTopValue={0}
+      intervalCheck
+      intervalDelay={100}
+      scrollCheck
+      scrollDelay={250}
+      scrollThrottle={-1}
+      resizeCheck
+      resizeDelay={250}
+      resizeThrottle={-1}
+      delayedCall={false}
+      partialVisibility={false}
+      onChange={handleChange}
+    />;
+  });
+
+  it("should not fail without optional parameters", () => {
+    <VisibilitySensor onChange={handleChange} />;
+  });
+
+  it("should fail without required parameters", () => {
+    // $ExpectError
+    <VisibilitySensor />;
+  });
+});
