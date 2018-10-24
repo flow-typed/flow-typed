ced9bc792398b0c94d29c36fffce2be2057da171
Ben James <benhjames@sky.com>
2018-10-24 21:27:28 +0100

    Use describe/it blocks in tests

--- definitions/npm/nprogress_v0.2.0/test_nprogress.v0.2.0.js
+++ definitions/npm/nprogress_v0.2.0/test_nprogress.v0.2.0.js
@@ -1,47 +1,68 @@
 // @flow
+import { describe, it } from "flow-typed-test";
 import nprogress from "nprogress";
 
-// $ExpectError
-nprogress.configure();
+describe("config", () => {
+  it("should fail on invalid config", () => {
+    // $ExpectError
+    nprogress.configure();
 
-nprogress.configure({
-  parent: "body",
-  minimum: 0,
-  trickle: true,
-  trickleSpeed: 20,
-  showSpinner: true,
-  easing: "linear",
-  speed: 20,
-  template: '<div class="bar" role="bar"></div>'
+    // $ExpectError
+    nprogress.configure({
+      unknown: "blah"
+    });
+  });
+
+  it("should pass on valid config", () => {
+    nprogress.configure({
+      parent: "body",
+      minimum: 0,
+      trickle: true,
+      trickleSpeed: 20,
+      showSpinner: true,
+      easing: "linear",
+      speed: 20,
+      template: '<div class="bar" role="bar"></div>'
+    });
+  });
 });
 
-// $ExpectError
-nprogress.configure({
-  unknown: "blah"
+describe("methods", () => {
+  it("should fail on unknown method", () => {
+    // $ExpectError
+    nprogress.unknown();
+  });
+
+  it("should pass on using known methods", () => {
+    nprogress.start();
+
+    nprogress.done();
+    nprogress.done(true);
+
+    nprogress.inc();
+    nprogress.inc(2);
+
+    nprogress.remove();
+
+    nprogress.set(2);
+  });
+
+  it("should be able to chain methods", () => {
+    nprogress
+      .start()
+      .inc(2)
+      .set(2)
+      .done(true)
+      .remove();
+  });
 });
 
-nprogress.start();
+describe("properties", () => {
+  it("can access properties correctly", () => {
+    const version: string = nprogress.version;
 
-nprogress.done();
-nprogress.done(true);
+    const status: number | null = nprogress.status;
 
-nprogress.inc();
-nprogress.inc(2);
-
-nprogress.remove();
-
-nprogress.set(2);
-
-const version: string = nprogress.version;
-
-const status: number | null = nprogress.status;
-
-const isStarted: boolean = nprogress.isStarted();
-
-// Can chain methods
-nprogress
-  .start()
-  .inc(2)
-  .set(2)
-  .done(true)
-  .remove();
+    const isStarted: boolean = nprogress.isStarted();
+  });
+});
