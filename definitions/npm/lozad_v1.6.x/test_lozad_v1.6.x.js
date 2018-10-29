bfb38640a3f1741c3bb47198acd91153faa7bcab
Nikola Ristic <risticnikola@outlook.com>
2018-10-29 20:02:21 +0100

    [lozad] Describe tests with describe and it

--- definitions/npm/lozad_v1.6.x/test_lozad_v1.6.x.js
+++ definitions/npm/lozad_v1.6.x/test_lozad_v1.6.x.js
@@ -1,63 +1,99 @@
 // @flow
+import { describe, it } from 'flow-typed-test'
 import lozad from 'lozad'
 
 let el = document.querySelector('.class')
 let els = document.querySelectorAll('.class')
+let observer
 
-let observer = lozad()
-observer.observe()
+describe('lozad', () => {
+	it('should create an instance without arguments', () => {
+		observer = lozad()
+		observer.observe()
+	})
 
-observer = lozad('.class')
+	it('should create an instance with string selector', () => {
+		observer = lozad('.class')
+		observer.observe()
+	})
 
-// $ExpectError
-observer = lozad(null)
-// $ExpectError
-observer = lozad(3)
-// $ExpectError
-observer = lozad(false)
+	it('should create an instance with NodeList', () => {
+		observer = lozad(els)
+		observer.observe()
+	})
 
-if(el) {
-	observer = lozad(el)
-}
-else {
-	// $ExpectError
-	observer = lozad(el)
-}
+	it('should not create an instance with invalid selector', () => {
+		// $ExpectError
+		observer = lozad(null)
+		// $ExpectError
+		observer = lozad(3)
+		// $ExpectError
+		observer = lozad(false)
+		// $ExpectError
+		observer = lozad({})
 
-observer = lozad(els)
+		if (el) {
+			observer = lozad(el)
+		}
+		else {
+			// $ExpectError
+			observer = lozad(el)
+		}
+	})
 
-observer = lozad('.class', {
-	rootMargin: '10px 10px 10px 10px',
-})
+	it('should create an instance with custom selector and options', () => {
+		observer = lozad('.class', {
+			rootMargin: '10px 10px 10px 10px',
+		})
 
-observer = lozad('.class', {
-	treshold: 0.1,
-})
+		observer = lozad('.class', {
+			treshold: 0.1,
+		})
 
-observer = lozad('.class', {
-	rootMargin: '10px 10px 10px 10px',
-	treshold: 0.1,
-})
+		observer = lozad('.class', {
+			rootMargin: '10px 10px 10px 10px',
+			treshold: 0.1,
+		})
+	})
 
-observer = lozad('.class', {
-	load: (el) => {
+	it('should create an instance with custom selector and callbacks', () => {
+		observer = lozad('.class', {
+			load: (el) => {},
+		})
 		
-	},
+		observer = lozad('.class', {
+			loaded: (el) => {},
+		})
+	})
 })
 
-observer = lozad('.class', {
-	loaded: (el) => {
+describe('lozad.triggerLoad', () => {
+	it('should work only with valid element', () => {
 
-	},
+		if (el) {
+			observer.triggerLoad(el)
+		} else {
+			// $ExpectError
+			observer.triggerLoad(el)
+		}
+	})
 })
 
-if(el) {
-	observer.triggerLoad(el)
-	observer.observer.unobserve(el)
-}
-else {
-	// $ExpectError
-	observer.triggerLoad(el)
-	// $ExpectError
-	observer.observer.unobserve(el)
-}
+describe('lozad.observer', () => {
+	it('should be an instance of IntersectionObserver', () => {
+		let instance = lozad()
+
+		;(instance.observer: IntersectionObserver)
+	})
+
+	it('should have working unobserve with valid element', () => {
+
+		if (el) {
+			observer.observer.unobserve(el)
+		}
+		else {
+			// $ExpectError
+			observer.observer.unobserve(el)
+		}
+	})
+})
