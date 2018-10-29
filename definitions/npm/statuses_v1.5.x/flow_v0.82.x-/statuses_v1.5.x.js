d1b2ca4d1699f190897008f588f847f78c030adf
Roman Gusev <rgusev@hwdtech.ru>
2018-10-29 15:05:57 +0600

    Fix wrong behavior description

--- definitions/npm/statuses_v1.5.x/flow_v0.82.x-/statuses_v1.5.x.js
+++ definitions/npm/statuses_v1.5.x/flow_v0.82.x-/statuses_v1.5.x.js
@@ -1,8 +1,7 @@
 declare module "statuses" {
   declare module.exports: {
     (string | number): number,
-    [number]: ?string,
-    [string]: ?number,
+    [number | string]: ?(string | number),
     redirect: { [number]: ?boolean },
     empty: { [number]: ?boolean },
     retry: { [number]: ?boolean }
