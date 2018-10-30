993b1aeb31461643694a23550873aae09396dbf4
Andrew Hansen <arahan@uw.edu>
2018-10-30 06:34:21 -0700

    fix(moment): allow maybe string types in moment instantiation (#2875)

--- definitions/npm/moment_v2.x.x/flow_v0.25.x-/moment_v2.x.x.js
+++ definitions/npm/moment_v2.x.x/flow_v0.25.x-/moment_v2.x.x.js
@@ -118,7 +118,7 @@ declare class moment$MomentDuration {
 }
 declare class moment$Moment {
   static ISO_8601: string;
-  static (): moment$Moment;
+  static (string?: ?string): moment$Moment;
   static (
     initDate:
       | moment$MomentOptions
@@ -130,19 +130,19 @@ declare class moment$Moment {
       | null
       | void
   ): moment$Moment;
-  static (string: string, format: string | Array<string>): moment$Moment;
+  static (string: ?string, format: string | Array<string>): moment$Moment;
   static (
-    string: string,
+    string: ?string,
     format: string | Array<string>,
     strict: boolean
   ): moment$Moment;
   static (
-    string: string,
+    string: ?string,
     format: string | Array<string>,
     locale: string
   ): moment$Moment;
   static (
-    string: string,
+    string: ?string,
     format: string | Array<string>,
     locale: string,
     strict: boolean
