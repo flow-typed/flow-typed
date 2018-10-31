149b8d2e6def8e0558acf58b6b27d03953a74c6a
Shinichi Kogai <kogai@users.noreply.github.com>
2018-11-01 05:06:50 +0900

    Enable to export named-export (#2920)

--- definitions/npm/react-copy-to-clipboard_v5.x.x/test_react-copy-to-clipboard_v5.x.x.js
+++ definitions/npm/react-copy-to-clipboard_v5.x.x/test_react-copy-to-clipboard_v5.x.x.js
@@ -1,28 +1,53 @@
 // @flow
 
 import React from "react";
-import CopyToClipboard from "react-copy-to-clipboard";
+import CopyToClipboard, {CopyToClipboard as NamedCopyToClipboard} from "react-copy-to-clipboard";
 import { it, describe } from "flow-typed-test";
 
 describe('react-copy-to-clipboard', () => {
-  it('should accept all parameters', () => {
-    <CopyToClipboard
-      text="Lorem ipsum dolor sit amet"
-      options={{ debug: true, message: 'Copy to clipboard: #{key}' }}
-      onCopy={(text: string, result: boolean) => {}}
-    >
-      Copy
-    </CopyToClipboard>
+  describe('default', () => {
+    it('should accept all parameters', () => {
+      <CopyToClipboard
+        text="Lorem ipsum dolor sit amet"
+        options={{ debug: true, message: 'Copy to clipboard: #{key}' }}
+        onCopy={(text: string, result: boolean) => {}}
+      >
+        Copy
+      </CopyToClipboard>
+    });
+  
+    it("should not fail without optional parameters", () => {
+      <CopyToClipboard text="Lorem ipsum dolor sit amet">
+        Copy
+      </CopyToClipboard>
+    });
+  
+    it("should fail without required parameters", () => {
+      // $ExpectError
+      <CopyToClipboard />;
+    });
   });
 
-  it("should not fail without optional parameters", () => {
-    <CopyToClipboard text="Lorem ipsum dolor sit amet">
-      Copy
-    </CopyToClipboard>
-  });
-
-  it("should fail without required parameters", () => {
-    // $ExpectError
-    <CopyToClipboard />;
+  describe('named export', () => {
+    it('should accept all parameters', () => {
+      <NamedCopyToClipboard
+        text="Lorem ipsum dolor sit amet"
+        options={{ debug: true, message: 'Copy to clipboard: #{key}' }}
+        onCopy={(text: string, result: boolean) => {}}
+      >
+        Copy
+      </NamedCopyToClipboard>
+    });
+  
+    it("should not fail without optional parameters", () => {
+      <NamedCopyToClipboard text="Lorem ipsum dolor sit amet">
+        Copy
+      </NamedCopyToClipboard>
+    });
+  
+    it("should fail without required parameters", () => {
+      // $ExpectError
+      <NamedCopyToClipboard />;
+    });
   });
 });
