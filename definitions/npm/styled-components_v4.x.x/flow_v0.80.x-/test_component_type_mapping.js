cb4d7c8040f5aaa4807cc8025cd5463ee876b688
Nino Miletich <ninomiletich@gambitresearch.com>
2018-11-02 17:02:32 +0000

    styled-components: fix tests

--- definitions/npm/styled-components_v4.x.x/flow_v0.80.x-/test_component_type_mapping.js
+++ definitions/npm/styled-components_v4.x.x/flow_v0.80.x-/test_component_type_mapping.js
@@ -8,18 +8,19 @@ import type {
 // Tests that we correctly map types correctly
 const Span1 : StyledElementType<'span'> = styled.span``
 const Span2 : StyledElementType<'span'> = styled('span')``
-const Div3 : StyledElementType<'div'> = styled('div')``
 
 // Test that we don't accidentally map incorrectly
 // $ExpectError - Wrong component type
-const Div2 : StyledElementType<'span'> = styled.div``
+const Div1 : StyledElementType<'span'> = styled.div``
 // $ExpectError - Wrong component type
-const Div3 : StyledElementType<'span'> = styled('div')``
+const Div2 : StyledElementType<'span'> = styled('div')``
 
 
 // Test it renders as the correct element
 const span1 : React.Element<'span'> = <Span1 />
 const span2 : React.Element<'span'> = <Span2 />
+
+const Div3 : StyledElementType<'div'> = styled('div')``
 // $ExpectError - Wrong element type
 const div3 : React.Element<'span'> = <Div3 />
 
