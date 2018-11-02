25f28eeac00f2525c4fa335cf0621e05c6ff81d6
Nino Miletich <ninomiletich@gambitresearch.com>
2018-11-02 16:33:10 +0000

    styled-components: test for missing elements

--- definitions/npm/styled-components_v4.x.x/flow_v0.80.x-/test_component_type_mapping.js
+++ definitions/npm/styled-components_v4.x.x/flow_v0.80.x-/test_component_type_mapping.js
@@ -1,3 +1,4 @@
+// @flow
 import * as React from 'react'
 import styled from 'styled-components'
 import type {
@@ -21,3 +22,19 @@ const span1 : React.Element<'span'> = <Span1 />
 const span2 : React.Element<'span'> = <Span2 />
 // $ExpectError - Wrong element type
 const div3 : React.Element<'span'> = <Div3 />
+
+// $ExpectError - test for non-existent element
+const derp1 = styled.derp``
+
+// $ExpectError - test for non-existent element
+const derp2 = styled('derp')``
+
+// Test we don't "accidentally style" something else:
+// $ExpectError
+const derp3 = styled(null)``
+
+// $ExpectError
+const derp3 = styled({})``
+
+// $ExpectError
+const derp3 = styled(1)``
