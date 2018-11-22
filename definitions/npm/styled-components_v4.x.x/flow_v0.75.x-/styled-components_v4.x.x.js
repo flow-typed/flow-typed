42355cb89a63de2634136f42818259ef6301d326
Nino Miletich <ninomiletich@gambitresearch.com>
2018-11-22 17:27:31 +0000

    styled-components: add styled-components/native libdef

--- definitions/npm/styled-components_v4.x.x/flow_v0.75.x-/styled-components_v4.x.x.js
+++ definitions/npm/styled-components_v4.x.x/flow_v0.75.x-/styled-components_v4.x.x.js
@@ -241,3 +241,161 @@ declare module 'styled-components' {
     [[call]]: <P : {}, C : React$ComponentType<P>>(C) => StyledComponentType<C>
   };
 }
+
+
+
+declare module 'styled-components/native' {
+
+  declare export type Interpolation =
+                                    | (<P: {}>(executionContext: P) => string)
+                                    | CSSRules
+                                    | KeyFrames
+                                    | string
+                                    | number
+
+
+  declare export type CSSRules = Interpolation[]
+
+  // This is not exported on purpose, since it's an implementation detail
+  declare type TaggedTemplateLiteral<R> = (strings : string[], ...interpolations : Interpolation[]) => R
+
+  declare export type CSSConstructor = TaggedTemplateLiteral<CSSRules>
+  declare export type KeyFramesConstructor = TaggedTemplateLiteral<KeyFrames>
+  declare export type CreateGlobalStyleConstructor = TaggedTemplateLiteral<React$ComponentType<*>>
+
+  declare interface Tag<T> {
+    styleTag: HTMLStyleElement | null;
+    getIds(): string[];
+    hasNameForId(id: string, name: string): boolean;
+    insertMarker(id: string): T;
+    insertRules(id: string, cssRules: string[], name: ?string): void;
+    removeRules(id: string): void;
+    css(): string;
+    toHTML(additionalAttrs: ?string): string;
+    toElement(): React$Element<*>;
+    clone(): Tag<T>;
+    sealed: boolean;
+  }
+
+  // The `any`/weak types in here all come from `styled-components` directly, since those definitions were just copied over
+  declare export class StyleSheet {
+    static get master() : StyleSheet;
+    static get instance() : StyleSheet;
+    static reset(forceServer? : boolean) : void;
+
+    id : number;
+    forceServer : boolean;
+    target : ?HTMLElement;
+    tagMap : {[string]: Tag<any>}; // eslint-disable-line flowtype/no-weak-types
+    deferred: { [string]: string[] | void };
+    rehydratedNames: { [string]: boolean };
+    ignoreRehydratedNames: { [string]: boolean };
+    tags: Tag<any>[]; // eslint-disable-line flowtype/no-weak-types
+    importRuleTag: Tag<any>; // eslint-disable-line flowtype/no-weak-types
+    capacity: number;
+    clones: StyleSheet[];
+
+    constructor(?HTMLElement) : this;
+    rehydrate() : this;
+    clone() : StyleSheet;
+    sealAllTags() : void;
+    makeTag(tag : ?Tag<any>) : Tag<any>; // eslint-disable-line flowtype/no-weak-types
+    getImportRuleTag() : Tag<any>; // eslint-disable-line flowtype/no-weak-types
+    getTagForId(id : string): Tag<any>; // eslint-disable-line flowtype/no-weak-types
+    hasId(id: string) : boolean;
+    hasNameForId(id: string, name: string) : boolean;
+    deferredInject(id : string, cssRules : string[]) : void;
+    inject(id: string, cssRules : string[], name? : string) : void;
+    remove(id : string) : void;
+    toHtml() : string;
+    toReactElements() : React$ElementType[];
+  }
+
+  declare export class KeyFrames {
+    id : string;
+    name : string;
+    rules : string[];
+
+    constructor(name : string, rules : string[]) : this;
+    inject(StyleSheet) : void;
+    toString() : string;
+    getName() : string;
+  }
+
+  // I think any is appropriate here?
+  // eslint-disable-next-line flowtype/no-weak-types
+  declare export type Theme = {+[string] : any}
+
+  declare export var css : CSSConstructor;
+  declare export var keyframes : KeyFramesConstructor;
+  declare export var createGlobalStyle : CreateGlobalStyleConstructor
+  declare export var ThemeProvider : React$ComponentType<{children?: ?React$Node, theme : Theme | (Theme) => Theme}>
+
+  // This is a bit hard to read. Not sure how to make it more readable. I think adding line-breaks makes it worse.
+  declare type InjectedProps = { theme : Theme | void }
+  declare  export function withTheme<Props : {}, Component: React$ComponentType<Props>>(WrappedComponent: Component) : React$ComponentType<$Diff<React$ElementConfig<Component>, InjectedProps>>;
+
+
+  // @HACK This is a cheat to hide that the underlying type is "just a string"
+  //       once we know of a better way, we should be able to update this accordingly.
+  //       I don't think there _is_ a good way, currently.
+  // @NOTE Also not too sure about the naming of this...
+  declare export type StyledElementType<T> = T;
+  declare export type StyledComponentType<C> = TaggedTemplateLiteral<C>
+
+  declare type StyledComponentList = {
+    ActivityIndicator:             StyledComponentType<React$ComponentType<{}>>,
+    ActivityIndicatorIOS:          StyledComponentType<React$ComponentType<{}>>,
+    ART:                           StyledComponentType<React$ComponentType<{}>>,
+    Button:                        StyledComponentType<React$ComponentType<{}>>,
+    DatePickerIOS:                 StyledComponentType<React$ComponentType<{}>>,
+    DrawerLayoutAndroid:           StyledComponentType<React$ComponentType<{}>>,
+    Image:                         StyledComponentType<React$ComponentType<{}>>,
+    ImageBackground:               StyledComponentType<React$ComponentType<{}>>,
+    ImageEditor:                   StyledComponentType<React$ComponentType<{}>>,
+    ImageStore:                    StyledComponentType<React$ComponentType<{}>>,
+    KeyboardAvoidingView:          StyledComponentType<React$ComponentType<{}>>,
+    ListView:                      StyledComponentType<React$ComponentType<{}>>,
+    MapView:                       StyledComponentType<React$ComponentType<{}>>,
+    Modal:                         StyledComponentType<React$ComponentType<{}>>,
+    NavigatorIOS:                  StyledComponentType<React$ComponentType<{}>>,
+    Picker:                        StyledComponentType<React$ComponentType<{}>>,
+    PickerIOS:                     StyledComponentType<React$ComponentType<{}>>,
+    ProgressBarAndroid:            StyledComponentType<React$ComponentType<{}>>,
+    ProgressViewIOS:               StyledComponentType<React$ComponentType<{}>>,
+    ScrollView:                    StyledComponentType<React$ComponentType<{}>>,
+    SegmentedControlIOS:           StyledComponentType<React$ComponentType<{}>>,
+    Slider:                        StyledComponentType<React$ComponentType<{}>>,
+    SliderIOS:                     StyledComponentType<React$ComponentType<{}>>,
+    SnapshotViewIOS:               StyledComponentType<React$ComponentType<{}>>,
+    Switch:                        StyledComponentType<React$ComponentType<{}>>,
+    RecyclerViewBackedScrollView:  StyledComponentType<React$ComponentType<{}>>,
+    RefreshControl:                StyledComponentType<React$ComponentType<{}>>,
+    SafeAreaView:                  StyledComponentType<React$ComponentType<{}>>,
+    StatusBar:                     StyledComponentType<React$ComponentType<{}>>,
+    SwipeableListView:             StyledComponentType<React$ComponentType<{}>>,
+    SwitchAndroid:                 StyledComponentType<React$ComponentType<{}>>,
+    SwitchIOS:                     StyledComponentType<React$ComponentType<{}>>,
+    TabBarIOS:                     StyledComponentType<React$ComponentType<{}>>,
+    Text:                          StyledComponentType<React$ComponentType<{}>>,
+    TextInput:                     StyledComponentType<React$ComponentType<{}>>,
+    ToastAndroid:                  StyledComponentType<React$ComponentType<{}>>,
+    ToolbarAndroid:                StyledComponentType<React$ComponentType<{}>>,
+    Touchable:                     StyledComponentType<React$ComponentType<{}>>,
+    TouchableHighlight:            StyledComponentType<React$ComponentType<{}>>,
+    TouchableNativeFeedback:       StyledComponentType<React$ComponentType<{}>>,
+    TouchableOpacity:              StyledComponentType<React$ComponentType<{}>>,
+    TouchableWithoutFeedback:      StyledComponentType<React$ComponentType<{}>>,
+    View:                          StyledComponentType<React$ComponentType<{}>>,
+    ViewPagerAndroid:              StyledComponentType<React$ComponentType<{}>>,
+    WebView:                       StyledComponentType<React$ComponentType<{}>>,
+    FlatList:                      StyledComponentType<React$ComponentType<{}>>,
+    SectionList:                   StyledComponentType<React$ComponentType<{}>>,
+    VirtualizedList:               StyledComponentType<React$ComponentType<{}>>,
+  }
+
+  declare export default StyledComponentList & {
+    [[call]]: <S : string>(S) => $ElementType<StyledComponentList, S>,
+    [[call]]: <P : {}, C : React$ComponentType<P>>(C) => StyledComponentType<C>
+  };
+}
