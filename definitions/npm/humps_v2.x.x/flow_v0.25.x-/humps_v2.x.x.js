1b712097ca14ea6df35e3da25c4238044182e88a
Vincent Driessen <me@nvie.com>
2019-01-06 22:38:40 +0100

    Add libdefs for humps (#3049)

---
+++ definitions/npm/humps_v2.x.x/flow_v0.25.x-/humps_v2.x.x.js
@@ -0,0 +1,36 @@
+// @flow strict
+
+/**
+ * Flow libdef for 'humps'
+ * See https://www.npmjs.com/package/humps
+ * by Vincent Driessen, 2018-12-21
+ */
+
+declare module "humps" {
+  declare type Options = {|
+    separator?: string,
+    split?: RegExp | string
+  |};
+
+  declare export function camelize(string): string;
+  declare export function pascalize(string): string;
+  declare export function decamelize(string, options?: Options): string;
+  declare export function depascalize(string, options?: Options): string;
+
+  declare export function camelizeKeys(
+    { +[string]: mixed },
+    options?: Options
+  ): { [string]: mixed };
+  declare export function pascalizeKeys(
+    { +[string]: mixed },
+    options?: Options
+  ): { [string]: mixed };
+  declare export function decamelizeKeys(
+    { +[string]: mixed },
+    options?: Options
+  ): { [string]: mixed };
+  declare export function depascalizeKeys(
+    { +[string]: mixed },
+    options?: Options
+  ): { [string]: mixed };
+}
