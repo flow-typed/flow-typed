877c877b36cce5c7b8b96f36716520a7c5a20e82
Vincent Driessen <me@nvie.com>
2019-01-06 10:04:48 +0100

    Add type assertion

--- definitions/npm/promise-limit_v2.x.x/test_promise-limit_v2.x.x.js
+++ definitions/npm/promise-limit_v2.x.x/test_promise-limit_v2.x.x.js
@@ -11,5 +11,6 @@ function dosomething_(): Promise<string> {
   return Promise.resolve("hi");
 }
 const dosomething = () => limiter(() => dosomething_());
+(dosomething: () => Promise<string>);
 
 dosomething().then(console.log);
