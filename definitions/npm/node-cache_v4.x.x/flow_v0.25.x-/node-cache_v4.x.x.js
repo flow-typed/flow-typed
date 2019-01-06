9cb06b1d680f617de27d739842c229b147627259
Vincent Driessen <me@nvie.com>
2019-01-06 22:39:22 +0100

    Make Options type private, as suggested

--- definitions/npm/node-cache_v4.x.x/flow_v0.25.x-/node-cache_v4.x.x.js
+++ definitions/npm/node-cache_v4.x.x/flow_v0.25.x-/node-cache_v4.x.x.js
@@ -5,17 +5,17 @@
  * by Vincent Driessen, 2018-12-19
  */
 
-declare type nodeCache$Options = {|
-  stdTTL?: number,
-  checkperiod?: number,
-  errorOnMissing?: boolean,
-  useClones?: boolean,
-  deleteOnExpire?: boolean
-|};
-
 declare module "node-cache" {
+  declare type Options = {|
+    stdTTL?: number,
+    checkperiod?: number,
+    errorOnMissing?: boolean,
+    useClones?: boolean,
+    deleteOnExpire?: boolean
+  |};
+
   declare class NodeCache {
-    constructor(options?: nodeCache$Options): NodeCache;
+    constructor(options?: Options): NodeCache;
     get(key: string): mixed | void;
     set(key: string, val: mixed, ttl?: number): boolean;
   }
