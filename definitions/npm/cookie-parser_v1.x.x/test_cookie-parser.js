ba756cc1975bcf39fd38a07c5145ae65b8e86cff
Vincent Driessen <me@nvie.com>
2019-01-10 11:17:03 +0100

    Copy over definitions from express

--- definitions/npm/cookie-parser_v1.x.x/test_cookie-parser.js
+++ definitions/npm/cookie-parser_v1.x.x/test_cookie-parser.js
@@ -1,16 +1,16 @@
 // @flow strict
 
-import { describe, it } from "flow-typed-test";
-import cookieParser from "cookie-parser";
-import type { Middleware } from "express";
+import { describe, it } from 'flow-typed-test';
+import cookieParser from 'cookie-parser';
+import type { Middleware } from 'cookie-parser';
 
-describe("cookie-parser", () => {
-  it("errors", () => {
+describe('cookie-parser', () => {
+  it('errors', () => {
     // $ExpectError
     cookieParser(42);
   });
 
-  it("works", () => {
+  it('works', () => {
     (cookieParser(): Middleware);
   });
 });
