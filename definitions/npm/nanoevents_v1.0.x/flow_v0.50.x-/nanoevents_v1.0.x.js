256ea6f76a7c7f9253b69339730f7a72b57706df
Oleg Nechiporenko <gang7e@gmail.com>
2019-01-18 19:53:17 +0300

    Added unbind listener type (#3081)

--- definitions/npm/nanoevents_v1.0.x/flow_v0.50.x-/nanoevents_v1.0.x.js
+++ definitions/npm/nanoevents_v1.0.x/flow_v0.50.x-/nanoevents_v1.0.x.js
@@ -2,7 +2,7 @@ declare module "nanoevents" {
   declare export default class NanoEvents<T> {
     constructor(): NanoEvents<T>,
 
-    on<K: $Keys<T>>(event: K, cb: ($ElementType<T, K>) => void): void,
+    on<K: $Keys<T>>(event: K, cb: ($ElementType<T, K>) => void): () => void,
 
     emit<K: $Keys<T>>(event: K, data?: $ElementType<T, K>): void
   }
