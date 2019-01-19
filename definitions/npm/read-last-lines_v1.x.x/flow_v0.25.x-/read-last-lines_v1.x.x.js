10f03d84f7c470c543ecee547b52ad1ab3310010
Charlie Blevins <blevins.charlie@gmail.com>
2019-01-19 16:23:32 -0500

    add newlines and remove console logs for read-last-lines types. use correct optional syntax

--- definitions/npm/read-last-lines_v1.x.x/flow_v0.25.x-/read-last-lines_v1.x.x.js
+++ definitions/npm/read-last-lines_v1.x.x/flow_v0.25.x-/read-last-lines_v1.x.x.js
@@ -1,6 +1,6 @@
 // @flow
 declare module 'read-last-lines' {
     declare module.exports: {
-        read(input_file_path: string, maxLineCount: number, encoding: ?string): Promise<string>
+        read(input_file_path: string, maxLineCount: number, encoding?: string): Promise<string>
     };
 }
--- definitions/npm/read-last-lines_v1.x.x/flow_v0.25.x-/test_read-last-lines_v1.x.x.js
+++ definitions/npm/read-last-lines_v1.x.x/flow_v0.25.x-/test_read-last-lines_v1.x.x.js
@@ -1,9 +1,7 @@
 // @flow
-console.log('TEST');
 import { describe, it } from 'flow-typed-test';
-const rll = require('read-last-lines');
+import rll from 'read-last-lines';
 describe('read', (done) => {
-
     rll.read('read-last-lines_v1.x.x.js', 25);
     rll.read('read-last-lines_v1.x.x.js', 25, 'utf-8');
     rll.read('read-last-lines_v1.x.x.js', 25, 'buffer');
