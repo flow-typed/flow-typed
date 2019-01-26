46a0983eeba1aad46accc8301e247980ddcd3cb8
Marc G <info@mouvedia.com>
2019-01-26 20:09:26 +0100

    refactor: describe/it

--- definitions/npm/cb-fetch_v1.10.x/test_cb-fetch_v1.10.x.js
+++ definitions/npm/cb-fetch_v1.10.x/test_cb-fetch_v1.10.x.js
@@ -1,30 +1,64 @@
 import request from 'cb-fetch';
+import { describe, it } from 'flow-typed-test';
 
-request({ responseType: "json" });
-request().post();
-request().hook('download', () => {});
-request().get().query();
-request().done({
-    success(response) {
-        response.headers.hasOwnProperty();
-    }
+describe('responseType', () => {
+  it('supports json', () => {
+    request({ responseType: "json" });
+  })
+  it("doesn't support ms-stream yet", () => {
+    // $ExpectError
+    request({ responseType: "ms-stream" });
+  })
 });
 
-// $ExpectError
-request({ responseType: "ms-stream" });
+describe('HTTP verbs', () => {
+  it('supports post', () => {
+    request().post();
+  })
+  it("doesn't support trace", () => {
+    // $ExpectError
+    request().trace();
+  })
+});
 
-// $ExpectError
-request().trace();
+describe('hooks', () => {
+  it('supports download', () => {
+    request().hook('download', () => {});
+  })
+  it("doesn't support upload yet", () => {
+    // $ExpectError
+    request().hook('upload', () => {});
+  })
+});
 
-// $ExpectError
-request().hook('upload', () => {});
+describe('get method', () => {
+  it('has a query method', () => {
+    request().get().query();
+  })
+  it("doesn't have a send method", () => {
+    // $ExpectError
+    request().get().send();
+  })
+});
 
-// $ExpectError
-request().get().send();
-
-request().done({
-    error(response) {
-        // $ExpectError
-        response.headers.hasOwnProperty();
-    }
+describe('response', () => {
+  describe('success', () => {
+    it("doesn't require refinement before using headers", () => {
+        request().done({
+            success(response) {
+                response.headers.hasOwnProperty();
+            }
+        });
+    })
+  });
+  describe('error', () => {
+    it("requires refinement before using headers", () => {
+        request().done({
+            error(response) {
+                // $ExpectError
+                response.headers.hasOwnProperty();
+            }
+        });
+    })
+  });
 });
