1089ca73912d416eeaa104e4e07438ea1cbd6044
andretshurotshka <andwebar@gmail.com>
2019-02-05 02:57:29 +0500

    Add describe and it

--- definitions/npm/zxcvbn_v4.x.x/test_zxcvbn.js
+++ definitions/npm/zxcvbn_v4.x.x/test_zxcvbn.js
@@ -2,17 +2,22 @@
 
 import zxcvbn from 'zxcvbn';
 import type { Result, Score, AttackTime, Feedback } from 'zxcvbn';
+import { describe, it } from 'flow-typed-test';
 
-const testA: Result = zxcvbn('123456');
-const guesses: number = testA.guesses;
-const guessesLog10: number = testA.guesses_log10;
-const score: Score = testA.score;
-const calcTime: number = testA.calc_time;
-const crackTimeSeconds: AttackTime = testA.crack_times_seconds;
-const onlineThrottling100PerHour =
-  crackTimeSeconds.online_throttling_100_per_hour;
-const feedback: Feedback = testA.feedback;
-const warning: string = feedback.warning;
-const suggestions: string[] = feedback.suggestions;
+describe('password', () => {
+  const testA: Result = zxcvbn('123456');
+  const guesses: number = testA.guesses;
+  const guessesLog10: number = testA.guesses_log10;
+  const score: Score = testA.score;
+  const calcTime: number = testA.calc_time;
+  const crackTimeSeconds: AttackTime = testA.crack_times_seconds;
+  const onlineThrottling100PerHour =
+    crackTimeSeconds.online_throttling_100_per_hour;
+  const feedback: Feedback = testA.feedback;
+  const warning: string = feedback.warning;
+  const suggestions: string[] = feedback.suggestions;
+});
 
-const test2: Result = zxcvbn('123456', ['abc']);
+describe('user input', () => {
+  const test2: Result = zxcvbn('123456', ['abc']);
+});
