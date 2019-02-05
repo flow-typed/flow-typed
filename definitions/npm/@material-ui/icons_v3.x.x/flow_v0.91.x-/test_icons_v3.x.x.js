136aa1b7c3ab89803605c79c91bda69c19de4e30
PolGuixe <pol.guixe@gmail.com>
2019-02-05 17:06:23 +0100

    added very basic test

---
+++ definitions/npm/@material-ui/icons_v3.x.x/flow_v0.91.x-/test_icons_v3.x.x.js
@@ -0,0 +1,8 @@
+// @flow
+import React from "react";
+
+// Direct icon module import.
+import MenuIcon from "@material-ui/icons/Menu";
+
+// $ExpectError invalid color value.
+let menuIcon = <MenuIcon color="black" />;
