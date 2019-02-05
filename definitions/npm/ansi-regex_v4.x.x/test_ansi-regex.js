55439ffc5e268b26411f25d9b304a8eb302981bb
andretshurotshka <andwebar@gmail.com>
2019-02-05 17:21:22 +0500

    Add describe and it

--- definitions/npm/ansi-regex_v4.x.x/test_ansi-regex.js
+++ definitions/npm/ansi-regex_v4.x.x/test_ansi-regex.js
@@ -1,16 +1,29 @@
 import ansiRegex from 'ansi-regex';
+import { describe, it } from 'flow-typed-test';
 
-ansiRegex(); // $ExpectType RegExp
+describe('ansi-regex', () => {
+  it('should be RegExp', () => {
+    ansiRegex(); // $ExpectType RegExp
+  });
 
-// From the ansi-regex README.md
-ansiRegex().test('\u001B[4mcake\u001B[0m'); // $ExpectType boolean
-// => true
+  it('should be boolean', () => {
+    // From the ansi-regex README.md
+    ansiRegex().test('\u001B[4mcake\u001B[0m'); // $ExpectType boolean
+    // => true
+  });
 
-ansiRegex().test('cake'); // $ExpectType boolean
-// => false
+  it('should be false boolean', () => {
+    ansiRegex().test('cake'); // $ExpectType boolean
+    // => false
+  });
 
-ansiRegex({ onlyFirst: true }).test('cake'); // $ExpectType boolean
-// => false
+  it('should be false boolean 2', () => {
+    ansiRegex({ onlyFirst: true }).test('cake'); // $ExpectType boolean
+    // => false
+  });
 
-'\u001B[4mcake\u001B[0m'.match(ansiRegex()); // $ExpectType RegExpMatchArray | null
-// => ['\u001B[4m', '\u001B[0m']
+  it('should be RegExpMatchArray | null', () => {
+    '\u001B[4mcake\u001B[0m'.match(ansiRegex()); // $ExpectType RegExpMatchArray | null
+    // => ['\u001B[4m', '\u001B[0m']
+  });
+});
