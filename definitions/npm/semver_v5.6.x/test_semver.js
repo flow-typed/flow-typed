dd8061949f88f0383c1f31fcf653afe92e097c79
Max Greenwald <maxgreenwald@me.com>
2019-02-18 15:21:28 -0500

    Use describe and it in tests

--- definitions/npm/semver_v5.6.x/test_semver.js
+++ definitions/npm/semver_v5.6.x/test_semver.js
@@ -1,10 +1,7 @@
 // @flow
 
+import { describe, it } from 'flow-typed-test';
 import semver, {Comparator, Range, SemVer} from 'semver';
-
-new SemVer('^3.2.0');
-
-(semver.cmp('1.2.3', '>', '1.2.4'): boolean);
 
 const optionOne = true;
 const optionTwo = {
@@ -12,78 +9,89 @@ const optionTwo = {
   includePrerelease: false,
 };
 
-// Comparator
-const comp = new Comparator('>=3.2.0');
-comp.test('5.3.2');
-(comp.operator: string);
-(comp.value: string);
+describe('semver', () => {
+  it('should flowtype comparators proprely', () => {
+    new SemVer('^3.2.0');
+    (semver.cmp('1.2.3', '>', '1.2.4'): boolean);
 
-// comparator object as a ctor arg is okay
-new Comparator(comp);
+    // Comparator
+    const comp = new Comparator('>=3.2.0');
+    comp.test('5.3.2');
+    (comp.operator: string);
+    (comp.value: string);
 
-// static/exported properties
-(semver.SEMVER_SPEC_VERSION: string);
-semver.re.forEach(r => r.test('foo'));
-semver.src.forEach(r => r.match(/foo/));
+    // comparator object as a ctor arg is okay
+    new Comparator(comp);
+  });
 
-// exported functions
-(semver.outside('1.2.3', '1.2', '>'): boolean);
-semver.clean('     =v3.0.2   ');
-semver.valid('foobar');
-semver.inc('1.2.3', 'major', true);
-semver.inc('1.2.3', 'major', true, 'beta');
-semver.inc('1.2.3', 'major', 'beta');
-semver.inc(new SemVer('1.2.3'), 'major', 'beta');
-(semver.major('1.2.3'): number);
-(semver.major(new SemVer('1.2.3')): number);
-(semver.minor('1.2.3'): number);
-(semver.minor(new SemVer('1.2.3')): number);
-(semver.patch('1.2.3'): number);
-(semver.patch(new SemVer('1.2.3')): number);
-(semver.intersects('1.2.3', Semver('2.9.2'), true) : boolean);
-(semver.intersects(new Range('3.x || 4.x')) : boolean);
-semver.sort(['9.8.7', new SemVer('1.2.3')]).forEach(x => console.log(x));
-semver.rsort(['9.8.7', new SemVer('1.2.3')]).forEach(x => console.log(x));
-semver.compareIdentifiers('9.8.7', new SemVer('1.2.3'));
-semver.rcompareIdentifiers('9.8.7', new SemVer('1.2.3'));
-semver.rcompare('9.8.7', new SemVer('1.2.3'));
-semver.diff('9.8.7', new SemVer('1.2.3'));
+  it('should flowtype exported functions/propeties properly', () => {
+    // static/exported properties
+    (semver.SEMVER_SPEC_VERSION: string);
+    semver.re.forEach(r => r.test('foo'));
+    semver.src.forEach(r => r.match(/foo/));
 
-semver.validRange('3.x || 4.x');
-semver.validRange(new Range('3.x || 4.x'));
-semver.satisfies('3.5.0', new Range('3.x || 4.x'));
-semver.satisfies('3.5.0', '3.x || 4.x', true);
-semver.maxSatisfying(['3.5.0', new SemVer('3.15.10')], '3.x || 4.x');
-semver.minSatisfying(['3.5.0', new SemVer('3.15.10')], '3.x || 4.x');
-semver.gtr('3.5.0', '3.x || 4.x');
-semver.gtr(new SemVer('3.5.0'), new Range('3.x || 4.x'));
-semver.ltr('3.5.0', '3.x || 4.x');
-semver.ltr(new SemVer('3.5.0'), new Range('3.x || 4.x'));
-semver.outside('2.5.0', '3.x || 4.x', '<');
-semver.outside(new SemVer('2.5.0'), new Range('3.x || 4.x'), '>', true);
-semver.parse('3.5.2');
-semver.parse('3.5.2', true);
-semver.toComparators('3.x || 4.x');
-semver.toComparators(new Range('3.x || 4.x'), true);
+    // exported functions
+    (semver.outside('1.2.3', '1.2', '>'): boolean);
+    semver.clean('     =v3.0.2   ');
+    semver.valid('foobar');
+    semver.inc('1.2.3', 'major', optionOne);
+    semver.inc('1.2.3', 'major', true, 'beta');
+    semver.inc('1.2.3', 'major', 'beta');
+    semver.inc(new SemVer('1.2.3'), 'major', 'beta');
+    (semver.major('1.2.3'): number);
+    (semver.major(new SemVer('1.2.3')): number);
+    (semver.minor('1.2.3'): number);
+    (semver.minor(new SemVer('1.2.3')): number);
+    (semver.patch('1.2.3'): number);
+    (semver.patch(new SemVer('1.2.3')): number);
+    (semver.intersects('1.2.3', Semver('2.9.2'), optionTwo) : boolean);
+    (semver.intersects(new Range('3.x || 4.x')) : boolean);
+    semver.sort(['9.8.7', new SemVer('1.2.3')]).forEach(x => console.log(x));
+    semver.rsort(['9.8.7', new SemVer('1.2.3')]).forEach(x => console.log(x));
+    semver.compareIdentifiers('9.8.7', new SemVer('1.2.3'));
+    semver.rcompareIdentifiers('9.8.7', new SemVer('1.2.3'));
+    semver.rcompare('9.8.7', new SemVer('1.2.3'));
+    semver.diff('9.8.7', new SemVer('1.2.3'));
 
-(semver.coerce('4.6.3.9.2-alpha2'): SemVer);
+    semver.validRange('3.x || 4.x');
+    semver.validRange(new Range('3.x || 4.x'));
+    semver.satisfies('3.5.0', new Range('3.x || 4.x'));
+    semver.satisfies('3.5.0', '3.x || 4.x', true);
+    semver.maxSatisfying(['3.5.0', new SemVer('3.15.10')], '3.x || 4.x');
+    semver.minSatisfying(['3.5.0', new SemVer('3.15.10')], '3.x || 4.x');
+    semver.gtr('3.5.0', '3.x || 4.x');
+    semver.gtr(new SemVer('3.5.0'), new Range('3.x || 4.x'));
+    semver.ltr('3.5.0', '3.x || 4.x');
+    semver.ltr(new SemVer('3.5.0'), new Range('3.x || 4.x'));
+    semver.outside('2.5.0', '3.x || 4.x', '<');
+    semver.outside(new SemVer('2.5.0'), new Range('3.x || 4.x'), '>', optionOne);
+    semver.parse('3.5.2');
+    semver.parse('3.5.2', true);
+    semver.toComparators('3.x || 4.x');
+    semver.toComparators(new Range('3.x || 4.x'), optionTwo);
 
-// $ExpectError
-semver.cmp('1.2.3', '> ', '1.2.4');
+    (semver.coerce('4.6.3.9.2-alpha2'): SemVer);
 
-// $ExpectError
-semver.outside('1.2.3', '1.2', '> ');
+    // $ExpectError
+    semver.cmp('1.2.3', '> ', '1.2.4');
 
-// Range
-new semver.Range('^1.2.x');
-const range = new Range('3.x || 4.x');
+    // $ExpectError
+    semver.outside('1.2.3', '1.2', '> ');
+  });
 
-range.test('3.4.5');
-(range.raw: string);
-(range.set[0][0].semver.version: string);
-(range.format(): string);
-(range.test('1.2.3'): boolean);
-(range.toString(): string);
+  it('should flowtype range functions properly', () => {
+    // Range
+    new semver.Range('^1.2.x');
+    const range = new Range('3.x || 4.x');
 
-// range object as a ctor arg is okay
-new Range(range);
+    range.test('3.4.5');
+    (range.raw: string);
+    (range.set[0][0].semver.version: string);
+    (range.format(): string);
+    (range.test('1.2.3'): boolean);
+    (range.toString(): string);
+
+    // range object as a ctor arg is okay
+    new Range(range);
+  });
+});
