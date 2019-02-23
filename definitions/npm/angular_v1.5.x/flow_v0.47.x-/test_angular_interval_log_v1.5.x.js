31e7ebb5d34fbddbad7e47b77beb1718a9de958c
Rob Porter <rob@weeverapps.com>
2019-02-22 21:01:01 -0500

    Add describe/it to new tests

--- definitions/npm/angular_v1.5.x/flow_v0.47.x-/test_angular_interval_log_v1.5.x.js
+++ definitions/npm/angular_v1.5.x/flow_v0.47.x-/test_angular_interval_log_v1.5.x.js
@@ -1,111 +1,215 @@
 // @flow
 import type { $Interval, $Log, AngularPromise } from 'angular';
+import { describe, it } from 'flow-typed-test';
 
-function testInterval($interval: $Interval) {
-  // can be called with two args: fn, delay
-  $interval(() => {}, 0);
-  // can be called with two args
-  $interval(() => {}, 0);
-  // third argument is a number
-  $interval(() => {}, 0, 1);
-  // fourth argument is a boolean
-  $interval(() => {}, 0, 1, true);
-  // remaining arguments are passed to the function
-  $interval((a, b, c) => {}, 0, 1, false, 'a', 'b', 'c');
-  //$ExpectError third argument must be a number
-  $interval(() => {}, 0, true);
-  //$ExpectError fourth argument must be a boolean
-  $interval(() => {}, 0, 1, 2);
-  //$ExpectError must have two arguments
-  $interval();
-  //$ExpectError must have two arguments
-  $interval(() => {});
-  //$ExpectError must have two arguments minimum where fn is Function and delay is number
-  $interval(123, 1);
-  //$ExpectError must have two arguments minimum where fn is Function and delay is number
-  $interval(() => {}, 'string');
-  // returns back angular promise
-  ($interval(() => {}, 0): AngularPromise<*>);
-}
+describe('angular $Interval', () => {
+  it('can be called', () => {
+    it('with two args: fn, delay', () => {
+      function testInterval($interval: $Interval) {
+        $interval(() => {}, 0);
+      }
+    });
 
-function testLog($log: $Log) {
-  // can be pass an argument of any type or none at all
-  $log.log('anything');
-  $log.log(1);
-  $log.log(() => {});
-  $log.log(null);
-  $log.log({ a: 1 });
-  $log.log(['a', 1, 3, { a: 10 }]);
-  $log.log(Symbol('test'));
-  $log.log();
+    it('with third argument as number', () => {
+      function testInterval($interval: $Interval) {
+        $interval(() => {}, 0, 1);
+      }
+    });
 
-  //$ExpectError cannot pass more than one argument
-  $log.log(1, 'x');
+    it('with fourth argument as boolean', () => {
+      function testInterval($interval: $Interval) {
+        $interval(() => {}, 0, 1, true);
+      }
+    });
 
-  // returns back nothing
-  ($log.log(1): void);
+    it('with additional arguments to be passed to the function', () => {
+      function testInterval($interval: $Interval) {
+        $interval((a, b, c) => {}, 0, 1, false, 'a', 'b', 'c');
+      }
+    });
+  });
 
-  // can be pass an argument of any type or none at all
-  $log.info('anything');
-  $log.info(1);
-  $log.info(() => {});
-  $log.info(null);
-  $log.info({ a: 1 });
-  $log.info(['a', 1, 3, { a: 10 }]);
-  $log.info(Symbol('test'));
-  $log.info();
+  it('cannot be called', () => {
+    it('with something other than a number in the third argument', () => {
+      function testInterval($interval: $Interval) {
+        //$ExpectError
+        $interval(() => {}, 0, true);
+      }
+    });
 
-  //$ExpectError cannot pass more than one argument
-  $log.info(1, 'x');
+    it('with something other than a boolean in the fourth argument', () => {
+      function testInterval($interval: $Interval) {
+        //$ExpectError
+        $interval(() => {}, 0, 1, 2);
+      }
+    });
 
-  // returns back nothing
-  ($log.info(1): void);
+    it('with less than two arguments', () => {
+      function testInterval($interval: $Interval) {
+        //$ExpectError
+        $interval(() => {});
+        //$ExpectError
+        $interval();
+      }
+    });
 
-  // can be pass an argument of any type or none at all
-  $log.warn('anything');
-  $log.warn(1);
-  $log.warn(() => {});
-  $log.warn(null);
-  $log.warn({ a: 1 });
-  $log.warn(['a', 1, 3, { a: 10 }]);
-  $log.warn(Symbol('test'));
-  $log.warn();
+    it('with something other than [function, number] in first two arguments', () => {
+      function testInterval($interval: $Interval) {
+        //$ExpectError
+        $interval(123, 1);
+        //$ExpectError
+        $interval(123, () => {});
+        //$ExpectError
+        $interval(() => {}, 'string');
+      }
+    });
+  });
 
-  //$ExpectError cannot pass more than one argument
-  $log.warn(1, 'x');
+  it('returns back the angular promise', () => {
+    function testInterval($interval: $Interval) {
+      ($interval(() => {}, 0): AngularPromise<*>);
+    }
+  });
+});
 
-  // returns back nothing
-  ($log.warn(1): void);
+describe('angular $Log', () => {
+  describe('log', () => {
+    it('can be passed an argument of any type or none at all', () => {
+      function testLog($log: $Log) {
+        $log.log('anything');
+        $log.log(1);
+        $log.log(() => {});
+        $log.log(null);
+        $log.log({ a: 1 });
+        $log.log(['a', 1, 3, { a: 10 }]);
+        $log.log(Symbol('test'));
+        $log.log();
+      }
+    });
 
-  // can be pass an argument of any type or none at all
-  $log.error('anything');
-  $log.error(1);
-  $log.error(() => {});
-  $log.error(null);
-  $log.error({ a: 1 });
-  $log.error(['a', 1, 3, { a: 10 }]);
-  $log.error(Symbol('test'));
-  $log.error();
+    it('cannot pass more than one argument', () => {
+      function testLog($log: $Log) {
+        //$ExpectError
+        $log.log(1, 'x');
+      }
+    });
 
-  //$ExpectError cannot pass more than one argument
-  $log.error(1, 'x');
+    it('returns back nothing', () => {
+      function testLog($log: $Log) {
+        ($log.log(1): void);
+      }
+    });
+  });
 
-  // returns back nothing
-  ($log.error(1): void);
+  describe('info', () => {
+    it('can be passed an argument of any type or none at all', () => {
+      function testLog($log: $Log) {
+        $log.info('anything');
+        $log.info(1);
+        $log.info(() => {});
+        $log.info(null);
+        $log.info({ a: 1 });
+        $log.info(['a', 1, 3, { a: 10 }]);
+        $log.info(Symbol('test'));
+        $log.info();
+      }
+    });
 
-  // can be pass an argument of any type or none at all
-  $log.debug('anything');
-  $log.debug(1);
-  $log.debug(() => {});
-  $log.debug(null);
-  $log.debug({ a: 1 });
-  $log.debug(['a', 1, 3, { a: 10 }]);
-  $log.debug(Symbol('test'));
-  $log.debug();
+    it('cannot pass more than one argument', () => {
+      function testLog($log: $Log) {
+        //$ExpectError
+        $log.info(1, 'x');
+      }
+    });
 
-  //$ExpectError cannot pass more than one argument
-  $log.debug(1, 'x');
+    it('returns back nothing', () => {
+      function testLog($log: $Log) {
+        ($log.info(1): void);
+      }
+    });
+  });
 
-  // returns back nothing
-  ($log.debug(1): void);
-}
+  describe('warn', () => {
+    it('can be passed an argument of any type or none at all', () => {
+      function testLog($log: $Log) {
+        $log.warn('anything');
+        $log.warn(1);
+        $log.warn(() => {});
+        $log.warn(null);
+        $log.warn({ a: 1 });
+        $log.warn(['a', 1, 3, { a: 10 }]);
+        $log.warn(Symbol('test'));
+        $log.warn();
+      }
+    });
+
+    it('cannot pass more than one argument', () => {
+      function testLog($log: $Log) {
+        //$ExpectError
+        $log.warn(1, 'x');
+      }
+    });
+
+    it('returns back nothing', () => {
+      function testLog($log: $Log) {
+        ($log.warn(1): void);
+      }
+    });
+  });
+
+  describe('error', () => {
+    it('can be passed an argument of any type or none at all', () => {
+      function testLog($log: $Log) {
+        $log.error('anything');
+        $log.error(1);
+        $log.error(() => {});
+        $log.error(null);
+        $log.error({ a: 1 });
+        $log.error(['a', 1, 3, { a: 10 }]);
+        $log.error(Symbol('test'));
+        $log.error();
+      }
+    });
+
+    it('cannot pass more than one argument', () => {
+      function testLog($log: $Log) {
+        //$ExpectError
+        $log.error(1, 'x');
+      }
+    });
+
+    it('returns back nothing', () => {
+      function testLog($log: $Log) {
+        ($log.error(1): void);
+      }
+    });
+  });
+
+  describe('debug', () => {
+    it('can be passed an argument of any type or none at all', () => {
+      function testLog($log: $Log) {
+        $log.debug('anything');
+        $log.debug(1);
+        $log.debug(() => {});
+        $log.debug(null);
+        $log.debug({ a: 1 });
+        $log.debug(['a', 1, 3, { a: 10 }]);
+        $log.debug(Symbol('test'));
+        $log.debug();
+      }
+    });
+
+    it('cannot pass more than one argument', () => {
+      function testLog($log: $Log) {
+        //$ExpectError
+        $log.debug(1, 'x');
+      }
+    });
+
+    it('returns back nothing', () => {
+      function testLog($log: $Log) {
+        ($log.debug(1): void);
+      }
+    });
+  });
+});
