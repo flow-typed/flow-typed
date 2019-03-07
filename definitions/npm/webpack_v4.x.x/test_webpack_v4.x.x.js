6cb74e56288ebd97e4871a9cb8a48b8dbac3896c
Sudesh Kumar Yadav <sudeshy8@gmail.com>
2019-03-07 14:23:14 +0530

    [webpack_4.x.x] Add more type definitions for node.js api (#3191)

--- definitions/npm/webpack_v4.x.x/test_webpack_v4.x.x.js
+++ definitions/npm/webpack_v4.x.x/test_webpack_v4.x.x.js
@@ -1,4 +1,6 @@
-import type { WebpackOptions } from 'webpack';
+import webpack from 'webpack';
+
+import type { Stats, WebpackOptions } from 'webpack';
 
 const options: WebpackOptions = {
   devServer: {
@@ -36,3 +38,18 @@ const options: WebpackOptions = {
     ],
   },
 };
+
+webpack(options, function(err, stats: Stats) {});
+
+const compiler1 = webpack(options);
+compiler1.run(function(err, stats: Stats) {});
+
+const compiler2 = webpack(options);
+const watching2 = compiler2.watch({}, function(err, stats: Stats) {});
+watching2.invalidate();
+watching2.close();
+
+const compiler3 = webpack([options, options]);
+const watching3 = compiler3.watch({}, function(err, stats: Stats) {});
+watching3.invalidate();
+watching3.close();
