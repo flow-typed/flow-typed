08e46f42d65e414981df87a19213cc13cfb2b1d1
Alexey Vykhrystyuk <AVykhrystyuk@gmail.com>
2019-03-12 23:23:06 +0300

    [autobind-decorator_v2.x.x] Add typescript defs from official repository

---
+++ definitions/npm/autobind-decorator_v2.x.x/flow_v0.25.x-/autobind-decorator_v2.x.x.js
@@ -0,0 +1,7 @@
+// types are copied from https://github.com/andreypopp/autobind-decorator
+declare const autobind: ClassDecorator & MethodDecorator;
+
+export declare const boundMethod: MethodDecorator;
+export declare const boundClass: ClassDecorator;
+
+export default autobind;
