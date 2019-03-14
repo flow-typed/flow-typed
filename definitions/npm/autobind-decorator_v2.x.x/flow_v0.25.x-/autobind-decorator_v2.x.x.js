b7a92bc735c7a40c52de9d19c8b8f0855fb04c5b
Alexey Vykhrystyuk <AVykhrystyuk@gmail.com>
2019-03-14 23:59:11 +0300

    [autobind-decorator_v2.x.x] Added missing types

--- definitions/npm/autobind-decorator_v2.x.x/flow_v0.25.x-/autobind-decorator_v2.x.x.js
+++ definitions/npm/autobind-decorator_v2.x.x/flow_v0.25.x-/autobind-decorator_v2.x.x.js
@@ -1,3 +1,15 @@
+interface TypedPropertyDescriptor<T> {
+  enumerable?: boolean;
+  configurable?: boolean;
+  writable?: boolean;
+  value?: T;
+  get?: () => T;
+  set?: (value: T) => void;
+}
+
+declare type ClassDecorator = <TFunction: Function>(target: TFunction) => TFunction;
+declare type MethodDecorator = <T>(target: Object, propertyKey: string | Symbol, descriptor: TypedPropertyDescriptor<T>) => TypedPropertyDescriptor<T>;
+
 declare module 'autobind-decorator' {
     declare var autobind: ClassDecorator & MethodDecorator;
 
