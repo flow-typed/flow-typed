d3b7c01653103c91bbaa6e4d7e2b99eecd12fa91
Alexey Vykhrystyuk <AVykhrystyuk@gmail.com>
2019-03-25 21:53:20 +0300

    [autobind-decorator_v2.x.x] Use describe/it to limit scope

--- definitions/npm/autobind-decorator_v2.x.x/test_autobind-decorator_v2.x.x.js
+++ definitions/npm/autobind-decorator_v2.x.x/test_autobind-decorator_v2.x.x.js
@@ -4,68 +4,87 @@ import autobind, { boundMethod, boundClass } from 'autobind-decorator';
 import type { TypedPropertyDescriptor } from 'autobind-decorator';
 
 class Foo {
-  method(): void {}
+  method(): void { }
   get property(): number { return 1; }
   set property(value: number): void { }
 }
 
-/* boundClass */
-
-(boundClass(Foo): typeof(Foo));
-
 function BarCtor() { }
-(boundClass(BarCtor): typeof(BarCtor));
 
-//$ExpectError BarCtor is incompatible with Foo
-(boundClass(BarCtor): typeof(Foo));
+describe('boundClass', () => {
+  it('returns result of correct type', () => {
+    (boundClass(Foo): typeof (Foo));
 
-// $ExpectError Foo is incompatible with string
-(boundClass(Foo): string);
+    (boundClass(BarCtor): typeof (BarCtor));
+  });
 
+  it('errors when types incompatible', () => {
+    //$ExpectError BarCtor is incompatible with Foo
+    (boundClass(BarCtor): typeof (Foo));
 
+    // $ExpectError Foo is incompatible with string
+    (boundClass(Foo): string);
+  });
+});
 
-/* boundMethod */
 
 const methodDescriptor = {
   enumerable: true,
   configurable: true,
   writable: true,
 };
-(boundMethod(Foo, 'method', methodDescriptor): TypedPropertyDescriptor<any>);
 
 const propertyDescriptor = {
   enumerable: true,
   configurable: true,
   writable: true,
   get: () => 1,
-  set: (value) => {},
+  set: (value) => { },
   value: 1,
 };
-(boundMethod(Foo, 'property', propertyDescriptor): TypedPropertyDescriptor<number>);
 
-// $ExpectError number is incompatible with string
-boundMethod(Foo, 123, propertyDescriptor);
+describe('boundMethod', () => {
+  it('returns result of correct type', () => {
+    (boundMethod(Foo, 'method', methodDescriptor): TypedPropertyDescriptor<any>);
 
-const propertyDescriptorString = {
-  enumerable: true,
-  configurable: true,
-  writable: true,
-  get: () => '1',
-  set: (value: string) => {},
-  value: '1',
-};
-// $ExpectError string is incompatible with number
-(boundMethod(Foo, 'property', propertyDescriptorString): TypedPropertyDescriptor<number>);
+    (boundMethod(Foo, 'property', propertyDescriptor): TypedPropertyDescriptor<number>);
+  });
+
+  it('errors when types incompatible', () => {
+    // $ExpectError number is incompatible with string
+    boundMethod(Foo, 123, propertyDescriptor);
+
+    const propertyDescriptorString = {
+      enumerable: true,
+      configurable: true,
+      writable: true,
+      get: () => '1',
+      set: (value: string) => { },
+      value: '1',
+    };
+    // $ExpectError string is incompatible with number
+    (boundMethod(Foo, 'property', propertyDescriptorString): TypedPropertyDescriptor<number>);
+  });
+});
 
 
-/* autobind */
 
-(autobind(Foo): typeof(Foo));
+describe('autobind', () => {
+  it('returns result of correct type', () => {
+    (autobind(Foo): typeof (Foo));
 
-(autobind(Foo, 'method', methodDescriptor): TypedPropertyDescriptor<any>);
+    (autobind(Foo, 'method', methodDescriptor): TypedPropertyDescriptor<any>);
+  });
 
-// $ExpectError Foo is incompatible with string
-(autobind(Foo): string);
+  it('errors when types incompatible', () => {
+    // $ExpectError Foo is incompatible with string
+    (autobind(Foo): string);
 
-// $ExpectError number is incompatible with string
-autobind(Foo, 123, propertyDescriptor)
+    // $ExpectError number is incompatible with string
+    autobind(Foo, 123, propertyDescriptor);
+  });
+});
+
+
+
+
