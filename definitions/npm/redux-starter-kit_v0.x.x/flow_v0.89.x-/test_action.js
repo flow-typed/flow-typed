4bdbc29dbe3bcc622dbc4854009aff7bd1807978
Omid Ebrahimi <ebrahimi.omid69@gmail.com>
2019-04-10 22:11:52 +0430

    [redux-starter-kit_v0.x.x] refactors test_action.js

--- definitions/npm/redux-starter-kit_v0.x.x/flow_v0.89.x-/test_action.js
+++ definitions/npm/redux-starter-kit_v0.x.x/flow_v0.89.x-/test_action.js
@@ -1,18 +1,26 @@
 // @flow
-import type {PayloadAction} from 'redux-starter-kit'
+import type { PayloadAction } from "redux-starter-kit";
+import { describe, it } from "flow-typed-test";
 
-opaque type Person = {name: string, id: number}
+opaque type Person = { name: string, id: number }
 
-const action1: PayloadAction<number, "increment"> = { type: "increment", payload: 3 };
+describe("PayloadAction<P, T>", () => {
 
-const action2: PayloadAction<Person> = {
-  type: "addPerson", payload: {name: "Omid", id: 1}
-};
+  it("should pass, if used properly", () => {
+    const action1: PayloadAction<number, "increment"> = { type: "increment", payload: 3 };
+    const action2: PayloadAction<Person> = {
+      type: "addPerson", payload: { name: "Omid", id: 1 }
+    };
+  });
 
-// $ExpectError
-const action3: PayloadAction<number> = { type: "increment", payload: "3" };
+  it("should raise an error, if payload had an invalid type", () => {
 
-// $ExpectError
-const action4: PayloadAction<Person> = {
-  type: "addPerson", payload: {name: "Omid"}
-};
+    // $ExpectError
+    const action1: PayloadAction<number> = { type: "increment", payload: "3" };
+
+    // $ExpectError
+    const action2: PayloadAction<Person> = { type: "addPerson", payload: { name: "Omid" } };
+
+  });
+
+});
