c3394e6b799bc6aa89d6d9c7f91b3c5f74de7496
David <offantik@gmail.com>
2019-04-28 23:43:55 +0300

    Add colors

---
+++ definitions/npm/@material-ui/core_v4.x.x/flow_v0.90.x-/core_v4.x.x.js
@@ -0,0 +1,125 @@
+declare module "@material-ui/core/colors" {
+  declare export var amber: $ElementType<$Exports<"@material-ui/core/colors/amber">, "default">;
+  declare export var blue: $ElementType<$Exports<"@material-ui/core/colors/blue">, "default">;
+  declare export var blueGrey: $ElementType<$Exports<"@material-ui/core/colors/blueGrey">, "default">;
+  declare export var brown: $ElementType<$Exports<"@material-ui/core/colors/brown">, "default">;
+  declare export var common: $ElementType<$Exports<"@material-ui/core/colors/common">, "default">;
+  declare export var cyan: $ElementType<$Exports<"@material-ui/core/colors/cyan">, "default">;
+  declare export var deepOrange: $ElementType<$Exports<"@material-ui/core/colors/deepOrange">, "default">;
+  declare export var deepPurple: $ElementType<$Exports<"@material-ui/core/colors/deepPurple">, "default">;
+  declare export var green: $ElementType<$Exports<"@material-ui/core/colors/green">, "default">;
+  declare export var grey: $ElementType<$Exports<"@material-ui/core/colors/grey">, "default">;
+  declare export var indigo: $ElementType<$Exports<"@material-ui/core/colors/indigo">, "default">;
+  declare export var lightBlue: $ElementType<$Exports<"@material-ui/core/colors/lightBlue">, "default">;
+  declare export var lightGreen: $ElementType<$Exports<"@material-ui/core/colors/lightGreen">, "default">;
+  declare export var lime: $ElementType<$Exports<"@material-ui/core/colors/lime">, "default">;
+  declare export var orange: $ElementType<$Exports<"@material-ui/core/colors/orange">, "default">;
+  declare export var pink: $ElementType<$Exports<"@material-ui/core/colors/pink">, "default">;
+  declare export var purple: $ElementType<$Exports<"@material-ui/core/colors/purple">, "default">;
+  declare export var red: $ElementType<$Exports<"@material-ui/core/colors/red">, "default">;
+  declare export var teal: $ElementType<$Exports<"@material-ui/core/colors/teal">, "default">;
+  declare export var yellow: $ElementType<$Exports<"@material-ui/core/colors/yellow">, "default">;
+}
+declare module "@material-ui/core/colors/amber" {
+  import type { Color } from "@material-ui/core";
+  declare export default Color;
+}
+declare module "@material-ui/core/colors/blue" {
+  import type { Color } from "@material-ui/core";
+  declare export default Color;
+}
+declare module "@material-ui/core/colors/blueGrey" {
+  import type { Color } from "@material-ui/core";
+  declare export default Color;
+}
+declare module "@material-ui/core/colors/brown" {
+  import type { Color } from "@material-ui/core";
+  declare export default Color;
+}
+declare module "@material-ui/core/colors/common" {
+  declare export type CommonColors = {|
+    black: string,
+    white: string
+  |};
+
+  declare export default CommonColors;
+}
+declare module "@material-ui/core/colors/cyan" {
+  import type { Color } from "@material-ui/core";
+  declare export default Color;
+}
+declare module "@material-ui/core/colors/deepOrange" {
+  import type { Color } from "@material-ui/core";
+  declare export default Color;
+}
+declare module "@material-ui/core/colors/deepPurple" {
+  import type { Color } from "@material-ui/core";
+  declare export default Color;
+}
+declare module "@material-ui/core/colors/green" {
+  import type { Color } from "@material-ui/core";
+  declare export default Color;
+}
+declare module "@material-ui/core/colors/grey" {
+  import type { Color } from "@material-ui/core";
+  declare export default Color;
+}
+declare module "@material-ui/core/colors/indigo" {
+  import type { Color } from "@material-ui/core";
+  declare export default Color;
+}
+declare module "@material-ui/core/colors/lightBlue" {
+  import type { Color } from "@material-ui/core";
+  declare export default Color;
+}
+declare module "@material-ui/core/colors/lightGreen" {
+  import type { Color } from "@material-ui/core";
+  declare export default Color;
+}
+declare module "@material-ui/core/colors/lime" {
+  import type { Color } from "@material-ui/core";
+  declare export default Color;
+}
+declare module "@material-ui/core/colors/orange" {
+  import type { Color } from "@material-ui/core";
+  declare export default Color;
+}
+declare module "@material-ui/core/colors/pink" {
+  import type { Color } from "@material-ui/core";
+  declare export default Color;
+}
+declare module "@material-ui/core/colors/purple" {
+  import type { Color } from "@material-ui/core";
+  declare export default Color;
+}
+declare module "@material-ui/core/colors/red" {
+  import type { Color } from "@material-ui/core";
+  declare export default Color;
+}
+declare module "@material-ui/core/colors/teal" {
+  import type { Color } from "@material-ui/core";
+  declare export default Color;
+}
+declare module "@material-ui/core/colors/yellow" {
+  import type { Color } from "@material-ui/core";
+  declare export default Color;
+}
+
+declare module "@material-ui/core" {
+  declare export type Color = {|
+    "50": string,
+    "100": string,
+    "200": string,
+    "300": string,
+    "400": string,
+    "500": string,
+    "600": string,
+    "700": string,
+    "800": string,
+    "900": string,
+    A100: string,
+    A200: string,
+    A400: string,
+    A700: string
+  |};
+}
