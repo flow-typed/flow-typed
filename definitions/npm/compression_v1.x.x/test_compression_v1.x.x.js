2076d7dc39da2d609252276df2fc4a3ed571a7f0
Edoardo L'Astorina <edoardo@blufra.me>
2019-06-08 11:33:29 +0100

    Unit Tests: Describe and It blocks

--- definitions/npm/compression_v1.x.x/test_compression_v1.x.x.js
+++ definitions/npm/compression_v1.x.x/test_compression_v1.x.x.js
@@ -1,18 +1,27 @@
+import { describe, it } from 'flow-typed-test';
 import compression from 'compression';
 
-compression();
+describe('compression', () => {
+  it('runs with no options', () => {
+    compression();
+  });
 
-compression({
-  chunkSize: 16384,
-  filter: (req, res) => true,
-  flush: 0,
-  finishFlush: 4,
-  level: 6,
-  memLevel: 8,
-  strategy: 0,
-  threshold: 1024,
-  windowBits: 15,
+  it('runs with options', () => {
+    compression({
+      chunkSize: 16384,
+      filter: (req, res) => true,
+      flush: 0,
+      finishFlush: 4,
+      level: 6,
+      memLevel: 8,
+      strategy: 0,
+      threshold: 1024,
+      windowBits: 15,
+    });
+  });
+
+  it('errors with extraneous options', () => {
+    // $ExpectError
+    compression({ babababa: '1' });
+  });
 });
-
-// $ExpectError
-compression({ babababa: '1' });
