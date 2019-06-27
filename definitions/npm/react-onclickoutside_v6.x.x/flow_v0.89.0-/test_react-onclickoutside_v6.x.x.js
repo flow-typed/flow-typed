ffd9f5f979da32bf6c5f8f0879ab0085b732c7f8
Björgvin Ragnarsson <nifgraup@gmail.com>
2019-06-27 20:06:20 +0300

    fixup! react-onclickoutside: return an AbstractComponent with instance

--- definitions/npm/react-onclickoutside_v6.x.x/flow_v0.89.0-/test_react-onclickoutside_v6.x.x.js
+++ definitions/npm/react-onclickoutside_v6.x.x/flow_v0.89.0-/test_react-onclickoutside_v6.x.x.js
@@ -1,56 +1,57 @@
-//@flow
-
 import React from 'react';
 import onClickOutside, { IGNORE_CLASS_NAME } from 'react-onclickoutside';
+import { describe, it } from 'flow-typed-test';
 
-IGNORE_CLASS_NAME.slice();
+describe('react-onclickoutside', () => {
+  it('IGNORE_CLASS_NAME', () => {
+    IGNORE_CLASS_NAME.slice();
 
-// $ExpectError Cannot perform arithmetic operation because string literal `ignore-react-onclickoutside` [1] is not a number.
-IGNORE_CLASS_NAME - 1;
+    // $ExpectError Cannot perform arithmetic operation because string literal `ignore-react-onclickoutside` [1] is not a number.
+    IGNORE_CLASS_NAME - 1;
+  });
 
-// class and const are in same line to make all flow versions unhappy in the same line.
-// $ExpectError Cannot call `onClickOutside` because string [1] is incompatible with boolean [2].
-class FailComponent extends React.Component<{ preventDefault: string }> {render() {return <div/>;}} const FailComponentWithClickOutside = onClickOutside(FailComponent);
-<FailComponentWithClickOutside />;
+  it('should not to wrap a component with preventDefault props of incorrect type', () => {
+    // $ExpectError Cannot call `onClickOutside` because string [1] is incompatible with boolean [2].
+    class FailComponent extends React.Component<{ preventDefault: string }> {
+      render() {
+        return <div/>;
+      }
+    }
+    const FailComponentWithClickOutside = onClickOutside(FailComponent);
+    <FailComponentWithClickOutside />;
+  });
 
-class A extends React.Component<{a: string, preventDefault: boolean }> {
-  render() {
-    return this.props.preventDefault ? <div>{this.props.a}</div> : null;
-  }
-}
+  describe('should wrap a component and', () => {
+    class A extends React.Component<{a: string, preventDefault: boolean }> {
+      render() {
+        return this.props.preventDefault ? <div>{this.props.a}</div> : null;
+      }
+    }
+    const B = onClickOutside(A);
 
-const B = onClickOutside(A);
+    it('not accept incorrect props', () => {
+      // $ExpectError
+      <B />;
+      // $ExpectError
+      <B a={12}/>;
+      // $ExpectError
+      <B a="foo" preventDefault="no" />;
+    });
 
-{
-  // $ExpectError
-  <B />
-}
+    it('accept correct props', () => {
+        <B a="foo" />;
+        <B a="foo" preventDefault={true} />;
+    });
 
-{
-  // $ExpectError
-  <B a={12}/>
-}
+    it('access a method on the wrapped component', () => {
+      const fn = ref => ref ? ref.getInstance().render() : null;
+      <B a="foo" ref={fn} />;
+    });
 
-{
-  <B a="foo" />
-}
-
-{
-  // $ExpectError
-  <B a="foo" preventDefault="no" />
-}
-
-{
-  <B a="foo" preventDefault={true} />
-}
-
-{
-  const fn = ref => ref ? ref.getInstance().render() : null;
-  <B a="foo" ref={fn}/>
-}
-
-{
-  // $ExpectError
-  const fn = ref => ref ? ref.getInstance().nonexisting() : null;
-  <B a="foo" ref={fn}/>
-}
+    it('error when a non-existing method is accessed', () => {
+      // $ExpectError
+      const fn = ref => ref ? ref.getInstance().nonexisting() : null;
+      <B a="foo" ref={fn} />;
+    });
+  });
+});
