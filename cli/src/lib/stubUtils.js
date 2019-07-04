70ae7337f324d76be375e8eb4ef37ffaa81ec784
Akinori MUSHA <knu@idaemons.org>
2019-07-04 17:23:57 +0900

    Generate stub declarations for directory modules
    With this change, stubs for `core-js/stable` and `core-js/es/symbol` are generated for the core-js package, for example.

--- cli/src/lib/stubUtils.js
+++ cli/src/lib/stubUtils.js
@@ -239,6 +239,11 @@ async function writeStub(
         if (name === 'index') {
           aliases.push(format(aliasTemplate, moduleName, '', packageName));
           aliases.push(format(aliasTemplate, moduleName, ext, packageName));
+        } else if (path.basename(name) === 'index') {
+          const dirModuleName = packageName + '/' + path.dirname(file);
+          fileDecls.push(format(moduleStubTemplate, dirModuleName));
+          aliases.push(format(aliasTemplate, moduleName, '', dirModuleName));
+          aliases.push(format(aliasTemplate, moduleName, ext, dirModuleName));
         } else {
           fileDecls.push(format(moduleStubTemplate, moduleName));
           aliases.push(format(aliasTemplate, moduleName, ext, moduleName));
