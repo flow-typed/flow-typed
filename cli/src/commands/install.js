8dd71ac316596dcc79f840632f5fbe9b1dd2c79f
andretshurotshka <andwebar@gmail.com>
2019-07-06 01:20:45 +0500

    Use plural messages (#3420)

--- cli/src/commands/install.js
+++ cli/src/commands/install.js
@@ -450,7 +450,7 @@ async function installNpmLibDefs({
           : ['a libdef', 'this package', 'it'];
       console.log(
         `\n` +
-          `I've generated ${'`'}any${'`'}-typed stubs for ${plural[1]}, but ` +
+          `We've generated ${'`'}any${'`'}-typed stubs for ${plural[1]}, but ` +
           `consider submitting \n` +
           `${plural[0]} for ${plural[2]} to ` +
           `${colors.bold('https://github.com/flowtype/flow-typed/')}\n`,
