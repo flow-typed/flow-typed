fee107a974e423964e0aa894ab73569125681ee4
jamesgeorge007 <jamesgeorge998001@gmail.com>
2019-07-06 15:45:12 +0530

    fix: eslint

--- cli/src/commands/info.js
+++ cli/src/commands/info.js
@@ -1,26 +1,23 @@
 // @flow
 
-import {path} from '../lib/node.js';
-
 import type {Argv} from 'yargs';
 import typeof Yargs from 'yargs';
 
 const envinfo = require('envinfo');
 
 export const name = 'info';
-export const description = 'Print debugging information concerning the local environment';
+export const description =
+  'Print debugging information concerning the local environment';
 export function setup(yargs: Yargs) {
-  return yargs
-    .strict();
+  return yargs.strict();
 }
-export async function run(argv: Argv) {
+export async function run(argv: Argv) { // eslint-disable-line
   console.log('\nEnvironment Info:');
-  const data = await envinfo
-      .run({
-        System: ['OS', 'CPU'],
-        Binaries: ['Node', 'Yarn', 'npm'],
-        Browsers: ['Chrome', 'Edge', 'Firefox', 'Safari'],
-        npmGlobalPackages: ['flow-typed'],
-      })
-  console.log(data);    
+  const data = await envinfo.run({
+    System: ['OS', 'CPU'],
+    Binaries: ['Node', 'Yarn', 'npm'],
+    Browsers: ['Chrome', 'Edge', 'Firefox', 'Safari'],
+    npmGlobalPackages: ['flow-typed'],
+  });
+  console.log(data);
 }
