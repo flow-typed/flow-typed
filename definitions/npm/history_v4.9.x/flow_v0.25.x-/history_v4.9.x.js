cbbf57e8a5bbc06fdad992febb21542b7f9e13c8
Mickey Burks <brks.mck@gmail.com>
2019-07-09 15:35:33 -0700

    [history_v4.9.x] Add parsePath, createPath definitions (#3431)

--- definitions/npm/history_v4.9.x/flow_v0.25.x-/history_v4.9.x.js
+++ definitions/npm/history_v4.9.x/flow_v0.25.x-/history_v4.9.x.js
@@ -1,8 +1,7 @@
-declare module "history" {
-
+declare module 'history' {
   declare function Unblock(): void;
 
-  declare export type Action = "PUSH" | "REPLACE" | "POP";
+  declare export type Action = 'PUSH' | 'REPLACE' | 'POP';
 
   declare export type BrowserLocation = {
     pathname: string,
@@ -14,19 +13,21 @@ declare module "history" {
   };
 
   declare interface IBrowserHistory {
-    length: number,
-    location: BrowserLocation,
-    action: Action,
-    push(path: string, state?: {}): void,
-    push(location: $Shape<BrowserLocation>): void,
-    replace(path: string, state?: {}): void,
-    replace(location: $Shape<BrowserLocation>): void,
-    go(n: number): void,
-    goBack(): void,
-    goForward(): void,
-    listen((location: BrowserLocation, action: Action) => void): void,
-    block(message: string): typeof Unblock,
-    block((location: BrowserLocation, action: Action) => string): typeof Unblock,
+    length: number;
+    location: BrowserLocation;
+    action: Action;
+    push(path: string, state?: {}): void;
+    push(location: $Shape<BrowserLocation>): void;
+    replace(path: string, state?: {}): void;
+    replace(location: $Shape<BrowserLocation>): void;
+    go(n: number): void;
+    goBack(): void;
+    goForward(): void;
+    listen((location: BrowserLocation, action: Action) => void): void;
+    block(message: string): typeof Unblock;
+    block(
+      (location: BrowserLocation, action: Action) => string
+    ): typeof Unblock;
   }
 
   declare export type BrowserHistory = IBrowserHistory;
@@ -36,11 +37,13 @@ declare module "history" {
     forceRefresh?: boolean,
     getUserConfirmation?: (
       message: string,
-      callback: (willContinue: boolean) => void,
+      callback: (willContinue: boolean) => void
     ) => void,
   };
 
-  declare function createBrowserHistory(opts?: BrowserHistoryOpts): BrowserHistory;
+  declare function createBrowserHistory(
+    opts?: BrowserHistoryOpts
+  ): BrowserHistory;
 
   declare export type MemoryLocation = {
     pathname: string,
@@ -52,23 +55,23 @@ declare module "history" {
   };
 
   declare interface IMemoryHistory {
-    length: number,
-    location: MemoryLocation,
-    action: Action,
-    index: number,
-    entries: Array<string>,
-    push(path: string, state?: {}): void,
-    push(location: $Shape<MemoryLocation>): void,
-    replace(path: string, state?: {}): void,
-    replace(location: $Shape<MemoryLocation>): void,
-    go(n: number): void,
-    goBack(): void,
-    goForward(): void,
+    length: number;
+    location: MemoryLocation;
+    action: Action;
+    index: number;
+    entries: Array<string>;
+    push(path: string, state?: {}): void;
+    push(location: $Shape<MemoryLocation>): void;
+    replace(path: string, state?: {}): void;
+    replace(location: $Shape<MemoryLocation>): void;
+    go(n: number): void;
+    goBack(): void;
+    goForward(): void;
     // Memory only
-    canGo(n: number): boolean,
-    listen((location: MemoryLocation, action: Action) => void): void,
-    block(message: string): typeof Unblock,
-    block((location: MemoryLocation, action: Action) => string): typeof Unblock,
+    canGo(n: number): boolean;
+    listen((location: MemoryLocation, action: Action) => void): void;
+    block(message: string): typeof Unblock;
+    block((location: MemoryLocation, action: Action) => string): typeof Unblock;
   }
 
   declare export type MemoryHistory = IMemoryHistory;
@@ -79,7 +82,7 @@ declare module "history" {
     keyLength?: number,
     getUserConfirmation?: (
       message: string,
-      callback: (willContinue: boolean) => void,
+      callback: (willContinue: boolean) => void
     ) => void,
   };
 
@@ -92,32 +95,38 @@ declare module "history" {
   };
 
   declare interface IHashHistory {
-    length: number,
-    location: HashLocation,
-    action: Action,
-    push(path: string, state?: {}): void,
-    push(location: $Shape<HashLocation>): void,
-    replace(path: string, state?: {}): void,
-    replace(location: $Shape<HashLocation>): void,
-    go(n: number): void,
-    goBack(): void,
-    goForward(): void,
-    listen((location: HashLocation, action: Action) => void): void,
-    block(message: string): typeof Unblock,
-    block((location: HashLocation, action: Action) => string): typeof Unblock,
-    push(path: string): void,
+    length: number;
+    location: HashLocation;
+    action: Action;
+    push(path: string, state?: {}): void;
+    push(location: $Shape<HashLocation>): void;
+    replace(path: string, state?: {}): void;
+    replace(location: $Shape<HashLocation>): void;
+    go(n: number): void;
+    goBack(): void;
+    goForward(): void;
+    listen((location: HashLocation, action: Action) => void): void;
+    block(message: string): typeof Unblock;
+    block((location: HashLocation, action: Action) => string): typeof Unblock;
+    push(path: string): void;
   }
 
   declare export type HashHistory = IHashHistory;
 
   declare type HashHistoryOpts = {
     basename?: string,
-    hashType: "slash" | "noslash" | "hashbang",
+    hashType: 'slash' | 'noslash' | 'hashbang',
     getUserConfirmation?: (
       message: string,
-      callback: (willContinue: boolean) => void,
+      callback: (willContinue: boolean) => void
     ) => void,
   };
 
   declare function createHashHistory(opts?: HashHistoryOpts): HashHistory;
+
+  declare function parsePath(path: string): BrowserLocation | MemoryLocation | HashLocation;
+
+  declare function createPath(
+    path: BrowserLocation | MemoryLocation | HashLocation
+  ): string;
 }
