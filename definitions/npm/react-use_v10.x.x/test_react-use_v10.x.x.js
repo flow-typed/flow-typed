de7d61638d39a98313c664072d76c75c3b2a626c
Parth Narielwala <parth.narielwala@trunkclub.com>
2019-08-01 17:20:25 -0500

    Add more hooks

--- definitions/npm/react-use_v10.x.x/test_react-use_v10.x.x.js
+++ definitions/npm/react-use_v10.x.x/test_react-use_v10.x.x.js
@@ -4,33 +4,71 @@ import { describe, it } from 'flow-typed-test';
 import * as hooks from 'react-use';
 
 describe('react-use', () => {
-  describe('usePrevious', () => {
-    it('should error when casting to another type', () => {
-      let MyComponent = () => {
-        const foo: number = 0;
+  it('useAsync', () => {
+    let MyComponent = () => {
+      // $ExpectError
+      hooks.useAsync(() => console.log('foo'));
+
+      type ResponseT = { data: string };
+      let loading, error, value;
+      [loading, error, value] = hooks.useAsync<ResponseT>(async () =>
+        Promise.resolve({ data: 'foo' })
+      );
+
+      [loading, error, value] = hooks.useAsync<ResponseT>(async () =>
         // $ExpectError
-        const bar: string = hooks.usePrevious(foo);
-      };
-    });
+        Promise.resolve()
+      );
+
+      // $ExpectError
+      hooks.useAsync(async () => Promise.resolve(), { foo: 'bar' });
+    };
   });
 
-  describe('useEffectOnce', () => {
-    it('should error when casting to another type', () => {
-      let MyComponent = () => {
-        hooks.useEffectOnce(() => {
-          console.log('should work');
-        });
+  it('useAsyncFn', () => {
+    let MyComponent = () => {
+      // $ExpectError
+      hooks.useAsync(() => console.log('foo'));
 
-        hooks.useEffectOnce(() => {
-          return () => console.log('should work');
-        });
+      type ResponseT = { data: string };
+      let loading, error, value, fn;
+      [[loading, error, value], fn] = hooks.useAsyncFn<ResponseT>(async () =>
+        Promise.resolve({ data: 'foo' })
+      );
 
+      [[loading, error, value], fn] = hooks.useAsyncFn<ResponseT>(async () =>
         // $ExpectError
-        hooks.useEffectOnce('should not work');
+        Promise.resolve()
+      );
 
-        // $ExpectError
-        hooks.useEffectOnce(() => 'should not work');
-      };
-    });
+      // $ExpectError
+      hooks.useAsync(async () => Promise.resolve(), { foo: 'bar' });
+    };
+  });
+
+  it('usePrevious', () => {
+    let MyComponent = () => {
+      const foo: number = 0;
+      // $ExpectError
+      const bar: string = hooks.usePrevious(foo);
+    };
+  });
+
+  it('useEffectOnce', () => {
+    let MyComponent = () => {
+      hooks.useEffectOnce(() => {
+        console.log('should work');
+      });
+
+      hooks.useEffectOnce(() => {
+        return () => console.log('should work');
+      });
+
+      // $ExpectError
+      hooks.useEffectOnce('should not work');
+
+      // $ExpectError
+      hooks.useEffectOnce(() => 'should not work');
+    };
   });
 });
