76d67ee8596f566e6cd6761d5f9625b2d9468d42
David <offantik@gmail.com>
2019-08-06 00:36:13 +0300

    Fix Object is any

--- definitions/npm/mysql_v2.x.x/flow_v0.104.x-/mysql_v2.x.x.js
+++ definitions/npm/mysql_v2.x.x/flow_v0.104.x-/mysql_v2.x.x.js
@@ -17,7 +17,7 @@ declare module "mysql" {
     connectTimeout?: number,
     stringifyObjects?: boolean,
     insecureAuth?: boolean,
-    typeCast?: boolean | ((field: Object, next: Function) => any),
+    typeCast?: boolean | ((field: {...}, next: Function) => any),
     queryFormat?: (query: string, values: ?mixed, timezone: string) => string,
     supportBigNumbers?: boolean,
     bigNumberStrings?: boolean,
@@ -27,13 +27,13 @@ declare module "mysql" {
     trace?: boolean,
     multipleStatements?: boolean,
     flags?: string,
-    ssl?: string | Object,
+    ssl?: string | {...},
     ...
   };
 
   declare type QueryOptions = {
     sql: string,
-    typeCast?: boolean | ((field: Object, next: Function) => any),
+    typeCast?: boolean | ((field: {...}, next: Function) => any),
     // string form is a separator used to produce column names
     nestTables?: boolean | string,
     values?: Array<mixed>,
@@ -73,7 +73,7 @@ declare module "mysql" {
 
     query(
       sql: QueryOptions,
-      values?: Array<mixed> | Object,
+      values?: Array<mixed> | {...},
       callback?: QueryCallback
     ): Query;
     query(sql: QueryOptions, callback?: QueryCallback): Query;
