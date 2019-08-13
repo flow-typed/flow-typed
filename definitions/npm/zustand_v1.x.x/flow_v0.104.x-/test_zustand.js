abd859d65264d14442c5bb09e2481d4e1b49be3f
paulshen <paulshen0@gmail.com>
2019-08-12 17:37:48 -0700

    Test equality and api

--- definitions/npm/zustand_v1.x.x/flow_v0.104.x-/test_zustand.js
+++ definitions/npm/zustand_v1.x.x/flow_v0.104.x-/test_zustand.js
@@ -1,6 +1,6 @@
 // @flow strict
 import create from 'zustand';
-import * as React from 'react';
+import shallow from 'zustand/shallow';
 
 function test_basic() {
   const [useStore] = create(set => ({
@@ -10,12 +10,39 @@ function test_basic() {
   }));
 
   function Counter() {
-    const count = useStore(state => state.count);
-    return <h1>{count}</h1>;
+    const count: number = useStore(state => state.count);
   }
 
   function Controls() {
-    const increase = useStore(state => state.increase);
-    return <button onClick={increase}>up</button>;
+    const increase: () => void = useStore(state => state.increase);
   }
+}
+
+function test_equality() {
+  const [useStore] = create(set => ({
+    foo: 1,
+    bar: 'baz',
+  }));
+
+  const { foo, bar } = useStore(
+    state => ({ foo: state.foo, bar: state.bar }),
+    shallow
+  );
+
+  (foo: number);
+  (bar: string);
+}
+
+function test_api() {
+  const [, api] = create(() => ({ a: 1, b: 2, c: 3 }));
+
+  const num: number = api.getState().a;
+  const unsub1 = api.subscribe(state => console.log('state changed', state));
+  const unsub2 = api.subscribe(a => console.log('a changed', a), {
+    selector: state => state.a,
+  });
+  api.setState({ a: 1 });
+  unsub1();
+  unsub2();
+  api.destroy();
 }
