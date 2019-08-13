9e050e8de6b144168007cbb82d322a7c907157ea
paulshen <paulshen0@gmail.com>
2019-08-12 17:55:12 -0700

    Improve tests

--- definitions/npm/zustand_v1.x.x/flow_v0.84.x-/test_shallow.js
+++ definitions/npm/zustand_v1.x.x/flow_v0.84.x-/test_shallow.js
@@ -1,5 +1,15 @@
 // @flow strict
+import { describe, it } from 'flow-typed-test';
 import shallow, { shallow as namedShallow } from 'zustand/shallow';
 
-(shallow({ a: 1 }, { b: 2 }): boolean);
-(namedShallow({ a: 1 }, { b: 2 }): boolean);
+describe('zustand shallow', () => {
+  it('works', () => {
+    (shallow({ a: 1 }, { b: 2 }): boolean);
+    (namedShallow({ a: 1 }, { b: 2 }): boolean);
+
+    // $ExpectError
+    (shallow({ a: 1 }, { b: 2 }): string);
+    // $ExpectError
+    (namedShallow({ a: 1 }, { b: 2 }): number);
+  });
+});
--- definitions/npm/zustand_v1.x.x/flow_v0.84.x-/test_zustand.js
+++ definitions/npm/zustand_v1.x.x/flow_v0.84.x-/test_zustand.js
@@ -1,48 +1,55 @@
 // @flow strict
+import { describe, it } from 'flow-typed-test';
 import create from 'zustand';
 import shallow from 'zustand/shallow';
 
-function test_basic() {
-  const [useStore] = create(set => ({
-    count: 0,
-    increase: () => set(state => ({ count: state.count + 1 })),
-    reset: () => set({ count: 0 }),
-  }));
+describe('zustand', () => {
+  it('supports basic use', () => {
+    const [useStore] = create(set => ({
+      count: 0,
+      increase: () => set(state => ({ count: state.count + 1 })),
+      reset: () => set({ count: 0 }),
+    }));
 
-  function Counter() {
-    const count: number = useStore(state => state.count);
-  }
+    function Counter() {
+      const count: number = useStore(state => state.count);
+    }
 
-  function Controls() {
-    const increase: () => void = useStore(state => state.increase);
-  }
-}
-
-function test_equality() {
-  const [useStore] = create(set => ({
-    foo: 1,
-    bar: 'baz',
-  }));
-
-  const { foo, bar } = useStore(
-    state => ({ foo: state.foo, bar: state.bar }),
-    shallow
-  );
-
-  (foo: number);
-  (bar: string);
-}
-
-function test_api() {
-  const [, api] = create(() => ({ a: 1, b: 2, c: 3 }));
-
-  const num: number = api.getState().a;
-  const unsub1 = api.subscribe(state => console.log('state changed', state));
-  const unsub2 = api.subscribe(a => console.log('a changed', a), {
-    selector: state => state.a,
+    function Controls() {
+      const increase: () => void = useStore(state => state.increase);
+    }
   });
-  api.setState({ a: 1 });
-  unsub1();
-  unsub2();
-  api.destroy();
-}
+
+  it('supports equality fn', () => {
+    const [useStore] = create(set => ({
+      foo: 1,
+      bar: 'baz',
+    }));
+
+    const { foo, bar } = useStore(
+      state => ({ foo: state.foo, bar: state.bar }),
+      shallow
+    );
+
+    (foo: number);
+    (bar: string);
+  });
+
+  it('supports api', () => {
+    const [, api] = create(() => ({ a: 1, b: 2, c: 3 }));
+
+    const num: number = api.getState().a;
+    // $ExpectError
+    const num: string = api.getState().b;
+    const unsub1 = api.subscribe(state => console.log('state changed', state));
+    const unsub2 = api.subscribe(a => console.log('a changed', a), {
+      selector: state => state.a,
+    });
+    api.setState({ a: 1 });
+    unsub1();
+    unsub2();
+    // $ExpectError
+    api.setState({ b: [] });
+    api.destroy();
+  });
+});
