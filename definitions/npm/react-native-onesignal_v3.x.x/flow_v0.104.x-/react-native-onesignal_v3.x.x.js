74f19da261f9273e6dd2f322c70fe93acd372ddb
David <offantik@gmail.com>
2019-08-19 12:55:36 +0300

    Update react-native-onesignal types

--- definitions/npm/react-native-onesignal_v3.x.x/flow_v0.104.x-/react-native-onesignal_v3.x.x.js
+++ definitions/npm/react-native-onesignal_v3.x.x/flow_v0.104.x-/react-native-onesignal_v3.x.x.js
@@ -1,38 +1,40 @@
-declare module "react-native-onesignal" {
-  // https://github.com/geektimecoil/react-native-onesignal/blob/master/index.js#L61-L64
+declare module 'react-native-onesignal' {
+  // https://github.com/geektimecoil/react-native-onesignal/blob/f7ce2a9c9592f3a497eda88fc6ab2dcf235a3f15/index.js#L80-L87
   declare export type EventTypes =
-    | "received"
-    | "opened"
-    | "ids"
-    | "emailSubscription";
+    | 'emailSubscription'
+    | 'ids'
+    | 'inAppMessageClicked'
+    | 'opened'
+    | 'received';
 
   // IOSPermissions & IOSPermissionsReturn:  number return getPermissionSubscriptionState method, boolean use in requestPermissions
   declare export type IOSPermissions = {|
     alert?: boolean,
     badge?: boolean,
-    sound?: boolean
+    sound?: boolean,
   |};
+
   declare export type IOSPermissionsReturn = {|
     alert: number,
     badge: number,
-    sound: number
+    sound: number,
   |};
 
   // https://documentation.onesignal.com/docs/react-native-sdk#section--setloglevel-
   declare export type LogLevels =
-    | 0 //None
-    | 1 //Fatal
-    | 2 //Errors
-    | 3 //Warnings
-    | 4 //Info
-    | 5 //Debug
-    | 6; //Verbose
+    | 0 // None
+    | 1 // Fatal
+    | 2 // Errors
+    | 3 // Warnings
+    | 4 // Info
+    | 5 // Debug
+    | 6; // Verbose
 
-  // https://documentation.onesignal.com/v5.0/docs/react-native-sdk#section-both-ios-and-android
+  // https://documentation.onesignal.com/docs/react-native-sdk#section-both-ios-and-android
   declare export type FocusBehaviors =
-    | 0 //None
-    | 1 //InAppAlert
-    | 2; //Notification
+    | 0 // None
+    | 1 // InAppAlert
+    | 2; // Notification
 
   declare export type PermissionSubscriptionState = $ReadOnly<{|
     emailAddress: ?string,
@@ -43,46 +45,72 @@ declare module "react-native-onesignal" {
     pushToken: ?string,
     subscriptionEnabled: boolean,
     userId: string,
-    userSubscriptionEnabled: boolean
+    userSubscriptionEnabled: boolean,
   |}>;
 
-  declare export type Tags = { [key: string]: string, ... };
+  declare export type Tags = { +[key: string]: string, ... };
+
+  // https://documentation.onesignal.com/docs/react-native-sdk#section-ios-initialization-parameters
+  declare export type IosInitOptions = {|
+    kOSSettingsKeyAutoPrompt?: boolean,
+    kOSSettingsKeyInAppLaunchURL?: boolean,
+    kOSSSettingsKeyPromptBeforeOpeningPushURL?: boolean,
+    kOSSettingsKeyInFocusDisplayOption?: 0 | 1 | 2,
+  |};
+
+  declare export type OnIdsEvent = $ReadOnly<{|
+    pushToken: string,
+    userId: string,
+  |}>;
+
+  declare export type InAppMessageClickEvent = $ReadOnly<{|
+    clickUrl?: string,
+    clickName?: string,
+    firstClick: boolean,
+    closesMessage: boolean,
+  |}>;
+
+  declare export type TriggerValue = string | number;
 
   declare export default class OneSignal {
-    static addEventListener(type: EventTypes, handler: Function): void;
+    static addEventListener(type: 'ids', handler: (OnIdsEvent) => mixed): void;
+    static addEventListener(
+      type: 'inAppMessageClicked',
+      handler: (InAppMessageClickEvent) => mixed
+    ): void;
+    static addEventListener(
+      type: 'emailSubscription' | 'opened' | 'received',
+      handler: (...args: any) => mixed
+    ): void;
 
-    static removeEventListener(type: EventTypes, handler: Function): void;
+    static removeEventListener(
+      type: EventTypes,
+      handler: (...args: any) => mixed
+    ): void;
 
     static clearListeners(): void;
 
-    static configure(): void;
-
-    static init(appId: string, iOSSettings?: Object): void;
+    static init(appId: string, iOSSettings?: IosInitOptions): void;
 
     static getPermissionSubscriptionState(
-      (state: PermissionSubscriptionState) => void
+      (state: PermissionSubscriptionState) => mixed
     ): void;
 
     static sendTag(key: string, value: string): void;
-
     static sendTags(tags: Tags): void;
-
-    static getTags((receivedTags: $ReadOnly<Tags>) => void): void;
-
+    static getTags((receivedTags: $ReadOnly<Tags>) => mixed): void;
     static deleteTag(key: string): void;
 
-    // https://github.com/geektimecoil/react-native-onesignal/blob/master/index.js#L238-L250
+    // https://github.com/geektimecoil/react-native-onesignal/blob/f7ce2a9c9592f3a497eda88fc6ab2dcf235a3f15/index.js#L276-L292
     static setEmail(email: string, emailAuthCode: string): void;
     static setEmail(
       email: string,
       emailAuthCode: string,
-      (error?: Error) => void
+      (error?: Error) => mixed
     ): void;
-
-    static setEmail(email: string, (error: ?Error) => void): void;
+    static setEmail(email: string, (error: ?Error) => mixed): void;
     static setEmail(email: string): void;
-
-    static logoutEmail((error: ?Error) => void): void;
+    static logoutEmail((error: ?Error) => mixed): void;
 
     static setLocationShared(shared: boolean): void;
 
@@ -93,10 +121,10 @@ declare module "react-native-onesignal" {
     static inFocusDisplaying(displayOption: FocusBehaviors): void;
 
     static postNotification(
-      contents: Object,
-      data: Array<*>,
+      contents: any,
+      data: any,
       playerId: string,
-      otherParameters: Object
+      otherParameters: any
     ): void;
 
     static syncHashedEmail(email: string): void;
@@ -104,35 +132,39 @@ declare module "react-native-onesignal" {
     static setLogLevel(nsLogLevel: LogLevels, visualLogLevel: LogLevels): void;
 
     static setRequiresUserPrivacyConsent(required: boolean): void;
-
     static provideUserConsent(granted: boolean): void;
-
     static userProvidedPrivacyConsent(): Promise<boolean>;
 
-    // IOS only
+    static setExternalUserId(userId: string): void;
+
+    static removeExternalUserId(): void;
+
+    // iOS only
     static registerForPushNotifications(): void;
-
-    static promptForPushNotificationsWithUserResponse(
-      (result: boolean) => void
-    ): void;
-
     static requestPermissions(permissions: ?IOSPermissions): void;
-
-    static checkPermissions(
-      (permissions: $ReadOnly<IOSPermissionsReturn>) => void
+    static promptForPushNotificationsWithUserResponse(
+      (result: boolean) => mixed
     ): void;
-
+    static checkPermissions(
+      (permissions: $ReadOnly<IOSPermissionsReturn>) => mixed
+    ): void;
     static promptForPushNotificationPermissions(
-      (result: boolean) => void
+      (result: boolean) => mixed
     ): void;
 
     // Android only
     static enableVibrate(enable: boolean): void;
-
     static enableSound(enable: boolean): void;
-
     static clearOneSignalNotifications(): void;
-
     static cancelNotification(id: number): void;
+    static addTrigger(key: string, value: TriggerValue): void;
+    static addTriggers(triggers: { +[key: string]: TriggerValue, ... }): void;
+    static removeTriggerForKey(key: string): void;
+    static removeTriggersForKeys(keys: $ReadOnlyArray<string>): void;
+    static pauseInAppMessages(pause: boolean): void;
+    static getTriggerValueForKey(
+      key: string,
+      handler: (value: ?TriggerValue) => mixed
+    ): void;
   }
 }
--- definitions/npm/react-native-onesignal_v3.x.x/flow_v0.104.x-/test_react-native-onesignal.js
+++ definitions/npm/react-native-onesignal_v3.x.x/flow_v0.104.x-/test_react-native-onesignal.js
@@ -1,121 +1,136 @@
 // @flow
 import OneSignal, {
-  type PermissionSubscriptionState
-} from "react-native-onesignal";
-import { it, describe } from "flow-typed-test";
+  type PermissionSubscriptionState,
+} from 'react-native-onesignal';
+import { it, describe } from 'flow-typed-test';
 
-describe("Listeners", () => {
-  it("must call addEventListener and removeEventListener with supported event types", () => {
+describe('Listeners', () => {
+  it('must call addEventListener and removeEventListener with supported event types', () => {
     const simpleHandler = () => {};
 
-    OneSignal.addEventListener("received", simpleHandler);
-    OneSignal.addEventListener("opened", simpleHandler);
-    OneSignal.addEventListener("ids", simpleHandler);
-    OneSignal.addEventListener("emailSubscription", simpleHandler);
+    OneSignal.addEventListener('received', simpleHandler);
+    OneSignal.addEventListener('opened', simpleHandler);
+    OneSignal.addEventListener('emailSubscription', simpleHandler);
+    OneSignal.addEventListener('ids', event => {
+      (event.pushToken: string);
+      (event.userId: string);
 
-    OneSignal.removeEventListener("received", simpleHandler);
-    OneSignal.removeEventListener("opened", simpleHandler);
-    OneSignal.removeEventListener("ids", simpleHandler);
-    OneSignal.removeEventListener("emailSubscription", simpleHandler);
+      // $ExpectError: check any
+      (event.userId: boolean);
+    });
+    OneSignal.addEventListener('inAppMessageClicked', event => {
+      (event.clickUrl: ?string);
+      (event.clickName: ?string);
+      (event.firstClick: boolean);
+      (event.closesMessage: boolean);
+
+      // $ExpectError: check any
+      (event.closesMessage: number);
+    });
+
+    OneSignal.removeEventListener('received', simpleHandler);
+    OneSignal.removeEventListener('opened', simpleHandler);
+    OneSignal.removeEventListener('ids', simpleHandler);
+    OneSignal.removeEventListener('emailSubscription', simpleHandler);
     OneSignal.clearListeners();
   });
 
-  it("must throw error when type not supported", () => {
+  it('must throw error when type not supported', () => {
     // $ExpectError
-    OneSignal.addEventListener("someEventName", simpleHandler);
+    OneSignal.addEventListener('someEventName', simpleHandler);
     // $ExpectError
-    OneSignal.removeEventListener("someEventName", simpleHandler);
+    OneSignal.removeEventListener('someEventName', simpleHandler);
   });
 });
 
-describe("Tags methods", () => {
-  describe("sendTag", () => {
-    it("must call sendTag with key,value arguments", () => {
-      OneSignal.sendTag("key", "value");
+describe('Tags methods', () => {
+  describe('sendTag', () => {
+    it('must call sendTag with key,value arguments', () => {
+      OneSignal.sendTag('key', 'value');
     });
 
     it("must throw error when args aren't enough", () => {
       // $ExpectError
       OneSignal.sendTag();
       // $ExpectError
-      OneSignal.sendTag("maybe tag");
+      OneSignal.sendTag('maybe tag');
     });
 
-    it("must call sendTag with ObjectMap tags", () => {
+    it('must call sendTag with ObjectMap tags', () => {
       OneSignal.sendTags({
-        key: "value",
-        key2: "value2"
+        key: 'value',
+        key2: 'value2',
       });
     });
   });
 
-  describe("getTags", () => {
-    it("must return read only tags object", () => {
+  describe('getTags', () => {
+    it('must return read only tags object', () => {
       OneSignal.getTags(receivedTags => {
         // $ExpectError
-        receivedTags.abc = "123";
+        receivedTags.abc = '123';
       });
     });
-    it("must throw error when it called without callback fn", () => {
+    it('must throw error when it called without callback fn', () => {
       // $ExpectError
       OneSignal.getTags();
     });
   });
 
-  it("must call deleteTag only with one string argument", () => {
-    OneSignal.deleteTag("key");
+  it('must call deleteTag only with one string argument', () => {
+    OneSignal.deleteTag('key');
     // $ExpectError
     OneSignal.deleteTag();
   });
 });
 
-describe("Email methods", () => {
-  describe("setEmail", () => {
-    it("must call setEmail with emailAuth code", () => {
-      OneSignal.setEmail("email", "emailAuthCode", (error: ?Error) => {});
-      OneSignal.setEmail("email", "emailAuthCode");
+describe('Email methods', () => {
+  describe('setEmail', () => {
+    it('must call setEmail with emailAuth code', () => {
+      OneSignal.setEmail('email', 'emailAuthCode', (error: ?Error) => {});
+      OneSignal.setEmail('email', 'emailAuthCode');
     });
 
-    it("must call setEmail without emailAuth code", () => {
-      OneSignal.setEmail("email", (error: ?Error) => {});
-      OneSignal.setEmail("email");
+    it('must call setEmail without emailAuth code', () => {
+      OneSignal.setEmail('email', (error: ?Error) => {});
+      OneSignal.setEmail('email');
     });
 
-    it("must throw error when call setEmail without args", () => {
+    it('must throw error when call setEmail without args', () => {
       // $ExpectError
       OneSignal.setEmail();
     });
   });
 
-  it("must call callback fn with error or void", () => {
+  it('must call callback fn with error or void', () => {
     OneSignal.logoutEmail((error: ?Error) => {});
 
     // $ExpectError
     OneSignal.logoutEmail();
   });
 
-  it("must call syncHashedEmail with string", () => {
-    OneSignal.syncHashedEmail("email");
+  it('must call syncHashedEmail with string', () => {
+    OneSignal.syncHashedEmail('email');
     // $ExpectError
     OneSignal.syncHashedEmail();
   });
 });
 
-describe("Others", () => {
-  it("must call setLocationShared with boolean", () => {
+describe('Others', () => {
+  it('must call setLocationShared with boolean', () => {
     OneSignal.setLocationShared(true);
     OneSignal.setLocationShared(false);
     // $ExpectError
     OneSignal.setLocationShared();
   });
 
-  it("must call setSubscription with boolean", () => {
+  it('must call setSubscription with boolean', () => {
     OneSignal.setSubscription(true);
     OneSignal.setSubscription(false);
     // $ExpectError
     OneSignal.setSubscription();
   });
-  it("must call inFocusDisplaying with FocusBehaviors type", () => {
+  it('must call inFocusDisplaying with FocusBehaviors type', () => {
     OneSignal.inFocusDisplaying(0);
     OneSignal.inFocusDisplaying(1);
     OneSignal.inFocusDisplaying(2);
@@ -126,8 +141,8 @@ describe("Others", () => {
   });
 });
 
-describe("setLogLevel", () => {
-  it("must call setLogLevel with supported nsLogLevel", () => {
+describe('setLogLevel', () => {
+  it('must call setLogLevel with supported nsLogLevel', () => {
     OneSignal.setLogLevel(0, 0);
     OneSignal.setLogLevel(1, 0);
     OneSignal.setLogLevel(2, 0);
@@ -137,7 +152,7 @@ describe("setLogLevel", () => {
     OneSignal.setLogLevel(6, 0);
   });
 
-  it("must call setLogLevel with supported visualLogLevel", () => {
+  it('must call setLogLevel with supported visualLogLevel', () => {
     OneSignal.setLogLevel(0, 0);
     OneSignal.setLogLevel(0, 1);
     OneSignal.setLogLevel(0, 2);
@@ -147,7 +162,7 @@ describe("setLogLevel", () => {
     OneSignal.setLogLevel(0, 6);
   });
 
-  it("must throw error when LogLevel more than 6", () => {
+  it('must throw error when LogLevel more than 6', () => {
     // $ExpectError
     OneSignal.setLogLevel(7, 0);
     // $ExpectError
@@ -157,57 +172,57 @@ describe("setLogLevel", () => {
   });
 });
 
-it("must call setRequiresUserPrivacyConsent with boolean", () => {
+it('must call setRequiresUserPrivacyConsent with boolean', () => {
   OneSignal.setRequiresUserPrivacyConsent(true);
   OneSignal.setRequiresUserPrivacyConsent(false);
   // $ExpectError
   OneSignal.setRequiresUserPrivacyConsent();
 });
 
-it("must call provideUserConsent with boolean", () => {
+it('must call provideUserConsent with boolean', () => {
   OneSignal.provideUserConsent(true);
   OneSignal.provideUserConsent(false);
   // $ExpectError
   OneSignal.provideUserConsent();
 });
 
-it("must return promise when call userProvidedPrivacyConsent", () => {
+it('must return promise when call userProvidedPrivacyConsent', () => {
   OneSignal.userProvidedPrivacyConsent().then(result => {
     (result: boolean);
   });
 });
 
-describe("IOS only", () => {
-  it("must have registerForPushNotifications function", () => {
+describe('IOS only', () => {
+  it('must have registerForPushNotifications function', () => {
     OneSignal.registerForPushNotifications();
   });
 
-  it("must call promptForPushNotificationsWithUserResponse with callback", () => {
+  it('must call promptForPushNotificationsWithUserResponse with callback', () => {
     OneSignal.promptForPushNotificationsWithUserResponse(result => {
       (result: boolean);
     });
     OneSignal.promptForPushNotificationsWithUserResponse(() => {});
   });
 
-  it("must call requestPermissions with Permissions", () => {
+  it('must call requestPermissions with Permissions', () => {
     OneSignal.requestPermissions({
       alert: true,
       badge: true,
-      sound: true
+      sound: true,
     });
     OneSignal.requestPermissions({
       alert: false,
-      badge: false
+      badge: false,
     });
 
     OneSignal.requestPermissions({
-      alert: false
+      alert: false,
     });
 
     OneSignal.requestPermissions();
   });
 
-  it("must call checkPermissions with callback adn return read only permissions", () => {
+  it('must call checkPermissions with callback adn return read only permissions', () => {
     OneSignal.checkPermissions(permissions => {
       // $ExpectError
       permissions.abc = 123;
@@ -221,7 +236,7 @@ describe("IOS only", () => {
     OneSignal.checkPermissions();
   });
 
-  it("must call promptForPushNotificationPermissions with callback", () => {
+  it('must call promptForPushNotificationPermissions with callback', () => {
     OneSignal.promptForPushNotificationPermissions(() => {});
     OneSignal.promptForPushNotificationPermissions(result => {
       (result: boolean);
@@ -229,64 +244,63 @@ describe("IOS only", () => {
   });
 });
 
-describe("Android only", () => {
-  it("must call enableVibrate with boolean", () => {
+describe('Android only', () => {
+  it('must call enableVibrate with boolean', () => {
     OneSignal.enableVibrate(true);
     OneSignal.enableVibrate(false);
     // $ExpectError
     OneSignal.enableVibrate();
   });
 
-  it("must call enableSound with boolean", () => {
+  it('must call enableSound with boolean', () => {
     OneSignal.enableSound(true);
     OneSignal.enableSound(false);
     // $ExpectError
     OneSignal.enableSound();
   });
 
-  it("must call cancelNotification with number", () => {
+  it('must call cancelNotification with number', () => {
     OneSignal.cancelNotification(0);
     // $ExpectError
-    OneSignal.cancelNotification("");
+    OneSignal.cancelNotification('');
     // $ExpectError
     OneSignal.cancelNotification();
   });
 
-  it("must call it without errors", () => {
+  it('must call it without errors', () => {
     OneSignal.clearOneSignalNotifications();
   });
 });
 
-it("must have static method", () => {
-  OneSignal.configure();
+it('must have static method', () => {
   OneSignal.promptLocation();
 });
 
-it("must call init with id and oprtions for IOS", () => {
+it('must call init with id and oprtions for IOS', () => {
   var IOSSettings = {};
 
-  OneSignal.init("id");
-  OneSignal.init("id", IOSSettings);
+  OneSignal.init('id');
+  OneSignal.init('id', IOSSettings);
   // $ExpectError
   OneSignal.init();
 });
 
-it("must return PermissionSubscriptionState", () => {
+it('must return PermissionSubscriptionState', () => {
   OneSignal.getPermissionSubscriptionState(state => {
     // $ExpectError - read only state
-    state.abc = "123";
+    state.abc = '123';
 
     (state: PermissionSubscriptionState);
   });
 });
 
-it("must call postNotification", () => {
+it('must call postNotification', () => {
   OneSignal.postNotification(
     {
-      en: "You got notification from user"
+      en: 'You got notification from user',
     },
     [],
-    "playerId",
-    { ios_attachments: { image1: "{image_url}" } }
+    'playerId',
+    { ios_attachments: { image1: '{image_url}' } }
   );
 });
