65d2c45a26f8075e390d79f17a289e858f954823
Kirill Danshin <kirill@danshin.pro>
2019-08-29 18:33:22 +0300

    fix git package support; fixes #3363
    Signed-off-by: Kirill Danshin <kirill@danshin.pro>

--- cli/src/lib/npm/__tests__/npmLibDefs-test.js
+++ cli/src/lib/npm/__tests__/npmLibDefs-test.js
@@ -386,6 +386,17 @@ describe('npmLibDefs', () => {
       });
     });
 
+    it('parses libs installed from git', () => {
+      expect(parsePkgNameVer('git@github.com:flow-typed/flow-typed.git')).toEqual({
+        pkgName: 'flow-typed',
+        pkgVersion: {
+          major: 0,
+          minor: 0,
+          patch: 0,
+        },
+      });
+    });
+
     it('parses wildcard minor libs', () => {
       expect(parsePkgNameVer('lib_v1.x.x')).toEqual({
         pkgName: 'lib',
