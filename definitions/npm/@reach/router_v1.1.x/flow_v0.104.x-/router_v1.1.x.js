90daa347f74e9f4767137e8c8ea4031f0199bb98
Evgeny Doroshenko <evgeny.doroshenko@jetbrains.com>
2019-09-03 13:26:09 +0300

    Fix @reach/router `Link` base type: use an exact tag name

--- definitions/npm/@reach/router_v1.1.x/flow_v0.104.x-/router_v1.1.x.js
+++ definitions/npm/@reach/router_v1.1.x/flow_v0.104.x-/router_v1.1.x.js
@@ -71,7 +71,7 @@ declare module '@reach/router' {
   |}>;
 
   declare export type LinkProps<State> = {
-    ...React$ElementProps<string>,
+    ...React$ElementProps<'a'>,
     children: React$Node,
     innerRef?: React$Ref<HTMLAnchorElement>,
     getProps?: (props: {
