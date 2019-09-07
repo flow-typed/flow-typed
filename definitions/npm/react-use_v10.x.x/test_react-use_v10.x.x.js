36a066622f8471a385a82794fe0af79888f30df0
Pascal Duez <pascalduez@users.noreply.github.com>
2019-09-07 14:44:51 +0200

    [react-use_v10.x.x] Init with few supported hooks (#3556)

---
+++ definitions/npm/react-use_v10.x.x/test_react-use_v10.x.x.js
@@ -0,0 +1,74 @@
+// @flow
+import React from 'react';
+import { describe, it } from 'flow-typed-test';
+import * as hooks from 'react-use';
+
+describe('react-use', () => {
+  it('useAsync', () => {
+    let MyComponent = () => {
+      // $ExpectError
+      hooks.useAsync(() => console.log('foo'));
+
+      type ResponseT = { data: string, ... };
+      let loading, error, value;
+      [loading, error, value] = hooks.useAsync<ResponseT>(async () =>
+        Promise.resolve({ data: 'foo' })
+      );
+
+      [loading, error, value] = hooks.useAsync<ResponseT>(async () =>
+        // $ExpectError
+        Promise.resolve()
+      );
+
+      // $ExpectError
+      hooks.useAsync(async () => Promise.resolve(), { foo: 'bar' });
+    };
+  });
+
+  it('useAsyncFn', () => {
+    let MyComponent = () => {
+      // $ExpectError
+      hooks.useAsync(() => console.log('foo'));
+
+      type ResponseT = { data: string, ... };
+      let loading, error, value, fn;
+      [[loading, error, value], fn] = hooks.useAsyncFn<ResponseT>(async () =>
+        Promise.resolve({ data: 'foo' })
+      );
+
+      [[loading, error, value], fn] = hooks.useAsyncFn<ResponseT>(async () =>
+        // $ExpectError
+        Promise.resolve()
+      );
+
+      // $ExpectError
+      hooks.useAsync(async () => Promise.resolve(), { foo: 'bar' });
+    };
+  });
+
+  it('usePrevious', () => {
+    let MyComponent = () => {
+      const foo: number = 0;
+      // $ExpectError
+      const bar: string = hooks.usePrevious(foo);
+    };
+  });
+
+  it('useEffectOnce', () => {
+    let MyComponent = () => {
+      hooks.useEffectOnce(() => {
+        console.log('should work');
+      });
+
+      hooks.useEffectOnce(() => {
+        return () => console.log('should work');
+      });
+
+      // $ExpectError
+      hooks.useEffectOnce('should not work');
+
+      // $ExpectError
+      hooks.useEffectOnce(() => 'should not work');
+    };
+  });
+});
