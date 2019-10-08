0b0c184aa7726425debb1f7ef5cd8db35bf9c1bd
Giancarlo Anemone <ganemone@uber.com>
2019-10-08 13:18:11 -0400

    Fix and consolodate puppeteer type definitions

--- definitions/npm/puppeteer_v1.20.x/flow_v0.76.x-v0.103.x/test_puppeteer_v1.20.x_connect.js
+++ definitions/npm/puppeteer_v1.20.x/flow_v0.76.x-v0.103.x/test_puppeteer_v1.20.x_connect.js
@@ -1,19 +1,19 @@
 // @flow
 
-import { launch } from 'puppeteer';
+import puppeteer from 'puppeteer';
 import { describe, it } from 'flow-typed-test';
 
-describe('launch', () => {
+describe('puppeteer.connect', () => {
   it('should complain if you pass in a string arg', () => {
-    // $ExpectError string is incompatible with `LaunchOptions`
-    launch('testing...');
+    // $ExpectError string is incompatible with `ConnectOptions`
+    puppeteer.connect('testing...');
   });
   it('should complain if an option arg key is of the wrong type', () => {
-    // $ExpectError string is incompatible with boolean inproperty `headless`
-    launch({ headless: 'testing...' });
+    // $ExpectError string is incompatible with boolean in property `ignoreHTTPSErrors`
+    puppeteer.connect({ ignoreHTTPSErrors: 'testing...' });
   });
-  it('should preserve the Browser type', () => {
-    launch().then(browser => {
+  it('should preserve Browser type', () => {
+    puppeteer.connect().then(browser => {
       // $ExpectError string is incompatible with enum
       browser.on('response');
       browser.newPage().then(page => {
@@ -23,10 +23,17 @@ describe('launch', () => {
           request.testing();
         });
       });
+      browser.on('targetcreated', target => {
+        target.createCDPSession().then(cdpSession => {
+          // $ExpectError property `connect` is missing in `CDPSession`
+          cdpSession.connect();
+          cdpSession.detach();
+        });
+      });
     });
   });
-  it('should preserver Page type through launch, etc.', () => {
-    launch().then(browser =>
+  it('should preserve Page type', () => {
+    puppeteer.connect().then(browser =>
       browser.newPage().then(page => {
         // $ExpectError string is incompatible with enum in type argument `K`
         page.on('testing...');
@@ -36,7 +43,7 @@ describe('launch', () => {
           request.testing();
         });
         page.on('response', response => {
-          response.request.url();
+          response.request().url();
           // $ExpectError property `testing` is missing in `Response`
           response.testing();
         });
--- definitions/npm/puppeteer_v1.20.x/flow_v0.76.x-v0.103.x/test_puppeteer_v1.20.x_executablePath.js
+++ definitions/npm/puppeteer_v1.20.x/flow_v0.76.x-v0.103.x/test_puppeteer_v1.20.x_executablePath.js
@@ -1,14 +1,14 @@
 // @flow
 
-import { executablePath } from 'puppeteer';
+import puppeteer from 'puppeteer';
 import { describe, it } from 'flow-typed-test';
 
-describe('executablePath', () => {
+describe('puppeteer.executablePath', () => {
   it('should return a string', () => {
-    (executablePath(): string);
+    (puppeteer.executablePath(): string);
   });
   it('should complain if passed args', () => {
     // $ExpectError no arguments are expected by function type
-    (executablePath('testing...'): string);
+    (puppeteer.executablePath('testing...'): string);
   });
 });
