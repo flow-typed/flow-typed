36d20fd6d2cb697c2db9516b899906b52ab05d78
Niklas Mischkulnig <mischnic@users.noreply.github.com>
2019-12-12 17:37:19 +0100

    Make options object strict

--- definitions/npm/semver_v6.2.x/flow_v0.104.x-/semver_v6.2.x.js
+++ definitions/npm/semver_v6.2.x/flow_v0.104.x-/semver_v6.2.x.js
@@ -203,7 +203,7 @@ declare module "semver" {
   // Coercion
   declare function coerce(
     version: string | SemVer,
-    options?: {...Options, rtl?: boolean}
+    options?: {|...Options, rtl?: boolean|}
   ): ?SemVer
 
   // Not explicitly documented, or deprecated
