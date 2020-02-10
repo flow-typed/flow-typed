0200aaee6d129e2ac5f4604470f64495670e9f9c
Pascal Duez <public@pascalduez.me>
2020-02-10 16:17:13 +0100

    [@sinonjs/fake-timers] Add definitions

---
+++ definitions/npm/@sinonjs/fake-timers_v6.x.x/flow_v0.104.x-/test_fake-timers_v6.x.x.js
@@ -0,0 +1,32 @@
+// @flow
+import { describe, it } from 'flow-typed-test';
+import FakeTimers from '@sinonjs/fake-timers';
+
+describe('@sinonjs/fake-timers', () => {
+  it('install', () => {
+    FakeTimers.install({
+      target: global,
+      now: 150,
+      loopLimit: 250,
+    });
+
+    // $ExpectError
+    FakeTimers.install(true);
+  });
+
+  it('clock', () => {
+    const clock = FakeTimers.install();
+
+    clock.next();
+    clock.tick(150);
+    clock.runAll();
+    const timeoutId = clock.setTimeout(() => {});
+    const intervalId = clock.setInterval(() => {});
+    clock.clearTimeout(timeoutId);
+
+    // $ExpectError
+    clock.clearTimeout(intervalId);
+    clock.clearInterval(intervalId);
+    clock.runToFrame();
+  });
+});
