dad3d6b066231c162a91f8dff6666021e4e738d1
Dmitry Gonchar <dgonchar@atlassian.com>
2020-03-26 15:19:02 +1100

    [reselect_v3.x.x.js] Redefine OutputSelector type as a callable object  (#3795)
    * reselect - fix tests for flow v0.121
    
    * reselect, validator - auto-replace // TEST comments with it()
    
    * reselect - backport #3732 fix to reselect_v3
    
    * re-reselect - add missing imports and fix tests for flow v0.121
    
    * reselect - fix indentation

--- definitions/npm/reselect_v2.x.x/flow_v0.104.x-/test_reselect_2.x.x.js
+++ definitions/npm/reselect_v2.x.x/flow_v0.104.x-/test_reselect_2.x.x.js
@@ -1,4 +1,5 @@
 /* @flow */
+import { it } from "flow-typed-test";
 import {
   createSelector,
   defaultMemoize,
@@ -6,80 +7,80 @@ import {
   createStructuredSelector
 } from 'reselect';
 
-// TEST: Should pass for 2 selectors given as arguments
 type State = {
   x: number,
   y: number,
   ...
 };
 
-const test1Selector = createSelector(
-  (state: State) => state.x,
-  (state: State) => state.y,
-  (x, y) => {
-    return x + y
-  }
-)({x: 100, y: 200})
-// END TEST
-
-// TEST: Should pass for 2 selectors given as array
-createSelector(
-  [
-    (state: State) => state.x,
-    (state: State) => state.y
-  ] ,
-  (x, y) => {
-    return x + y
-  }
-)({x: 100, y: 200})
-// END TEST
-
-// TEST: Should pass when selectors have additional Props argument
 type TestProps = {
   x: number,
   ...
 };
 
-createSelector(
-  (state: State, props: TestProps) => state.x + props.x,
-  (state: State, props: TestProps) => state.y + props.x,
-  (x, y) => {
-    return x + y
-  }
-)({x: 100, y: 200}, {
-  x: 10
+it('Should pass for 2 selectors given as arguments', () => {
+  const test1Selector = createSelector(
+    (state: State) => state.x,
+    (state: State) => state.y,
+    (x, y) => {
+      return x + y
+    }
+  )({x: 100, y: 200})
 });
-// END TEST
 
-// TEST: Should pass for additional arguments
-createSelector(
-  (state: State, props: TestProps, test) => state.x + props.x + test,
-  (state: State, props: TestProps, test) => state.y + props.x + test,
-  (x, y) => {
-    return x + y
-  }
-)({x: 100, y: 200}, {
-  x: 10
-}, 10);
-// END TEST
+it('Should pass for 2 selectors given as array', () => {
+  createSelector(
+    [
+      (state: State) => state.x,
+      (state: State) => state.y
+    ] ,
+    (x, y) => {
+      return x + y
+    }
+  )({x: 100, y: 200})
+});
+
+it('Should pass when selectors have additional Props argument', () => {
+  createSelector(
+    (state: State, props: TestProps) => state.x + props.x,
+    (state: State, props: TestProps) => state.y + props.x,
+    (x, y) => {
+      return x + y
+    }
+  )({x: 100, y: 200}, {
+    x: 10
+  });
+});
+
+it('Should pass for additional arguments', () => {
+  createSelector(
+    (state: State, props: TestProps, test) => state.x + props.x + test,
+    (state: State, props: TestProps, test) => state.y + props.x + test,
+    (x, y) => {
+      return x + y
+    }
+  )({x: 100, y: 200}, {
+    x: 10
+  }, 10);
+});
 
 defaultMemoize((a: number) => a + 1)(2);
 defaultMemoize((a: number) => a + 1, (a, b) => a + b)(2);
 
-// TEST 8: Should threat newly created selector as normal one
-let x = createSelectorCreator(defaultMemoize)(
-  (state) => state.x,
-  (state) => state.y,
-  (x, y) => {
-    return x + y;
-  }
-)
+it('Should threat newly created selector as normal one', () => {
+  let x = createSelectorCreator(defaultMemoize)(
+    (state) => state.x,
+    (state) => state.y,
+    (x, y) => {
+      return x + y;
+    }
+  )
 
-x({
-  x: 10,
-  y: 20
-})
-// END TEST
+  x({
+    x: 10,
+    y: 20
+  })
+});
 
 createStructuredSelector({
   first: (state) => state.x,
@@ -107,6 +108,7 @@ createSelector(
   (x, y) => {
     return x + y
   }
+// $ExpectError: property `d` is missing state but exists in TestState2
 )({x: 100, y: 200});
 
 createSelector(
@@ -118,7 +120,6 @@ createSelector(
   }
 )({x: 100, y: 200}, { x: 20 });
 
-// $ExpectError: Should not result do not include property
 createSelector(
   (state) => state.x,
   (state) => state.y,
@@ -128,6 +129,7 @@ createSelector(
       y
     }
   }
+// $ExpectError: property `d` is missing createSelector() return object
 )({x: 100, y: 200}, { x: 20 }).d;
 
 // $ExpectError
@@ -173,8 +175,9 @@ createSelector(
     first: (state) => state.x,
     second: (state) => state.y
   }),
-  // $ExpectError: Return types of input selectors propogate
+  // $ExpectError: Return types of input selectors propagate
   ({first, second}: {first: number, second: number}) => first + second
+// $ExpectError: property `second` is missing in object
 )({
   x: 10,
   y: false
