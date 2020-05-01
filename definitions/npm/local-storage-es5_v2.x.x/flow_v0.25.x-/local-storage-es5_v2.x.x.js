b501122010177b3b040f894bc3f597adc92a1947
Brian Chen <brian.chen@tabcorp.com.au>
2020-05-01 21:20:49 +1000

    correct some types

--- definitions/npm/local-storage-es5_v2.x.x/flow_v0.25.x-/local-storage-es5_v2.x.x.js
+++ definitions/npm/local-storage-es5_v2.x.x/flow_v0.25.x-/local-storage-es5_v2.x.x.js
@@ -1,14 +1,12 @@
 declare module 'local-storage-es5' {
-  declare type Storage = WindowLocalStorage | WindowSessionStorage
-
   declare type Callback = (value: any, old: any, url: string) => void
 
   declare type Accessor = {|
-    set: (key: string, value: any) => any,
+    set: (key: string, value: any) => boolean,
     get: (key: string) => any,
     remove: (key: string) => any,
     clear: (key: string) => any,
-    backend: (store?: Storage) => Storage,
+    backend: (store: Storage) => Storage,
     on: (key: string, fn: Callback) => void,
     off: (key: string, fn: Callback) => void,
   |};
