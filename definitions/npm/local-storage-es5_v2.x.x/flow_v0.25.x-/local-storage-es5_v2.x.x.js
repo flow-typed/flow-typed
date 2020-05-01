59d5549c3353ae8102e50e49d4e0c3a255eaf437
Brian Chen <brian.chen@tabcorp.com.au>
2020-05-01 21:08:05 +1000

    make exact obj

--- definitions/npm/local-storage-es5_v2.x.x/flow_v0.25.x-/local-storage-es5_v2.x.x.js
+++ definitions/npm/local-storage-es5_v2.x.x/flow_v0.25.x-/local-storage-es5_v2.x.x.js
@@ -3,7 +3,7 @@ declare module 'local-storage-es5' {
 
   declare type Callback = (value: any, old: any, url: string) => void
 
-  declare type Accessor = {
+  declare type Accessor = {|
     set: (key: string, value: any) => any,
     get: (key: string) => any,
     remove: (key: string) => any,
@@ -11,7 +11,7 @@ declare module 'local-storage-es5' {
     backend: (store?: Storage) => Storage,
     on: (key: string, fn: Callback) => void,
     off: (key: string, fn: Callback) => void,
-  };
+  |};
 
   declare module.exports: Accessor;
 }
