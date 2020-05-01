23892749bb704d31cddd286e271fd37bd9df264d
Brian Chen <brian.chen@tabcorp.com.au>
2020-05-01 21:40:06 +1000

    fix exact errors

--- definitions/npm/local-storage-es5_v2.x.x/flow_v0.25.x-/local-storage-es5_v2.x.x.js
+++ definitions/npm/local-storage-es5_v2.x.x/flow_v0.25.x-/local-storage-es5_v2.x.x.js
@@ -2,11 +2,11 @@ declare module 'local-storage-es5' {
   declare type Callback = (value: any, old: any, url: string) => void
 
   declare type Accessor = {|
-    set: (key: string, value: string | number | boolean | {}) => boolean,
+    set: (key: string, value: string | number | boolean | { ... }) => boolean,
     get: (key: string) => any,
     remove: (key: string) => any,
     clear: (key: string) => any,
-    backend: (store: Storage) => Storage,
+    backend: (store?: Storage) => Storage,
     on: (key: string, fn: Callback) => void,
     off: (key: string, fn: Callback) => void,
   |};
