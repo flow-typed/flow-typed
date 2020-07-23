387a235736365dfd392de3f2d535461806b904e3
Drew Larson <me@drewlarson.ca>
2020-07-22 20:52:37 -0600

    Add batch function (#3835)
    Co-authored-by: Jordan Lys <lys.jordan@gmail.com>
    Co-authored-by: Drew Larson <me@drewlarson.ca>"
    
    Co-authored-by: Jordan Lys <lys.jordan@gmail.com>

--- definitions/npm/react-redux_v7.x.x/flow_v0.89.x-v0.103.x/react-redux_v7.x.x.js
+++ definitions/npm/react-redux_v7.x.x/flow_v0.89.x-v0.103.x/react-redux_v7.x.x.js
@@ -278,6 +278,8 @@ declare module "react-redux" {
     connectAdvancedOptions: ?(ConnectAdvancedOptions & EFO),
   ): (component: Com) => React$ComponentType<OP> & $Shape<ST>;
 
+  declare export function batch(() => void): void
+
   declare export default {
     Provider: typeof Provider,
     createProvider: typeof createProvider,
@@ -286,5 +288,6 @@ declare module "react-redux" {
     useDispatch: typeof useDispatch,
     useSelector: typeof useSelector,
     useStore: typeof useStore,
+    batch: typeof batch,
   };
 }
