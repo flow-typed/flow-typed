f41d94d53524a3ea0bf3b55dc3d4b042eca78c93
Aidin Gharibnavaz <aidin@aidinhut.com>
2020-08-14 21:33:19 +1200

    [react-select_v1.x.x] Fix the type of 'cache' prop (#3875)

--- definitions/npm/react-select_v1.x.x/flow_v0.104.x-/react-select_v1.x.x.js
+++ definitions/npm/react-select_v1.x.x/flow_v0.104.x-/react-select_v1.x.x.js
@@ -170,7 +170,7 @@ declare module 'react-select' {
     wrapperStyle?: {...},
     isSearchable?: boolean,
     // whether to cache the search results
-    cache?: boolean
+    cache?: {...} | false
   |};
 
   declare type AsyncProps = {|
