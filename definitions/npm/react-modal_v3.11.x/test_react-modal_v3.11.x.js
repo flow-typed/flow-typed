e78d3359f848d486066087486e580640391a68f2
Akinori MUSHA <knu@idaemons.org>
2020-09-09 15:11:02 +0900

    Specify the exact error types

--- definitions/npm/react-modal_v3.11.x/test_react-modal_v3.11.x.js
+++ definitions/npm/react-modal_v3.11.x/test_react-modal_v3.11.x.js
@@ -9,7 +9,7 @@ describe('Modal.setAppElement', () => {
     ReactModal.setAppElement('#foo');
     ReactModal.setAppElement(document.body);
 
-    // $FlowExpectedError
+    // $FlowExpectedError[incompatible-call]
     ReactModal.setAppElement(1);
   });
 });
@@ -18,55 +18,55 @@ it('isOpen property should be a boolean', () => {
   <ReactModal />;
 
   <ReactModal isOpen />;
-  // $FlowExpectedError
+  // $FlowExpectedError[incompatible-type]
   <ReactModal isOpen={1} />;
 });
 
 it('portalClassName property should be a string', () => {
   < ReactModal portalClassName="foo" />;
-  // $FlowExpectedError
+  // $FlowExpectedError[incompatible-type]
   <ReactModal portalClassName={1} />;
 });
 
 it('bodyOpenClassName property should be a string', () => {
   < ReactModal bodyOpenClassName="foo" />;
-  // $FlowExpectedError
+  // $FlowExpectedError[incompatible-type]
   <ReactModal bodyOpenClassName={1} />;
 });
 
 it('ariaHideApp property should be a boolean', () => {
   <ReactModal ariaHideApp />;
-  // $FlowExpectedError
+  // $FlowExpectedError[incompatible-type]
   <ReactModal ariaHideApp={1} />;
 });
 
 it('closeTimeoutMS property should be a number', () => {
   <ReactModal closeTimeoutMS={10} />;
-  // $FlowExpectedError
+  // $FlowExpectedError[incompatible-type]
   <ReactModal closeTimeoutMS />;
 });
 
 it('shouldFocusAfterRender property should be a boolean', () => {
   <ReactModal shouldFocusAfterRender />;
-  // $FlowExpectedError
+  // $FlowExpectedError[incompatible-type]
   <ReactModal shouldFocusAfterRender={1} />;
 });
 
 it('shouldCloseOnEsc property should be a boolean', () => {
   <ReactModal shouldCloseOnEsc />;
-  // $FlowExpectedError
+  // $FlowExpectedError[incompatible-type]
   <ReactModal shouldCloseOnEsc={1} />;
 });
 
 it('shouldCloseOnOverlayClick property should be a boolean', () => {
   <ReactModal shouldCloseOnOverlayClick />;
-  // $FlowExpectedError
+  // $FlowExpectedError[incompatible-type]
   <ReactModal shouldCloseOnOverlayClick={1} />;
 });
 
 it('shouldReturnFocusAfterClose property should be a boolean', () => {
   <ReactModal shouldReturnFocusAfterClose />;
-  // $FlowExpectedError
+  // $FlowExpectedError[incompatible-type]
   <ReactModal shouldReturnFocusAfterClose={1} />;
 });
 
@@ -74,16 +74,16 @@ it('style should be an object with optional content and overlay settings', () =>
   <ReactModal style={{}} />;
   <ReactModal style={{ content: {} }} />;
   <ReactModal style={{ overlay: {} }} />;
-  // $FlowExpectedError
+  // $FlowExpectedError[incompatible-type]
   <ReactModal style={{ content: 1 }} />;
-  // $FlowExpectedError
+  // $FlowExpectedError[incompatible-type]
   <ReactModal style={{ overlay: 1 }} />;
 });
 
 it('parentSelector property should be a callback', () => {
-  // $FlowExpectedError
+  // $FlowExpectedError[incompatible-type]
   <ReactModal parentSelector={1} />;
-  // $FlowExpectedError
+  // $FlowExpectedError[incompatible-type]
   <ReactModal parentSelector="#foo" />;
   <ReactModal parentSelector={() => document.body} />;
 });
@@ -91,26 +91,26 @@ it('parentSelector property should be a callback', () => {
 it('onAfterOpen property should be a callback', () => {
   <ReactModal onAfterOpen={() => { }} />;
   <ReactModal onAfterOpen={async () => { }} />;
-  // $FlowExpectedError
+  // $FlowExpectedError[incompatible-type]
   <ReactModal onAfterOpen={1} />;
 });
 
 it('onRequestClose property should be a callback', () => {
   <ReactModal onRequestClose={() => { }} />;
   <ReactModal onRequestClose={e => { }} />;
-  // $FlowExpectedError
+  // $FlowExpectedError[incompatible-type]
   <ReactModal onRequestClose={1} />;
 });
 
 it('role property should be a string', () => {
   < ReactModal role="foo" />;
-  // $FlowExpectedError
+  // $FlowExpectedError[incompatible-type]
   <ReactModal role={1} />;
 });
 
 it('contentLabel property should be a string', () => {
   < ReactModal contentLabel="foo" />;
-  // $FlowExpectedError
+  // $FlowExpectedError[incompatible-type]
   <ReactModal contentLabel={1} />;
 });
 
@@ -118,7 +118,7 @@ it('aria property should be an object', () => {
   <ReactModal aria={{
     labelledby: 'foo'
   }} />;
-  // $FlowExpectedError
+  // $FlowExpectedError[incompatible-type]
   <ReactModal aria={1} />;
 });
 
@@ -129,7 +129,7 @@ it('className can be a string or an object', () => {
     afterOpen: "foo",
     beforeClose: "foo"
   }} />;
-  // $FlowExpectedError
+  // $FlowExpectedError[incompatible-type]
   <ReactModal className={1} />;
 });
 
@@ -140,24 +140,24 @@ it('overlayClassName can be a string or an object', () => {
     afterOpen: "foo",
     beforeClose: "foo"
   }} />;
-  // $FlowExpectedError
+  // $FlowExpectedError[incompatible-type]
   <ReactModal overlayClassName={1} />;
 });
 
 it('overlayRef property should be a ref callback ', () => {
   let n;
   <ReactModal overlayRef={node => { n = node; }} />;
-  // $FlowExpectedError
+  // $FlowExpectedError[incompatible-type]
   <ReactModal overlayRef={1} />;
-  // $FlowExpectedError
+  // $FlowExpectedError[incompatible-type]
   <ReactModal overlayRef="foo" />;
 });
 
 it('containerRef property should be a ref callback ', () => {
   let n;
   <ReactModal containerRef={node => { n = node; }} />;
-  // $FlowExpectedError
+  // $FlowExpectedError[incompatible-type]
   <ReactModal containerRef={1} />;
-  // $FlowExpectedError
+  // $FlowExpectedError[incompatible-type]
   <ReactModal containerRef="foo" />;
 });
