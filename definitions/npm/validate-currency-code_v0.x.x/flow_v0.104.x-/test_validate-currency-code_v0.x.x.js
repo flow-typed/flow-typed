117bf619c8baed957276b990d9b86a28a29792e5
Neil Wang <neil.wang@alumni.unc.edu>
2020-11-16 18:31:19 -0800

    add error codes to error suppressions, remove unnecessary declares

--- definitions/npm/validate-currency-code_v0.x.x/flow_v0.104.x-/test_validate-currency-code_v0.x.x.js
+++ definitions/npm/validate-currency-code_v0.x.x/flow_v0.104.x-/test_validate-currency-code_v0.x.x.js
@@ -5,9 +5,9 @@ import isCurrencyCode from 'validate-currency-code';
 
 const value: boolean = isCurrencyCode('USD')
 
-// $FlowExpectedError if string is not passed into validator
+// $FlowExpectedError[incompatible-call] if string is not passed into validator
 isCurrencyCode()
 
-// $FlowExpectedError if number is passed into validator
+// $$FlowFixMe[incompatible-call] if number is passed into validator
 isCurrencyCode(1)
 
