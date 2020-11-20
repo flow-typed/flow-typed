6fe24936fd16034eed30876942b263eddcc7ea88
Mats Byrkjeland <matsbyr@gmail.com>
2020-11-20 17:01:43 +0100

    Add a couple of tests for findTrips

--- definitions/npm/@entur/sdk_v2.x.x/test_sdk_v2.x.x.js
+++ definitions/npm/@entur/sdk_v2.x.x/test_sdk_v2.x.x.js
@@ -2,16 +2,38 @@ import { describe, it } from 'flow-typed-test';
 import createEnturService from '@entur/sdk'
 
 describe('createEnturService', () => {
-  it('create a service', () => {
+  it('creates a service', () => {
     const service = createEnturService({
       clientName: 'flow-typed'
     })
   });
 
-  it('fail if using wrong typefor service', () => {
+  it('fails if using wrong typefor service', () => {
     // $FlowExpectedError[incompatible-type] wrong return type
     const numberService: number = createEnturService({
       clientName: 'flow-typed'
     })
+  });
+})
+
+describe('findTrips', () => {
+  it('can call findTrips', () => {
+    const service = createEnturService({
+      clientName: 'flow-typed'
+    })
+
+    service.findTrips(
+      'Oslo',
+      'Bergen'
+    )
+  });
+
+  it('fails if using parameter is missing', () => {
+    const service = createEnturService({
+      clientName: 'flow-typed'
+    })
+
+    // $FlowExpectedError[incompatible-call]
+    service.findTrips('Oslo')
   });
 })
