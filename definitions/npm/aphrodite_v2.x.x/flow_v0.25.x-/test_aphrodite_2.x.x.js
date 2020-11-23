3c5df0ae098aad98f44d1aaf37b406261eb1afc8
Brian Chen <brian.chen@tabcorp.com.au>
2020-11-23 21:11:25 +1100

    add aphrodite

---
+++ definitions/npm/aphrodite_v2.x.x/flow_v0.25.x-/test_aphrodite_2.x.x.js
@@ -0,0 +1,100 @@
+// @flow
+import {
+  StyleSheet,
+  StyleSheetServer,
+  StyleSheetTestUtils,
+  minify,
+  css,
+  flushToStyleTag,
+  injectAndGetClassName,
+  defaultSelectorHandlers,
+  reset,
+  resetInjectedStyle,
+} from 'aphrodite';
+import {
+  StyleSheet as noImportantStyleSheet,
+  css as noImportantCss,
+} from 'aphrodite/no-important';
+
+describe('aphrodite', () => {
+  it('can create styles', () => {
+    const styles = StyleSheet.create({
+      test: {
+        // $FlowExpectedError[incompatible-call] does not accept numbers
+        color: 0,
+      },
+      test1: {
+        backgroundColor: 'black',
+      },
+    });
+    const s: string = css(styles.test, styles.test1);
+  });
+
+  it('renderStatic', () => {
+    const func = () => '';
+    const rendered = StyleSheetServer.renderStatic(func);
+    rendered.html.toUpperCase();
+    rendered.css.content.toUpperCase();
+    rendered.css.renderedClassNames.map((o) => o.toUpperCase());
+  });
+
+  it('suppressStyleInjection', () => {
+    StyleSheetTestUtils.suppressStyleInjection();
+    // $FlowExpectedError[extra-arg] takes no args
+    StyleSheetTestUtils.suppressStyleInjection('test');
+  });
+
+  it('clearBufferAndResumeStyleInjection', () => {
+    StyleSheetTestUtils.clearBufferAndResumeStyleInjection();
+    // $FlowExpectedError[extra-arg] takes no args
+    StyleSheetTestUtils.clearBufferAndResumeStyleInjection('test');
+  });
+
+  it('getBufferedStyles', () => {
+    const t: Array<string> = StyleSheetTestUtils.getBufferedStyles();
+    // $FlowExpectedError[extra-arg] takes no args
+    StyleSheetTestUtils.getBufferedStyles('test');
+  });
+
+  it('minify', () => {
+    minify(true);
+    // $FlowExpectedError[incompatible-call]
+    minify('test');
+  });
+
+  it('flushToStyleTag', () => {
+    flushToStyleTag();
+    // $FlowExpectedError[extra-arg] takes no args
+    flushToStyleTag('test');
+  });
+
+  it('injectAndGetClassName & defaultSelectorHandlers', () => {
+    const t: string = injectAndGetClassName(true, [], defaultSelectorHandlers);
+    // $FlowExpectedError[incompatible-call]
+    injectAndGetClassName(true, [], '');
+  });
+
+  it('reset', () => {
+    reset();
+    // $FlowExpectedError[extra-arg]
+    reset('test');
+  });
+
+  it('resetInjectedStyle', () => {
+    const t: void = resetInjectedStyle('test');
+    // $FlowExpectedError[incompatible-call]
+    resetInjectedStyle();
+  });
+});
+
+describe('aphrodite/no-important', () => {
+  it('should work as normal', () => {
+    const styles = noImportantStyleSheet.create({
+      test: {
+        // $FlowExpectedError[incompatible-call] does not accept numbers
+        color: 0,
+      },
+    });
+    const s: string = noImportantCss(styles.test);
+  });
+});
