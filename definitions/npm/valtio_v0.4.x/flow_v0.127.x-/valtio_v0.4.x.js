f47376d4190e731e5b17509234063996473a24b4
Pascal Duez <pascalduez@users.noreply.github.com>
2020-11-25 22:07:50 +0100

    [valtio_v0.4.x] Add snapshot utility (#3946)
    - Links to documentation: https://github.com/pmndrs/valtio
    - Link to GitHub or NPM: https://github.com/pmndrs/valtio
    - Type of contribution: addition

---
+++ definitions/npm/valtio_v0.4.x/flow_v0.127.x-/valtio_v0.4.x.js
@@ -0,0 +1,9 @@
+declare module 'valtio' {
+  declare export function proxy<T: { ... }>(initialObject?: T): T;
+  declare export function useProxy<T: { ... }>(proxy: T): T;
+  declare export function snapshot<T: { ... }>(proxy: T): T;
+  declare export function subscribe(
+    proxy: any,
+    callback: () => void
+  ): () => void;
+}
