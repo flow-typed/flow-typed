9477c049af77101d11fa23c97ee6b77f151f7a74
Eliot Alter <eliot@stripe.com>
2021-01-07 08:42:37 -0800

    Don't use global types for match-sorter_v5.x.x

--- definitions/npm/match-sorter_v5.x.x/flow_v0.104.x-/match-sorter_v5.x.x.js
+++ definitions/npm/match-sorter_v5.x.x/flow_v0.104.x-/match-sorter_v5.x.x.js
@@ -1,25 +1,26 @@
-type $npm$matchSorter$KeyFn<T> = (item: T) => string | Array<String>;
-type $npm$matchSorter$KeyObj = {|
-  key: string,
-  minRanking?: number,
-  maxRanking?: number,
-  threshold?: number,
-|};
-
-type $npm$matchSorter$Rankings = {|
-  CASE_SENSITIVE_EQUAL: number,
-  EQUAL: number,
-  STARTS_WITH: number,
-  WORD_STARTS_WITH: number,
-  CONTAINS: number,
-  ACRONYM: number,
-  MATCHES: number,
-  NO_MATCH: number,
-|};
-
 declare module 'match-sorter' {
+  declare type Rankings = {|
+    CASE_SENSITIVE_EQUAL: number,
+    EQUAL: number,
+    STARTS_WITH: number,
+    WORD_STARTS_WITH: number,
+    CONTAINS: number,
+    ACRONYM: number,
+    MATCHES: number,
+    NO_MATCH: number,
+  |};
+
+  declare type KeyFn<T> = (item: T) => string | Array<String>;
+
+  declare type KeyObj = {|
+    key: string,
+    minRanking?: number,
+    maxRanking?: number,
+    threshold?: number,
+  |};
+
   declare type Options<T> = {|
-    keys: Array<string | $npm$matchSorter$KeyFn<T> | $npm$matchSorter$KeyObj>,
+    keys: Array<string | KeyFn<T> | KeyObj>,
     threshold?: number,
     keepDiacritics?: boolean,
   |};
@@ -30,6 +31,6 @@ declare module 'match-sorter' {
       value: string,
       options?: Options<T>
     ): Array<T>,
-    rankings: $npm$matchSorter$Rankings,
+    rankings: Rankings,
   |};
 }
