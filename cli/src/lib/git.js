775addb83f4d0250919427efc8a879d8e9170107
Brian Chen <brian.chen@tabcorp.com.au>
2021-02-01 16:53:37 +1100

    try to fetch all

--- cli/src/lib/git.js
+++ cli/src/lib/git.js
@@ -74,6 +74,7 @@ export async function getDiff() {
     let {stdout} = await child_process.spawnP(gitPath, ['diff', '--name-only']);
 
     if (stdout === '') {
+      await child_process.spawnP(gitPath, ['fetch', '--all']);
       // We are probably already on master, so compare to the last commit.
       const {stdout: headDiff} = await child_process.spawnP(gitPath, [
         'diff',
