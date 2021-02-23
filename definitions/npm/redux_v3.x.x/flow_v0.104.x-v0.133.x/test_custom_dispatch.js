889dc6c53606ac9186e8998feca76852a3140085
Brian Chen <brian.chen@tabcorp.com.au>
2021-02-24 07:15:04 +1100

    fix v3 tests

--- definitions/npm/redux_v3.x.x/flow_v0.104.x-v0.133.x/test_custom_dispatch.js
+++ definitions/npm/redux_v3.x.x/flow_v0.104.x-v0.133.x/test_custom_dispatch.js
@@ -23,11 +23,11 @@ createStore(
   [1],
   applyMiddleware((api: MiddlewareAPI<State, Action, Dispatch>) => {
     api.dispatch({ type: 'A' });
-    // $FlowExpectedError
+    // $FlowExpectedError[incompatible-call]
     api.dispatch({ type: 'wrong' }); // wrong action
 
     api.dispatch((dispatch, getState) => {});
-    // $FlowExpectedError
+    // $FlowExpectedError[incompatible-call]
     api.dispatch(() => false);
 
     return next => next;
@@ -42,7 +42,7 @@ declare var dispatch: Dispatch;
 
 const ac1 = bindActionCreators((n: number) => ({ type: 'A' }), dispatch);
 ac1(1);
-// $FlowExpectedError
+// $FlowExpectedError[incompatible-call]
 bindActionCreators((n: number) => ({ type: 'wrong' }), dispatch); // wrong action
 
 const ac2 = bindActionCreators(
@@ -50,7 +50,7 @@ const ac2 = bindActionCreators(
   dispatch
 );
 ac2(1);
-// $FlowExpectedError
+// $FlowExpectedError[incompatible-call]
 bindActionCreators((n: number) => (dispatch, getState) => 'wrong', dispatch); // wrong thunk
 
 //
@@ -61,9 +61,9 @@ declare var myEnhancer: StoreEnhancer<State, Action, Dispatch>;
 const store: Store = createStore(reducer, [1], myEnhancer);
 
 store.dispatch({ type: 'A' });
-// $FlowExpectedError
+// $FlowExpectedError[incompatible-call]
 store.dispatch({ type: 'wrong' }); // wrong action
 
 store.dispatch((dispatch, getState) => {});
-// $FlowExpectedError
+// $FlowExpectedError[incompatible-call]
 store.dispatch((dispatch, getState) => false); // wrong action
