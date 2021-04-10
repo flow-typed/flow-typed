eb80f6a481a3caa3ed76a3cc83e8d28cb7571f12
Brian Chen <brian.chen@tabcorp.com.au>
2021-04-10 17:12:46 +1000

    use redux defs directly copied from redux

--- definitions/npm/redux-thunk_v2.x.x/flow_v0.83.x-/redux-thunk_v2.x.x.js
+++ definitions/npm/redux-thunk_v2.x.x/flow_v0.83.x-/redux-thunk_v2.x.x.js
@@ -1,4 +1,4 @@
-// Copied basic type structure from redux so that
+// Copied types from redux so that
 // redux-thunk can be used with redux and redux-mock-store.
 //
 // This should be replaced with imports in the not too distant future once we
@@ -6,24 +6,24 @@
 declare module '@@redux' {
   declare export type DispatchAPI<A> = (action: A) => A;
 
-  declare export type Dispatch = DispatchAPI<{ type: any, ... }>;
+  declare export type Dispatch<A: { type: *, ... }> = DispatchAPI<A>;
 
-  declare export type MiddlewareAPI = {
-    dispatch: Dispatch,
-    getState(): any,
+  declare export type MiddlewareAPI<S, A, D = Dispatch<A>> = {
+    dispatch: D,
+    getState(): S,
     ...
   };
 
-  declare export type Middleware = (
-    api: MiddlewareAPI
-  ) => (next: any) => any;
+  declare export type Middleware<S, A, D = Dispatch<A>> = (
+    api: MiddlewareAPI<S, A, D>
+  ) => (next: D) => D;
 }
 
 declare module 'redux-thunk' {
   import type { Middleware } from '@@redux';
 
-  declare export type Thunk = Middleware & {|
-    withExtraArgument(arg: $NonMaybeType<mixed>): Middleware,
+  declare export type Thunk = Middleware<any, any> & {|
+    withExtraArgument(arg: $NonMaybeType<mixed>): Middleware<any, any>,
   |};
 
   declare module.exports: Thunk;
