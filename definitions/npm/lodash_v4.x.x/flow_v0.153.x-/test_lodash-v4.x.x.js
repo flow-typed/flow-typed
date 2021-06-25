b570a9c3bd37337c1d890abaebe2dd7988e261ce
Brian Chen <brian.chen@tabcorp.com.au>
2021-06-25 21:55:04 +1000

    align errors with older version

--- definitions/npm/lodash_v4.x.x/flow_v0.153.x-/test_lodash-v4.x.x.js
+++ definitions/npm/lodash_v4.x.x/flow_v0.153.x-/test_lodash-v4.x.x.js
@@ -528,9 +528,11 @@ describe('Collection', () => {
 
   it('invokeMap', function () {
     (invokeMap([[5, 1, 7], [3, 2, 1]], 'sort'): number[][]);
+    // $FlowExpectedError[method-unbinding] caused using string split function
     (invokeMap(['123', '456'], String.prototype.split, ''): string[][]);
 
     // $FlowExpectedError[incompatible-call]
+    // $FlowExpectedError[method-unbinding] caused using string split function
     invokeMap([123, 456], String.prototype.split, '');
   })
 
