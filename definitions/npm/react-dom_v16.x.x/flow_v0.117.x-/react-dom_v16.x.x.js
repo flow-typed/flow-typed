ad3adf2de8afd46ced6cbeb20dd5733cd3a7fa73
Abel Toledano <atoledano@tuenti.com>
2021-06-30 16:10:25 +0200

    Fix Thenable in react-dom test-utils to work fine in Flow >0.153

--- definitions/npm/react-dom_v16.x.x/flow_v0.117.x-/react-dom_v16.x.x.js
+++ definitions/npm/react-dom_v16.x.x/flow_v0.117.x-/react-dom_v16.x.x.js
@@ -1,23 +1,23 @@
 declare module 'react-dom' {
   declare function findDOMNode(
-    componentOrElement: Element | ?React$Component<any, any>,
+    componentOrElement: Element | ?React$Component<any, any>
   ): null | Element | Text;
 
   declare function render<ElementType: React$ElementType>(
     element: React$Element<ElementType>,
     container: Element,
-    callback?: () => void,
+    callback?: () => void
   ): React$ElementRef<ElementType>;
 
   declare function hydrate<ElementType: React$ElementType>(
     element: React$Element<ElementType>,
     container: Element,
-    callback?: () => void,
+    callback?: () => void
   ): React$ElementRef<ElementType>;
 
   declare function createPortal(
     node: React$Node,
-    container: Element,
+    container: Element
   ): React$Portal;
 
   declare function unmountComponentAtNode(container: any): boolean;
@@ -29,15 +29,15 @@ declare module 'react-dom' {
     b: B,
     c: C,
     d: D,
-    e: E,
+    e: E
   ): void;
   declare function unstable_renderSubtreeIntoContainer<
-    ElementType: React$ElementType,
+    ElementType: React$ElementType
   >(
     parentComponent: React$Component<any, any>,
     nextElement: React$Element<ElementType>,
     container: any,
-    callback?: () => void,
+    callback?: () => void
   ): React$ElementRef<ElementType>;
 }
 
@@ -46,62 +46,67 @@ declare module 'react-dom/server' {
   declare function renderToStaticMarkup(element: React$Node): string;
   declare function renderToNodeStream(element: React$Node): stream$Readable;
   declare function renderToStaticNodeStream(
-    element: React$Node,
+    element: React$Node
   ): stream$Readable;
   declare var version: string;
 }
 
-type Thenable = { then(resolve: () => mixed, reject?: () => mixed): mixed, ... };
+declare interface Thenable {
+  then(resolve: () => mixed, reject?: () => mixed): mixed;
+}
 
 declare module 'react-dom/test-utils' {
-  declare var Simulate: { [eventName: string]: (element: Element, eventData?: Object) => void, ... };
+  declare var Simulate: {
+    [eventName: string]: (element: Element, eventData?: Object) => void,
+    ...,
+  };
   declare function renderIntoDocument(
-    instance: React$Element<any>,
+    instance: React$Element<any>
   ): React$Component<any, any>;
   declare function mockComponent(
     componentClass: React$ElementType,
-    mockTagName?: string,
+    mockTagName?: string
   ): Object;
   declare function isElement(element: React$Element<any>): boolean;
   declare function isElementOfType(
     element: React$Element<any>,
-    componentClass: React$ElementType,
+    componentClass: React$ElementType
   ): boolean;
   declare function isDOMComponent(instance: any): boolean;
   declare function isCompositeComponent(
-    instance: React$Component<any, any>,
+    instance: React$Component<any, any>
   ): boolean;
   declare function isCompositeComponentWithType(
     instance: React$Component<any, any>,
-    componentClass: React$ElementType,
+    componentClass: React$ElementType
   ): boolean;
   declare function findAllInRenderedTree(
     tree: React$Component<any, any>,
-    test: (child: React$Component<any, any>) => boolean,
+    test: (child: React$Component<any, any>) => boolean
   ): Array<React$Component<any, any>>;
   declare function scryRenderedDOMComponentsWithClass(
     tree: React$Component<any, any>,
-    className: string,
+    className: string
   ): Array<Element>;
   declare function findRenderedDOMComponentWithClass(
     tree: React$Component<any, any>,
-    className: string,
+    className: string
   ): ?Element;
   declare function scryRenderedDOMComponentsWithTag(
     tree: React$Component<any, any>,
-    tagName: string,
+    tagName: string
   ): Array<Element>;
   declare function findRenderedDOMComponentWithTag(
     tree: React$Component<any, any>,
-    tagName: string,
+    tagName: string
   ): ?Element;
   declare function scryRenderedComponentsWithType(
     tree: React$Component<any, any>,
-    componentClass: React$ElementType,
+    componentClass: React$ElementType
   ): Array<React$Component<any, any>>;
   declare function findRenderedComponentWithType(
     tree: React$Component<any, any>,
-    componentClass: React$ElementType,
+    componentClass: React$ElementType
   ): ?React$Component<any, any>;
   declare function act(callback: () => void | Thenable): Thenable;
 }
