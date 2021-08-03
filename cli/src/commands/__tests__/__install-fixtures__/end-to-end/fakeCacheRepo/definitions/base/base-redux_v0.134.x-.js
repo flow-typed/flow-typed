6fb4c4fd589f4ed970dbc29f38587bc519ac1ce2
Brian Chen <brian.chen@tabcorp.com.au>
2021-08-03 18:15:09 +1000

    create fixtures that are necessary for testing install

---
+++ cli/src/commands/__tests__/__install-fixtures__/end-to-end/fakeCacheRepo/definitions/base/base-redux_v0.134.x-.js
@@ -0,0 +1,15 @@
+declare module '@flow-typed/base-redux' {
+  declare export type Redux$DispatchAPI<A> = (action: A) => A;
+
+  declare export type Redux$Dispatch<A: { type: any, ... }> = Redux$DispatchAPI<A>;
+
+  declare export type Redux$MiddlewareAPI<S, A, D = Redux$Dispatch<A>> = {
+    dispatch: D,
+    getState(): S,
+    ...
+  };
+
+  declare export type Redux$Middleware<S, A, D = Redux$Dispatch<A>> = (
+    api: Redux$MiddlewareAPI<S, A, D>
+  ) => (next: D) => D;
+}
