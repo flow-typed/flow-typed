69616781a763a618bc707a2df7068ed1fe2ae557
Brian Chen <brian.chen@tabcorp.com.au>
2021-08-03 10:42:49 +1000

    update tests to not need to be warpped in react looking comps

--- definitions/npm/react-redux_v7.x.x/flow_v0.142.x-/test_useDispatch.js
+++ definitions/npm/react-redux_v7.x.x/flow_v0.142.x-/test_useDispatch.js
@@ -10,98 +10,51 @@ type Dispatch = Action => Action;
 
 describe('useDispatch', () => {
   it('returns type `Dispatch` which accepts only type `Action` as param', () => {
-    function Com() {
-      const dispatch = useDispatch<Dispatch>();
-      return (
-        <button
-          onClick={function() {
-            const action = dispatch({ type: 'action' });
-            (action.type: string);
-            // $FlowExpectedError[incompatible-cast] will be string because of passed generic
-            (action.type: number);
-          }}
-        >
-          Dispatch time
-        </button>
-      );
-    }
+    const dispatch = useDispatch<Dispatch>();
+
+    const action = dispatch({ type: 'action' });
+    (action.type: string);
+    // $FlowExpectedError[incompatible-cast] will be string because of passed generic
+    (action.type: number);
   });
 
   it('errors if returned type is passed invalid Action', () => {
-    function Com() {
-      const dispatch = useDispatch<Dispatch>();
-      return (
-        <div
-          onClick={() => {
-            // $FlowExpectedError[incompatible-call]: return value of `useDispatch` should make `Dispatch` and expect an `Action`.
-            dispatch();
-          }}
-        >
-          Dispatch time
-        </div>
-      );
-    }
+    const dispatch = useDispatch<Dispatch>();
+
+    // $FlowExpectedError[incompatible-call]: return value of `useDispatch` should make `Dispatch` and expect an `Action`.
+    dispatch();
   });
 
   it('handles action creator passed in and is typed', () => {
-    function Com() {
-      const dispatch = useDispatch();
-      return (
-        <button
-          onClick={function() {
-            const action = dispatch({ anything: 'string' });
-            (action.anything: string);
-            // $FlowExpectedError[incompatible-cast] will be string because of passed action
-            (action.anything: number);
-          }}
-        >
-          Dispatch time
-        </button>
-      );
-    }
+    const dispatch = useDispatch();
+
+    const action = dispatch({ anything: 'string' });
+    (action.anything: string);
+    // $FlowExpectedError[incompatible-cast] will be string because of passed action
+    (action.anything: number);
   });
 
   it('handles thunks being passed to return the right value', () => {
-    function Com() {
-      const thunkAction = () => (dispatch) => {
-        return '';
-      }
-      const dispatch = useDispatch();
-      return (
-        <div
-          onClick={() => {
-            const a = dispatch(thunkAction());
-            a.toString();
-            // $FlowExpectedError[incompatible-cast] it will return a string, not any which could be cast to number
-            (a: number)
-          }}
-        >
-          Dispatch time
-        </div>
-      );
+    const thunkAction = () => (dispatch) => {
+      return '';
     }
+    const dispatch = useDispatch();
+
+    const a = dispatch(thunkAction());
+    a.toString();
+    // $FlowExpectedError[incompatible-cast] it will return a string, not any which could be cast to number
+    (a: number)
   });
 
-  it('handles thunks defined as a promise to keep their type', (done) => {
-    function Com() {
-      const thunkPromiseAction = () => (dispatch) => new Promise((resolve) => {
-        resolve('');
-      });
-      const dispatch = useDispatch();
-      return (
-        <div
-          onClick={() => {
-            dispatch(thunkPromiseAction()).then((a) => {
-              a.toString();
-              // $FlowExpectedError[incompatible-cast] it will return a string, not any which could be cast to number
-              (a: number)
-              done();
-            });
-          }}
-        >
-          Dispatch time
-        </div>
-      );
-    }
+  it('handles thunks defined as a promise to keep their type', () => {
+    const thunkPromiseAction = () => (dispatch) => new Promise((resolve) => {
+      resolve('');
+    });
+    const dispatch = useDispatch();
+
+    const dispatched = dispatch(thunkPromiseAction());
+    (dispatched: Promise<string>);
+    // $FlowExpectedError[incompatible-cast] it will return a string, not any which could be cast to number
+    (dispatched: Promise<number>);
   });
 });
