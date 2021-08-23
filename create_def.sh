c737fe4c47cd53672ad3d00a7a5aeaeb54d27ad9
Brian Chen <brian.chen@tabcorp.com.au>
2021-08-23 23:57:47 +1000

    add some error messages

--- create_def.sh
+++ create_def.sh
@@ -3,7 +3,18 @@ set -e
 
 LIB_NAME=$1
 VERSION=$2
+
 FLOW_VERSION="0.83.x"
+INFO_MESSAGE="Please try again in the format of \"./create_def.sh lib-name 1.x.x\""
+
+if [ $# -eq 0 ]; then
+    echo "ERROR: No arguments provided, expected first arg as library name and second as version. ${INFO_MESSAGE}"
+    exit 1
+fi
+if [ -z "$VERSION" ]; then
+    echo "ERROR: No version provided. ${INFO_MESSAGE}"
+    exit 1
+fi
 
 ROOT_DEF_DIR="definitions/npm/${LIB_NAME}_v${VERSION}"
 DEF_DIR="${ROOT_DEF_DIR}/flow_v${FLOW_VERSION}-"
