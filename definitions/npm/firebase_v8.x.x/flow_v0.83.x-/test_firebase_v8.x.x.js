93a6a2f507a4846bab07f018de12d22d6e797498
Brian Chen <brian.chen@tabcorp.com.au>
2021-11-12 18:55:45 +1100

    add auth tests

--- definitions/npm/firebase_v8.x.x/flow_v0.83.x-/test_firebase_v8.x.x.js
+++ definitions/npm/firebase_v8.x.x/flow_v0.83.x-/test_firebase_v8.x.x.js
@@ -1,9 +1,80 @@
 // @flow
 import { describe, it } from 'flow-typed-test';
-// import library from 'firebase';
+import firebase from 'firebase/app';
+import analytics from 'firebase/analytics';
+import auth from 'firebase/auth';
+import firestore from 'firebase/firestore';
 
 describe('firebase', () => {
-  it('', () => {
+  describe('auth', () => {
+    const auth = firebase.auth();
 
+    it('currentUser', () => {
+      // $FlowExpectedError[incompatible-use] current user won't exist if not logged in
+      auth.currentUser.email;
+
+      if (auth.currentUser) {
+        auth.currentUser.email.toLowerCase();
+      }
+    });
+
+    it('createUserWithEmailAndPassword', () => {
+      auth.createUserWithEmailAndPassword('test', 'test').then(({ user }) => {
+        user.email.toLowerCase();
+        // $FlowExpectedError[prop-missing] it's typed as current user obj
+        user.random
+      });
+      // $FlowExpectedError[incompatible-call] missing password
+      auth.createUserWithEmailAndPassword('test');
+      // $FlowExpectedError[incompatible-call] must have email and password
+      auth.createUserWithEmailAndPassword();
+    });
+
+    it('onAuthStateChanged', () => {
+      const empty: void = auth.onAuthStateChanged((res) => {
+        res.email.toLowerCase();
+        // $FlowExpectedError[prop-missing] it's typed as current user obj
+        res.random
+      });
+    });
+
+    it('signInWithEmailAndPassword', () => {
+      auth.signInWithEmailAndPassword('test', 'test').then(({ user }) => {
+        user.email.toLowerCase();
+        // $FlowExpectedError[prop-missing] it's typed as current user obj
+        user.random
+      });
+      // $FlowExpectedError[incompatible-call] missing password
+      auth.signInWithEmailAndPassword('test');
+      // $FlowExpectedError[incompatible-call] must have email and password
+      auth.signInWithEmailAndPassword();
+    });
+
+    it('signOut', () => {
+      auth.signOut().then((res) => {
+        (res: void);
+      });
+
+      // $FlowExpectedError[extra-arg] accepts no args
+      auth.signOut('test')
+    });
+  });
+});
+
+describe('firebase/analytics', () => {
+  it('returns void', () => {
+    (analytics: void)
+  });
+});
+
+describe('firebase/auth', () => {
+  it('returns void', () => {
+    (auth: void)
+  });
+});
+
+describe('firebase/firestore', () => {
+  it('returns void', () => {
+    (firestore: void)
   });
 });
