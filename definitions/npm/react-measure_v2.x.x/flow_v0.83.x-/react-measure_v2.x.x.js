fe94e72401db1d25ed6c6deeb8b24ef7cb3d58df
Brian Chen <brian.chen@tabcorp.com.au>
2021-11-23 09:29:54 +1100

    handle examples from readme

--- definitions/npm/react-measure_v2.x.x/flow_v0.83.x-/react-measure_v2.x.x.js
+++ definitions/npm/react-measure_v2.x.x/flow_v0.83.x-/react-measure_v2.x.x.js
@@ -1,47 +1,72 @@
 declare module 'react-measure' {
-  declare type ContentRect = {|
-    offset: {|
-      offsetTop: number,
-      offsetLeft: number,
-      offsetWidth: number,
-      offsetHeight: number,
-    |},
-    scroll: {|
-      scrollTop: number,
-      scrollLeft: number,
-      scrollWidth: number,
-      scrollHeight: number,
-    |},
-    bounds: {|
-      width: number,
-      height: number,
-      top: number,
-      right: number,
-      bottom: number,
-      left: number,
-    |},
-    margin: {|
-      top: number,
-      right: number,
-      bottom: number,
-      left: number,
-    |},
+  declare type MeasurementType =
+    | 'client'
+    | 'offset'
+    | 'scroll'
+    | 'bounds'
+    | 'margin';
+
+  declare type TopLeft = {|
+    +top: number,
+    +left: number,
   |};
 
-  declare class Measure extends React$Component<{|
+  declare type BottomRight = {|
+    +bottom: number,
+    +right: number,
+  |};
+
+  declare type Dimensions = {|
+    +width: number,
+    +height: number,
+  |};
+
+  declare type Margin = {|
+    ...TopLeft,
+    ...BottomRight
+  |};
+
+  declare type Rect = {|
+    ...TopLeft,
+    ...Dimensions,
+  |};
+
+  declare type BoundingRect = {|
+    ...Dimensions,
+    ...Margin,
+  |};
+
+  declare type ContentRect = {|
+    client?: Rect,
+    offset?: Rect,
+    scroll?: Rect,
+    bounds?: BoundingRect,
+    margin?: Margin,
+    entry?: any,
+  |};
+
+  declare type MeasuredComponentProps = {|
+    measureRef: (ref: Element | null) => void,
+    measure: () => void,
+    contentRect: ContentRect,
+  |};
+
+  declare type MeasureProps = {|
     client?: boolean,
     offset?: boolean,
     scroll?: boolean,
     bounds?: boolean,
     margin?: boolean,
-    innerRef?: (HTMLElement) => void,
-    onResize?: (ContentRect) => void,
-    children?: ({|
-      measureRef: () => void,
-      measure: () => void,
-      contentRect: ContentRect,
-    |}) => React$Node,
-  |}> {}
+    innerRef?: HTMLElement | null,
+    onResize?: (contentRect: ContentRect) => void;
+    children?: (props: MeasuredComponentProps) => React$Node,
+  |};
 
-  declare export default typeof Measure;
+  declare type MeasureComponent = (props: MeasureProps) => React$Node;
+
+  declare export var withContentRect: (
+    types: Array<MeasurementType> | MeasurementType,
+  ) => (children: MeasureProps['children']) => MeasureComponent;
+
+  declare export default MeasureComponent;
 }
--- definitions/npm/react-measure_v2.x.x/flow_v0.83.x-/test_react-measure_v2.x.x.js
+++ definitions/npm/react-measure_v2.x.x/flow_v0.83.x-/test_react-measure_v2.x.x.js
@@ -1,13 +1,48 @@
 // @flow
 import { describe, it } from 'flow-typed-test';
 import * as React from 'React'
-import Measure from 'react-measure'
+import Measure, { withContentRect } from 'react-measure'
 
 describe('react-measure', () => {
-  it('', () => {
-    const fail = () => (
-      // $FlowExpectedError[incompatible-type]
-      <Measure bounds="string" />
-    )
+  it('handles basic use case', () => {
+    const dimensions = {
+      width: -1,
+      height: -1,
+    };
+
+    const { width, height } = dimensions;
+
+    (
+      <Measure
+        bounds
+        onResize={contentRect => {
+
+        }}
+      >
+        {({ measureRef }) => (
+          <div ref={measureRef} className="className">
+            I can do cool things with my dimensions now :D
+            {height > 250 && (
+              <div>Render responsive content based on the component size!</div>
+            )}
+          </div>
+        )}
+      </Measure>
+    );
   });
+
+  describe('withContentRect', () => {
+    it('calls correctly', () => {
+      const ItemToMeasure = withContentRect('bounds')(
+        ({ measureRef, measure, contentRect }) => (
+          <div ref={measureRef}>
+            Some content here
+            <pre>{JSON.stringify(contentRect, null, 2)}</pre>
+          </div>
+        )
+      );
+
+      <ItemToMeasure />
+    });
+  })
 });
