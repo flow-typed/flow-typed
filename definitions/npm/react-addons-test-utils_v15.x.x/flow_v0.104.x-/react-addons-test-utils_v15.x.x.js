ee15dafcd96612b1e59efc3fbce59fea4d415824
Brian Chen <brian.chen@tabcorp.com.au>
2021-11-22 15:57:58 +1100

    fix broken lib def (#4165)

--- definitions/npm/react-addons-test-utils_v15.x.x/flow_v0.104.x-/react-addons-test-utils_v15.x.x.js
+++ definitions/npm/react-addons-test-utils_v15.x.x/flow_v0.104.x-/react-addons-test-utils_v15.x.x.js
@@ -1,56 +1,54 @@
-import React from "react";
-
 declare module "react-addons-test-utils" {
   declare var Simulate: { [eventName: string]: (element: Element, eventData?: Object) => void, ... };
   declare function renderIntoDocument(
-    instance: React.Element<any>
-  ): React.Component;
+    instance: React$Element<any>
+  ): React$Component<any>;
   declare function mockComponent(
-    componentClass: React.ElementType,
+    componentClass: React$ElementType,
     mockTagName?: string
   ): Object;
-  declare function isElement(element: React.Element<any>): boolean;
+  declare function isElement(element: React$Element<any>): boolean;
   declare function isElementOfType(
-    element: React.Element<any>,
-    componentClass: React.ElementType
+    element: React$Element<any>,
+    componentClass: React$ElementType
   ): boolean;
-  declare function isDOMComponent(instance: React.Component): boolean;
-  declare function isCompositeComponent(instance: React.Component): boolean;
+  declare function isDOMComponent(instance: React$Component<any>): boolean;
+  declare function isCompositeComponent(instance: React$Component<any>): boolean;
   declare function isCompositeComponentWithType(
-    instance: React.Component,
-    componentClass: React.ElementType
+    instance: React$Component<any>,
+    componentClass: React$ElementType
   ): boolean;
   declare function findAllInRenderedTree(
-    tree: React.Component,
-    test: (child: React.Component) => boolean
-  ): Array<React.Component>;
+    tree: React$Component<any>,
+    test: (child: React$Component<any>) => boolean
+  ): Array<React$Component<any>>;
   declare function scryRenderedDOMComponentsWithClass(
-    tree: React.Component,
+    tree: React$Component<any>,
     className: string
   ): Array<Element>;
   declare function findRenderedDOMComponentWithClass(
-    tree: React.Component,
+    tree: React$Component<any>,
     className: string
   ): ?Element;
   declare function scryRenderedDOMComponentsWithTag(
-    tree: React.Component,
+    tree: React$Component<any>,
     tagName: string
   ): Array<Element>;
   declare function findRenderedDOMComponentWithTag(
-    tree: React.Component,
+    tree: React$Component<any>,
     tagName: string
   ): ?Element;
   declare function scryRenderedComponentsWithType(
-    tree: React.Component,
-    componentClass: React.ElementType
-  ): Array<React.Component>;
+    tree: React$Component<any>,
+    componentClass: React$ElementType
+  ): Array<React$Component<any>>;
   declare function findRenderedComponentWithType(
-    tree: React.Component,
-    componentClass: React.ElementType
-  ): ?React.Component;
+    tree: React$Component<any>,
+    componentClass: React$ElementType
+  ): ?React$Component<any>;
   declare class ReactShallowRender {
-    render(element: React.Element<any>): void;
-    getRenderOutput(): React.Element<any>;
+    render(element: React$Element<any>): void;
+    getRenderOutput(): React$Element<any>;
   }
   declare function createRenderer(): ReactShallowRender;
 }
