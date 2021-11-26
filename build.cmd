c0bd48678ccd6590390985169c2575d5ecf8abf4
Brian Chen <brian.chen@tabcorp.com.au>
2021-11-26 23:09:05 +1100

    try with new line chars

--- build.cmd
+++ build.cmd
@@ -1,8 +1,8 @@
-cd definitions
-call yarn install
-call yarn test
-cd ..\cli
-call yarn
-call yarn run flow
-node dist\cli.js validate-defs ..\definitions
+cd definitions && \
+call yarn install && \
+call yarn test && \
+cd ..\cli && \
+call yarn && \
+call yarn run flow && \
+node dist\cli.js validate-defs ..\definitions && \
 node dist\cli.js run-tests --onlyChanged
