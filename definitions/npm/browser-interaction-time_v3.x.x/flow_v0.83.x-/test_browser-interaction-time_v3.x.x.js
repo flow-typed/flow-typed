faf35e5ca83883b986e1ce5ebca147640c50e077
Brian Chen <brian.chen@tabcorp.com.au>
2022-01-03 06:43:20 +1100

    create definition

---
+++ definitions/npm/browser-interaction-time_v3.x.x/flow_v0.83.x-/test_browser-interaction-time_v3.x.x.js
@@ -0,0 +1,9 @@
+// @flow
+import { describe, it } from 'flow-typed-test';
+// import library from 'browser-interaction-time';
+
+describe('browser-interaction-time', () => {
+  it('', () => {
+
+  });
+});
