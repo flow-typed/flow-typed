06a9606075a635facc972a133565ef4816407fd1
Brian Chen <brian.chen@tabcorp.com.au>
2022-01-05 12:38:20 +1100

    adjust tests

--- definitions/npm/password-validator_v5.x.x/flow_v0.134.x-/password-validator_v5.x.x.js
+++ definitions/npm/password-validator_v5.x.x/flow_v0.134.x-/password-validator_v5.x.x.js
@@ -1,26 +1,69 @@
 declare module 'password-validator' {
-  declare type PasswordSchemaRules = {|
-    digits(count?: number): PasswordSchemaRules, // specifies password must include digits (optionally provide count paramenter to specify at least n digits)
-    letters(count?: number): PasswordSchemaRules, // specifies password must include letters (optionally provide count paramenter to specify at least n letters)
-    lowercase(count?: number): PasswordSchemaRules, // specifies password must include lowercase letters (optionally provide count paramenter to specify at least n lowercase letters)
-    uppercase(count?: number): PasswordSchemaRules, // specifies password must include uppercase letters (optionally provide count paramenter to specify at least n uppercase letters)
-    symbols(count?: number): PasswordSchemaRules, // specifies password must include symbols (optionally provide count paramenter to specify at least n symbols)
-    spaces(count?: number): PasswordSchemaRules, // specifies password must include spaces (optionally provide count paramenter to specify at least n spaces)
-    min(len: number): PasswordSchemaRules, // specifies minimum length
-    max(len: number): PasswordSchemaRules, // specifies maximum length
-    oneOf(list: string[]): PasswordSchemaRules, // specifies the whitelisted values
-    not(regex?: RegExp): PasswordSchemaRules, // inverts the result of validations applied next
-    is(): PasswordSchemaRules, // inverts the effect of not()
-    has(regex?: RegExp): PasswordSchemaRules, // inverts the effect of not() and applies a regex (optional)
-  |};
-
-  declare type PasswordPolicySchema = {|
-    ...PasswordSchemaRules,
-    validate: (password: string) => boolean,
-  |};
-
   declare class PasswordValidator {
-    constructor(): PasswordPolicySchema;
+    constructor(): this;
+
+    /**
+     * specifies password must include digits (optionally provide count parameter to specify at least n digits)
+     */
+    digits(count?: number, description?: string): this,
+    /**
+     * specifies password must include letters (optionally provide count parameter to specify at least n letters)
+     */
+    letters(count?: number, description?: string): this,
+    /**
+     * specifies password must include lowercase letters (optionally provide count parameter to specify at least n lowercase letters)
+     */
+    lowercase(count?: number, description?: string): this,
+    /**
+     * specifies password must include uppercase letters (optionally provide count parameter to specify at least n uppercase letters)
+     */
+    uppercase(count?: number, description?: string): this,
+    /**
+     * specifies password must include symbols (optionally provide count parameter to specify at least n symbols)
+     */
+    symbols(count?: number, description?: string): this,
+    /**
+     * specifies password must include spaces (optionally provide count parameter to specify at least n spaces)
+     */
+    spaces(count?: number, description?: string): this,
+    /**
+     * specifies minimum length
+     */
+    min(len: number, description?: string): this,
+    /**
+     * specifies maximum length
+     */
+    max(len: number, description?: string): this,
+    /**
+     * specifies the whitelisted values
+     */
+    oneOf(list: Array<string>): this,
+    /**
+     * inverts the result of validations applied next
+     */
+    not(regex?: RegExp, description?: string): this,
+    /**
+     * inverts the effect of not()
+     */
+    is(): this,
+    /**
+     * inverts the effect of not() and applies a regex (optional)
+     */
+    has(regex?: RegExp, description?: string): this,
+
+    /**
+     * Validate against a password string
+     */
+     validate(
+      password: string,
+    ): boolean,
+    validate(
+      password: string,
+      options: {|
+        list?: boolean,
+        details?: boolean,
+      |},
+    ): boolean | Array<string | { [key: string]: any, ... }>,
   }
 
   declare module.exports: typeof PasswordValidator;
--- definitions/npm/password-validator_v5.x.x/flow_v0.134.x-/test_password-validator_v5.x.x.js
+++ definitions/npm/password-validator_v5.x.x/flow_v0.134.x-/test_password-validator_v5.x.x.js
@@ -1,11 +1,10 @@
 // @flow
 import { describe, it } from 'flow-typed-test';
-import type { PasswordPolicySchema } from 'password-validator';
 import PasswordValidator from 'password-validator';
 
 describe('password-validator', () => {
   it('should work with valid arguments', () => {
-    const schema: PasswordPolicySchema = new PasswordValidator();
+    const schema = new PasswordValidator();
 
     // prettier-ignore
     schema
@@ -18,7 +17,15 @@ describe('password-validator', () => {
       .has().not().spaces()
       .has(/(.*[A-Z]){2,}/);
 
-    schema.validate('3L}Ls4[4}qQB>UgN');
+    (schema.validate('3L}Ls4[4}qQB>UgN'): boolean);
+    // $FlowExpectedError[incompatible-cast] if passing options it could be array
+    const valid = (schema.validate('3L}Ls4[4}qQB>UgN', {}): boolean);
+
+    if (typeof valid === 'boolean') {
+      (valid: boolean)
+    } else {
+      valid[0].test.toLowerCase();
+    }
   });
 
   it('should fail with incorrect arguments', () => {
@@ -33,55 +40,55 @@ describe('password-validator', () => {
     // $FlowExpectedError[incompatible-call]
     schema.has('foo');
     // $FlowExpectedError[extra-arg]
-    schema.has(/.*/, 'foo');
+    schema.has(/.*/, 'foo', 'bar');
 
     // $FlowExpectedError[incompatible-call]
     schema.not('foo');
     // $FlowExpectedError[extra-arg]
-    schema.not(/.*/, 'bar');
+    schema.not(/.*/, 'foo', 'bar');
 
     // $FlowExpectedError[incompatible-call]
     schema.is().min();
     // $FlowExpectedError[incompatible-call]
     schema.is().min('1');
     // $FlowExpectedError[extra-arg]
-    schema.is().min(1, 'foo');
+    schema.is().min(1, 'foo', 'bar');
 
     // $FlowExpectedError[incompatible-call]
     schema.is().max();
     // $FlowExpectedError[incompatible-call]
     schema.is().max('1');
     // $FlowExpectedError[extra-arg]
-    schema.is().max(1, 'foo');
+    schema.is().max(1, 'foo', 'bar');
 
     // $FlowExpectedError[incompatible-call]
     schema.has().spaces('1');
     // $FlowExpectedError[extra-arg]
-    schema.has().spaces(1, 'foo');
+    schema.has().spaces(1, 'foo', 'bar');
 
     // $FlowExpectedError[incompatible-call]
     schema.has().symbols('1');
     // $FlowExpectedError[extra-arg]
-    schema.has().symbols(1, 'foo');
+    schema.has().symbols(1, 'foo', 'bar');
 
     // $FlowExpectedError[incompatible-call]
     schema.has().uppercase('1');
     // $FlowExpectedError[extra-arg]
-    schema.has().uppercase(1, 'foo');
+    schema.has().uppercase(1, 'foo', 'bar');
 
     // $FlowExpectedError[incompatible-call]
     schema.has().letters('1');
     // $FlowExpectedError[extra-arg]
-    schema.has().letters(1, 'foo');
+    schema.has().letters(1, 'foo', 'bar');
 
     // $FlowExpectedError[incompatible-call]
     schema.has().digits('1');
     // $FlowExpectedError[extra-arg]
-    schema.has().digits(1, 'foo');
+    schema.has().digits(1, 'foo', 'bar');
 
     // $FlowExpectedError[incompatible-call]
     schema.has().oneOf([1, 2, 3]);
     // $FlowExpectedError[extra-arg]
-    schema.has().oneOf(['foo', 'bar', 'baz'], 'foo');
+    schema.has().oneOf(['foo', 'bar', 'baz'], 'foo', 'bar');
   });
 });
