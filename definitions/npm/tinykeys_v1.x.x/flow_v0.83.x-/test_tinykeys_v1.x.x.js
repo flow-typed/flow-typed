b8906aa827ec725304d60b0db7631e661b692129
Brian Chen <brian.chen@tabcorp.com.au>
2022-03-04 05:48:06 +1100

    [tinykeys] Add v1 definitions (#4275)
    * add tinykeys
    
    * add another error test

---
+++ definitions/npm/tinykeys_v1.x.x/flow_v0.83.x-/test_tinykeys_v1.x.x.js
@@ -0,0 +1,72 @@
+// @flow
+import { describe, it } from 'flow-typed-test';
+import tinykeys, { createKeybindingsHandler, parseKeybinding } from 'tinykeys';
+
+describe('tinykeys', () => {
+  it('basic usage', () => {
+    const unsub = tinykeys(window, {
+      "Shift+D": () => {
+        alert("The 'Shift' and 'd' keys were pressed at the same time")
+      },
+      "y e e t": () => {
+        alert("The keys 'y', 'e', 'e', and 't' were pressed in order")
+      },
+      "$mod+KeyD": event => {
+        event.preventDefault()
+        alert("Either 'Control+d' or 'Meta+d' were pressed")
+      },
+    });
+
+    tinykeys(
+      window,
+      {
+        "Shift+D": () => {
+          alert("The 'Shift' and 'd' keys were pressed at the same time")
+        },
+      },
+      {
+        event: "keyup",
+      },
+    );
+
+    unsub();
+    // $FlowExpectedError[extra-arg]
+    unsub('');
+    tinykeys(
+      window,
+      {
+        "Shift+D": () => {
+          alert("The 'Shift' and 'd' keys were pressed at the same time")
+        },
+      },
+      // $FlowExpectedError[prop-missing]
+      {
+        random: "keyup",
+      },
+    );
+  });
+
+  it('createKeybindingsHandler', () => {
+    let handler = createKeybindingsHandler({
+      "Shift+D": () => {
+        alert("The 'Shift' and 'd' keys were pressed at the same time")
+      },
+      "y e e t": () => {
+        alert("The keys 'y', 'e', 'e', and 't' were pressed in order")
+      },
+      "$mod+KeyD": event => {
+        event.preventDefault()
+        alert("Either 'Control+d' or 'Meta+d' were pressed")
+      },
+    })
+
+    window.addEventListener("keydown", handler)
+  });
+
+  it('parseKeybinding', () => {
+    let parsedShortcut = parseKeybinding('$mod+Shift+K $mod+1')
+
+    // $FlowExpectedError[incompatible-call]
+    parseKeybinding();
+  });
+});
