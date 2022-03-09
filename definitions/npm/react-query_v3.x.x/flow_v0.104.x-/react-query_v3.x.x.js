ae9b693793c776548d53f863e576adaddfa66861
Ian Duvall <ianduvall@users.noreply.github.com>
2022-03-09 14:10:39 -0600

    react-query_v3: add 'tracked' to notifyOnChangeProps and fix tests (#4283)

--- definitions/npm/react-query_v3.x.x/flow_v0.104.x-/react-query_v3.x.x.js
+++ definitions/npm/react-query_v3.x.x/flow_v0.104.x-/react-query_v3.x.x.js
@@ -448,7 +448,7 @@ declare module "react-query" {
      * If set, the component will only re-render if any of the listed properties change.
      * When set to `['data', 'error']`, the component will only re-render when the `data` or `error` properties change.
      */
-    notifyOnChangeProps?: string[],
+    notifyOnChangeProps?: string[] | "tracked",
     /**
      * If set, the component will not re-render if any of the listed properties change.
      */
