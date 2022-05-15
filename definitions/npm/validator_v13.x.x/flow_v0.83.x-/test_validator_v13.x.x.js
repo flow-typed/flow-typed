4250a7ac125202a4d7a50b8e0a62391e746e53cf
Brian Chen <bzchen93@gmail.com>
2022-05-15 19:23:58 +1000

    create test struct

--- definitions/npm/validator_v13.x.x/flow_v0.83.x-/test_validator_v13.x.x.js
+++ definitions/npm/validator_v13.x.x/flow_v0.83.x-/test_validator_v13.x.x.js
@@ -3,7 +3,549 @@ import { describe, it } from 'flow-typed-test';
 import validator from 'validator';
 
 describe('validator', () => {
-  it('', () => {
+  describe('blacklist', () => {
+    it('works', () => {
 
+    });
+  });
+
+  describe('contains', () => {
+    it('works', () => {
+
+    });
+  });
+
+  describe('equals', () => {
+    it('works', () => {
+
+    });
+  });
+
+  describe('escape', () => {
+    it('works', () => {
+
+    });
+  });
+
+  describe('isAfter', () => {
+    it('works', () => {
+
+    });
+  });
+
+  describe('isAlpha', () => {
+    it('works', () => {
+
+    });
+  });
+
+  describe('isAlphanumeric', () => {
+    it('works', () => {
+
+    });
+  });
+
+  describe('isBase32', () => {
+    it('works', () => {
+
+    });
+  });
+
+  describe('isBase58', () => {
+    it('works', () => {
+
+    });
+  });
+
+  describe('isBase64', () => {
+    it('works', () => {
+
+    });
+  });
+
+  describe('isBIC', () => {
+    it('works', () => {
+
+    });
+  });
+
+  describe('isBoolean', () => {
+    it('works', () => {
+
+    });
+  });
+
+  describe('isBtcAddress', () => {
+    it('works', () => {
+
+    });
+  });
+
+  describe('isByteLength', () => {
+    it('works', () => {
+
+    });
+  });
+
+  describe('isCreditCard', () => {
+    it('works', () => {
+
+    });
+  });
+
+  describe('isCurrency', () => {
+    it('works', () => {
+
+    });
+  });
+
+  describe('isDataURI', () => {
+    it('works', () => {
+
+    });
+  });
+
+  describe('isDate', () => {
+    it('works', () => {
+
+    });
+  });
+
+  describe('isDecimal', () => {
+    it('works', () => {
+
+    });
+  });
+
+  describe('isDivisibleBy', () => {
+    it('works', () => {
+
+    });
+  });
+
+  describe('isEAN', () => {
+    it('works', () => {
+
+    });
+  });
+
+  describe('isEmail', () => {
+    it('works', () => {
+
+    });
+  });
+
+  describe('isEmpty', () => {
+    it('works', () => {
+
+    });
+  });
+
+  describe('isEthereumAddress', () => {
+    it('works', () => {
+
+    });
+  });
+
+  describe('isFloat', () => {
+    it('works', () => {
+
+    });
+  });
+
+  describe('isFQDN', () => {
+    it('works', () => {
+
+    });
+  });
+
+  describe('isFullWidth', () => {
+    it('works', () => {
+
+    });
+  });
+
+  describe('isHalfWidth', () => {
+    it('works', () => {
+
+    });
+  });
+
+  describe('isHash', () => {
+    it('works', () => {
+
+    });
+  });
+
+  describe('isHexadecimal', () => {
+    it('works', () => {
+
+    });
+  });
+
+  describe('isHexColor', () => {
+    it('works', () => {
+
+    });
+  });
+
+  describe('isHSL', () => {
+    it('works', () => {
+
+    });
+  });
+
+  describe('isIBAN', () => {
+    it('works', () => {
+
+    });
+  });
+
+  describe('isIdentityCard', () => {
+    it('works', () => {
+
+    });
+  });
+
+  describe('isIMEI', () => {
+    it('works', () => {
+
+    });
+  });
+
+  describe('isIn', () => {
+    it('works', () => {
+
+    });
+  });
+
+  describe('isInt', () => {
+    it('works', () => {
+
+    });
+  });
+
+  describe('isIP', () => {
+    it('works', () => {
+
+    });
+  });
+
+  describe('isIPRange', () => {
+    it('works', () => {
+
+    });
+  });
+
+  describe('isISBN', () => {
+    it('works', () => {
+
+    });
+  });
+
+  describe('isISIN', () => {
+    it('works', () => {
+
+    });
+  });
+
+  describe('isISO31661Alpha2', () => {
+    it('works', () => {
+
+    });
+  });
+
+  describe('isISO31661Alpha3', () => {
+    it('works', () => {
+
+    });
+  });
+
+  describe('isISO4217', () => {
+    it('works', () => {
+
+    });
+  });
+
+  describe('isISO8601', () => {
+    it('works', () => {
+
+    });
+  });
+
+  describe('isISRC', () => {
+    it('works', () => {
+
+    });
+  });
+
+  describe('isISSN', () => {
+    it('works', () => {
+
+    });
+  });
+
+  describe('isJSON', () => {
+    it('works', () => {
+
+    });
+  });
+
+  describe('isJWT', () => {
+    it('works', () => {
+
+    });
+  });
+
+  describe('isLatLong', () => {
+    it('works', () => {
+
+    });
+  });
+
+  describe('isLength', () => {
+    it('works', () => {
+
+    });
+  });
+
+  describe('isLicensePlate', () => {
+    it('works', () => {
+
+    });
+  });
+
+  describe('isLocale', () => {
+    it('works', () => {
+
+    });
+  });
+
+  describe('isLowercase', () => {
+    it('works', () => {
+
+    });
+  });
+
+  describe('isMACAddress', () => {
+    it('works', () => {
+
+    });
+  });
+
+  describe('isMagnetURI', () => {
+    it('works', () => {
+
+    });
+  });
+
+  describe('isMD5', () => {
+    it('works', () => {
+
+    });
+  });
+
+  describe('isMimeType', () => {
+    it('works', () => {
+
+    });
+  });
+
+  describe('isMobilePhone', () => {
+    it('works', () => {
+
+    });
+  });
+
+  describe('isMACAddress', () => {
+    it('works', () => {
+
+    });
+  });
+
+  describe('isMongoId', () => {
+    it('works', () => {
+
+    });
+  });
+
+  describe('isMultibyte', () => {
+    it('works', () => {
+
+    });
+  });
+
+  describe('isNumeric', () => {
+    it('works', () => {
+
+    });
+  });
+
+  describe('isOctal', () => {
+    it('works', () => {
+
+    });
+  });
+
+  describe('isPassportNumber', () => {
+    it('works', () => {
+
+    });
+  });
+
+  describe('isPort', () => {
+    it('works', () => {
+
+    });
+  });
+
+  describe('isPostalCode', () => {
+    it('works', () => {
+
+    });
+  });
+
+  describe('isRFC3339', () => {
+    it('works', () => {
+
+    });
+  });
+
+  describe('isRgbColor', () => {
+    it('works', () => {
+
+    });
+  });
+
+  describe('isSemVer', () => {
+    it('works', () => {
+
+    });
+  });
+
+  describe('isSlug', () => {
+    it('works', () => {
+
+    });
+  });
+
+  describe('isStrongPassword', () => {
+    it('works', () => {
+
+    });
+  });
+
+  describe('isSurrogatePair', () => {
+    it('works', () => {
+
+    });
+  });
+
+  describe('isTaxID', () => {
+    it('works', () => {
+
+    });
+  });
+
+  describe('isUppercase', () => {
+    it('works', () => {
+
+    });
+  });
+
+  describe('isURL', () => {
+    it('works', () => {
+
+    });
+  });
+
+  describe('isUUID', () => {
+    it('works', () => {
+
+    });
+  });
+
+  describe('isVariableWidth', () => {
+    it('works', () => {
+
+    });
+  });
+
+  describe('isVAT', () => {
+    it('works', () => {
+
+    });
+  });
+
+  describe('isWhitelisted', () => {
+    it('works', () => {
+
+    });
+  });
+
+  describe('ltrim', () => {
+    it('works', () => {
+
+    });
+  });
+
+  describe('matches', () => {
+    it('works', () => {
+
+    });
+  });
+
+  describe('normalizeEmail', () => {
+    it('works', () => {
+
+    });
+  });
+
+  describe('rtrim', () => {
+    it('works', () => {
+
+    });
+  });
+
+  describe('stripLow', () => {
+    it('works', () => {
+
+    });
+  });
+
+  describe('toBoolean', () => {
+    it('works', () => {
+
+    });
+  });
+
+  describe('toDate', () => {
+    it('works', () => {
+
+    });
+  });
+
+  describe('toFloat', () => {
+    it('works', () => {
+
+    });
+  });
+
+  describe('toInt', () => {
+    it('works', () => {
+
+    });
+  });
+
+  describe('trim', () => {
+    it('works', () => {
+
+    });
+  });
+
+  describe('whitelist', () => {
+    it('works', () => {
+
+    });
   });
 });
