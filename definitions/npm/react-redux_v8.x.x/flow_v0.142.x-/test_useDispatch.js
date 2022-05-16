79e3d2c00561e75f79e5d7f72f8da4c3c5785a77
Brian Chen <bzchen93@gmail.com>
2022-05-17 07:17:33 +1000

    copy v7 to v8

---
+++ definitions/npm/react-redux_v8.x.x/flow_v0.142.x-/test_useDispatch.js
@@ -0,0 +1,150 @@
+// @flow
+import { describe, it } from 'flow-typed-test';
+import React from 'react';
+import { useDispatch } from 'react-redux';
+
+type Action = {|
+  type: 'action',
+|};
+type Dispatch = Action => Action;
+
+describe('useDispatch', () => {
+  it('returns type `Dispatch` which accepts only type `Action` as param', () => {
+    const dispatch = useDispatch<Dispatch>();
+
+    const action = dispatch({ type: 'action' });
+    (action.type: string);
+    // $FlowExpectedError[incompatible-cast] will be string because of passed generic
+    (action.type: number);
+  });
+
+  it('errors if returned type is passed invalid Action', () => {
+    const dispatch = useDispatch<Dispatch>();
+
+    // $FlowExpectedError[incompatible-call]: return value of `useDispatch` should make `Dispatch` and expect an `Action`.
+    dispatch();
+  });
+
+  it('handles action creator passed in and is typed', () => {
+    const dispatch = useDispatch();
+
+    const action = dispatch({ anything: 'string' });
+    (action.anything: string);
+    // $FlowExpectedError[incompatible-cast] will be string because of passed action
+    (action.anything: number);
+  });
+
+  it('handles thunks being passed to return the right value', () => {
+    const thunkAction = () => (dispatch) => {
+      return '';
+    }
+    const dispatch = useDispatch();
+
+    const a = dispatch(thunkAction());
+    a.toString();
+    // $FlowExpectedError[incompatible-cast] it will return a string, not any which could be cast to number
+    (a: number)
+  });
+
+  it('handles thunks defined as a promise to keep their type', () => {
+    const thunkPromiseAction = () => (dispatch) => new Promise((resolve) => {
+      resolve('');
+    });
+    const dispatch = useDispatch();
+
+    const dispatched = dispatch(thunkPromiseAction());
+    (dispatched: Promise<string>);
+    // $FlowExpectedError[incompatible-cast] it will return a string, not any which could be cast to number
+    (dispatched: Promise<number>);
+  });
+
+  it('handles thunks defined as a 1 arg tuple', () => {
+    type ThunkAction = () => ((...args: [
+      () => void,
+    ]) => string);
+    const thunkAction: ThunkAction = () => (dispatch) => {
+      dispatch();
+      return '';
+    }
+    const dispatch = useDispatch();
+
+    const a = dispatch(thunkAction());
+    a.toString();
+    // $FlowExpectedError[incompatible-cast] it will return a string, not any which could be cast to number
+    (a: number)
+  });
+
+  it('handles thunks defined as a 2 arg tuple', () => {
+    type ThunkAction = () => ((...args: [
+      () => void,
+      () => { [key: string]: any },
+    ]) => string);
+    const thunkAction: ThunkAction = () => (dispatch, getState) => {
+      dispatch();
+      getState().property1.property2;
+      return '';
+    }
+    const dispatch = useDispatch();
+
+    const a = dispatch(thunkAction());
+    a.toString();
+    // $FlowExpectedError[incompatible-cast] it will return a string, not any which could be cast to number
+    (a: number)
+  });
+
+  it('handles thunks defined as a 3 arg tuple', () => {
+    type ThunkAction = () => ((...args: [
+      () => void,
+      () => { [key: string]: any },
+      {| test: number |},
+    ]) => string);
+    const thunkAction: ThunkAction = () => (dispatch, getState, extraArg) => {
+      dispatch();
+      getState().property1.property2;
+      extraArg.test.toFixed(2);
+      return '';
+    }
+    const dispatch = useDispatch();
+
+    const a = dispatch(thunkAction());
+    a.toString();
+    // $FlowExpectedError[incompatible-cast] it will return a string, not any which could be cast to number
+    (a: number)
+  });
+
+  it('handles thunks defined as each arg', () => {
+    type ThunkAction = () => ((
+      dispatch: () => void,
+      getState: () => { [key: string]: any },
+      extraArg: {| test: number |},
+    ) => string);
+    const thunkAction: ThunkAction = () => (dispatch, getState, extraArg) => {
+      dispatch();
+      getState().property1.property2;
+      extraArg.test.toFixed(2);
+      return '';
+    }
+    const dispatch = useDispatch();
+
+    const a = dispatch(thunkAction());
+    a.toString();
+    // $FlowExpectedError[incompatible-cast] it will return a string, not any which could be cast to number
+    (a: number)
+  });
+
+  it('handles thunks without a clear args', () => {
+    type ThunkAction = () => ((...args: Array<any>) => string);
+    const thunkAction: ThunkAction = () => (dispatch, getState, extraArg) => {
+      dispatch.test.a();
+      getState.test.a();
+      extraArg.test.a();
+      return '';
+    }
+    const dispatch = useDispatch();
+
+    const a = dispatch(thunkAction());
+    a.toString();
+    // $FlowExpectedError[incompatible-cast] it will return a string, not any which could be cast to number
+    (a: number)
+  });
+});
---
+++ definitions/npm/react-redux_v8.x.x/flow_v0.89.x-v0.103.x/test_useSelector.js
@@ -0,0 +1,48 @@
+// @flow
+
+import { describe, it } from 'flow-typed-test';
+import React from 'react';
+import { useSelector, shallowEqual } from 'react-redux';
+
+type State = {|
+  a: number,
+|};
+
+describe('useSelector', () => {
+  it('passes State as first parameter', () => {
+    function Com() {
+      // $FlowExpectedError: the state has no `b`
+      const count = useSelector<State, number>(state => state.b);
+      return <div>{count}</div>;
+    }
+  });
+
+  it('passes type of second parameter as params to `equalityFn`', () => {
+    function Com2() {
+      // $FlowExpectedError: `equalityFn` is passed params of the second type, do not have `.size`
+      const count = useSelector<State, number>(
+        state => state.a,
+        (a, b) => a.size === b.size
+      );
+      return <div>{count}</div>;
+    }
+  });
+
+  it('returns type of second parameter', () => {
+    function Com3() {
+      const count = useSelector<State, number>(
+        state => state.a,
+        (a, b) => a === b
+      );
+      // `count` is type `number` and allows addition
+      return <div>{count + 5}</div>;
+    }
+  });
+
+  it('can use shallowEqual as the `equalityFn`', () => {
+    function Com4() {
+      const count = useSelector<State, number>(state => state.a, shallowEqual);
+      return <div>{count}</div>;
+    }
+  });
+});
