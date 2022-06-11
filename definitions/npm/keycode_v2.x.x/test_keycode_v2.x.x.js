2d14b0421ed535d8a042fcbb6c49bfb6476e0f1c
Brian Chen <bzchen93@gmail.com>
2022-06-11 12:03:31 +1000

    expand keycodes range

--- definitions/npm/keycode_v2.x.x/test_keycode_v2.x.x.js
+++ definitions/npm/keycode_v2.x.x/test_keycode_v2.x.x.js
@@ -16,7 +16,7 @@ describe('keycode', () => {
   });
 
   it('errors parsing invalid arg', () => {
-    // $FlowExpectedError
+    // $FlowExpectedError[incompatible-call]
     keycode({});
   });
 });
