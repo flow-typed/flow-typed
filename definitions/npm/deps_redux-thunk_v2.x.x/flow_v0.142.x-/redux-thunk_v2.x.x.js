a2f24a3e297ca2c342ac0e5f9ecf3e5f45f14a73
Brian Chen <bzchen93@gmail.com>
2022-07-10 11:48:48 +1000

    revert definitions change

--- definitions/npm/deps_redux-thunk_v2.x.x/flow_v0.142.x-/redux-thunk_v2.x.x.js
+++
@@ -1,9 +0,0 @@
-declare module 'redux-thunk' {
-  import type { Middleware } from 'redux';
-
-  declare export type Thunk = Middleware<any, any> & {|
-    withExtraArgument(arg: $NonMaybeType<mixed>): Middleware<any, any>,
-  |};
-
-  declare module.exports: Thunk;
-}
