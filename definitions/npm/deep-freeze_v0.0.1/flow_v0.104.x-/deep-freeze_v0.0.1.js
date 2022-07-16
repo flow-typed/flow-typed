3793c65651cf071ddba2ffa3379e8846e1919aeb
Paul Dufour <pdufour@users.noreply.github.com>
2022-07-16 03:55:49 +0100

    Add new type for deep freeze (#4341)
    * Add new type for deep freeze
    
    * remove test
    
    * add function type
    
    * test

--- definitions/npm/deep-freeze_v0.0.1/flow_v0.104.x-/deep-freeze_v0.0.1.js
+++ definitions/npm/deep-freeze_v0.0.1/flow_v0.104.x-/deep-freeze_v0.0.1.js
@@ -1,3 +1,16 @@
 declare module 'deep-freeze' {
-  declare module.exports: <T>(o: T) => T;
+  declare type deepFreezeFnType = {|
+    <T>(a: $ReadOnlyArray<T>): $ReadOnlyArray<DeepReadOnly<T>>,
+    <T>(a: T[]): $ReadOnlyArray<DeepReadOnly<T>>,
+    <T: boolean|string|number>(p: T): T,
+    <T>(o: T): $ReadOnly<
+      $ObjMapi<T, <P>(P) => DeepReadOnly<$ElementType<T, P>>>
+    >,
+    <T: Function>(f: T): T,
+  |};
+
+  declare export type DeepReadOnly<T> = $Call<deepFreezeFnType, T>
+
+  declare module.exports: deepFreezeFnType;
+
 }
