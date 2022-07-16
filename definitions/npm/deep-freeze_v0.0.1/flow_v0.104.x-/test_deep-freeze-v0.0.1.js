ab29426a58808c2c00d24bc9e3c007c43ba403e5
paul.dufour <paul.dufour@uber.com>
2022-07-16 00:43:01 +0000

    Add new type for deep freeze

--- definitions/npm/deep-freeze_v0.0.1/flow_v0.104.x-/test_deep-freeze-v0.0.1.js
+++ definitions/npm/deep-freeze_v0.0.1/flow_v0.104.x-/test_deep-freeze-v0.0.1.js
@@ -1,13 +1,111 @@
 // @flow
 
-import deepFreeze from 'deep-freeze';
+import deepFreeze, { type DeepReadOnly } from 'deep-freeze';
+import { it, describe } from 'flow-typed-test';
 
-const obj: { foo: string, ... } = deepFreeze({foo: 'bar'});
-const arr: Array<string> = deepFreeze(['foo', 'bar']);
-const num: number = deepFreeze(42);
+describe('No properties', () => {
+  const test1 = deepFreeze(({}: { ... }));
 
-// $FlowExpectedError input type must match output type
-const notObj: { foo: string, ... } = deepFreeze(['foo', 'bar']);
+  it('throws error', () => {
+    // $FlowExpectedError[prop-missing]
+    test1.disabled = true;
+  });
+});
 
-// $FlowExpectedError input type must match output type
-const notArr: Array<string> = deepFreeze({foo: 'bar'});
+describe('Object', () => {
+  const test1 = deepFreeze(({ disabled: false }: {| disabled: boolean |}));
+
+  it('throws error', () => {
+    // $FlowExpectedError[cannot-write]
+    test1.disabled = true;
+
+    // $FlowExpectedError[prop-missing]
+    test1.newprop = true;
+  });
+});
+
+describe('$ReadOnly Object', () => {
+  const test1 = deepFreeze(
+    ({ disabled: false }: $ReadOnly<{| disabled: boolean |}>)
+  );
+
+  it('throws error', () => {
+    // $FlowExpectedError[cannot-write]
+    test1.disabled = true;
+
+    // $FlowExpectedError[prop-missing]
+    test1.newprop = true;
+  });
+});
+
+describe('Array', () => {
+  const test1: $ReadOnlyArray<DeepReadOnly<{| name: string |}>> = deepFreeze([
+    { name: 'foo' },
+  ]);
+
+  it('raises error', () => {
+    // $FlowExpectedError[cannot-write]
+    test1[0].name = true;
+  });
+});
+
+describe('$ReadOnlyArray', () => {
+  const test1: $ReadOnlyArray<DeepReadOnly<{| name: string |}>> = deepFreeze(
+    ([{ name: 'foo' }]: $ReadOnlyArray<{| name: string |}>)
+  );
+
+  it('raises error', () => {
+    // $FlowExpectedError[cannot-write]
+    test1[0].name = true;
+  });
+});
+
+describe('Nested object', () => {
+  const test1: $ReadOnlyArray<DeepReadOnly<{| name: string |}>> = deepFreeze(
+    ([{ name: 'foo' }]: $ReadOnlyArray<{| name: string |}>)
+  );
+
+  it('raises error', () => {
+    // $FlowExpectedError[cannot-write]
+    test1[0].name = true;
+  });
+});
+
+describe('Complex object', () => {
+  const test1: $ReadOnlyArray<DeepReadOnly<{foo: Array<{bar: Array<{foobar: boolean, ...}>, ...}>, ...}>> = deepFreeze([
+    {
+      foo: [
+        {
+          bar: [
+            {
+              foobar: false,
+            },
+          ],
+        },
+      ],
+    },
+  ]);
+
+  it('raises error', () => {
+    // $FlowExpectedError[prop-missing]
+    test1[0].push('foo');
+
+    // $FlowExpectedError[prop-missing]
+    test1[0].newprop = true;
+
+    // $FlowExpectedError[cannot-write]
+    test1[0].foo[0].bar[0].foobar = true;
+
+    // $FlowExpectedError[incompatible-type]
+    // $FlowExpectedError[cannot-write]
+    test1[1] = {foo: "value"}
+  });
+});
+
+describe('Number', () => {
+  const test1: number = deepFreeze(1);
+});
+
+describe('string', () => {
+  const test1: string = deepFreeze('3');
+});
