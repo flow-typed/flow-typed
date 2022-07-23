69683c59d7b023ff82c30cd36ecc3009161922c2
Brian Chen <bzchen93@gmail.com>
2022-07-23 21:18:17 +1000

    add bundle size definition

---
+++ definitions/npm/webpack-bundle-analyzer_v4.x.x/flow_v0.104.x-/test_webpack-bundle-analyzer_v4.x.x.js
@@ -0,0 +1,9 @@
+// @flow
+import { describe, it } from 'flow-typed-test';
+// import library from 'webpack-bundle-analyzer';
+
+describe('webpack-bundle-analyzer', () => {
+  it('', () => {
+
+  });
+});
