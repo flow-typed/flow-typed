1d4f212116846aef3d5b6d5250939230675e01a0
Filip Spiridonov <fspiridonov@uber.com>
2022-07-26 16:45:21 -0400

    Fix error and warnings

--- definitions/npm/@mapbox/mapbox-gl-supported_v2.x.x/flow_v0.25.x-/mapbox-gl-supported_v2.x.x.js
+++ definitions/npm/@mapbox/mapbox-gl-supported_v2.x.x/flow_v0.25.x-/mapbox-gl-supported_v2.x.x.js
@@ -3,6 +3,7 @@
 declare module '@mapbox/mapbox-gl-supported' {
   declare type OptionsT = {
     failIfMajorPerformanceCaveat: boolean,
+    ...
   };
   declare export function supported(options?: OptionsT): boolean;
   declare export function notSupportedReason(options?: OptionsT): ?string;
--- definitions/npm/@mapbox/mapbox-gl-supported_v2.x.x/test_mapbox-gl-supported_v2.x.x.js
+++ definitions/npm/@mapbox/mapbox-gl-supported_v2.x.x/test_mapbox-gl-supported_v2.x.x.js
@@ -5,24 +5,26 @@ import { supported, notSupportedReason } from '@mapbox/mapbox-gl-supported';
 
 describe('supported', () => {
   it('should fail when the options format is incorrect', () => {
-    // $FlowExpectedError
-    supported({});
+    // $FlowExpectedError[prop-missing]
+    supported({ incorrectProp: false });
   });
 
   it('should not fail when the options format is correct', () => {
     supported({ failIfMajorPerformanceCaveat: true });
+    supported({ failIfMajorPerformanceCaveat: true, otherProp: 100 });
     const isSupported = supported();
   });
 });
 
 describe('notSupportedReason', () => {
   it('should fail when the options format is incorrect', () => {
-    // $FlowExpectedError
-    notSupportedReason({});
+    // $FlowExpectedError[prop-missing]
+    notSupportedReason({ incorrectProp: false });
   });
 
   it('should not fail when the options format is correct', () => {
     notSupportedReason({ failIfMajorPerformanceCaveat: true });
+    notSupportedReason({ failIfMajorPerformanceCaveat: true, otherProp: 100 });
     const reason = notSupportedReason();
   });
 });
