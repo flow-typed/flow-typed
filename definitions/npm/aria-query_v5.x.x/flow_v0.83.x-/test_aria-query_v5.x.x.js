137be744b8d70be2c6e04c88c50493c566de132c
Brian Chen <bzchen93@gmail.com>
2022-07-30 10:24:59 +1000

    add base tests

--- definitions/npm/aria-query_v5.x.x/flow_v0.83.x-/test_aria-query_v5.x.x.js
+++ definitions/npm/aria-query_v5.x.x/flow_v0.83.x-/test_aria-query_v5.x.x.js
@@ -1,9 +1,40 @@
 // @flow
-import { describe, it } from 'flow-typed-test';
-// import library from 'aria-query';
+import { describe, test } from 'flow-typed-test';
+import {
+  aria,
+  dom,
+  elementRoles,
+  roles,
+  roleElements,
+} from 'aria-query';
 
 describe('aria-query', () => {
-  it('', () => {
+  test('aria', () => {
+    const value = aria.get('aria-activedescendant');
+    if (value) {
+      value.type.toLowerCase();
+      if (value.values) {
+        (value.values[0]: string | boolean);
+      }
+    }
+
+    // $FlowExpectedError[incompatible-call]
+    aria.get('foo')
+  });
+
+  test('dom', () => {
+
+  });
+
+  test('elementRoles', () => {
+
+  });
+
+  test('roles', () => {
+
+  });
+
+  test('roleElements', () => {
 
   });
 });
