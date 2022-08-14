1184972079c189453057bc305e87bcc035e0e631
Brian Chen <bzchen93@gmail.com>
2022-08-14 10:40:24 +1000

    test

--- definitions/npm/micromatch_v4.x.x/flow_v0.83.x-/test_micromatch_v4.x.x.js
+++ definitions/npm/micromatch_v4.x.x/flow_v0.83.x-/test_micromatch_v4.x.x.js
@@ -6,5 +6,19 @@ const micromatch = require('micromatch');
 describe('micromatch', () => {
   test('main', () => {
     (micromatch(['foo', 'bar', 'baz', 'qux'], ['f*', 'b*']): Array<string>);
+    micromatch(['foo', 'bar', 'baz', 'qux'], 'test');
+
+    // $FlowExpectedError[incompatible-cast]
+    (micromatch(['foo', 'bar', 'baz', 'qux'], ['f*', 'b*']): string);
+    // $FlowExpectedError[incompatible-call]
+    micromatch('test', 'test');
+    // $FlowExpectedError[incompatible-call]
+    micromatch(['foo', 'bar', 'baz', 'qux']);
+    // $FlowExpectedError[incompatible-call]
+    micromatch(['foo', 'bar', 'baz', 'qux'], 1);
+  });
+
+  test('options', () => {
+
   });
 });
