0edd5dcbd24fd878f48e294017ea1b0b80d08a90
Brian Chen <bzchen93@gmail.com>
2022-08-22 17:44:52 +1000

    add options tests

--- definitions/npm/micromatch_v4.x.x/flow_v0.83.x-/test_micromatch_v4.x.x.js
+++ definitions/npm/micromatch_v4.x.x/flow_v0.83.x-/test_micromatch_v4.x.x.js
@@ -19,6 +19,210 @@ describe('micromatch', () => {
   });
 
   test('options', () => {
+    micromatch([], 'test', {});
+    micromatch([], 'test', {
+      basename: true,
+      bash: true,
+      capture: true,
+      contains: true,
+      cwd: '',
+      debug: true,
+      dot: true,
+      expandRange: (left, right, { basename }) => {
+        left.toLowerCase();
+        right.toLowerCase();
+        return '';
+      },
+      failglob: true,
+      fastpaths: true,
+      flags: true,
+      format: (returnedString) => {
+        returnedString.toLowerCase();
+        return '';
+      },
+      ignore: '',
+      keepQuotes: true,
+      literalBrackets: true,
+      lookbehinds: true,
+      matchBase: true,
+      maxLength: 1,
+      nobrace: true,
+      nobracket: true,
+      nocase: true,
+      noext: true,
+      noextglob: true,
+      nonegate: true,
+      noquantifiers: true,
+      onIgnore: (item) => {
+        item.glob.toLowerCase();
+        item.regex.test('');
+        item.input.toLowerCase();
+        item.output.toLowerCase();
+      },
+      onMatch: (item) => {
+        item.glob.toLowerCase();
+        item.regex.test('');
+        item.input.toLowerCase();
+        item.output.toLowerCase();
+      },
+      onResult: (item) => {
+        item.glob.toLowerCase();
+        item.regex.test('');
+        item.input.toLowerCase();
+        item.output.toLowerCase();
+      },
+      posix: true,
+      prepend: true,
+      regex: true,
+      strictBrackets: true,
+      strictSlashes: true,
+      unescape: true,
+      windows: true,
+    });
 
+    micromatch([], 'test', {
+      // $FlowExpectedError[incompatible-call]
+      basename: '',
+    });
+    micromatch([], 'test', {
+      // $FlowExpectedError[incompatible-call]
+      bash: '',
+    });
+    micromatch([], 'test', {
+      // $FlowExpectedError[incompatible-call]
+      capture: '',
+    });
+    micromatch([], 'test', {
+      // $FlowExpectedError[incompatible-call]
+      contains: '',
+    });
+    micromatch([], 'test', {
+      // $FlowExpectedError[incompatible-call]
+      cwd: 1,
+    });
+    micromatch([], 'test', {
+      // $FlowExpectedError[incompatible-call]
+      debug: '',
+    });
+    micromatch([], 'test', {
+      // $FlowExpectedError[incompatible-call]
+      dot: '',
+    });
+    micromatch([], 'test', {
+      // $FlowExpectedError[incompatible-call]
+      expandRange: '',
+    });
+    micromatch([], 'test', {
+      // $FlowExpectedError[incompatible-call]
+      failglob: '',
+    });
+    micromatch([], 'test', {
+      // $FlowExpectedError[incompatible-call]
+      fastpaths: '',
+    });
+    micromatch([], 'test', {
+      // $FlowExpectedError[incompatible-call]
+      flags: '',
+    });
+    micromatch([], 'test', {
+      // $FlowExpectedError[incompatible-call]
+      format: '',
+    });
+    micromatch([], 'test', {
+      // $FlowExpectedError[incompatible-call]
+      ignore: 1,
+    });
+    micromatch([], 'test', {
+      // $FlowExpectedError[incompatible-call]
+      keepQuotes: '',
+    });
+    micromatch([], 'test', {
+      // $FlowExpectedError[incompatible-call]
+      literalBrackets: '',
+    });
+    micromatch([], 'test', {
+      // $FlowExpectedError[incompatible-call]
+      lookbehinds: '',
+    });
+    micromatch([], 'test', {
+      // $FlowExpectedError[incompatible-call]
+      matchBase: '',
+    });
+    micromatch([], 'test', {
+      // $FlowExpectedError[incompatible-call]
+      maxLength: '',
+    });
+    micromatch([], 'test', {
+      // $FlowExpectedError[incompatible-call]
+      nobrace: '',
+    });
+    micromatch([], 'test', {
+      // $FlowExpectedError[incompatible-call]
+      nobracket: '',
+    });
+    micromatch([], 'test', {
+      // $FlowExpectedError[incompatible-call]
+      nocase: '',
+    });
+    micromatch([], 'test', {
+      // $FlowExpectedError[incompatible-call]
+      noext: '',
+    });
+    micromatch([], 'test', {
+      // $FlowExpectedError[incompatible-call]
+      noextglob: '',
+    });
+    micromatch([], 'test', {
+      // $FlowExpectedError[incompatible-call]
+      noglobstar: '',
+    });
+    micromatch([], 'test', {
+      // $FlowExpectedError[incompatible-call]
+      nonegate: '',
+    });
+    micromatch([], 'test', {
+      // $FlowExpectedError[incompatible-call]
+      noquantifiers: '',
+    });
+    micromatch([], 'test', {
+      // $FlowExpectedError[incompatible-call]
+      onIgnore: '',
+    });
+    micromatch([], 'test', {
+      // $FlowExpectedError[incompatible-call]
+      onMatch: '',
+    });
+    micromatch([], 'test', {
+      // $FlowExpectedError[incompatible-call]
+      onResult: '',
+    });
+    micromatch([], 'test', {
+      // $FlowExpectedError[incompatible-call]
+      posix: '',
+    });
+    micromatch([], 'test', {
+      // $FlowExpectedError[incompatible-call]
+      prepend: '',
+    });
+    micromatch([], 'test', {
+      // $FlowExpectedError[incompatible-call]
+      regex: '',
+    });
+    micromatch([], 'test', {
+      // $FlowExpectedError[incompatible-call]
+      strictBrackets: '',
+    });
+    micromatch([], 'test', {
+      // $FlowExpectedError[incompatible-call]
+      strictSlashes: '',
+    });
+    micromatch([], 'test', {
+      // $FlowExpectedError[incompatible-call]
+      unescape: '',
+    });
+    micromatch([], 'test', {
+      // $FlowExpectedError[incompatible-call]
+      windows: '',
+    });
   });
 });
