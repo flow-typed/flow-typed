aed7446434b06f37f04193c6a40a6add8b47817c
Brian Chen <bzchen93@gmail.com>
2022-11-18 20:29:09 +1100

    test env

---
+++ definitions/environments/node/flow_v0.155.x-/node.js
@@ -0,0 +1,36 @@
+// https://nodejs.org/api/fs.html#promises-api
+// hihi
+
+declare module 'fs/promises' {
+  declare module.exports: $Exports<'fs'>['promises'];
+}
+
+// https://nodejs.org/api/esm.html#node-imports
+
+declare module 'node:events' {
+  declare module.exports: $Exports<'events'>;
+}
+
+declare module 'node:fs' {
+  declare module.exports: $Exports<'fs'>;
+}
+
+declare module 'node:fs/promises' {
+  declare module.exports: $Exports<'fs'>['promises'];
+}
+
+declare module 'node:os' {
+  declare module.exports: $Exports<'os'>;
+}
+
+declare module 'node:path' {
+  declare module.exports: $Exports<'path'>;
+}
+
+declare module 'node:util' {
+  declare module.exports: $Exports<'util'>;
+}
+
+declare module 'node:url' {
+  declare module.exports: $Exports<'url'>;
+}
