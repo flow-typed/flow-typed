0e051b07a4f7f1f497cf839d7af90b296b9c3359
Brian Chen <bzchen93@gmail.com>
2023-08-21 19:17:09 +1000

    add def

---
+++ definitions/npm/copy-dir_v1.x.x/flow_v0.83.x-/copy-dir_v1.x.x.js
@@ -0,0 +1,34 @@
+declare module 'copy-dir' {
+  declare type Options = {|
+    /**
+     * keep addTime or modifyTime if true
+     */
+    utimes:
+      | false
+      | {|
+        atime: string | number | Date,
+        mtime: string | number | Date,
+      |},
+    /**
+     * keep file mode if true
+     */
+    mode?: boolean | number,
+    /**
+     * cover if file exists
+     */
+    cover?: boolean,
+    /**
+     * file filter
+     */
+    filter?:
+      | boolean
+      | (state: string, filepath: string, filename: string) => boolean,
+  |};
+
+  declare type Callback = (error: Error) => void
+
+  declare module.exports: {
+    (from: string, to: string, options: Options, callback: Callback) => void,
+    sync: (from: string, to: string, options: Options) => void,
+  };
+}
---
+++ definitions/npm/copy-dir_v1.x.x/flow_v0.83.x-/test_copy-dir_v1.x.x.js
@@ -0,0 +1,9 @@
+// @flow
+import { describe, it } from 'flow-typed-test';
+// import library from 'copy-dir';
+
+describe('copy-dir', () => {
+  it('', () => {
+
+  });
+});
