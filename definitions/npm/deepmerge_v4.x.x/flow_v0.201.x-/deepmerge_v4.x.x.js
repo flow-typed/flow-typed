4c563a32a243f5880ac8b5cd727f2d9629000584
Brian Chen <bzchen93@gmail.com>
2023-09-02 10:55:25 +1000

    update arraymerge options

--- definitions/npm/deepmerge_v4.x.x/flow_v0.201.x-/deepmerge_v4.x.x.js
+++ definitions/npm/deepmerge_v4.x.x/flow_v0.201.x-/deepmerge_v4.x.x.js
@@ -1,11 +1,15 @@
 declare module 'deepmerge' {
-  declare type Options = {
+  declare type ArrayMergeOptions  = {|
+    isMergeableObject: (value: { ... }) => boolean,
+    cloneUnlessOtherwiseSpecified: (value: { ... }, options?: Options) =>  { ... },
+  |};
+
+  declare type Options = {|
     clone?: boolean,
-    arrayMerge?: (destination: any[], source: any[], options?: Options) => Array<any>,
+    arrayMerge?: (destination: any[], source: any[], options: ArrayMergeOptions) => Array<any>,
     isMergeableObject?: (value: { ... }) => boolean,
     customMerge?: (key: string, options?: Options) => ((x: any, y: any) => any) | void,
-    ...
-  }
+  |};
 
   declare module.exports: {
     <A, B>(a: A, b: B, options?: Options): A & B,
--- definitions/npm/deepmerge_v4.x.x/flow_v0.201.x-/test_deepmerge_v4.x.x.js
+++ definitions/npm/deepmerge_v4.x.x/flow_v0.201.x-/test_deepmerge_v4.x.x.js
@@ -1,8 +1,49 @@
-const deepmerge = require("deepmerge");
+// @flow
+import { describe, test } from 'flow-typed-test';
 
-const m1: { a: string, b: string, ... } = deepmerge({a: ''}, { b: '' })
+const deepmerge = require('deepmerge');
 
-const m2: { a: string, b: string, ... } = deepmerge.all([ {a: ''}, { b: '' } ])
+describe('deepmerge', () => {
+  test('basic', () => {
+    const m1: { a: string, b: string, ... } = deepmerge({a: ''}, { b: '' });
 
-// $FlowExpectedError[incompatible-type]
-const m3: { a: number, b: string, ... } = deepmerge({a: ''}, { b: '' })
+    const m2: { a: string, b: string, ... } = deepmerge.all([ {a: ''}, { b: '' } ]);
+
+    // $FlowExpectedError[incompatible-type]
+    const m3: { a: number, b: string, ... } = deepmerge({a: ''}, { b: '' });
+  });
+
+  describe('options', () => {
+    test('arrayMerge', () => {
+      deepmerge({ a: '' }, { b: '' }, {
+        arrayMerge: (dest, src) => [],
+      });
+
+      deepmerge({ a: '' }, { b: '' }, {
+        arrayMerge: (dest, src, options) => {
+          options.cloneUnlessOtherwiseSpecified({});
+          options.cloneUnlessOtherwiseSpecified({}, {
+            clone: true,
+          });
+          (options.isMergeableObject({}): boolean);
+
+          return [];
+        },
+      });
+
+      deepmerge({ a: '' }, { b: '' }, {
+        arrayMerge: (dest, src, options) => {
+          // $FlowExpectedError[prop-missing]
+          options.cloneUnlessOtherwiseSpecified({}, {
+            foo: 'bar',
+          });
+          // $FlowExpectedError[incompatible-cast]
+          (options.isMergeableObject({}): string);
+
+          return [];
+        },
+      });
+    });
+  });
+});
+
