6fef4ebfa434885e1db24f338c14d4ad33d6a0fa
Pascal Duez <pascalduez@users.noreply.github.com>
2023-09-29 11:52:04 +0200

    [env:node] Add assert module (#4520)

--- definitions/environments/node/flow_v0.155.x-/node.js
+++ definitions/environments/node/flow_v0.155.x-/node.js
@@ -1,18 +1,12 @@
-// https://nodejs.org/api/fs.html#promises-api
-
-declare module 'fs/promises' {
-  declare module.exports: $Exports<'fs'>['promises'];
-}
-
-declare module 'process' {
-  declare module.exports: Process;
-}
-
-declare module 'node:process' {
-  declare module.exports: $Exports<'process'>;
-}
-
 // https://nodejs.org/api/esm.html#node-imports
+
+declare module 'node:assert' {
+  declare module.exports: $Exports<'assert'>;
+}
+
+declare module 'node:assert/strict' {
+  declare module.exports: $Exports<'assert'>['strict'];
+}
 
 declare module 'node:events' {
   declare module.exports: $Exports<'events'>;
@@ -22,16 +16,28 @@ declare module 'node:fs' {
   declare module.exports: $Exports<'fs'>;
 }
 
-declare module 'node:fs/promises' {
-  declare module.exports: $Exports<'fs'>['promises'];
-}
-
 declare module 'node:os' {
   declare module.exports: $Exports<'os'>;
 }
 
+declare module 'fs/promises' {
+  declare module.exports: $Exports<'fs'>['promises'];
+}
+
+declare module 'node:fs/promises' {
+  declare module.exports: $Exports<'fs'>['promises'];
+}
+
 declare module 'node:path' {
   declare module.exports: $Exports<'path'>;
+}
+
+declare module 'process' {
+  declare module.exports: Process;
+}
+
+declare module 'node:process' {
+  declare module.exports: $Exports<'process'>;
 }
 
 declare module 'node:util' {
