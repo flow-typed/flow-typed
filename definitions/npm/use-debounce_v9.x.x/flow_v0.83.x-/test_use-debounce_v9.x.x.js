857c061205dc67ad916f3a580eeb0d78d4d981f5
Brian Chen <bzchen93@gmail.com>
2023-10-11 08:27:52 +1100

    add all valid use cases

--- definitions/npm/use-debounce_v9.x.x/flow_v0.83.x-/test_use-debounce_v9.x.x.js
+++ definitions/npm/use-debounce_v9.x.x/flow_v0.83.x-/test_use-debounce_v9.x.x.js
@@ -1,9 +1,44 @@
 // @flow
-import { describe, it } from 'flow-typed-test';
-// import library from 'use-debounce';
+import { describe, test } from 'flow-typed-test';
+import * as React from 'react';
+import { useDebounce, useDebouncedCallback, useThrottledCallback } from 'use-debounce';
 
 describe('use-debounce', () => {
-  it('', () => {
-
+  test('useDebounce', () => {
+    const [value] = useDebounce('test', 1000, {
+      maxWait: 1,
+      leading: true,
+      trailing: true,
+      equalityFn: (a, b) => {
+        return a.toLowerCase() === b.toLowerCase();
+      },
+    });
+    (value: string);
   });
+
+  test('useDebouncedCallback', () => {
+    const [value, setValue] = React.useState('test');
+    // Debounce callback
+    const debounced = useDebouncedCallback(
+      // function
+      (value: string) => {
+        setValue(value);
+      },
+      // delay in ms
+      1000
+    );
+  });
+
+  test('useThrottledCallback', () => {
+    const [value, setValue] = React.useState('test');
+    // Debounce callback
+    const debounced = useThrottledCallback(
+      // function
+      (value: string) => {
+        setValue(value);
+      },
+      // delay in ms
+      1000
+    );
+  })
 });
