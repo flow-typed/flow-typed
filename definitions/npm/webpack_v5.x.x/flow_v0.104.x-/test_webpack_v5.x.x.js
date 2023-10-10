442a74fd4d0667107251ba6fc4ca6b6f6d3d8309
Brian Chen <bzchen93@gmail.com>
2023-10-11 07:44:46 +1100

    restructure tests

--- definitions/npm/webpack_v5.x.x/flow_v0.104.x-/test_webpack_v5.x.x.js
+++ definitions/npm/webpack_v5.x.x/flow_v0.104.x-/test_webpack_v5.x.x.js
@@ -1,3 +1,4 @@
+import { describe, test } from 'flow-typed-test';
 import webpack from 'webpack';
 
 import type { WebpackError, Stats, WebpackOptions } from 'webpack';
@@ -72,35 +73,43 @@ const options: WebpackOptions = {
   ],
 };
 
-webpack(options, function(err: WebpackError, stats: Stats) {
-  if (err) {
-    console.error(err.stack || err);
-    if (err.details) {
-      console.error(err.details);
-    }
-    return;
-  }
+describe('webpack', () => {
+  test('basic', () => {
+    webpack(options, function(err: WebpackError, stats: Stats) {
+      if (err) {
+        console.error(err.stack || err);
+        if (err.details) {
+          console.error(err.details);
+        }
+        return;
+      }
 
-  const info = stats.toJson();
+      const info = stats.toJson();
 
-  if (stats.hasErrors()) {
-    console.error(info.errors);
-  }
+      if (stats.hasErrors()) {
+        console.error(info.errors);
+      }
 
-  if (stats.hasWarnings()) {
-    console.warn(info.warnings);
-  }
+      if (stats.hasWarnings()) {
+        console.warn(info.warnings);
+      }
+    });
+
+    const compiler1 = webpack(options);
+    compiler1.run(function(err: WebpackError, stats: Stats) {});
+
+    const compiler2 = webpack(options);
+    const watching2 = compiler2.watch({}, function(err: WebpackError, stats: Stats) {});
+    watching2.invalidate();
+    watching2.close();
+
+    const compiler3 = webpack([options, options]);
+    const watching3 = compiler3.watch({}, function(err: WebpackError, stats: Stats) {});
+    watching3.invalidate();
+    watching3.close();
+  });
+
+  test('ProgressPlugin', () => {
+
+  });
 });
-
-const compiler1 = webpack(options);
-compiler1.run(function(err: WebpackError, stats: Stats) {});
-
-const compiler2 = webpack(options);
-const watching2 = compiler2.watch({}, function(err: WebpackError, stats: Stats) {});
-watching2.invalidate();
-watching2.close();
-
-const compiler3 = webpack([options, options]);
-const watching3 = compiler3.watch({}, function(err: WebpackError, stats: Stats) {});
-watching3.invalidate();
-watching3.close();
