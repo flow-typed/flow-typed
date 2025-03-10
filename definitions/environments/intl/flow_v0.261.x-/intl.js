2a7a4c828389bf3d96e6912daf15288eecda661f
Sam Zhou <sam@developersam.com>
2025-03-10 14:48:42 -0400

    Remove types in intl environment that are still available in flow

--- definitions/environments/intl/flow_v0.261.x-/intl.js
+++ definitions/environments/intl/flow_v0.261.x-/intl.js
@@ -38,16 +38,6 @@ declare class Intl$Collator {
   static supportedLocalesOf (locales?: Intl$Locales): Intl$Locale[];
 }
 
-declare type Intl$CollatorOptions = {
-  localeMatcher?: 'lookup' | 'best fit',
-  usage?: 'sort' | 'search',
-  sensitivity?: 'base' | 'accent' | 'case' | 'variant',
-  ignorePunctuation?: boolean,
-  numeric?: boolean,
-  caseFirst?: 'upper' | 'lower' | 'false',
-  ...
-}
-
 type FormatToPartsType = | 'day' | 'dayPeriod' | 'era' | 'hour' | 'literal'
   | 'minute' | 'month' | 'second' | 'timeZoneName' | 'weekday' | 'year';
 
@@ -91,23 +81,6 @@ declare class Intl$DateTimeFormat {
   };
 
   static supportedLocalesOf (locales?: Intl$Locales): Intl$Locale[];
-}
-
-declare type Intl$DateTimeFormatOptions = {
-  localeMatcher?: 'lookup' | 'best fit',
-  timeZone?: string,
-  hour12?: boolean,
-  formatMatcher?: 'basic' | 'best fit',
-  weekday?: 'narrow' | 'short' | 'long',
-  era?: 'narrow' | 'short' | 'long',
-  year?: 'numeric' | '2-digit',
-  month?: 'numeric' | '2-digit' | 'narrow' | 'short' | 'long',
-  day?: 'numeric' | '2-digit',
-  hour?: 'numeric' | '2-digit',
-  minute?: 'numeric' | '2-digit',
-  second?: 'numeric' | '2-digit',
-  timeZoneName?: 'short' | 'long',
-  ...
 }
 
 declare class Intl$LocaleClass {
@@ -171,20 +144,6 @@ declare class Intl$NumberFormat {
   };
 
   static supportedLocalesOf (locales?: Intl$Locales): Intl$Locale[];
-}
-
-declare type Intl$NumberFormatOptions = {
-  localeMatcher?: 'lookup' | 'best fit',
-  style?: 'decimal' | 'currency' | 'percent' | 'unit',
-  currency?: string,
-  currencyDisplay?: 'symbol' | 'code' | 'name' | 'narrowSymbol',
-  useGrouping?: boolean,
-  minimumIntegerDigits?: number,
-  minimumFractionDigits?: number,
-  maximumFractionDigits?: number,
-  minimumSignificantDigits?: number,
-  maximumSignificantDigits?: number,
-  ...
 }
 
 declare class Intl$PluralRules {
